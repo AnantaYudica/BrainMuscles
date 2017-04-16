bea33c7d10dcd3f2065db2baf44cacd12d4d56f3
AnantaYudica <nta040689@gmail.com>
2017-04-16 22:04:13 +0700

    Add Class Info

---
+++ include/test/test/Info.h
@@ -0,0 +1,32 @@
+#ifndef TEST_TEST_INFO_H_
+#define TEST_TEST_INFO_H_
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace test
+		{
+			template<size_t BEGIN_RELATIVE, size_t BEGIN_FILENAME, size_t BEGIN_EXT>
+			class Info;
+		}
+	}
+}
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace test
+		{
+			template<size_t BEGIN_RELATIVE, size_t BEGIN_FILENAME, size_t BEGIN_EXT>
+			class Info
+			{
+
+			};
+		}
+	}
+}
+
+
+#endif // TEST_TEST_INFO_H_
