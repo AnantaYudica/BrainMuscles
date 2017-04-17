3fb96bec98fb1d6d1da995fb938d50098bf0417c
AnantaYudica <nta040689@gmail.com>
2017-04-18 03:15:04 +0700

    Add Class Info::Base

---
+++ include/test/test/info/Base.h
@@ -0,0 +1,35 @@
+#ifndef TEST_TEST_INFO_BASE_H_
+#define TEST_TEST_INFO_BASE_H_
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace test
+		{
+			namespace info
+			{
+				class Base;
+			}
+		}
+	}
+}
+
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace test
+		{
+			namespace info
+			{
+				class Base
+				{
+
+				};
+			}
+		}
+	}
+}
+#endif // ! TEST_TEST_INFO_BASE_H_
