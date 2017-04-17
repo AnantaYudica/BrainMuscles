c4fb9a47f04ed9df5550ace0f7e64de7c7e3665d
AnantaYudica <nta040689@gmail.com>
2017-04-18 05:44:59 +0700

    Replace from class Storage to class Integers

---
+++ include/type/trait/Integers.h
@@ -0,0 +1,50 @@
+#ifndef TYPE_TRAIT_INTEGERS_H
+#define TYPE_TRAIT_INTEGERS_H
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace trait
+		{
+			template <typename TYPE, TYPE VALUE, TYPE ... VALUES>
+			struct Integers;
+		}
+	}
+	
+}
+
+#include "type\trait\integers\ToTypeArray.h"
+#include "type\trait\integers\GetIndex.h"
+#include "type\trait\integers\GetSize.h"
+#include "type\trait\integers\IsContainValue.h"
+#include "type\trait\integers\Filler.h"
+#include "type\trait\integers\Function.h"
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace trait
+		{
+			template <typename TYPE, TYPE VALUE, TYPE ... VALUES>
+			struct Integers :
+				public Integers<TYPE, VALUES ...>
+			{
+				typedef Integers<TYPE, VALUES ...> Next;
+				typedef TYPE Type;
+				constexpr static TYPE Value = VALUE;
+			};
+
+			template <typename TYPE, TYPE VALUE>
+			struct Integers<TYPE, VALUE>
+			{
+				typedef void Next;
+				typedef TYPE Type;
+				constexpr static TYPE Value = VALUE;
+			};
+		}
+	}
+}
+
+#endif // !TYPE_TRAIT_INTEGERS_H
