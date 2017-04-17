f790d3ac8e77d9ffc6aed27b8a825910a85ca37a
AnantaYudica <nta040689@gmail.com>
2017-04-18 05:26:55 +0700

    Add class Message

---
+++ include/test/test/Message.h
@@ -0,0 +1,31 @@
+#ifndef TEST_TEST_MESSAGE_H_
+#define TEST_TEST_MESSAGE_H_
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace test
+		{
+			template<typename... ARGS>
+			class Message;
+		}
+	}
+}
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace test
+		{
+			template<typename... ARGS>
+			class Message
+			{
+
+			};
+		}
+	}
+}
+
+#endif // ! TEST_TEST_MESSAGE_H_
