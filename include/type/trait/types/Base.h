c62f08706bae694a676688bb0a12dd399ea126bc
AnantaYudica <nta040689@gmail.com>
2017-04-19 05:18:18 +0700

    Add class types::Base

---
+++ include/type/trait/types/Base.h
@@ -0,0 +1,38 @@
+#ifndef TYPE_TRAIT_TYPES_BASE_H_
+#define TYPE_TRAIT_TYPES_BASE_H_
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace trait
+		{
+			namespace types
+			{
+				template<typename CURRENT_TYPE, typename... TYPES>
+				class Base;
+			}
+		}
+	}
+}
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace trait
+		{
+			namespace types
+			{
+				template<typename CURRENT_TYPE, typename... TYPES>
+				class Base
+				{
+
+
+				};
+			}
+		}
+	}
+}
+
+#endif //!TYPE_TRAIT_TYPES_BASE_H_
