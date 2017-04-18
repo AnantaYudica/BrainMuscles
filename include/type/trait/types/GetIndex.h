4482221b38776468a1f50513a454c2259876273f
AnantaYudica <nta040689@gmail.com>
2017-04-18 08:05:43 +0700

    Add members of class GetIndex

--- include/type/trait/types/GetIndex.h
+++ include/type/trait/types/GetIndex.h
@@ -28,7 +28,50 @@ namespace BrainMuscles
 			{
 				template<typename TYPES, size_t INDEX>
 				class GetIndex
-				{};
+				{
+				private:
+				public:
+					typedef typename GetIndex<typename TYPES::Next, INDEX - 1>::Type Type;
+					static constexpr Type Cast(Type types);
+				};
+
+				template<size_t INDEX>
+				class GetIndex<void, INDEX>
+				{
+				private:
+				public:
+					typedef void Type;
+					static constexpr void Cast();
+				};
+
+				template<typename TYPES>
+				class GetIndex<TYPES, 0>
+				{
+				private:
+				public:
+					typedef typename TYPES::Current Type;
+					static constexpr Type Cast(Type types);
+				};
+
+				
+
+				template<typename TYPES, size_t INDEX>
+				constexpr typename GetIndex<TYPES, INDEX>::Type GetIndex<TYPES, INDEX>::Cast(Type types)
+				{
+					return types;
+				}
+
+				template<typename TYPES>
+				constexpr typename GetIndex<TYPES, 0>::Type GetIndex<TYPES, 0>::Cast(Type types)
+				{
+					return types;
+				}
+
+				template<size_t INDEX>
+				 constexpr void GetIndex<void, INDEX>::Cast()
+				{
+				}
+
 			}
 		}
 	}
