3fe874513857b1a2fbeee7291d5ff1de70b7ce2e
AnantaYudica <nta040689@gmail.com>
2017-04-18 09:04:44 +0700

    Add class message::Base

---
+++ include/test/test/message/Base.h
@@ -0,0 +1,37 @@
+#ifndef TEST_TEST_MESSAGE_BASE_H_
+#define TEST_TEST_MESSAGE_BASE_H_
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace test
+		{
+			namespace message
+			{
+				class Base;
+			}
+		}
+	}
+}
+
+#include "test\test\Message.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace test
+		{
+			namespace message
+			{
+				class Base
+				{
+					
+				};
+			}
+		}
+	}
+}
+
+#endif //!TEST_TEST_MESSAGE_BASE_H_
