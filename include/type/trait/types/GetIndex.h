5f598037722edf9fac3adbca4efe25c3eed4c0bd
AnantaYudica <nta040689@gmail.com>
2017-04-18 07:24:23 +0700

    Add class GetIndex

---
+++ include/type/trait/types/GetIndex.h
@@ -0,0 +1,37 @@
+#ifndef TYPE_TRAIT_TYPES_GETINDEX_H_
+#define TYPE_TRAIT_TYPES_GETINDEX_H_
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace trait
+		{
+			namespace types
+			{
+				template<typename TYPES, size_t INDEX>
+				class GetIndex;
+			}
+		}
+	}
+}
+
+#include "type\trait\Types.h"
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace trait
+		{
+			namespace types
+			{
+				template<typename TYPES, size_t INDEX>
+				class GetIndex
+				{};
+			}
+		}
+	}
+}
+
+#endif //! TYPE_TRAIT_TYPES_GETINDEX_H_
