5ca2b7efb0a25b3836c8a56028314c343e2a9b30
AnantaYudica <nta040689@gmail.com>
2017-04-19 01:06:35 +0700

    Add class Function

---
+++ include/type/trait/types/Function.h
@@ -0,0 +1,40 @@
+#ifndef TYPE_TRAIT_TYPES_FUNCTION_H_
+#define TYPE_TRAIT_TYPES_FUNCTION_H_
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace trait
+		{
+			namespace types
+			{
+				template<typename RETURN, typename TYPES, typename... ARGS>
+				class Function;
+			}
+		}
+	}
+}
+
+#include "type\trait\Types.h"
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace trait
+		{
+			namespace types
+			{
+				template<typename RETURN, typename TYPES, typename... ARGS>
+				class Function
+				{
+				public:
+
+				};
+			}
+		}
+	}
+}
+
+#endif //!TYPE_TRAIT_TYPES_FUNCTION_H_
