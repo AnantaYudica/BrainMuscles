7a0f1fe4f898e363a82252679323c4c3bca335c4
AnantaYudica <nta040689@gmail.com>
2017-04-19 09:25:04 +0700

    Insert definition member of class Message

--- include/test/test/Message.h
+++ include/test/test/Message.h
@@ -13,6 +13,9 @@ namespace BrainMuscles
 	}
 }
 
+#include "type\trait\Types.h"
+#include "test\test\message\Base.h"
+
 namespace BrainMuscles
 {
 	namespace test
@@ -20,10 +23,58 @@ namespace BrainMuscles
 		namespace test
 		{
 			template<typename... ARGS>
-			class Message
+			class Message : 
+				public message::Base
 			{
-
+			private:
+				typedef type::trait::Types<ARGS...> ValuesType;
+				typedef type::trait::types::Function<int, ValuesType, FILE*, const char *> Function_fprintf;
+				const char * m_message;
+				ValuesType m_values;
+			public:
+				Message(const char * message, ARGS... values);
+				void Call_fprintf(FILE * out) const;
 			};
+
+			template<>
+			class Message<> :
+				public message::Base
+			{
+			private:
+				typedef type::trait::types::Function<int, type::trait::Types<>, FILE*, const char *> Function_fprintf;
+				const char * m_message;
+				type::trait::Types<> m_values;
+			public:
+				Message(const char * message);
+				void Call_fprintf(FILE * out) const;
+			};
+
+			template<typename... ARGS>
+			Message<ARGS...>::Message(const char * message, ARGS... values) :
+				message::Base(),
+				m_values(values...),
+				m_message(message)
+			{
+			
+			}
+
+			template<typename... ARGS>
+			void Message<ARGS...>::Call_fprintf(FILE * out) const
+			{
+				Function_fprintf::Caller(fprintf, m_values, out, m_message);
+			}
+
+			Message<>::Message(const char * message) :
+				message::Base(),
+				m_message(message)
+			{
+				m_values = type::trait::Types<>();
+			}
+
+			void Message<>::Call_fprintf(FILE * out) const
+			{
+				Function_fprintf::Caller(fprintf, m_values, out, m_message);
+			}
 		}
 	}
 }
