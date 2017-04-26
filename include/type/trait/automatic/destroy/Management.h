ae1746243a300e1ff50d7e5806b0b9fd22cce04f
AnantaYudica <nta040689@gmail.com>
2017-04-26 16:00:11 +0700

    Insert Definition class automatic::destroy::Management

--- include/type/trait/automatic/destroy/Management.h
+++ include/type/trait/automatic/destroy/Management.h
@@ -39,9 +39,43 @@ namespace BrainMuscles
 						std::vector<Base*> m_list;
 					public:
 						~Management();
-						static void Register(Base*);
-						//void UnRegister(const destroy::Base&);
+						static void Register(Base* base);
+						static void Unregister(Base * base);
 					};
+
+					Management Management::ms_instance = Management();
+
+					Management::Management()
+					{}
+
+					Management::~Management()
+					{
+						std::vector<Base*> temp;
+						std::swap(temp, m_list);
+						m_list.clear();
+						for (auto i = temp.begin(); i != temp.end(); i++)
+						{
+							(*i)->Destructor();
+						}
+					}
+
+					void Management::Register(Base * base)
+					{
+						auto found = std::find(ms_instance.m_list.begin(), ms_instance.m_list.end(), base);
+						if (found == ms_instance.m_list.end())
+						{
+							ms_instance.m_list.push_back(base);
+						}
+					}
+
+					void Management::Unregister(Base * base)
+					{
+						auto found = std::find(ms_instance.m_list.begin(), ms_instance.m_list.end(), base);
+						if (found != ms_instance.m_list.end())
+						{
+							ms_instance.m_list.erase(found);
+						}
+					}
 				}
 			}
 		}
