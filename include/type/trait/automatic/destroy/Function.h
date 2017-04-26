c8157063dbfe3a462f520563cb6a2dd70e28e3eb
AnantaYudica <nta040689@gmail.com>
2017-04-26 15:59:43 +0700

    Insert Definition class automatic::destroy::Function

--- include/type/trait/automatic/destroy/Function.h
+++ include/type/trait/automatic/destroy/Function.h
@@ -11,7 +11,6 @@ namespace BrainMuscles
 			{
 				namespace destroy
 				{
-					template<typename RETURN, typename... ARGS>
 					class Function;
 				}
 			}
@@ -31,13 +30,45 @@ namespace BrainMuscles
 			{
 				namespace destroy
 				{
-					template<typename RETURN, typename... ARGS>
 					class Function :
 						public Base
 					{
-
-
+					public:
+						typedef Base::FUNCTION_DESTROY_TYPE FUNCTION_DESTROY_TYPE;
+					private:
+						FUNCTION_DESTROY_TYPE m_functionDestroy;
+					public:
+						Function(const Function & function);
+						Function(const Function * pointer);
+						Function(FUNCTION_DESTROY_TYPE function_destroy);
+						~Function();
+						void Destructor();
 					};
+
+					Function::Function(const Function & function) :
+						m_functionDestroy(function.m_functionDestroy)
+					{
+
+					}
+
+					Function::Function(const Function * pointer) :
+						m_functionDestroy(pointer->m_functionDestroy)
+					{
+					}
+
+					Function::Function(FUNCTION_DESTROY_TYPE function_destroy) :
+						m_functionDestroy(function_destroy)
+					{
+					}
+
+					Function::~Function()
+					{
+					}
+
+					void Function::Destructor()
+					{
+						m_functionDestroy();
+					}
 				}
 			}
 		}
