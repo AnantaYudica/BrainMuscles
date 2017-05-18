f6053e98410090526c0b010ef707460e9c1c41cd
AnantaYudica <nta040689@gmail.com>
2017-05-18 21:01:37 +0700

    Add Class Container for Primary template

---
+++ include/type/Container.h
@@ -0,0 +1,18 @@
+#ifndef TYPE_CONTAINER_H_
+#define TYPE_CONTAINER_H_
+
+#include "type\container\Base.h"
+#include "type\container\Element.h"
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		template<typename ELEMENT, typename CONTAINER_TYPE>
+		class Container
+		{
+		};
+	}
+}
+
+#endif //!TYPE_CONTAINER_H_
