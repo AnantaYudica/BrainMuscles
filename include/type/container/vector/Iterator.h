7ea0f919f7571fad376545a68859419d22a61ab4
AnantaYudica <nta040689@gmail.com>
2017-05-20 09:58:45 +0700

    Add definition Iterator for derived Iterator vector

---
+++ include/type/container/vector/Iterator.h
@@ -0,0 +1,86 @@
+#ifndef TYPE_CONTAINER_VECTOR_ITERATOR_H_
+#define TYPE_CONTAINER_VECTOR_ITERATOR_H_
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace container
+		{
+			namespace vector
+			{
+				template<typename TYPE, typename HANDLE, typename ITERATOR_TAG_TYPE>
+				class Iterator;
+			}
+		}
+	}
+}
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace container
+		{
+			namespace vector
+			{
+				template<typename TYPE, typename HANDLE, typename ITERATOR_TAG_TYPE>
+				class Iterator : 
+					public ITERATOR_TAG_TYPE
+				{
+				public:
+					typedef ITERATOR_TAG_TYPE BaseType;
+					typedef typename ITERATOR_TAG_TYPE::HandleBaseType HandleBaseType;
+					typedef typename ITERATOR_TAG_TYPE::HandleType HandleType;
+					typedef typename ITERATOR_TAG_TYPE::IteratorType IteratorType;
+				public:
+					Iterator();
+					template<typename OTHER_ITERATOR_TAG_TYPE>
+					Iterator(const Iterator<TYPE, HANDLE, ITERATOR_TAG_TYPE>& iterator);
+					Iterator(const HandleType& handle);
+					Iterator(IteratorType* pointer);
+					Iterator(const IteratorType& rhs);
+					~Iterator();
+				};
+
+				template<typename TYPE, typename HANDLE, typename ITERATOR_TAG_TYPE>
+				Iterator<TYPE, HANDLE, ITERATOR_TAG_TYPE>::Iterator() :
+					BaseType(),
+					HandleBaseType()
+				{}
+
+				template<typename TYPE, typename HANDLE, typename ITERATOR_TAG_TYPE>
+				template<typename OTHER_ITERATOR_TAG_TYPE>
+				Iterator<TYPE, HANDLE, ITERATOR_TAG_TYPE>::Iterator(const Iterator<TYPE, HANDLE, ITERATOR_TAG_TYPE>& iterator) :
+					BaseType(iterator),
+					HandleBaseType(iterator.GetHandle())
+				{}
+
+				template<typename TYPE, typename HANDLE, typename ITERATOR_TAG_TYPE>
+				Iterator<TYPE, HANDLE, ITERATOR_TAG_TYPE>::Iterator(const HandleType& handle) :
+					BaseType(handle),
+					HandleBaseType(handle)
+				{}
+
+				template<typename TYPE, typename HANDLE, typename ITERATOR_TAG_TYPE>
+				Iterator<TYPE, HANDLE, ITERATOR_TAG_TYPE>::Iterator(IteratorType* pointer) :
+					BaseType(pointer),
+					HandleBaseType(*pointer)
+				{}
+
+				template<typename TYPE, typename HANDLE, typename ITERATOR_TAG_TYPE>
+				Iterator<TYPE, HANDLE, ITERATOR_TAG_TYPE>::Iterator(const IteratorType& rhs) :
+					BaseType(rhs),
+					HandleBaseType(rhs)
+				{}
+
+				template<typename TYPE, typename HANDLE, typename ITERATOR_TAG_TYPE>
+				Iterator<TYPE, HANDLE, ITERATOR_TAG_TYPE>::~Iterator()
+				{}
+			}
+		}
+	}
+}
+
+
+#endif //!TYPE_CONTAINER_VECTOR_ITERATOR_H_
