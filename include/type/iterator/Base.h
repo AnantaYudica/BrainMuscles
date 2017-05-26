4d73f4419bf0a6620ef56ce230b4b39b49873b28
AnantaYudica <nta040689@gmail.com>
2017-05-26 15:39:44 +0700

    Add partial template specialization Cloneable Handle in class iterator::Base

--- include/type/iterator/Base.h
+++ include/type/iterator/Base.h
@@ -1,17 +1,8 @@
 #ifndef TYPE_ITERATOR_BASE_H_
 #define TYPE_ITERATOR_BASE_H_
 
-namespace BrainMuscles
-{
-	namespace type
-	{
-		namespace iterator
-		{
-			template<typename HANDLE, typename DERIVED>
-			class Base;
-		}
-	}
-}
+#include "type\Cloneable.h"
+#include <type_traits>
 
 namespace BrainMuscles
 {
@@ -19,14 +10,28 @@ namespace BrainMuscles
 	{
 		namespace iterator
 		{
-			template<typename HANDLE, typename DERIVED>
+			template<typename HANDLE, typename DERIVED, typename = void>
+			class Base;
+		}
+	}
+}
+
+
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace iterator
+		{
+			template<typename HANDLE, typename DERIVED, typename VOID>
 			class Base
 			{
 			public:
 				typedef DERIVED DerivedType;
 				typedef HANDLE HandleType;
 				typedef HANDLE * PointerHandle;
-				typedef Base<HANDLE, DERIVED> BaseType;
+				typedef Base<HANDLE, DERIVED, VOID> BaseType;
 			private:
 				PointerHandle m_handle;
 			protected:
@@ -41,25 +46,123 @@ namespace BrainMuscles
 				virtual DERIVED* ThisDerived() = 0;
 				HandleType& GetHandle();
 				const HandleType& GetHandle() const;
+				bool IsNullHandle() const;
 				BaseType& operator=(const BaseType& rhs);
 			};
 
 			template<typename HANDLE, typename DERIVED>
-			Base<HANDLE, DERIVED>::Base() :
+			class Base<HANDLE, DERIVED, typename std::enable_if<std::is_base_of<BrainMuscles::type::Cloneable, HANDLE>::value, void>::type>
+			{
+			public:
+				typedef DERIVED DerivedType;
+				typedef HANDLE HandleType;
+				typedef HANDLE * PointerHandle;
+				typedef Base<HANDLE, DERIVED, typename std::enable_if<std::is_base_of<BrainMuscles::type::Cloneable, HANDLE>::value, void>::type> BaseType;
+			private:
+				PointerHandle m_handle;
+			protected:
+				Base();
+				Base(const HandleType& handle);
+				Base(const DerivedType& derived);
+				template<typename DERIVED_HANDLE, typename... ARGS>
+				Base(ARGS... args);
+			public:
+				virtual ~Base();
+			protected:
+				virtual DERIVED* ThisDerived() = 0;
+				HandleType& GetHandle();
+				const HandleType& GetHandle() const;
+				bool IsNullHandle() const;
+				BaseType& operator=(const BaseType& rhs);
+			};
+
+			///////////////////////////////////////////////////////////////////////////
+			template<typename HANDLE, typename DERIVED, typename VOID>
+			Base<HANDLE, DERIVED, VOID>::Base() :
+				m_handle(0)
+			{}
+
+			template<typename HANDLE, typename DERIVED, typename VOID>
+			Base<HANDLE, DERIVED, VOID>::Base(const HandleType& handle) :
+				m_handle(new HandleType(handle))
+			{}
+
+			template<typename HANDLE, typename DERIVED, typename VOID>
+			Base<HANDLE, DERIVED, VOID>::Base(const DerivedType& derived)
+			{
+				if (!derived.IsNullHandle())
+				{
+					m_handle = new HandleType(derived.GetHandle());
+				}
+				else
+				{
+					m_handle = 0;
+				}
+			}
+
+			template<typename HANDLE, typename DERIVED, typename VOID>
+			template<typename... ARGS>
+			Base<HANDLE, DERIVED, VOID>::Base(ARGS... args) :
+				m_handle(new HandleType(args...))
+			{
+			}
+
+			template<typename HANDLE, typename DERIVED, typename VOID>
+			Base<HANDLE, DERIVED, VOID>::~Base()
+			{
+				if (m_handle)
+				{
+					delete m_handle;
+				}
+			}
+
+			template<typename HANDLE, typename DERIVED, typename VOID>
+			typename Base<HANDLE, DERIVED, VOID>::HandleType&
+			Base<HANDLE, DERIVED, VOID>::GetHandle()
+			{
+				return *m_handle;
+			}
+
+			template<typename HANDLE, typename DERIVED, typename VOID>
+			const typename Base<HANDLE, DERIVED, VOID>::HandleType&
+			Base<HANDLE, DERIVED, VOID>::GetHandle() const
+			{
+				return *m_handle;
+			}
+
+			template<typename HANDLE, typename DERIVED, typename VOID>
+			bool Base<HANDLE, DERIVED, VOID>::IsNullHandle() const
+			{
+				return m_handle == 0;
+			}
+
+			template<typename HANDLE, typename DERIVED, typename VOID>
+			Base<HANDLE, DERIVED, VOID>& Base<HANDLE, DERIVED, VOID>::operator=(const BaseType& rhs)
+			{
+				if (!rhs.IsNullHandle())
+				{
+					delete m_handle;
+				}
+				m_handle = new HandleType(rhs.GetHandle());
+				return *this;
+			}
+			///////////////////////////////////////////////////////////////////////////
+			template<typename HANDLE, typename DERIVED>
+			Base<HANDLE, DERIVED, typename std::enable_if<std::is_base_of<BrainMuscles::type::Cloneable, HANDLE>::value, void>::type>::Base() :
 				m_handle(0)
 			{}
 
 			template<typename HANDLE, typename DERIVED>
-			Base<HANDLE, DERIVED>::Base(const HandleType& handle) :
-				m_handle(new HandleType(handle))
+			Base<HANDLE, DERIVED, typename std::enable_if<std::is_base_of<BrainMuscles::type::Cloneable, HANDLE>::value, void>::type>::Base(const HandleType& handle) :
+				m_handle(handle.Clone())
 			{}
 
 			template<typename HANDLE, typename DERIVED>
-			Base<HANDLE, DERIVED>::Base(const DerivedType& derived)
+			Base<HANDLE, DERIVED, typename std::enable_if<std::is_base_of<BrainMuscles::type::Cloneable, HANDLE>::value, void>::type>::Base(const DerivedType& derived)
 			{
-				if (derived.m_handle != 0)
+				if (!derived.IsNullHandle())
 				{
-					m_handle = new HandleType(*derived.m_handle);
+					m_handle = derived.Clone();
 				}
 				else
 				{
@@ -68,14 +171,14 @@ namespace BrainMuscles
 			}
 
 			template<typename HANDLE, typename DERIVED>
-			template<typename... ARGS>
-			Base<HANDLE, DERIVED>::Base(ARGS... args) :
-				m_handle(new HandleType(args...))
+			template<typename DERIVED_HANDLE, typename... ARGS>
+			Base<HANDLE, DERIVED, typename std::enable_if<std::is_base_of<BrainMuscles::type::Cloneable, HANDLE>::value, void>::type>::Base(ARGS... args) :
+				m_handle(HANDLE::Constructor<DERIVED_HANDLE>(args...))
 			{
 			}
 
 			template<typename HANDLE, typename DERIVED>
-			Base<HANDLE, DERIVED>::~Base()
+			Base<HANDLE, DERIVED, typename std::enable_if<std::is_base_of<BrainMuscles::type::Cloneable, HANDLE>::value, void>::type>::~Base()
 			{
 				if (m_handle)
 				{
@@ -84,29 +187,37 @@ namespace BrainMuscles
 			}
 
 			template<typename HANDLE, typename DERIVED>
-			typename Base<HANDLE, DERIVED>::HandleType&
-			Base<HANDLE, DERIVED>::GetHandle()
+			typename Base<HANDLE, DERIVED, typename std::enable_if<std::is_base_of<BrainMuscles::type::Cloneable, HANDLE>::value, void>::type>::HandleType&
+				Base<HANDLE, DERIVED, typename std::enable_if<std::is_base_of<BrainMuscles::type::Cloneable, HANDLE>::value, void>::type>::GetHandle()
 			{
 				return *m_handle;
 			}
 
 			template<typename HANDLE, typename DERIVED>
-			const typename Base<HANDLE, DERIVED>::HandleType& 
-			Base<HANDLE, DERIVED>::GetHandle() const
+			const typename Base<HANDLE, DERIVED, typename std::enable_if<std::is_base_of<BrainMuscles::type::Cloneable, HANDLE>::value, void>::type>::HandleType&
+			Base<HANDLE, DERIVED, typename std::enable_if<std::is_base_of<BrainMuscles::type::Cloneable, HANDLE>::value, void>::type>::GetHandle() const
 			{
 				return *m_handle;
 			}
 
 			template<typename HANDLE, typename DERIVED>
-			Base<HANDLE, DERIVED>& Base<HANDLE, DERIVED>::operator=(const BaseType& rhs)
+			bool Base<HANDLE, DERIVED, typename std::enable_if<std::is_base_of<BrainMuscles::type::Cloneable, HANDLE>::value, void>::type>::IsNullHandle() const
 			{
-				if (rhs.m_handle != 0)
+				return m_handle == 0;
+			}
+
+			template<typename HANDLE, typename DERIVED>
+			Base<HANDLE, DERIVED, typename std::enable_if<std::is_base_of<BrainMuscles::type::Cloneable, HANDLE>::value, void>::type>& 
+			Base<HANDLE, DERIVED, typename std::enable_if<std::is_base_of<BrainMuscles::type::Cloneable, HANDLE>::value, void>::type>::operator=(const BaseType& rhs)
+			{
+				if (!rhs.IsNullHandle())
 				{
 					delete m_handle;
 				}
-				m_handle = new HandleType(*rhs.m_handle);
+				m_handle = rhs.Clone();
 				return *this;
 			}
+			///////////////////////////////////////////////////////////////////////////
 		}
 	}
 }
