db985236c9cf3e88abefd4e8538ce36b95a0a961
AnantaYudica <nta040689@gmail.com>
2017-06-10 00:39:06 +0700

    Add Project Type-Container-Vector for experiment Add main program for Project Type-Container-Vector

---
+++ experiment/type/container/type-container-vector.cpp
@@ -0,0 +1,12 @@
+#define USING_TEST
+#include "test\Test.h"
+
+#include "type\container\Vector.h"
+
+int main()
+{
+	Debug("Vector");
+	BrainMuscles::type::container::Vector<int> vec_a;
+
+	return 1;
+}
