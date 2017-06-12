722451aaeefa16e35f4dbf2199080de07ebee37b
AnantaYudica <nta040689@gmail.com>
2017-06-12 08:58:20 +0700

    Modified add parameter template for const Type Modified add Assign overloading for parameter const type

--- include/type/container/member/modifier/M_Assign.h
+++ include/type/container/member/modifier/M_Assign.h
@@ -11,7 +11,7 @@ namespace BrainMuscles
 			{
 				namespace modifier
 				{
-					template <typename INPUT_ITERATOR_TYPE, typename VALUE_TYPE, typename SIZE_TYPE>
+					template <typename INPUT_ITERATOR_TYPE, typename INPUT_ITERATOR_CONST_TYPE, typename VALUE_TYPE, typename SIZE_TYPE>
 					class M_Assign;
 				}
 			}
@@ -31,7 +31,7 @@ namespace BrainMuscles
 			{
 				namespace modifier
 				{
-					template <typename INPUT_ITERATOR_TYPE, typename VALUE_TYPE, typename SIZE_TYPE>
+					template <typename INPUT_ITERATOR_TYPE, typename INPUT_ITERATOR_CONST_TYPE, typename VALUE_TYPE, typename SIZE_TYPE>
 					class M_Assign
 					{
 					protected:
@@ -39,15 +39,16 @@ namespace BrainMuscles
 					public:
 						virtual ~M_Assign();
 						virtual void Assign(INPUT_ITERATOR_TYPE first, INPUT_ITERATOR_TYPE last) = 0;
+						virtual void Assign(INPUT_ITERATOR_CONST_TYPE first, INPUT_ITERATOR_CONST_TYPE last) = 0;
 						virtual void Assign(SIZE_TYPE n, const VALUE_TYPE& val) = 0;
 						virtual void Assign(std::initializer_list<VALUE_TYPE> il) = 0;
 					};
 
-					template <typename INPUT_ITERATOR_TYPE, typename VALUE_TYPE, typename SIZE_TYPE>
+					template <typename INPUT_ITERATOR_TYPE, typename INPUT_ITERATOR_CONST_TYPE, typename VALUE_TYPE, typename SIZE_TYPE>
 					M_Assign<INPUT_ITERATOR_TYPE, VALUE_TYPE, SIZE_TYPE>::M_Assign()
 					{}
 
-					template <typename INPUT_ITERATOR_TYPE, typename VALUE_TYPE, typename SIZE_TYPE>
+					template <typename INPUT_ITERATOR_TYPE, typename INPUT_ITERATOR_CONST_TYPE, typename VALUE_TYPE, typename SIZE_TYPE>
 					M_Assign<INPUT_ITERATOR_TYPE, VALUE_TYPE, SIZE_TYPE>::~M_Assign()
 					{}
 				}
