898fcc91b85638807e928c0bc11dc48b002e3711
AnantaYudica <nta040689@gmail.com>
2017-06-14 05:28:10 +0700

    Add definition::ConstIterator and definition::Iterator for define definition iterator of Array

---
+++ include/type/container/array/iterator/definition/ConstIterator.h
@@ -0,0 +1,34 @@
+#ifndef TYPE_CONTAINER_ARRAY_ITERATOR_DEFINITION_CONSTITERATOR_H_
+#define TYPE_CONTAINER_ARRAY_ITERATOR_DEFINITION_CONSTITERATOR_H_
+
+#include "type\iterator\derived\Definition.h"
+#include "type\container\array\Iterator.h"
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace container
+		{
+			namespace array
+			{
+				namespace iterator
+				{
+					namespace definition
+					{
+						template<typename TYPE, size_t SIZE, typename HANDLE>
+						struct ConstIterator :
+							public BrainMuscles::type::iterator::derived::Definition<
+								BrainMuscles::type::container::array::Iterator<TYPE, SIZE, HANDLE>,
+								TYPE,
+								const TYPE&,
+								const TYPE*>
+						{};
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif //!TYPE_CONTAINER_ARRAY_ITERATOR_DEFINITION_CONSTITERATOR_H_
---
+++ include/type/container/array/iterator/definition/Iterator.h
@@ -0,0 +1,34 @@
+#ifndef TYPE_CONTAINER_ARRAY_ITERATOR_DEFINITION_ITERATOR_H_
+#define TYPE_CONTAINER_ARRAY_ITERATOR_DEFINITION_ITERATOR_H_
+
+#include "type\iterator\derived\Definition.h"
+#include "type\container\array\Iterator.h"
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace container
+		{
+			namespace array
+			{
+				namespace iterator
+				{
+					namespace definition
+					{
+						template<typename TYPE, size_t SIZE, typename HANDLE>
+						struct Iterator :
+							public BrainMuscles::type::iterator::derived::Definition<
+								BrainMuscles::type::container::array::Iterator<TYPE, SIZE, HANDLE>,
+								TYPE,
+								TYPE&,
+								TYPE*>
+						{};
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif //!TYPE_CONTAINER_ARRAY_ITERATOR_DEFINITION_ITERATOR_H_
