9d8f35fb3a76e8ed39b67049c74d7aaa5e7f1fc5
AnantaYudica <nta040689@gmail.com>
2017-06-15 05:36:14 +0700

    Modified add dependency derive\info\IsType.h Modified remove parameter template DERIVED Modified add parameter template DERIVED_INFO Modified change alias type DerivedType with DERIVED_INFO::DerivedType Modified return type ThisDerived with DerivedType

--- include/type/iterator/member/M_Equality.h
+++ include/type/iterator/member/M_Equality.h
@@ -3,6 +3,7 @@
 
 #include "type\iterator\Base.h"
 #include "type\iterator\handle\info\IsType.h"
+#include "type\iterator\derived\info\IsType.h"
 
 namespace BrainMuscles
 {
@@ -12,15 +13,16 @@ namespace BrainMuscles
 		{
 			namespace member
 			{
-				template<typename DERIVED, typename HANDLE, typename HANDLE_INFO>
+				template<typename HANDLE, typename DERIVED_INFO, typename HANDLE_INFO>
 				class M_Equality :
 					public virtual BrainMuscles::type::iterator::Base<HANDLE, DERIVED>
 				{
+					static_assert(BrainMuscles::type::iterator::derived::info::IsType<DERIVED_INFO>::Value, "[DERIVED_INFO Requires class derived::Info<Definition>");
 					static_assert(BrainMuscles::type::iterator::handle::info::IsType<HANDLE_INFO>::Value, "Requires class Info<Definition>");
 				public:
-					typedef DERIVED DerivedType;
+					typedef typename DERIVED_INFO::DerivedType DerivedType;
 					typedef HANDLE HandleType;
-					typedef BrainMuscles::type::iterator::Base<HANDLE, DERIVED> BaseType;
+					typedef BrainMuscles::type::iterator::Base<HANDLE, DERIVED_INFO> BaseType;
 				protected:
 					M_Equality();
 					M_Equality(const DerivedType& derived);
@@ -30,7 +32,7 @@ namespace BrainMuscles
 				public:
 					virtual ~M_Equality();
 				protected:
-					virtual DERIVED* ThisDerived() = 0;
+					virtual DerivedType* ThisDerived() = 0;
 
 					virtual bool OnRequestEqual(DerivedType& rhs) = 0;
 				public:
@@ -39,39 +41,39 @@ namespace BrainMuscles
 
 				};
 
-				template<typename DERIVED, typename HANDLE, typename HANDLE_INFO>
-				M_Equality<DERIVED, HANDLE, HANDLE_INFO>::M_Equality() :
+				template<typename HANDLE, typename DERIVED_INFO, typename HANDLE_INFO>
+				M_Equality<HANDLE, DERIVED_INFO, HANDLE_INFO>::M_Equality() :
 					BaseType()
 				{}
 
-				template<typename DERIVED, typename HANDLE, typename HANDLE_INFO>
-				M_Equality<DERIVED, HANDLE, HANDLE_INFO>::M_Equality(const DerivedType& derived) :
+				template<typename HANDLE, typename DERIVED_INFO, typename HANDLE_INFO>
+				M_Equality<HANDLE, DERIVED_INFO, HANDLE_INFO>::M_Equality(const DerivedType& derived) :
 					BaseType(derived)
 				{}
 
-				template<typename DERIVED, typename HANDLE, typename HANDLE_INFO>
-				M_Equality<DERIVED, HANDLE, HANDLE_INFO>::M_Equality(const HandleType& handle) :
+				template<typename HANDLE, typename DERIVED_INFO, typename HANDLE_INFO>
+				M_Equality<HANDLE, DERIVED_INFO, HANDLE_INFO>::M_Equality(const HandleType& handle) :
 					BaseType(handle)
 				{}
 
-				template<typename DERIVED, typename HANDLE, typename HANDLE_INFO>
+				template<typename HANDLE, typename DERIVED_INFO, typename HANDLE_INFO>
 				template<typename... ARGS>
-				M_Equality<DERIVED, HANDLE, HANDLE_INFO>::M_Equality(ARGS... args) :
+				M_Equality<HANDLE, DERIVED_INFO, HANDLE_INFO>::M_Equality(ARGS... args) :
 					BaseType(args...)
 				{}
 
-				template<typename DERIVED, typename HANDLE, typename HANDLE_INFO>
-				M_Equality<DERIVED, HANDLE, HANDLE_INFO>::~M_Equality()
+				template<typename HANDLE, typename DERIVED_INFO, typename HANDLE_INFO>
+				M_Equality<HANDLE, DERIVED_INFO, HANDLE_INFO>::~M_Equality()
 				{}
 
-				template<typename DERIVED, typename HANDLE, typename HANDLE_INFO>
-				bool M_Equality<DERIVED, HANDLE, HANDLE_INFO>::operator==(DerivedType& rhs)
+				template<typename HANDLE, typename DERIVED_INFO, typename HANDLE_INFO>
+				bool M_Equality<HANDLE, DERIVED_INFO, HANDLE_INFO>::operator==(DerivedType& rhs)
 				{
 					return OnRequestEqual(rhs);
 				}
 
-				template<typename DERIVED, typename HANDLE, typename HANDLE_INFO>
-				bool M_Equality<DERIVED, HANDLE, HANDLE_INFO>::operator!=(DerivedType& rhs)
+				template<typename HANDLE, typename DERIVED_INFO, typename HANDLE_INFO>
+				bool M_Equality<HANDLE, DERIVED_INFO, HANDLE_INFO>::operator!=(DerivedType& rhs)
 				{
 					return !OnRequestEqual(rhs);
 				}
