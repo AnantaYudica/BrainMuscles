b525caec666816a03d777c6ad8ea7e684219078f
AnantaYudica <nta040689@gmail.com>
2017-06-15 05:53:12 +0700

    Modified add dependency derive\info\IsType.h Modified remove parameter template TYPE, DERIVED Modified add parameter template DERIVED_INFO Modified parameter template Base class with replace DERIVED to DERIVED_INFO modified add validation for DERIVED_INFO Modified remove alias type Type Modified add alias type ReferenceType and PointerType Modified change alias type DerivedType with DERIVED_INFO::DerivedType Modified return type ThisDerived with DerivedType, OnRequestReference with ReferenceType, OnRequestPointer with PointerType, operator* with ReferenceType and operator-> with PointerType

--- include/type/iterator/member/M_LeftValue.h
+++ include/type/iterator/member/M_LeftValue.h
@@ -3,6 +3,7 @@
 
 #include "type\iterator\Base.h"
 #include "type\iterator\handle\info\IsType.h"
+#include "type\iterator\derived\info\IsType.h"
 
 namespace BrainMuscles
 {
@@ -12,16 +13,18 @@ namespace BrainMuscles
 		{
 			namespace member
 			{
-				template<typename TYPE, typename DERIVED, typename HANDLE, typename HANDLE_INFO>
+				template<typename HANDLE, typename DERIVED_INFO, typename HANDLE_INFO>
 				class M_LeftValue :
-					public virtual BrainMuscles::type::iterator::Base<HANDLE, DERIVED>
+					public virtual BrainMuscles::type::iterator::Base<HANDLE, DERIVED_INFO>
 				{
+					static_assert(BrainMuscles::type::iterator::derived::info::IsType<DERIVED_INFO>::Value, "[DERIVED_INFO Requires class derived::Info<Definition>");
 					static_assert(BrainMuscles::type::iterator::handle::info::IsType<HANDLE_INFO>::Value, "Requires class Info<Definition>");
 				public:
-					typedef DERIVED DerivedType;
+					typedef typename DERIVED_INFO::DerivedType DerivedType;
+					typedef typename DERIVED_INFO::ReferenceType ReferenceType;
+					typedef typename DERIVED_INFO::PointerType PointerType;
 					typedef HANDLE HandleType;
-					typedef BrainMuscles::type::iterator::Base<HANDLE, DERIVED> BaseType;
-					typedef TYPE ValueType;
+					typedef BrainMuscles::type::iterator::Base<HANDLE, DERIVED_INFO> BaseType;
 				protected:
 					M_LeftValue();
 					M_LeftValue(const DerivedType& derived);
@@ -31,51 +34,51 @@ namespace BrainMuscles
 				public:
 					virtual ~M_LeftValue();
 				protected:
-					virtual DERIVED* ThisDerived() = 0;
+					virtual DerivedType* ThisDerived() = 0;
 
-					virtual ValueType& OnRequestReference() = 0;
-					virtual ValueType* OnRequestPointer() = 0;
+					virtual ReferenceType OnRequestReference() = 0;
+					virtual PointerType OnRequestPointer() = 0;
 				public:
-					ValueType& operator*();
-					ValueType* operator->();
+					ReferenceType operator*();
+					PointerType operator->();
 
 				};
 
-				template<typename TYPE, typename DERIVED, typename HANDLE, typename HANDLE_INFO>
-				M_LeftValue<TYPE, DERIVED, HANDLE, HANDLE_INFO>::M_LeftValue() :
+				template<typename HANDLE, typename DERIVED_INFO, typename HANDLE_INFO>
+				M_LeftValue<HANDLE, DERIVED_INFO, HANDLE_INFO>::M_LeftValue() :
 					BaseType()
 				{}
 
-				template<typename TYPE, typename DERIVED, typename HANDLE, typename HANDLE_INFO>
-				M_LeftValue<TYPE, DERIVED, HANDLE, HANDLE_INFO>::M_LeftValue(const DerivedType& derived) :
+				template<typename HANDLE, typename DERIVED_INFO, typename HANDLE_INFO>
+				M_LeftValue<HANDLE, DERIVED_INFO, HANDLE_INFO>::M_LeftValue(const DerivedType& derived) :
 					BaseType(derived)
 				{}
 
-				template<typename TYPE, typename DERIVED, typename HANDLE, typename HANDLE_INFO>
-				M_LeftValue<TYPE, DERIVED, HANDLE, HANDLE_INFO>::M_LeftValue(const HandleType& handle) :
+				template<typename HANDLE, typename DERIVED_INFO, typename HANDLE_INFO>
+				M_LeftValue<HANDLE, DERIVED_INFO, HANDLE_INFO>::M_LeftValue(const HandleType& handle) :
 					BaseType(handle)
 				{}
 
-				template<typename TYPE, typename DERIVED, typename HANDLE, typename HANDLE_INFO>
+				template<typename HANDLE, typename DERIVED_INFO, typename HANDLE_INFO>
 				template<typename... ARGS>
-				M_LeftValue<TYPE, DERIVED, HANDLE, HANDLE_INFO>::M_LeftValue(ARGS... args) :
+				M_LeftValue<HANDLE, DERIVED_INFO, HANDLE_INFO>::M_LeftValue(ARGS... args) :
 					BaseType(args...)
 				{}
 
-				template<typename TYPE, typename DERIVED, typename HANDLE, typename HANDLE_INFO>
-				M_LeftValue<TYPE, DERIVED, HANDLE, HANDLE_INFO>::~M_LeftValue()
+				template<typename HANDLE, typename DERIVED_INFO, typename HANDLE_INFO>
+				M_LeftValue<HANDLE, DERIVED_INFO, HANDLE_INFO>::~M_LeftValue()
 				{}
 
-				template<typename TYPE, typename DERIVED, typename HANDLE, typename HANDLE_INFO>
-				typename M_LeftValue<TYPE, DERIVED, HANDLE, HANDLE_INFO>::ValueType&
-				M_LeftValue<TYPE, DERIVED, HANDLE, HANDLE_INFO>::operator*()
+				template<typename HANDLE, typename DERIVED_INFO, typename HANDLE_INFO>
+				typename M_LeftValue<HANDLE, DERIVED_INFO, HANDLE_INFO>::ReferenceType
+				M_LeftValue<HANDLE, DERIVED_INFO, HANDLE_INFO>::operator*()
 				{
 					return OnRequestReference();
 				}
 
-				template<typename TYPE, typename DERIVED, typename HANDLE, typename HANDLE_INFO>
-				typename M_LeftValue<TYPE, DERIVED, HANDLE, HANDLE_INFO>::ValueType*
-				M_LeftValue<TYPE, DERIVED, HANDLE, HANDLE_INFO>::operator->()
+				template<typename HANDLE, typename DERIVED_INFO, typename HANDLE_INFO>
+				typename M_LeftValue<HANDLE, DERIVED_INFO, HANDLE_INFO>::PointerType
+				M_LeftValue<HANDLE, DERIVED_INFO, HANDLE_INFO>::operator->()
 				{
 					return OnRequestPointer();
 				}
