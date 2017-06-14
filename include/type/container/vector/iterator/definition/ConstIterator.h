f96f620b63132f5f79e7f27e7fcbbf8516e3dd9e
AnantaYudica <nta040689@gmail.com>
2017-06-14 13:58:59 +0700

    Add definition::ConstIterator and definition::Iterator for define definition iterator of Vector

---
+++ include/type/container/vector/iterator/definition/ConstIterator.h
@@ -0,0 +1,52 @@
+#ifndef TYPE_CONTAINER_VECTOR_ITERATOR_DEFINITION_CONSTITERATOR_H_
+#define TYPE_CONTAINER_VECTOR_ITERATOR_DEFINITION_CONSTITERATOR_H_
+
+#include "type\iterator\derived\Definition.h"
+#include "type\iterator\Tag.h"
+
+#include "type\container\vector\Iterator.h"
+#include "type\container\vector\iterator\RandomAccess.h"
+#include "type\container\vector\iterator\Input.h"
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace container
+		{
+			namespace vector
+			{
+				namespace iterator
+				{
+					namespace definition
+					{
+						template<BrainMuscles::type::iterator::Tag TAG, typename TYPE, typename HANDLE>
+						struct ConstIterator
+						{
+						};
+
+						template<typename TYPE, typename HANDLE>
+						struct ConstIterator<BrainMuscles::type::iterator::tag::random_access, TYPE, HANDLE> :
+							public BrainMuscles::type::iterator::derived::Definition<
+								BrainMuscles::type::container::vector::Iterator<
+									BrainMuscles::type::container::vector::iterator::RandomAccess<
+										TYPE, HANDLE>>, TYPE, const TYPE&, const TYPE*>
+						{
+						};
+
+						template<typename TYPE, typename HANDLE>
+						struct ConstIterator<BrainMuscles::type::iterator::tag::input, TYPE, HANDLE> :
+							public BrainMuscles::type::iterator::derived::Definition<
+								BrainMuscles::type::container::vector::Iterator<
+									BrainMuscles::type::container::vector::iterator::Input<
+										TYPE, HANDLE>>, TYPE, const TYPE&, const TYPE*>
+						{
+						};
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif //!TYPE_CONTAINER_VECTOR_ITERATOR_DEFINITION_CONSTITERATOR_H_
---
+++ include/type/container/vector/iterator/definition/Iterator.h
@@ -0,0 +1,52 @@
+#ifndef TYPE_CONTAINER_VECTOR_ITERATOR_DEFINITION_ITERATOR_H_
+#define TYPE_CONTAINER_VECTOR_ITERATOR_DEFINITION_ITERATOR_H_
+
+#include "type\iterator\derived\Definition.h"
+#include "type\iterator\Tag.h"
+
+#include "type\container\vector\Iterator.h"
+#include "type\container\vector\iterator\RandomAccess.h"
+#include "type\container\vector\iterator\Input.h"
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace container
+		{
+			namespace vector
+			{
+				namespace iterator
+				{
+					namespace definition
+					{
+						template<BrainMuscles::type::iterator::Tag TAG, typename TYPE, typename HANDLE>
+						struct Iterator
+						{
+						};
+
+						template<typename TYPE, typename HANDLE>
+						struct Iterator<BrainMuscles::type::iterator::tag::random_access, TYPE, HANDLE> :
+							public BrainMuscles::type::iterator::derived::Definition<
+							BrainMuscles::type::container::vector::Iterator<
+							BrainMuscles::type::container::vector::iterator::RandomAccess<
+							TYPE, HANDLE>>, TYPE, TYPE&, TYPE*>
+						{
+						};
+
+						template<typename TYPE, typename HANDLE>
+						struct Iterator<BrainMuscles::type::iterator::tag::input, TYPE, HANDLE> :
+							public BrainMuscles::type::iterator::derived::Definition<
+							BrainMuscles::type::container::vector::Iterator<
+							BrainMuscles::type::container::vector::iterator::Input<
+							TYPE, HANDLE>>, TYPE, TYPE&, TYPE*>
+						{
+						};
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif //!TYPE_CONTAINER_VECTOR_ITERATOR_DEFINITION_ITERATOR_H_
