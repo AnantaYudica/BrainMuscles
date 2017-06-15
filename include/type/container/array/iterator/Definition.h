6392603f1c5f513f61c85bf34da1b6317458c3d1
AnantaYudica <nta040689@gmail.com>
2017-06-15 14:26:49 +0700

    Add iterator::Definition class for helper search definition type {ConstIterator, Iterator} by Handle Type

---
+++ include/type/container/array/iterator/Definition.h
@@ -0,0 +1,49 @@
+#ifndef TYPE_CONTAINER_ARRAY_ITERATOR_DEFINITION_H_
+#define TYPE_CONTAINER_ARRAY_ITERATOR_DEFINITION_H_
+
+#include <array>
+#include "type\container\array\iterator\definition\ConstIterator.h"
+#include "type\container\array\iterator\definition\Iterator.h"
+#include "type\iterator\derived\definition\Base.h"
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace container
+		{
+			namespace array
+			{
+				namespace iterator
+				{
+					template<typename TYPE, size_t SIZE, typename HANDLE>
+					struct Definition :
+						public BrainMuscles::type::iterator::derived::definition::Base
+					{};
+
+					template<typename TYPE, size_t SIZE>
+					struct Definition<TYPE, SIZE, std::array<TYPE, SIZE>::const_iterator> :
+						public BrainMuscles::type::container::array::iterator::definition::ConstIterator<TYPE, SIZE, std::array<TYPE, SIZE>::const_iterator>
+					{};
+
+					template<typename TYPE, size_t SIZE>
+					struct Definition<TYPE, SIZE, std::array<TYPE, SIZE>::const_reverse_iterator> :
+						public BrainMuscles::type::container::array::iterator::definition::ConstIterator<TYPE, SIZE, std::array<TYPE, SIZE>::const_iterator>
+					{};
+
+					template<typename TYPE, size_t SIZE>
+					struct Definition<TYPE, SIZE, std::array<TYPE, SIZE>::iterator> :
+						public BrainMuscles::type::container::array::iterator::definition::Iterator<TYPE, SIZE, std::array<TYPE, SIZE>::iterator>
+					{};
+
+					template<typename TYPE, size_t SIZE>
+					struct Definition<TYPE, SIZE, std::array<TYPE, SIZE>::reverse_iterator> :
+						public BrainMuscles::type::container::array::iterator::definition::Iterator<TYPE, SIZE, std::array<TYPE, SIZE>::reverse_iterator>
+					{};
+				}
+			}
+		}
+	}
+}
+
+#endif //!TYPE_CONTAINER_ARRAY_ITERATOR_DEFINITION_H_
