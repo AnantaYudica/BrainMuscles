abd4e25563f1bb3a8c7926a2d6419731cc6b6d40
AnantaYudica <nta040689@gmail.com>
2017-06-21 09:18:23 +0700

    Add struct BrainMuscles::type::vector::iterator::handle::definition::Iterator for define class BrainMuscles::type::vector::iterator::Handle without const Add struct BrainMuscles::type::vector::iterator::handle::definition::ConstIterator for define class BrainMuscles::type::vector::iterator::Handle with const

---
+++ include/type/container/vector/iterator/handle/definition/ConstIterator.h
@@ -0,0 +1,32 @@
+#ifndef TYPE_CONTAINER_VECTOR_ITERATOR_HANDLE_DEFINITION_CONSTITERATOR_H_
+#define TYPE_CONTAINER_VECTOR_ITERATOR_HANDLE_DEFINITION_CONSTITERATOR_H_
+
+#include "type\iterator\handle\Definition.h"
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace container
+		{
+			namespace vector
+			{
+				namespace iterator
+				{
+					namespace handle
+					{
+						namespace definition
+						{
+							template <typename TYPE>
+							struct ConstIterator :
+								public BrainMuscles::type::iterator::handle::Definition<const TYPE>
+							{};
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif //!TYPE_CONTAINER_VECTOR_ITERATOR_HANDLE_DEFINITION_CONSTITERATOR_H_
---
+++ include/type/container/vector/iterator/handle/definition/Iterator.h
@@ -0,0 +1,32 @@
+#ifndef TYPE_CONTAINER_VECTOR_ITERATOR_HANDLE_DEFINITION_ITERATOR_H_
+#define TYPE_CONTAINER_VECTOR_ITERATOR_HANDLE_DEFINITION_ITERATOR_H_
+
+#include "type\iterator\handle\Definition.h"
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace container
+		{
+			namespace vector
+			{
+				namespace iterator
+				{
+					namespace handle
+					{
+						namespace definition
+						{
+							template <typename TYPE>
+							struct Iterator :
+								public BrainMuscles::type::iterator::handle::Definition<TYPE>
+							{};
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif //!TYPE_CONTAINER_VECTOR_ITERATOR_HANDLE_DEFINITION_ITERATOR_H_
