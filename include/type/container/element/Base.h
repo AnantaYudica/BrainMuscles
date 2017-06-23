b287cb2ad5158b8fd578810d0f451f0612d209ae
AnantaYudica <nta040689@gmail.com>
2017-06-23 10:25:21 +0700

    Add struct BrainMuscles::container::element::Base for default type of BrainMuscles::type::container::Element Add Alias Type Type, DifferenceType, PointerType, LeftValueReferenceType, RightValueReferenceType, ConstPointerType, ConstLeftValueReferenceType, ConstRightValueReferenceType in BrainMuscles::container::element::Base

---
+++ include/type/container/element/Base.h
@@ -0,0 +1,28 @@
+#ifndef TYPE_CONTAINER_ELEMENT_BASE_H_
+#define TYPE_CONTAINER_ELEMENT_BASE_H_
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace container
+		{
+			namespace element 
+			{
+				struct Base
+				{
+					typedef void Type;
+					typedef void DifferenceType;
+					typedef void PointerType;
+					typedef void LeftValueReferenceType;
+					typedef void RightValueReferenceType;
+					typedef void ConstPointerType;
+					typedef void ConstLeftValueReferenceType;
+					typedef void ConstRightValueReferenceType;
+				};
+			}
+		}
+	}
+}
+
+#endif //!TYPE_CONTAINER_ELEMENT_BASE_H_
