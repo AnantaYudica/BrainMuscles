1de709c5847aefdb31293ed5ac188850a69dc9b4
AnantaYudica <nta040689@gmail.com>
2017-06-27 18:54:28 +0700

    Add dependency with type\container\element\IsType.h Add dependency with type\container\definition\IsType.h Modified in BrainMuscles::type::container::member::modifier::M_Fill  - replace parameter template from VALUE_TYPE to DEFINITION_TYPE  - add alias type DefinitionType, ElementType and Type with  protected access  - replace parameter abstract member function Fill from VALUE_TYPE to Type

--- include/type/container/member/modifier/M_Fill.h
+++ include/type/container/member/modifier/M_Fill.h
@@ -11,13 +11,16 @@ namespace BrainMuscles
 			{
 				namespace modifier
 				{
-					template<typename VALUE_TYPE>
+					template<typename DEFINITION_TYPE>
 					class M_Fill;
 				}
 			}
 		}
 	}
 }
+
+#include "type\container\element\IsType.h"
+#include "type\container\definition\IsType.h"
 
 namespace BrainMuscles
 {
@@ -29,22 +32,29 @@ namespace BrainMuscles
 			{
 				namespace modifier
 				{
-					template<typename VALUE_TYPE>
+					template<typename DEFINITION_TYPE>
 					class M_Fill
 					{
+					protected:
+						typedef typename BrainMuscles::type::container
+							::definition::IsType<DEFINITION_TYPE, true>::Type	DefinitionType;
+						typedef typename BrainMuscles::type::container
+							::element::IsType<typename DefinitionType
+							::ElementType, true>::Type							ElementType;
+						typedef typename ElementType::Type						Type;
 					protected:
 						M_Fill();
 					public:
 						virtual ~M_Fill();
-						virtual void Fill(const VALUE_TYPE& value) = 0;
+						virtual void Fill(const Type& value) = 0;
 					};
 
-					template<typename VALUE_TYPE>
-					M_Fill<VALUE_TYPE>::M_Fill()
+					template<typename DEFINITION_TYPE>
+					M_Fill<DEFINITION_TYPE>::M_Fill()
 					{}
 
-					template<typename VALUE_TYPE>
-					M_Fill<VALUE_TYPE>::~M_Fill()
+					template<typename DEFINITION_TYPE>
+					M_Fill<DEFINITION_TYPE>::~M_Fill()
 					{}
 				}
 			}
