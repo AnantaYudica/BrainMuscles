59cc211776f7fdd5ecd3054f202daab129c682bd
AnantaYudica <nta040689@gmail.com>
2017-06-27 11:53:59 +0700

    Add File \include\type\container\array\Definition.h Add forward declaration class BrainMuscles::type::container::array::Definition Add Dependency :  - standard cstdlib  - standard array  - type\Container.h  - type\container\definition\Base.h  - type\container\Element.h  - type\container\array\Iterator.h Add in BrainMuscles::type::container::array::Definition  - Add parameter template TYPE and SIZE  - Add base class with BrainMuscles::type::container::definition::Base  - Add Alias type ElementType, ContainerType, HandleContainerType, RandomAccessIteratorType, RandomAccessConstIteratorType, RandomAccessReverseIteratorType, RandomAccessConstReverseIteratorType  - Add Alias type InputIteratorType, InputConstIteratorType, InputReverseIteratorType, InputConstReverseIteratorType with default type  - Add static member variable Size with size_t type and default value is SIZE

---
+++ include/type/container/array/Definition.h
@@ -0,0 +1,67 @@
+#ifndef TYPE_CONTAINER_ARRAY_DEFINITION_H_
+#define TYPE_CONTAINER_ARRAY_DEFINITION_H_
+
+#include <cstdlib>
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace container
+		{
+			namespace array
+			{
+				template<typename TYPE, size_t SIZE>
+				struct Definition;
+			}
+		}
+	}
+}
+
+#include <array>
+
+#include "type\Container.h"
+#include "type\container\definition\Base.h"
+#include "type\container\Element.h"
+
+#include "type\container\array\Iterator.h"
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace container
+		{
+			namespace array
+			{
+				template<typename TYPE, size_t SIZE>
+				struct Definition :
+					public BrainMuscles::type::container::definition::Base
+				{
+					typedef typename BrainMuscles::type
+						::container::Element<TYPE>												ElementType;
+					typedef typename BrainMuscles::type::Container<
+						BrainMuscles::type::container::array::Definition<TYPE, SIZE>>			ContainerType;
+					typedef std::array<TYPE, SIZE>												HandleContainerType;
+					typedef BrainMuscles::type::container::array::Iterator<
+						TYPE, SIZE, typename std::array<TYPE, SIZE>::iterator>					RandomAccessIteratorType;
+					typedef BrainMuscles::type::container::array::Iterator<
+						TYPE, SIZE, typename std::array<TYPE, SIZE>::const_iterator>			RandomAccessConstIteratorType;
+					typedef BrainMuscles::type::container::array::Iterator<
+						TYPE, SIZE, typename std::array<TYPE, SIZE>::reverse_iterator>			RandomAccessReverseIteratorType;
+					typedef BrainMuscles::type::container::array::Iterator<
+						TYPE, SIZE, typename std::array<TYPE, SIZE>::const_reverse_iterator>	RandomAccessConstReverseIteratorType;
+					typedef void InputIteratorType;
+					typedef void InputConstIteratorType;
+					typedef void InputReverseIteratorType;
+					typedef void InputConstReverseIteratorType;
+
+					static constexpr size_t Size = SIZE;
+				};
+			}
+		}
+	}
+}
+
+
+#endif //!TYPE_CONTAINER_ARRAY_DEFINITION_H_
