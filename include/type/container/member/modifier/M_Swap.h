d0da98ed4ffa0a0baefde30ffa774bfd5d9bc640
AnantaYudica <nta040689@gmail.com>
2017-06-27 19:25:39 +0700

    Add dependency with type\container\definition\IsType.h Modified in BrainMuscles::type::container::member::modifier::M_Swap  - replace parameter template from CONTAINER_TYPE to DEFINITION_TYPE  - add alias type DefinitionType and ContainerType with protected access  - replace parameter abstract member function type Swap from CONTAINER_TYPE to ContainerType

--- include/type/container/member/modifier/M_Swap.h
+++ include/type/container/member/modifier/M_Swap.h
@@ -11,13 +11,15 @@ namespace BrainMuscles
 			{
 				namespace modifier
 				{
-					template<typename CONTAINER_TYPE>
+					template<typename DEFINITION_TYPE>
 					class M_Swap;
 				}
 			}
 		}
 	}
 }
+
+#include "type\container\definition\IsType.h"
 
 namespace BrainMuscles
 {
@@ -29,22 +31,26 @@ namespace BrainMuscles
 			{
 				namespace modifier
 				{
-					template<typename CONTAINER_TYPE>
+					template<typename DEFINITION_TYPE>
 					class M_Swap
 					{
+					protected:
+						typedef typename BrainMuscles::type::container
+							::definition::IsType<DEFINITION_TYPE, true>::Type	DefinitionType;
+						typedef typename DefinitionType::ContainerType			ContainerType;
 					protected:
 						M_Swap();
 					public:
 						virtual ~M_Swap();
-						virtual void Swap(CONTAINER_TYPE& container) = 0;
+						virtual void Swap(ContainerType& container) = 0;
 					};
 
-					template<typename CONTAINER_TYPE>
-					M_Swap<CONTAINER_TYPE>::M_Swap()
+					template<typename DEFINITION_TYPE>
+					M_Swap<DEFINITION_TYPE>::M_Swap()
 					{}
 
-					template<typename CONTAINER_TYPE>
-					M_Swap<CONTAINER_TYPE>::~M_Swap()
+					template<typename DEFINITION_TYPE>
+					M_Swap<DEFINITION_TYPE>::~M_Swap()
 					{}
 
 				}
