f6e69da5285f05e9e84c955bbbb43ee52091120b
AnantaYudica <nta040689@gmail.com>
2017-06-27 15:49:48 +0700

    Add dependency with type\container\definition\IsType.h Modified in BrainMuscles::type::container::member::iterator::M_ReverseIterator  - replace parameter template from REVERSE_ITERATOR to DEFINITION_TYPE  - remove parameter template CONST_REVERSE_ITERATOR  - add alias type DefinitionType, RandomAccessReverseIteratorType and RandomAccessConstReverseIteratorType  - replace return abstract member function type ReverseBegin and ReverseEnd from REVERSE_ITERATOR to RandomAccessReverseIteratorType and from CONST_REVERSE_ITERATOR to RandomAccessConstReverseIteratorType

--- include/type/container/member/iterator/M_ReverseIterator.h
+++ include/type/container/member/iterator/M_ReverseIterator.h
@@ -11,13 +11,15 @@ namespace BrainMuscles
 			{
 				namespace iterator
 				{
-					template<typename REVERSE_ITERATOR, typename CONST_REVERSE_ITERATOR>
+					template<typename DEFINITION_TYPE>
 					class M_ReverseIterator;
 				}
 			}
 		}
 	}
 }
+
+#include "type\container\definition\IsType.h"
 
 namespace BrainMuscles
 {
@@ -29,25 +31,30 @@ namespace BrainMuscles
 			{
 				namespace iterator
 				{
-					template<typename REVERSE_ITERATOR, typename CONST_REVERSE_ITERATOR>
+					template<typename DEFINITION_TYPE>
 					class M_ReverseIterator
 					{
+					protected:
+						typedef typename BrainMuscles::type::container
+							::definition::IsType<DEFINITION_TYPE, true>::Type					DefinitionType;
+						typedef typename DefinitionType::RandomAccessReverseIteratorType		RandomAccessReverseIteratorType;
+						typedef typename DefinitionType::RandomAccessConstReverseIteratorType	RandomAccessConstReverseIteratorType;
 					protected:
 						M_ReverseIterator();
 					public:
 						virtual ~M_ReverseIterator();
-						virtual REVERSE_ITERATOR ReverseBegin() = 0;
-						virtual REVERSE_ITERATOR ReverseEnd() = 0;
-						virtual CONST_REVERSE_ITERATOR ReverseBegin() const = 0;
-						virtual CONST_REVERSE_ITERATOR ReverseEnd() const = 0;
+						virtual RandomAccessReverseIteratorType ReverseBegin() = 0;
+						virtual RandomAccessReverseIteratorType ReverseEnd() = 0;
+						virtual RandomAccessConstReverseIteratorType ReverseBegin() const = 0;
+						virtual RandomAccessConstReverseIteratorType ReverseEnd() const = 0;
 					};
 
-					template<typename REVERSE_ITERATOR, typename CONST_REVERSE_ITERATOR>
-					M_ReverseIterator<REVERSE_ITERATOR, CONST_REVERSE_ITERATOR>::M_ReverseIterator()
+					template<typename DEFINITION_TYPE>
+					M_ReverseIterator<DEFINITION_TYPE>::M_ReverseIterator()
 					{}
 
-					template<typename REVERSE_ITERATOR, typename CONST_REVERSE_ITERATOR>
-					M_ReverseIterator<REVERSE_ITERATOR, CONST_REVERSE_ITERATOR>::~M_ReverseIterator()
+					template<typename DEFINITION_TYPE>
+					M_ReverseIterator<DEFINITION_TYPE>::~M_ReverseIterator()
 					{}
 				}
 			}
