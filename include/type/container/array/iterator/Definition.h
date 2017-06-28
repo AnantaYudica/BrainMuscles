0c7e5f0aea98370d3e0476d3dc6b070652c1f2af
AnantaYudica <nta040689@gmail.com>
2017-06-28 20:53:58 +0700

    Add dependency with std cstddef Modified in Primary template BrainMuscles::type::container::array::iterator::Definition  - add prefix type of size_t with namespace std  - replace parameter template from HANDLE to HANDLE_TYPE  - add alias type Type, ReferenceType, PointerType, ConstReferenceType and ConstPointerType Modified in other specialize template of BrainMuscles::type::container::array::iterator::Definition  - add prefix type of size_t with namespace std  - updated base class  - add alias type Type, ReferenceType, PointerType, ConstReferenceType and ConstPointerType

--- include/type/container/array/iterator/Definition.h
+++ include/type/container/array/iterator/Definition.h
@@ -1,6 +1,8 @@
 #ifndef TYPE_CONTAINER_ARRAY_ITERATOR_DEFINITION_H_
 #define TYPE_CONTAINER_ARRAY_ITERATOR_DEFINITION_H_
 
+#include <cstddef>
+
 #include <array>
 #include "type\container\array\iterator\definition\ConstIterator.h"
 #include "type\container\array\iterator\definition\Iterator.h"
@@ -16,30 +18,80 @@ namespace BrainMuscles
 			{
 				namespace iterator
 				{
-					template<typename TYPE, size_t SIZE, typename HANDLE>
+					template<typename TYPE, std::size_t SIZE, typename HANDLE_TYPE>
 					struct Definition :
 						public BrainMuscles::type::iterator::derived::definition::Base
-					{};
+					{
+						typedef void Type;
+						typedef void ReferenceType;
+						typedef void PointerType;
+						typedef void ConstReferenceType;
+						typedef void ConstPointerType;
+					};
 
-					template<typename TYPE, size_t SIZE>
+					template<typename TYPE, std::size_t SIZE>
 					struct Definition<TYPE, SIZE, typename std::array<TYPE, SIZE>::const_iterator> :
-						public BrainMuscles::type::container::array::iterator::definition::ConstIterator<TYPE, SIZE, typename std::array<TYPE, SIZE>::const_iterator>
-					{};
+						public BrainMuscles::type::container::array::iterator::definition::ConstIterator<TYPE, SIZE>
+					{
+						typedef typename BrainMuscles::type::container::array::iterator
+							::definition::ConstIterator<TYPE, SIZE>::Type					Type;
+						typedef typename BrainMuscles::type::container::array::iterator
+							::definition::ConstIterator<TYPE, SIZE>::ReferenceType			ReferenceType;
+						typedef typename BrainMuscles::type::container::array::iterator
+							::definition::ConstIterator<TYPE, SIZE>::PointerType			PointerType;
+						typedef typename BrainMuscles::type::container::array::iterator
+							::definition::ConstIterator<TYPE, SIZE>::ConstReferenceType		ConstReferenceType;
+						typedef typename BrainMuscles::type::container::array::iterator
+							::definition::ConstIterator<TYPE, SIZE>::ConstPointerType		ConstPointerType;
+					};
 
-					template<typename TYPE, size_t SIZE>
+					template<typename TYPE, std::size_t SIZE>
 					struct Definition<TYPE, SIZE, typename std::array<TYPE, SIZE>::const_reverse_iterator> :
-						public BrainMuscles::type::container::array::iterator::definition::ConstIterator<TYPE, SIZE, typename std::array<TYPE, SIZE>::const_reverse_iterator>
-					{};
+						public BrainMuscles::type::container::array::iterator::definition::ConstIterator<TYPE, SIZE>
+					{
+						typedef typename BrainMuscles::type::container::array::iterator
+							::definition::ConstIterator<TYPE, SIZE>::Type					Type;
+						typedef typename BrainMuscles::type::container::array::iterator
+							::definition::ConstIterator<TYPE, SIZE>::ReferenceType			ReferenceType;
+						typedef typename BrainMuscles::type::container::array::iterator
+							::definition::ConstIterator<TYPE, SIZE>::PointerType			PointerType;
+						typedef typename BrainMuscles::type::container::array::iterator
+							::definition::ConstIterator<TYPE, SIZE>::ConstReferenceType		ConstReferenceType;
+						typedef typename BrainMuscles::type::container::array::iterator
+							::definition::ConstIterator<TYPE, SIZE>::ConstPointerType		ConstPointerType;
+					};
 
-					template<typename TYPE, size_t SIZE>
+					template<typename TYPE, std::size_t SIZE>
 					struct Definition<TYPE, SIZE, typename std::array<TYPE, SIZE>::iterator> :
-						public BrainMuscles::type::container::array::iterator::definition::Iterator<TYPE, SIZE, typename std::array<TYPE, SIZE>::iterator>
-					{};
+						public BrainMuscles::type::container::array::iterator::definition::Iterator<TYPE, SIZE>
+					{
+						typedef typename BrainMuscles::type::container::array::iterator
+							::definition::Iterator<TYPE, SIZE>::Type					Type;
+						typedef typename BrainMuscles::type::container::array::iterator
+							::definition::Iterator<TYPE, SIZE>::ReferenceType			ReferenceType;
+						typedef typename BrainMuscles::type::container::array::iterator
+							::definition::Iterator<TYPE, SIZE>::PointerType				PointerType;
+						typedef typename BrainMuscles::type::container::array::iterator
+							::definition::Iterator<TYPE, SIZE>::ConstReferenceType		ConstReferenceType;
+						typedef typename BrainMuscles::type::container::array::iterator
+							::definition::Iterator<TYPE, SIZE>::ConstPointerType		ConstPointerType;
+					};
 
-					template<typename TYPE, size_t SIZE>
+					template<typename TYPE, std::size_t SIZE>
 					struct Definition<TYPE, SIZE, typename std::array<TYPE, SIZE>::reverse_iterator> :
-						public BrainMuscles::type::container::array::iterator::definition::Iterator<TYPE, SIZE, typename std::array<TYPE, SIZE>::reverse_iterator>
-					{};
+						public BrainMuscles::type::container::array::iterator::definition::Iterator<TYPE, SIZE>
+					{
+						typedef typename BrainMuscles::type::container::array
+							::iterator::definition::Iterator<TYPE, SIZE>::Type					Type;
+						typedef typename BrainMuscles::type::container::array
+							::iterator::definition::Iterator<TYPE, SIZE>::ReferenceType			ReferenceType;
+						typedef typename BrainMuscles::type::container::array
+							::iterator::definition::Iterator<TYPE, SIZE>::PointerType			PointerType;
+						typedef typename BrainMuscles::type::container::array
+							::iterator::definition::Iterator<TYPE, SIZE>::ConstReferenceType	ConstReferenceType;
+						typedef typename BrainMuscles::type::container::array
+							::iterator::definition::Iterator<TYPE, SIZE>::ConstPointerType		ConstPointerType;
+					};
 				}
 			}
 		}
