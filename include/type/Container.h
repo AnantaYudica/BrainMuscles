8c9359b298ef29dc4d2dbfc0a28b55afaba7a4f4
AnantaYudica <nta040689@gmail.com>
2017-06-28 19:39:41 +0700

    Remove dependency with type\container\definition\Base.h Remove dependency with type\container\definition\IsType.h Modified in BrainMuscles::type::Container  - replace parameter template from DEFINITION_TYPE to HANDLE_CONTAINER_TYPE  - remove validation of DEFINITION_TYPE /*Make it simple!!*/

--- include/type/Container.h
+++ include/type/Container.h
@@ -1,19 +1,13 @@
 #ifndef TYPE_CONTAINER_H_
 #define TYPE_CONTAINER_H_
 
-#include "type\container\definition\Base.h"
-#include "type\container\definition\IsType.h"
-
 namespace BrainMuscles
 {
 	namespace type
 	{
-		template<typename DEFINITION_TYPE>
+		template<typename HANDLE_CONTAINER_TYPE>
 		class Container
-		{
-			static_assert(BrainMuscles::type::container::definition::IsType<DEFINITION_TYPE>::Value, 
-				"[DEFINITION_TYPE] requires base of class BrainMuscles::type::container::definition::Base");
-		};
+		{};
 	}
 }
 
