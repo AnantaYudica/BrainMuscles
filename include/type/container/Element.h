375916f410c67e896ac913334f68638cb60d17a3
AnantaYudica <nta040689@gmail.com>
2017-06-28 14:34:57 +0700

    Modified in BrainMuscles::type::container::Element  - add parameter template SIZE_TYPE with default type std::size_t  - update default parameter template type CONST_POINTER_TYPE with const TYPE*, CONST_LEFT_VALUE_REFERENCE_TYPE with const TYPE& and CONST_RIGHT_VALUE_REFERENCE_TYPE with const TYPE&&  - add alias type SizeType

--- include/type/container/Element.h
+++ include/type/container/Element.h
@@ -9,11 +9,11 @@ namespace BrainMuscles
 	{
 		namespace container
 		{
-			template<typename TYPE, typename DISTANCE_TYPE = std::ptrdiff_t, typename POINTER_TYPE = TYPE*,
+			template<typename TYPE, typename DISTANCE_TYPE = std::ptrdiff_t, typename SIZE_TYPE = std::size_t, typename POINTER_TYPE = TYPE*,
 				typename LEFT_VALUE_REFERENCE_TYPE = TYPE&, typename RIGHT_VALUE_REFERENCE_TYPE = TYPE&&,
-				typename CONST_POINTER_TYPE = const POINTER_TYPE, 
-				typename CONST_LEFT_VALUE_REFERENCE_TYPE = const LEFT_VALUE_REFERENCE_TYPE,
-				typename CONST_RIGHT_VALUE_REFERENCE_TYPE = const RIGHT_VALUE_REFERENCE_TYPE>
+				typename CONST_POINTER_TYPE = const TYPE*,
+				typename CONST_LEFT_VALUE_REFERENCE_TYPE = const TYPE&,
+				typename CONST_RIGHT_VALUE_REFERENCE_TYPE = const TYPE&&>
 			struct Element;
 
 		}
@@ -30,7 +30,7 @@ namespace BrainMuscles
 	{
 		namespace container
 		{
-			template<typename TYPE, typename DISTANCE_TYPE, typename POINTER_TYPE, typename LEFT_VALUE_REFERENCE_TYPE,
+			template<typename TYPE, typename DISTANCE_TYPE, typename SIZE_TYPE, typename POINTER_TYPE, typename LEFT_VALUE_REFERENCE_TYPE,
 				typename RIGHT_VALUE_REFERENCE_TYPE, typename CONST_POINTER_TYPE, typename CONST_LEFT_VALUE_REFERENCE_TYPE,
 				typename CONST_RIGHT_VALUE_REFERENCE_TYPE>
 			struct Element :
@@ -39,6 +39,7 @@ namespace BrainMuscles
 				static_assert(type::trait::conditional::Or<std::is_pointer<TYPE>::value, std::is_object<TYPE>::value>::value, "test");
 				typedef TYPE								Type;
 				typedef DISTANCE_TYPE						DifferenceType;
+				typedef SIZE_TYPE							SizeType;
 				typedef POINTER_TYPE						PointerType;
 				typedef LEFT_VALUE_REFERENCE_TYPE			LeftValueReferenceType;
 				typedef RIGHT_VALUE_REFERENCE_TYPE			RightValueReferenceType;
