b2f65f5134c9a86b3285d6f6f307115fc97d73eb
AnantaYudica <nta040689@gmail.com>
2017-07-06 01:16:24 +0700

    Add dependency with type\iterator\derived\definition\Base.h Add dependency with type\container\vector\iterator\definition\ConstIterator.h Add dependency with type\container\vector\iterator\definition\Iterator.h Add dependency with type\container\vector\iterator\handle\ConstIterator.h Add dependency with type\container\vector\iterator\handle\ConstReverseIterator.h Add dependency with type\container\vector\iterator\handle\Iterator.h Add dependency with type\container\vector\iterator\handle\ReverseIterator.h Add struct BrainMuscles::type::container::vector::iterator::Definition Added in struct BrainMuscles::type::container::vector::iterator::Definition  - add parameter template ELEMENT_TYPE and HANDLE_TYPE  - add primary template  - add specialize template with HANDLE_TYPE = {BrainMuscles::type::container::vector::iterator::handle::ConstIterator, BrainMuscles::type::container::vector::iterator::handle::ConstReverseIterator, BrainMuscles::type::container::vector::iterator::handle::Iterator, BrainMuscles::type::container::vector::iterator::handle::ReverseIterator}  - add base class with BrainMuscles::type::iterator::derived::definition::Base in primary template  - add base class with BrainMuscles::type::container::vector::iterator::definition::ConstIterator and BrainMuscles::type::container::vector::iterator::definition::Iterator in specialized template  - add alias type Type, ReferenceType, PointerType, ConstReferenceType and  ConstPointerType in primary template and specialized template with public access  - add alias type IteratorDefinitionType in specialized template with protected access

---
+++ include/type/container/vector/iterator/Definition.h
@@ -0,0 +1,104 @@
+#ifndef TYPE_CONTAINER_VECTOR_ITERATOR_DEFINITION_H_
+#define TYPE_CONTAINER_VECTOR_ITERATOR_DEFINITION_H_
+
+#include "type\iterator\derived\definition\Base.h"
+
+#include "type\container\vector\iterator\definition\ConstIterator.h"
+
+#include "type\container\vector\iterator\definition\Iterator.h"
+
+#include "type\container\vector\iterator\handle\ConstIterator.h"
+
+#include "type\container\vector\iterator\handle\ConstReverseIterator.h"
+
+#include "type\container\vector\iterator\handle\Iterator.h"
+
+#include "type\container\vector\iterator\handle\ReverseIterator.h"
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace container
+		{
+			namespace vector
+			{
+				namespace iterator
+				{
+					template<typename ELEMENT_TYPE, typename HANDLE_TYPE>
+					struct Definition :
+						public BrainMuscles::type::iterator::derived::definition::Base
+					{
+						typedef void Type;
+						typedef void ReferenceType;
+						typedef void PointerType;
+						typedef void ConstReferenceType;
+						typedef void ConstPointerType;
+					};
+
+					template<typename ELEMENT_TYPE>
+					struct Definition<ELEMENT_TYPE, BrainMuscles::type::container::vector::iterator::handle::ConstIterator<ELEMENT_TYPE>> :
+						public BrainMuscles::type::container::vector::iterator::definition::ConstIterator<ELEMENT_TYPE>
+					{
+					protected:
+						typedef BrainMuscles::type::container::vector::iterator
+							::definition::ConstIterator<ELEMENT_TYPE>				IteratorDefinitionType;
+					public:
+						typedef typename IteratorDefinitionType::Type				Type;
+						typedef typename IteratorDefinitionType::ReferenceType		ReferenceType;
+						typedef typename IteratorDefinitionType::PointerType		PointerType;
+						typedef typename IteratorDefinitionType::ConstReferenceType	ConstReferenceType;
+						typedef typename IteratorDefinitionType::ConstPointerType	ConstPointerType;
+					};
+
+					template<typename ELEMENT_TYPE>
+					struct Definition<ELEMENT_TYPE, BrainMuscles::type::container::vector::iterator::handle::ConstReverseIterator<ELEMENT_TYPE>> :
+						public BrainMuscles::type::container::vector::iterator::definition::ConstIterator<ELEMENT_TYPE>
+					{
+					protected:
+						typedef BrainMuscles::type::container::vector::iterator
+							::definition::ConstIterator<ELEMENT_TYPE>				IteratorDefinitionType;
+					public:
+						typedef typename IteratorDefinitionType::Type				Type;
+						typedef typename IteratorDefinitionType::ReferenceType		ReferenceType;
+						typedef typename IteratorDefinitionType::PointerType		PointerType;
+						typedef typename IteratorDefinitionType::ConstReferenceType	ConstReferenceType;
+						typedef typename IteratorDefinitionType::ConstPointerType	ConstPointerType;
+					};
+
+					template<typename ELEMENT_TYPE>
+					struct Definition<ELEMENT_TYPE, BrainMuscles::type::container::vector::iterator::handle::Iterator<ELEMENT_TYPE>> :
+						public BrainMuscles::type::container::vector::iterator::definition::Iterator<ELEMENT_TYPE>
+					{
+					protected:
+						typedef BrainMuscles::type::container::vector::iterator
+							::definition::Iterator<ELEMENT_TYPE>					IteratorDefinitionType;
+					public:
+						typedef typename IteratorDefinitionType::Type				Type;
+						typedef typename IteratorDefinitionType::ReferenceType		ReferenceType;
+						typedef typename IteratorDefinitionType::PointerType		PointerType;
+						typedef typename IteratorDefinitionType::ConstReferenceType	ConstReferenceType;
+						typedef typename IteratorDefinitionType::ConstPointerType	ConstPointerType;
+					};
+
+					template<typename ELEMENT_TYPE>
+					struct Definition<ELEMENT_TYPE, BrainMuscles::type::container::vector::iterator::handle::ReverseIterator<ELEMENT_TYPE>> :
+						public BrainMuscles::type::container::vector::iterator::definition::Iterator<ELEMENT_TYPE>
+					{
+					protected:
+						typedef BrainMuscles::type::container::vector::iterator
+							::definition::Iterator<ELEMENT_TYPE>					IteratorDefinitionType;
+					public:
+						typedef typename IteratorDefinitionType::Type				Type;
+						typedef typename IteratorDefinitionType::ReferenceType		ReferenceType;
+						typedef typename IteratorDefinitionType::PointerType		PointerType;
+						typedef typename IteratorDefinitionType::ConstReferenceType	ConstReferenceType;
+						typedef typename IteratorDefinitionType::ConstPointerType	ConstPointerType;
+					};
+				}
+			}
+		}
+	}
+}
+
+#endif //!TYPE_CONTAINER_VECTOR_ITERATOR_DEFINITION_H_
