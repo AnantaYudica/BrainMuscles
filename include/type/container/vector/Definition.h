6a0cb0e27bdbf9dbf5e2b1e31171b3954dc8d9d5
AnantaYudica <nta040689@gmail.com>
2017-07-13 14:51:51 +0700

    Add dependency with std cstddef Add dependency with std vector Add dependency with type\iterator\Tag.h Add dependency with type\container\definition\Base.h Add dependency with type\container\Element.h Add dependency with type\container\Base.h Add dependency with type\container\vector\Iterator.h Add dependency with type\container\vector\iterator\handle\definition\ConstIterator.h Add dependency with type\container\vector\iterator\handle\definition\Iterator.h Add struct BrainMuscles::type::container::vector::Definition Added in struct BrainMuscles::type::container::vector::Definition  - Add template parameter TYPE  - Add base class BrainMuscles::type::container::definition::Base  - Add alias type ElementType, ContainerType, HandleContainerType, BaseType, RandomAccessIteratorType, RandomAccessConstIteratorType, RandomAccessReverseIteratorType, RandomAccessConstReverseIteratorType, InputIteratorType, InputConstIteratorType and InputReverseIteratorType, InputConstReverseIteratorType with public access

---
+++ include/type/container/vector/Definition.h
@@ -0,0 +1,92 @@
+#ifndef TYPE_CONTAINER_VECTOR_DEFINITION_H_
+#define TYPE_CONTAINER_VECTOR_DEFINITION_H_
+
+#include <cstddef>
+#include <vector>
+
+#include "type\iterator\Tag.h"
+
+#include "type\container\definition\Base.h"
+
+#include "type\container\Element.h"
+
+#include "type\container\Base.h"
+
+#include "type\container\vector\Iterator.h"
+
+#include "type\container\vector\iterator\handle\definition\ConstIterator.h"
+#include "type\container\vector\iterator\handle\definition\Iterator.h"
+
+namespace BrainMuscles
+{
+	namespace type
+	{
+		namespace container
+		{
+			namespace vector
+			{
+				template<typename TYPE>
+				struct Definition :
+					public BrainMuscles::type::container::definition::Base
+				{
+					typedef BrainMuscles::type
+						::container::Element<TYPE>									ElementType;
+					typedef BrainMuscles::type::Container<std::vector<TYPE>>		ContainerType;
+					typedef std::vector<TYPE>										HandleContainerType;
+					typedef BrainMuscles::type
+						::container::Base<std::vector<TYPE>>						BaseType;
+					typedef BrainMuscles::type::container::vector::Iterator<
+						BrainMuscles::type::iterator::tag::random_access, 
+						ElementType, BrainMuscles::type::container::vector
+							::iterator::Handle<BrainMuscles::type::container
+								::vector::iterator::handle::definition::
+								Iterator<ElementType>>>								RandomAccessIteratorType;
+					typedef BrainMuscles::type::container::vector::Iterator<
+						BrainMuscles::type::iterator::tag::random_access,
+						ElementType, BrainMuscles::type::container::vector
+							::iterator::Handle<BrainMuscles::type::container
+								::vector::iterator::handle::definition::
+								ConstIterator<ElementType>>>						RandomAccessConstIteratorType;
+					typedef BrainMuscles::type::container::vector::Iterator<
+						BrainMuscles::type::iterator::tag::random_access, 
+						ElementType, BrainMuscles::type::container::vector
+							::iterator::Handle<BrainMuscles::type::container
+								::vector::iterator::handle::definition::
+								Iterator<ElementType>>>								RandomAccessReverseIteratorType;
+					typedef BrainMuscles::type::container::vector::Iterator<
+						BrainMuscles::type::iterator::tag::random_access,
+						ElementType, BrainMuscles::type::container::vector
+							::iterator::Handle<BrainMuscles::type::container
+								::vector::iterator::handle::definition::
+								ConstIterator<ElementType>>>						RandomAccessConstReverseIteratorType;
+					typedef BrainMuscles::type::container::vector::Iterator<
+						BrainMuscles::type::iterator::tag::input,
+						ElementType, BrainMuscles::type::container::vector
+							::iterator::Handle<BrainMuscles::type::container
+								::vector::iterator::handle::definition::
+								Iterator<ElementType>>>								InputIteratorType;
+					typedef BrainMuscles::type::container::vector::Iterator<
+						BrainMuscles::type::iterator::tag::input,
+						ElementType, BrainMuscles::type::container::vector
+							::iterator::Handle<BrainMuscles::type::container
+								::vector::iterator::handle::definition::
+								ConstIterator<ElementType>>>						InputConstIteratorType;
+					typedef BrainMuscles::type::container::vector::Iterator<
+						BrainMuscles::type::iterator::tag::input,
+						ElementType, BrainMuscles::type::container::vector
+							::iterator::Handle<BrainMuscles::type::container
+								::vector::iterator::handle::definition::
+								Iterator<ElementType>>>								InputReverseIteratorType;
+					typedef BrainMuscles::type::container::vector::Iterator<
+						BrainMuscles::type::iterator::tag::input,
+						ElementType, BrainMuscles::type::container::vector
+							::iterator::Handle<BrainMuscles::type::container
+								::vector::iterator::handle::definition::
+								ConstIterator<ElementType>>>						InputConstReverseIteratorType;
+				};
+			}
+		}
+	}
+}
+
+#endif //!TYPE_CONTAINER_VECTOR_DEFINITION_H_
