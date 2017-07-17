84cded11c94a032c0c971649bfae65d90f938ec6
AnantaYudica <nta040689@gmail.com>
2017-07-17 14:07:26 +0700

    Add source for experiment with /include/test/File.h

---
+++ experiment/test/test-file.cpp
@@ -0,0 +1,14 @@
+#include "test\File.h"
+
+#include <cstdio>
+
+using namespace std;
+using namespace BrainMuscles;
+
+int main(int argc, char *argv[])
+{
+	test::File::SetInfo(stdout);
+	test::File::SetError(stderr);
+
+	return 1;
+}
