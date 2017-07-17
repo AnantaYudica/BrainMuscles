cf4a3f628fee8b08f1a61f240777aa8cbb602c88
AnantaYudica <nta040689@gmail.com>
2017-07-17 11:05:23 +0700

    Add dependency with test\Configure.h Add dependency with std cstdio Add dependency with type\Singleton.h Add dependency with type\file\Info.h Add class BrainMuscles::test::File Add in class BrainMuscles::test::File  - Add base class BrainMuscles::type::Singleton  - Add private access to BrainMuscles::type::Singleton  - Add member variable m_fileInfo and m_fileError with FILE* type  - Add static member variable Info with BrainMuscles::test::file::Info& type  - Add Constructor File()  - Add Destructor ~File()  - Add static function SetInfo and SetError

---
+++ include/test/File.h
@@ -0,0 +1,65 @@
+#ifndef TEST_FILE_H_
+#define TEST_FILE_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include <cstdio>
+#include "type\Singleton.h"
+
+#include "test\file\Info.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		class File :
+			public BrainMuscles::type::Singleton<File, File, File&>
+		{
+			friend class BrainMuscles::type::Singleton<File, File, File&>;
+		private:
+			FILE* m_fileInfo;
+			FILE* m_fileError;
+		public:
+			static BrainMuscles::test::file::Info& Info;
+		private:
+			File();
+			~File();
+		public:
+			template<unsigned int N>
+			static void SetInfo(char (&file_info)[N]);
+			static void SetInfo(FILE * info);
+			template<unsigned int N>
+			static void SetError(char (&file_error)[N]);
+			static void SetError(FILE * error);
+
+		};
+
+		BrainMuscles::test::file::Info& File::Info = BrainMuscles::test::file::Info::GetInstance();
+
+		File::File() :
+			m_fileInfo(stdout),
+			m_fileError(stderr)
+		{}
+
+		template<unsigned int N>
+		void File::SetInfo(char(&file_info)[N])
+		{}
+
+		void File::SetInfo(FILE * info)
+		{}
+
+		template<unsigned int N>
+		void File::SetError(char (&file_error)[N])
+		{}
+
+		void File::SetError(FILE * error)
+		{}
+
+	}
+}
+
+#endif
+
+#endif //!TEST_FILE_H_
