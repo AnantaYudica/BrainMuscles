66b8d616d4eb9aaf8d652df2b29ac3b11165a67d
AnantaYudica <nta040689@gmail.com>
2017-07-24 06:52:51 +0700

    Add dependency with test\Configure.h Add dependency with std typeindex Add class BrainMuscles::test::simple::memory::block::Header Add in class BrainMuscles::test::simple::memory::block::Header  - Add member variable m_allocation, m_size with std::size_t for type and private access  - Add member variable m_indexType with std::type_index for type and private access  - Add construction Header(), Header(const std::size_t& size, const std::size_t& allocation, const std::type_index& type_index), Header(const Header& header) and Header(Header* ptr_header) with public access  - Add destructor ~Header()  - Add member function GetSize and GetAllocation with const std::size_t& for return type and public access  - Add member function GetTypeIndex with const std::type_index& for return type and public access

---
+++ include/test/simple/memory/block/Header.h
@@ -0,0 +1,88 @@
+#ifndef TEST_SIMPLE_MEMORY_BLOCK_HEADER_H_
+#define TEST_SIMPLE_MEMORY_BLOCK_HEADER_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include <typeindex>
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace memory
+			{
+				namespace block
+				{
+					class Header
+					{
+					private:
+						std::size_t m_allocation;
+						std::size_t m_size;
+						std::type_index m_indexType;
+					public:
+						Header();
+						Header(const std::size_t& size, const std::size_t& allocation, const std::type_index& type_index);
+						Header(const Header& header);
+						Header(Header* ptr_header);
+					public:
+						~Header();
+					public:
+						const std::size_t& GetSize();
+						const std::size_t& GetAllocation();
+						const std::type_index& GetTypeIndex();
+					};
+					
+					Header::Header() :
+						m_allocation(0),
+						m_size(0),
+						m_indexType(typeid(void))
+					{}
+
+					Header::Header(const std::size_t& size, const std::size_t& allocation, const std::type_index& type_index) :
+						m_size(size),
+						m_allocation(allocation),
+						m_indexType(type_index)
+					{}
+
+					Header::Header(const Header& header) :
+						m_size(header.m_size),
+						m_allocation(header.m_allocation),
+						m_indexType(header.m_indexType)
+					{}
+					
+					Header::Header(Header* ptr_header) :
+						m_size(ptr_header->m_size),
+						m_allocation(ptr_header->m_allocation),
+						m_indexType(ptr_header->m_indexType)
+					{}
+
+					Header::~Header()
+					{}
+
+					const std::size_t& Header::GetSize()
+					{
+						return m_size;
+					}
+
+					const std::size_t& Header::GetAllocation()
+					{
+						return m_allocation;
+					}
+
+					const std::type_index& Header::GetTypeIndex()
+					{
+						return m_indexType;
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_MEMORY_BLOCK_HEADER_H_
