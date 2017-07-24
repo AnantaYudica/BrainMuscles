acc2dc6d44dc080e0f8653a1477bf5a53589fb82
AnantaYudica <nta040689@gmail.com>
2017-07-24 08:49:52 +0700

    Add dependency with test\Configure.h Add dependency with test\simple\memory\block\Data.h Add class BrainMuscles::test::simple::memory::Block Add in class BrainMuscles::test::simple::memory::Block  - Add alias type HeaderDataType  - Add member variable m_headerBlock with HeaderDataType* for type and private access  - Add member variable m_bodyBlock with void* for type and private access  - Add constructor Block(void* block)  - Add static member function GetAllocationSize() with typename TYPE and std::size_t for parameter template, std::size_t for return type and public access  - Add member function GetBlockHeader with HeaderDataType* for return type and public access  - Add member function GetBlockBody with void* for return type and public access

---
+++ include/test/simple/memory/Block.h
@@ -0,0 +1,63 @@
+#ifndef TEST_SIMPLE_MEMORY_BLOCK_H_
+#define TEST_SIMPLE_MEMORY_BLOCK_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\memory\block\Data.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace memory
+			{
+				class Block
+				{
+				public:
+					typedef BrainMuscles::test::simple::memory::block::Header HeaderDataType;
+				private:
+					HeaderDataType* m_headerBlock;
+					void* m_bodyBlock;
+				public:
+					Block(void * block);
+				public:
+					template<typename TYPE, std::size_t SIZE>
+					static std::size_t GetAllocationSize();
+				public:
+					HeaderDataType* GetBlockHeader();
+					void* GetBlockBody();
+				};
+
+				Block::Block(void * block) :
+					m_headerBlock(reinterpret_cast<HeaderDataType*>(block)),
+					m_bodyBlock(reinterpret_cast<HeaderDataType*>(block) + 1)
+				{}
+
+				template<typename TYPE, std::size_t SIZE>
+				std::size_t Block::GetAllocationSize()
+				{
+					return sizeof(BrainMuscles::test::simple::memory::block::Data<TYPE, SIZE>);
+				}
+
+				typename Block::HeaderDataType*
+				Block::GetBlockHeader()
+				{
+					return m_headerBlock;
+				}
+
+				void* Block::GetBlockBody()
+				{
+					return m_bodyBlock;
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_MEMORY_BLOCK_H_
