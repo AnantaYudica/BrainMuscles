04beec4ce770c9ead886f1e20ce207aeccd50d88
AnantaYudica <nta040689@gmail.com>
2017-07-24 11:39:35 +0700

    Add dependency with test\Configure.h Add dependency with test\simple\memory\pool\linkers\Links.h Add class BrainMuscles::test::simple::memory::pool::Linkers Add in class BrainMuscles::test::simple::memory::pool::Linkers  - Add alias type DataType, LinksType and ConditionFunctionType  - Add member variable m_head and m_tail with LinksType* for type and private access  - Add constructor Linkers() with public access  - Add destructor ~Linkers() with public access  - Add static member function DefaultFindCondition with bool for return, void* input for first parameter, void* compare for second parameter and public access  - Add member function IsHeadNull and IsHeadAndTail with bool for return type and private access  - Add member function FindIf with LinksType* for return type, void* for first parameter, ConditionFunctionType for second parameter, default value DefaultFindCondition for second parameter and private access  - Add member function PushHead and PushTail with void for return type, const DataType& for first parameter and private access  - Add member function PopHead and PopTail with void for return type and private access  - Add member function PopBody with void for return type, LinksType* for first parameter and private access  - Add member function Push with void for return type, const DataType& for first parameter and public access  - Add member function PopIf with DataType for return type, void* for first parameter, ConditionFunctionType for second parameter and public access  - Add member function Pop with DataType for return type, void( for first parameter and public access  - Add member function Clear with void for return type and public access

---
+++ include/test/simple/memory/pool/Linkers.h
@@ -0,0 +1,211 @@
+#ifndef TEST_SIMPLE_MEMORY_POOL_LINKERS_H_
+#define TEST_SIMPLE_MEMORY_POOL_LINKERS_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\memory\pool\linkers\Links.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace memory
+			{
+				namespace pool
+				{
+					class Linkers
+					{
+					public:
+						typedef BrainMuscles::test::simple::memory::pool::linkers::Data DataType;
+						typedef BrainMuscles::test::simple::memory::pool::linkers::Links LinksType;
+						typedef bool(&ConditionFunctionType)(void* input, void* compare);
+					private:
+						LinksType* m_head;
+						LinksType* m_tail;
+					public:
+						Linkers();
+						~Linkers();
+					public:
+						static bool DefaultFindCondition(void* input, void* compare);
+					private:
+						bool IsHeadNull();
+						bool IsHeadAndTail();
+						LinksType* FindIf(void* block, ConditionFunctionType condition = DefaultFindCondition);
+						void PushHead(const DataType& data);
+						void PushTail(const DataType& data);
+						void PopHead();
+						void PopTail();
+						void PopBody(LinksType* curr);
+					public:
+						void Push(const DataType& data);
+						DataType PopIf(void* block, ConditionFunctionType condition);
+						DataType Pop(void* block);
+						void Clear();
+					};
+
+					Linkers::Linkers() :
+						m_head(0),
+						m_tail(0)
+					{}
+
+					Linkers::~Linkers()
+					{}
+
+					bool Linkers::DefaultFindCondition(void* input, void* compare)
+					{
+						return input == compare;
+					}
+
+					bool Linkers::IsHeadNull()
+					{
+						return m_head == 0;
+					}
+
+					bool Linkers::IsHeadAndTail()
+					{
+						return m_head == m_tail;
+					}
+
+					typename Linkers::LinksType*
+					Linkers::FindIf(void* block, ConditionFunctionType condition)
+					{
+						if (IsHeadNull())
+						{
+							return 0;
+						}
+						LinksType* curr = m_head;
+						while (curr != 0)
+						{
+							if (condition(block, curr->GetBlock()))
+							{
+								return curr;
+							}
+							curr = curr->GetNext();
+						}
+						return 0;
+					}
+
+					void Linkers::PushHead(const DataType& data)
+					{
+						m_head = m_tail = new LinksType(data);
+					}
+
+					void Linkers::PushTail(const DataType& data)
+					{
+						m_tail->SetNext(new LinksType(data));
+						m_tail->GetNext()->SetPrev(m_tail);
+						m_tail = m_tail->GetNext();
+					}
+
+					void Linkers::PopHead()
+					{
+						if (IsHeadNull())
+						{
+							return;
+						}
+						if (IsHeadAndTail())
+						{
+							delete m_head;
+							m_head = m_tail = 0;
+						}
+						else
+						{
+							LinksType* curr = m_head;
+							m_head = m_head->GetNext();
+							m_head->SetPrev(0);
+							delete curr;
+						}
+					}
+
+					void Linkers::PopTail()
+					{
+						if (IsHeadNull())
+						{
+							return;
+						}
+						if (IsHeadAndTail())
+						{
+							PopHead();
+						}
+						else
+						{
+							LinksType* curr = m_tail;
+							m_tail = m_tail->GetPrev();
+							m_tail->SetNext(0);
+							delete curr;
+						}
+					}
+
+					void Linkers::PopBody(LinksType* curr)
+					{
+						if (curr == 0)
+						{
+							return;
+						}
+						else if (curr == m_head)
+						{
+							PopHead();
+						}
+						else if (curr == m_tail)
+						{
+							PopTail();
+						}
+						else
+						{
+							curr->GetPrev()->SetNext(curr->GetNext());
+							curr->GetNext()->SetPrev(curr->GetPrev());
+							delete curr;
+						}
+					}
+
+					void Linkers::Push(const DataType& data)
+					{
+						if(IsHeadNull())
+						{
+							PushHead(data);
+						}
+						else
+						{
+							PushTail(data);
+						}
+					}
+
+					typename Linkers::DataType
+					Linkers::PopIf(void* block, ConditionFunctionType condition)
+					{
+						LinksType* curr = FindIf(block, condition);
+						if (curr == 0)
+						{
+							return DataType(0, 0);
+						}
+						DataType data(curr);
+						PopBody(curr);
+						return data;
+					}
+
+					typename Linkers::DataType 
+					Linkers::Pop(void* block)
+					{
+						return PopIf(block, Linkers::DefaultFindCondition);
+					}
+
+					void Linkers::Clear()
+					{
+						while (!IsHeadNull())
+						{
+							PopTail();
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_MEMORY_POOL_LINKERS_H_
