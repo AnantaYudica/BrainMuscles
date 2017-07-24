a5b93f004ddbe08784e00f05cfffa538398591ad
AnantaYudica <nta040689@gmail.com>
2017-07-24 10:49:15 +0700

    Add dependency with test\Configure.h Add dependency with std cstddef Add class BrainMuscles::test::simple::memory::pool::linkers::Data Add in class BrainMuscles::test::simple::memory::pool::linkers::Data  - Add member variable m_size with std::size_t for type and private access  - Add member variable m_block with void* for type and private access  - Constructor Data(const std::size_t& size, void* block), Data(const Data& data) and Data(Data* ptr_data) with public access  - Add member function GetSize with const std::size_t for return type and public access  - Add member function GetBlock with void* for return type and public access  - Add member operator function operator== with void* for parameter function, bool for return type and public access

---
+++ include/test/simple/memory/pool/linkers/Data.h
@@ -0,0 +1,76 @@
+#ifndef TEST_SIMPLE_MEMORY_POOL_LINKERS_DATA_H_
+#define TEST_SIMPLE_MEMORY_POOL_LINKERS_DATA_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include <cstddef>
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace memory
+			{
+				namespace pool
+				{
+					namespace linkers
+					{
+						class Data
+						{
+						private:
+							std::size_t m_size;
+							void* m_block;
+						public:
+							Data(const std::size_t& size, void* block);
+							Data(const Data& data);
+							Data(Data* ptr_data);
+						public:
+							const std::size_t& GetSize();
+							void* GetBlock();
+						public:
+							bool operator==(void* block);
+						};
+
+						Data::Data(const std::size_t& size, void* block) :
+							m_size(size),
+							m_block(block)
+						{}
+
+						Data::Data(const Data& data) :
+							m_size(data.m_size),
+							m_block(data.m_block)
+						{}
+
+						Data::Data(Data* ptr_data) :
+							m_size(ptr_data->m_size),
+							m_block(ptr_data->m_block)
+						{}
+
+						const std::size_t& Data::GetSize()
+						{
+							return m_size;
+						}
+
+						void* Data::GetBlock()
+						{
+							return m_block;
+						}
+
+						bool Data::operator==(void* block)
+						{
+							return m_block == block;
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_MEMORY_POOL_LINKERS_DATA_H_
