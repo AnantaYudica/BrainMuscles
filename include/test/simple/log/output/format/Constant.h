3df32d4dba7c850386f404818513b575ce3548c8
AnantaYudica <nta040689@gmail.com>
2017-07-31 10:56:22 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Add dependency with cstdint Add class BrainMuscles::test::simple::log::output::format::Constant with final class Added in class BrainMuscles::test::simple::log::output::format::Constant  - Delete default Constructor  - Add alias type SizeType, InputType, PrecisionInputType, WidthInputType, FlagArrayType, LengthArrayType, PrecisionArrayType, SpecifierArrayType, WidthArrayType and FormatStringType  - Add static member variable BeginCharacter, BeginPrecisionCharacter, SizeBeginCharacter, SizeBeginPercisionCharacter, SizeMaxFlag, SizeMaxLength, SizeMaxSpecifier, SizeMaxWidth, SizeMaxPrecision, SizeMaxFormat, SizeMaxFormatString

---
+++ include/test/simple/log/output/format/Constant.h
@@ -0,0 +1,67 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_FORMAT_CONSTANT_H_
+#define TEST_SIMPLE_LOG_OUTPUT_FORMAT_CONSTANT_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include <cstdint>
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace format
+					{
+						class Constant final
+						{
+						public:
+							Constant() = delete;
+						public:
+							typedef unsigned short	SizeType;
+							typedef std::uint16_t	InputType;
+							typedef InputType		PrecisionInputType;
+							typedef InputType		WidthInputType;
+							
+							static constexpr char BeginCharacter = '%';
+							static constexpr char BeginPrecisionCharacter = '.';
+
+							static constexpr SizeType SizeBeginCharacter = 1;
+							static constexpr SizeType SizeBeginPercisionCharacter = 1;
+							static constexpr SizeType SizeMaxFlag = 1;
+							static constexpr SizeType SizeMaxLength = 2;
+							static constexpr SizeType SizeMaxSpecifier = 1;
+							static constexpr SizeType SizeMaxWidth = 5;
+							static constexpr SizeType SizeMaxPrecision = 5;
+							static constexpr SizeType SizeMaxFormat = SizeBeginCharacter + 
+																		SizeMaxFlag + 
+																		SizeMaxWidth + 
+																		SizeBeginPercisionCharacter + 
+																		SizeMaxPrecision + 
+																		SizeMaxLength + 
+																		SizeMaxSpecifier;
+							static constexpr SizeType SizeMaxFormatString = SizeMaxFormat + 1;
+
+							typedef char FlagArrayType[SizeMaxFlag];
+							typedef char LengthArrayType[SizeMaxLength];
+							typedef char PrecisionArrayType[SizeMaxPrecision];
+							typedef char SpecifierArrayType[SizeMaxSpecifier];
+							typedef char WidthArrayType[SizeMaxWidth];
+							typedef char FormatStringType[SizeMaxFormatString];
+						};
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_FORMAT_CONSTANT_H_
