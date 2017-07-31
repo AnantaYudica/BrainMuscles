7875902c172577d9e781fe9c65d38b498c9dde27
AnantaYudica <nta040689@gmail.com>
2017-07-31 12:36:09 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Add dependency with type_traits Add class BrainMuscles::test::simple::log::output::format::length::Type with final class Added in class BrainMuscles::test::simple::output::format::length::Type  - Add parameter template TYPE  - Remove default constructor  - Add variables static member IsDefault, IsChar, IsShort, IsLong, IsLongLong and IsLongDouble

---
+++ include/test/simple/log/output/format/length/Type.h
@@ -0,0 +1,61 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_FORMAT_LENGTH_TYPE_H_
+#define TEST_SIMPLE_LOG_OUTPUT_FORMAT_LENGTH_TYPE_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include <type_traits>
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace format
+					{
+						namespace length
+						{
+							template<typename TYPE>
+							class Type final
+							{
+								Type() = delete;
+							public:
+								static constexpr bool IsDefault = 
+									std::is_same<TYPE, int>::value ||
+									std::is_same<TYPE, unsigned int>::value ||
+									std::is_same<TYPE, float>::value ||
+									std::is_same<TYPE, double>::value ||
+									std::is_same<TYPE, char*>::value ||
+									std::is_same<TYPE, void*>::value;
+								static constexpr bool IsChar =
+									std::is_same<TYPE, char>::value ||
+									std::is_same<TYPE, unsigned char>::value;
+								static constexpr bool IsShort = 
+									std::is_same<TYPE, short>::value ||
+									std::is_same<TYPE, unsigned short>::value;
+								static constexpr bool IsLong =
+									std::is_same<TYPE, long>::value ||
+									std::is_same<TYPE, unsigned long>::value;
+								static constexpr bool IsLongLong =
+									std::is_same<TYPE, long long>::value ||
+									std::is_same<TYPE, unsigned long long>::value;
+								static constexpr bool IsLongDouble =
+									std::is_same<TYPE, long double>::value;
+							};
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_FORMAT_LENGTH_TYPE_H_
