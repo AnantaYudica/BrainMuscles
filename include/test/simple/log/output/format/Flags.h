70361f85cc1c3617534a26755c8b17a7fefd5979
AnantaYudica <nta040689@gmail.com>
2017-07-31 13:40:19 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Add dependency with algorithm Add dependency with test\simple\log\output\format\Constant.h Add dependency with test\simple\log\output\format\Helper.h Add class BrainMuscles::test::simple::log::output::format::Flags Added in class BrainMuscles::test::simple::log::output::format::Flags  - Add alias type ConstantType with private access  - Add aliases type SizeType, FormatStringType and FlagArrayType with public access  - Add variable member 'm_flag' with FlagArrayType for type and private access  - Add constructor    - Add access constructor with private    - Add parameter template variadic ARGS    - Add parameter constructor 'args' with const ARGS&... for type  - Add default constructor    - Add access constructor with public  - Add constructor    - Add access constructor with public    - Add parameter constructor 'flags' with const Flags& for type  - Add constructor    - Add access constructor with public    - Add parameter constructor 'ptr_flags' with Flags* for type  - Add function const member 'GetSize'    - Add access function with public    - Add return type SizeType  - Add function const member 'GetValue'    - Add access function with public    - Add return type const char*  - Add function member 'CopyToFormat'    - Add access function with public    - Add parameters function 'format' with FormatStringType& for type and 'index' with SizeType& for type  - Add functions static member 'Negative', 'Positive', 'Space', 'Hash' and 'Zero' with public access and Flags for return type

---
+++ include/test/simple/log/output/format/Flags.h
@@ -0,0 +1,124 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_FORMAT_FLAGS_H_
+#define TEST_SIMPLE_LOG_OUTPUT_FORMAT_FLAGS_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include <algorithm>
+
+#include "test\simple\log\output\format\Constant.h"
+
+#include "test\simple\log\output\format\Helper.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace format
+					{
+						class Flags
+						{
+						private:
+							typedef BrainMuscles::test::simple::log::output
+								::format::Constant							ConstantType;
+						public:
+							typedef typename ConstantType::SizeType			SizeType;
+							typedef typename ConstantType::FormatStringType	FormatStringType;
+							typedef typename ConstantType::FlagArrayType	FlagArrayType;
+						private:
+							FlagArrayType m_flag;
+						private:
+							template<typename... ARGS>
+							Flags(const ARGS&... args);
+						public:
+							Flags();
+							Flags(const Flags& flags);
+							Flags(Flags* ptr_flags);
+						public:
+							SizeType GetSize() const;
+							const char* GetValue() const; 
+						public:
+							void CopyToFormat(FormatStringType& format, SizeType& index);
+						public:
+							static Flags Negative();
+							static Flags Positive();
+							static Flags Space();
+							static Flags Hash();
+							static Flags Zero();
+						};
+
+						Flags::Flags()
+						{}
+
+						template<typename... ARGS>
+						Flags::Flags(const ARGS&... args) :
+							m_flag{ args... }
+						{}
+
+						Flags::Flags(const Flags& flags)
+						{
+							std::copy(std::begin(flags.m_flag), std::end(flags.m_flag), m_flag);
+						}
+
+						Flags::Flags(Flags* ptr_flags)
+						{
+							std::copy(std::begin(ptr_flags->m_flag), std::end(ptr_flags->m_flag), m_flag);
+						}
+
+						typename Flags::SizeType Flags::GetSize() const
+						{
+							return ConstantType::SizeMaxFlag;
+						}
+
+						const char* Flags::GetValue() const
+						{
+							return m_flag;
+						}
+
+						void Flags::CopyToFormat(FormatStringType& format, SizeType& index)
+						{
+							BrainMuscles::test::simple::log::output::format::helper::
+								CopyToFormat<SizeType, FlagArrayType, FormatStringType>(m_flag, GetSize(), format, index);
+						}
+
+						Flags Flags::Negative()
+						{
+							return Flags('-');
+						}
+
+						Flags Flags::Positive()
+						{
+							return Flags('+');
+						}
+
+						Flags Flags::Space()
+						{
+							return Flags(' ');
+						}
+
+						Flags Flags::Hash()
+						{
+							return Flags('#');
+						}
+
+						Flags Flags::Zero()
+						{
+							return Flags('0');
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_FORMAT_FLAGS_H_
