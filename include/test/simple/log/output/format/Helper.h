f2d56963ac985a3ee0e4dce39ce70afb574f846f
AnantaYudica <nta040689@gmail.com>
2017-07-31 13:04:10 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Add dependency with type_traits Add function template BrainMuscles::test::simple::log::output::format::helper:: CastUnsignedIntegerToArrayOfChar Added in function template BrainMuscles::test::simple::log::output::format::helper::CastUnsignedIntegerToArrayOfChar  - Add parameters template SIZE_TYPE, INPUT_TYPE, ARRAY_TYPE  - Add return type SIZE_TYPE  - Add parameters function 'in' with const INPUT_TYPE& for type and 'out' with ARRAY_TYPE& for type Add function template BrainMuscles::test::simple::log::output::format::helper::CopyToFormat Added in function template BrainMuscles::test::simple::log::output::format::helper::CopyToFormat  - Add parameters template SIZE_TYPE, ARRAY_TYPE and FORMAT_STRING  - Add return type void  - Add parameters function 'value' with ARRAY_TYPE& for type, 'size_value' with const SIZE_TYPE& for type, 'format_string' with FORMAT_STRING& for type and 'index' with SIZE_TYPE& for type

---
+++ include/test/simple/log/output/format/Helper.h
@@ -0,0 +1,54 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_FORMAT_HELPER_H_
+#define TEST_SIMPLE_LOG_OUTPUT_FORMAT_HELPER_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include <type_traits>
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace format
+					{
+						namespace helper
+						{
+							template<typename SIZE_TYPE, typename INPUT_TYPE, typename ARRAY_TYPE>
+							SIZE_TYPE CastUnsignedIntegerToArrayOfChar(const INPUT_TYPE& in, ARRAY_TYPE& out)
+							{
+								static_assert(std::is_unsigned<INPUT_TYPE>::value, "INPUT_TYPE required unsigned");
+								SIZE_TYPE index = 0;
+								for (INPUT_TYPE val = in; val > 0; val /= 10, index++)
+								{
+									out[index] = 48 + (val % 10);
+								}
+								return index;
+							}
+
+							template<typename SIZE_TYPE, typename ARRAY_TYPE, typename FORMAT_STRING>
+							void CopyToFormat(ARRAY_TYPE& value, const SIZE_TYPE& size_value, FORMAT_STRING& format_string, SIZE_TYPE& index)
+							{
+								for (SIZE_TYPE i = 0; i < size_value; ++i)
+								{
+									format_string[index++] = value[i];
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_FORMAT_HELPER_H_
