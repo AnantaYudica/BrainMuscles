2771a2ce6948d733812432e1c5c9f5419387db45
AnantaYudica <nta040689@gmail.com>
2017-07-31 15:18:32 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Add dependency with algorithm Add dependency with test\simple\log\output\format\Constant.h Add dependency with test\simple\log\output\format\Helper.h Add class BrainMuscles::test::simple::log::output::format::Precision Added in class BrainMuscles::test::simple::log::output::format::Precision  - Add alias type BrainMuscles::test::simple::log::output::format::Constant with 'ConstantType' for name alias and private to access  - Add aliases type with public to access    - Add alias type ConstantType::SizeType with 'SizeType' for name alias    - Add alias type ConstantType::PrecisionInputType with 'PrecisionInputType' for name alias    - Add alias type ConstantType::PrecisionArrayType with 'PrecisionArrayType' for name alias    - Add alias type ConstantType::FormatStringType with 'FormatStringType' for name alias  - Add variables member with private to access    - Add variable 'm_size' with SizeType for type    - Add variable 'm_precision' with PrecisionArrayType for type  - Add default constructor with public to access  - Add constructor    - Add access constructor with public    - Add parameter constructor 'precision' with const Precision& for type  - Add constructor    - Add access constructor with public    - Add parameter constructor 'ptr_precision' with Precision* for type  - Add functions const member with public to access    - Add function 'GetSize' with SizeType for return type    - Add function 'GetValue' with const char* for return type  - Add functions member with public to access    - Add function 'SetValue'      - Add parameter function 'value' with const PrecisionInputType& for type    - Add function 'CopyToFormat'      - Add parameter function 'format' with FormatString& for type      - Add parameter function 'index' with SizeType& for type

---
+++ include/test/simple/log/output/format/Precision.h
@@ -0,0 +1,97 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_FORMAT_PRECISION_H_
+#define TEST_SIMPLE_LOG_OUTPUT_FORMAT_PRECISION_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include <algorithm>
+
+#include "test\simple\log\output\format\Constant.h"
+
+#include "test\simple\log\output\format\Helper.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace format
+					{
+						class Precision
+						{
+						private:
+							typedef BrainMuscles::test::simple::log::output::
+								format::Constant								ConstantType;
+						public:
+							typedef typename ConstantType::SizeType				SizeType;
+							typedef typename ConstantType::PrecisionInputType	PrecisionInputType;
+							typedef typename ConstantType::PrecisionArrayType	PrecisionArrayType;
+							typedef typename ConstantType::FormatStringType		FormatStringType;
+						private:
+							SizeType m_size;
+							PrecisionArrayType m_precision;
+						public:
+							Precision();
+							Precision(const Precision& precision);
+							Precision(Precision* ptr_precision);
+						public:
+							void SetValue(const PrecisionInputType& value);
+							SizeType GetSize() const;
+							const char* GetValue() const;
+						public:
+							void CopyToFormat(FormatStringType& format, SizeType& index);
+						};
+
+						Precision::Precision() :
+							m_size(0)
+						{}
+
+						Precision::Precision(const Precision& precision) :
+							m_size(precision.m_size)
+						{
+							std::copy(std::begin(precision.m_precision), std::begin(precision.m_precision) + m_size, m_precision);
+						}
+
+						Precision::Precision(Precision* ptr_precision) :
+							m_size(ptr_precision->m_size)
+						{
+							std::copy(std::begin(ptr_precision->m_precision), std::begin(ptr_precision->m_precision) + m_size, m_precision);
+						}
+
+						void Precision::SetValue(const PrecisionInputType& value)
+						{
+							m_size = BrainMuscles::test::simple::log::output::format::helper::
+								CastUnsignedIntegerToArrayOfChar<SizeType, PrecisionInputType, PrecisionArrayType>(value, m_precision);
+						}
+
+						typename Precision::SizeType Precision::GetSize() const
+						{
+							return m_size;
+						}
+
+						const char* Precision::GetValue() const
+						{
+							return m_precision;
+						}
+
+						void Precision::CopyToFormat(FormatStringType& format, SizeType& index)
+						{
+							BrainMuscles::test::simple::log::output::format::helper::
+								CopyToFormat<SizeType, PrecisionArrayType, FormatStringType>(m_precision, GetSize(), format, index);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_FORMAT_PRECISION_H_
