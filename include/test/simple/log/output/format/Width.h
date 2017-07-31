7b8536ed6b05c697f59c57c8c220d968f91fb48f
AnantaYudica <nta040689@gmail.com>
2017-07-31 17:52:35 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Add dependency with algorithm Add dependency with test\simple\log\output\format\Constant.h Add dependency with test\simple\log\output\format\helper.h Add class BrainMuscles::test::simple::log::output::format::Width Added in class BrainMuscles::test:simple::log::output::format::Width  - Add alias type BrainMuscles::test::simple::log::output::format::Constant with 'ConstantType' for name alias and private to access  - Add aliases type with public to access    - Add alias type ConstantType::SizeType with 'SizeType' for name alias    - Add alias type ConstantType::WidthInputType with 'WidthInputType' for name alias    - Add alias type ConstantType::FormatStringType with 'FormatStringType' for name alias  - Add variables member with private access    - Add variable 'm_size' with SizeType for type    - Add variable 'm_width' with WidthArrayType for type  - Add default constructor with public to access  - Add constructor    - Add access constructor with public    - Add parameter constructor 'width' with const Width& for type  - Add constructor    - Add access constructor with public    - Add parameter construct 'ptr_width' with Width* for type  - Add functions const member with public to access    - Add function 'GetSize' with SizeType for return type    - Add function 'GetValue' with const char* for return type  - Add functions member with public access    - Add function 'SetValue'      - Add parameter function 'value' with WidthInputType for type    - Add function 'CopyToFormat'      - Add parameter function 'format' with FormatStringType& for type      - Add parameter function 'index' with SizeType& for type

---
+++ include/test/simple/log/output/format/Width.h
@@ -0,0 +1,97 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_FORMAT_WIDTH_H_
+#define TEST_SIMPLE_LOG_OUTPUT_FORMAT_WIDTH_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include <algorithm>
+
+#include "test\simple\log\output\format\Constant.h"
+
+#include "test\simple\log\output\format\Helper.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace format
+					{
+						class Width
+						{
+						private:
+							typedef BrainMuscles::test::simple::log::output
+								::format::Constant							ConstantType;
+						public:
+							typedef typename ConstantType::SizeType			SizeType;
+							typedef typename ConstantType::WidthInputType	WidthInputType;
+							typedef typename ConstantType::WidthArrayType	WidthArrayType;
+							typedef typename ConstantType::FormatStringType	FormatStringType;
+						private:
+							SizeType m_size;
+							WidthArrayType m_width;
+						public:
+							Width();
+							Width(const Width& width);
+							Width(Width* ptr_width);
+						public:
+							void SetValue(const WidthInputType& value);
+							SizeType GetSize() const;
+							const char* GetValue() const;
+						public:
+							void CopyToFormat(FormatStringType& format, SizeType& index);
+						};
+
+						Width::Width() :
+							m_size(0)
+						{}
+
+						Width::Width(const Width& width) :
+							m_size(width.m_size)
+						{
+							std::copy(std::begin(width.m_width), std::begin(width.m_width) + m_size, m_width);
+						}
+
+						Width::Width(Width* ptr_width) :
+							m_size(ptr_width->m_size)
+						{
+							std::copy(std::begin(ptr_width->m_width), std::begin(ptr_width->m_width) + m_size, m_width);
+						}
+
+						void Width::SetValue(const WidthInputType& value)
+						{
+							m_size = BrainMuscles::test::simple::log::output::format::helper::
+								CastUnsignedIntegerToArrayOfChar<SizeType, WidthInputType, WidthArrayType>(value, m_width);
+						}
+
+						typename Width::SizeType Width::GetSize() const
+						{
+							return m_size;
+						}
+
+						const char* Width::GetValue() const
+						{
+							return m_width;
+						}
+
+						void Width::CopyToFormat(FormatStringType& format, SizeType& index)
+						{
+							BrainMuscles::test::simple::log::output::format::helper::
+								CopyToFormat<SizeType, WidthArrayType, FormatStringType>(m_width, GetSize(), format, index);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_FORMAT_WIDTH_H_
