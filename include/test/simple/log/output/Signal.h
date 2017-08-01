6aba4b696fb890f180f216f85ce7d108041ca07a
AnantaYudica <nta040689@gmail.com>
2017-08-01 12:58:54 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Add class BrainMuscles::test::simple::log::output::Signal Added in class BrainMuscles::test::simple::log::output::Signal - Add alias type 'FlagType' with unsinged char for type - Add variables static constexpr member with FlagType for type and private to access   - Add 'OutputSignalBitIndex' with 1 for value   - Add 'PrintSignalBitIndex' with 1 << 1 for value   - Add 'NewLineSignalBitIndex' with 1 << 2 for value - Add variable member 'm_flag' with FlagType for type and private to access - Add default constructor with public to access - Add destructor with public to access - Add functions member 'Default', 'OutputStartSignal', 'OutputEndSignal', 'PrintStartSignal', 'PrintEndSignal', NewlineSignal' and 'CurrentLineSignal' with void for return type and public to access - Add functions member 'IsOutputStart', 'OutputEndSignal', IsPrintStart', 'IsPrintEnd' and 'IsNewLine' with bool for return type and public to access

---
+++ include/test/simple/log/output/Signal.h
@@ -0,0 +1,132 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_SIGNAL_H_
+#define TEST_SIMPLE_LOG_OUTPUT_SIGNAL_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					class Signal
+					{
+					private:
+						typedef unsigned char FlagType;
+						static constexpr FlagType OutputSignalBitIndex = 1;
+						static constexpr FlagType PrintSignalBitIndex = 1 << 1;
+						static constexpr FlagType NewlineSignalBitIndex = 1 << 2;
+					private:
+						FlagType m_flag;
+					public:
+						Signal();
+						~Signal();
+					public:
+						void Default();
+					public:
+						void OutputStartSignal();
+						void OutputEndSignal();
+						bool IsOutputStart();
+						bool IsOutputEnd();
+					public:
+						void PrintStartSignal();
+						void PrintEndSignal();
+						bool IsPrintStart();
+						bool IsPrintEnd();
+					public:
+						void NewlineSignal();
+						void CurrentlineSignal();
+						bool IsNewline();
+					};
+
+					Signal::Signal() :
+						m_flag(0)
+					{}
+
+					Signal::~Signal()
+					{}
+
+					void Signal::Default()
+					{
+						OutputEndSignal();
+						PrintEndSignal();
+					}
+
+					void Signal::OutputStartSignal()
+					{
+						m_flag |= OutputSignalBitIndex;
+					}
+
+					void Signal::OutputEndSignal()
+					{
+						if (IsOutputStart())
+						{
+							m_flag ^= OutputSignalBitIndex;
+						}
+					}
+
+					bool Signal::IsOutputStart()
+					{
+						return (m_flag & OutputSignalBitIndex);
+					}
+
+					bool Signal::IsOutputEnd()
+					{
+						return !IsOutputStart();
+					}
+
+					void Signal::PrintStartSignal()
+					{
+						m_flag |= PrintSignalBitIndex;
+					}
+
+					void Signal::PrintEndSignal()
+					{
+						if (IsPrintStart())
+						{
+							m_flag ^= PrintSignalBitIndex;
+						}
+					}
+
+					bool Signal::IsPrintStart()
+					{
+						return (m_flag & PrintSignalBitIndex);
+					}
+
+					bool Signal::IsPrintEnd()
+					{
+						return !(IsPrintStart());
+					}
+
+					void Signal::NewlineSignal()
+					{
+						m_flag |= NewlineSignalBitIndex;
+					}
+
+					void Signal::CurrentlineSignal()
+					{
+						if (IsNewline())
+						{
+							m_flag ^= NewlineSignalBitIndex;
+						}
+					}
+
+					bool Signal::IsNewline()
+					{
+						return (m_flag & NewlineSignalBitIndex);
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_SIGNAL_H_
