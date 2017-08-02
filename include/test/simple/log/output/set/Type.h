9cecf10cd9d4a40550f902a5531521cb261449c4
AnantaYudica <nta040689@gmail.com>
2017-08-02 15:28:38 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Added in macro if defined _USING_TEST  - Add enum BrainMuscles::test::simple::log::output::set::Type  - Added in enum BrainMuscles::test::simple::log::output::set::Type    - Add 'local_value' with 0 for value    - Add 'global_value' with 1 for value

---
+++ include/test/simple/log/output/set/Type.h
@@ -0,0 +1,34 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_SET_TYPE_H_
+#define TEST_SIMPLE_LOG_OUTPUT_SET_TYPE_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace set
+					{
+						enum Type
+						{
+							local_value = 0,
+							global_value = 1
+						};
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif 
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_SET_TYPE_H_
