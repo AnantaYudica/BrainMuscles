bc251f1ce04afc2f9e94928e243ae7bb0b202eeb
AnantaYudica <nta040689@gmail.com>
2017-08-08 01:54:05 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if _USING_TEST_ Added in macro if _USING_TEST_  - Add dependency with test\simple\log\output\format\Value.h  - Add dependency with test\simple\log\output\Arguments.h  - Add dependency with test\simple\log\output\Set.h  - Add class BrainMuscles::test::simple::log::output::handle::Character  - Added in class BrainMuscles::test::simple::log::output::handle::Character    - Add Base class with BrainMuscles::test::simple::log::output::Set and arguments template BrainMuscles::test::simple::log::output::format::Value, void and void    - Add aliases type with public to access      - Add alias type 'BaseType' with BrainMuscles::test::simple::log::output::Set<BrainMuscles::test::simple::log::output::format::Value, void, void> for type      - Add alias type 'SetTypeType' with BrainMuscles::test::simple::log::output::set::Type for type      - Add alias type 'ValueType' with BrainMuscles::test::simple::log::output::format::Value for type      - Add alias type 'ArgumentsType' with BrainMuscles::test::simple::log::output::Arguments<Character, int>    - Add default constructor with public to access    - Add function member 'operator()' with ArgumentsType for return type, parameter function 'character' and const int& for type and public to access

---
+++ include/test/simple/log/output/handle/Character.h
@@ -0,0 +1,60 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_HANDLE_CHARACTER_H_
+#define TEST_SIMPLE_LOG_OUTPUT_HANDLE_CHARACTER_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\log\output\format\Value.h"
+
+#include "test\simple\log\output\Arguments.h"
+#include "test\simple\log\output\Set.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace handle
+					{
+						class Character :
+							public BrainMuscles::test::simple::log::output::Set<
+								BrainMuscles::test::simple::log::output::format::Value, void, void>
+						{
+						public:
+							typedef BrainMuscles::test::simple::log::output::Set<
+								BrainMuscles::test::simple::log::output::format
+								::Value, void, void>									BaseType;
+							typedef BrainMuscles::test::simple::log::output::set::Type	SetTypeType;
+							typedef BrainMuscles::test::simple::log::output::format
+								::Value													ValueType;
+							typedef BrainMuscles::test::simple::log::output::Arguments<
+								Character, int>											ArgumentsType;
+						public:
+							Character();
+							ArgumentsType operator()(const int& character);
+						};
+
+						Character::Character() :
+							BaseType(SetTypeType::local_value, &ValueType::CharacterEnable)
+						{}
+
+						typename Character::ArgumentsType Character::operator()(const int& character)
+						{
+							return ArgumentsType(character);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_HANDLE_CHARACTER_H_
