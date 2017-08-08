671fa6ef8e2a83626481f2699479ac1cae888a74
AnantaYudica <nta040689@gmail.com>
2017-08-08 15:38:24 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Added in macro if defined _USING_TEST_  - Add dependency with test\simple\log\output\format\Value.h  - Add dependency with test\simple\log\output\Arguments.h  - Add dependency with test\simple\log\output\Set.h  - Add class BrainMuscles::test::simple::log::output::handle::Decimal  - Added in class BrainMuscles::test::simple::log::output::handle::Decimal    - Add base class with BrainMuscles::test::simple::log::output::Set<BrainMuscles::test::simple::log::output::format::Value, void, void> and public inheritance    - Add aliases type with public access      - Add alias type 'ValueType' with BrainMuscles::test::simple::log::output::format::Value for type      - Add alias type 'BaseType' with BrainMuscles::test::simple::log::output::Set<ValueType, void, void> for type      - Add alias type 'SetTypeType' with BrainMuscles::test::simple::log::output::set::Type for type      - Add alias type ArgumentsType with parameter template 'TYPE' and BrainMuscles::test::simple::log::output::Arguments<Decimal, TYPE> for type    - Add default constructor with public to access    - Add functions member 'operator()' with ArgumentsType<unsigned char>, ArgumentsType<char>, ArgumentsType<unsigned short>, ArgumentsType<short>, ArgumentsType<unsigned int>, ArgumentsType<int>, ArgumentsType<unsigned long>, ArgumentsType<long>, ArgumentsType<unsigned long long> and ArgumentsType<long long> for return type and parameter function 'value' with const unsigned char&, const char&, const unsigned short&, const short&, const unsigned int&, const int&, const unsigned long&, const long&, const unsigned long long& and const long long& for type

---
+++ include/test/simple/log/output/handle/Decimal.h
@@ -0,0 +1,125 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_HANDLE_DECIMAL_H_
+#define TEST_SIMPLE_LOG_OUTPUT_HANDLE_DECIMAL_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\log\output\format\Value.h"
+
+#include "test\simple\log\output\Arguments.h"
+#include "test\simple\log\output\Set.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace handle
+					{
+						class Decimal :
+							public BrainMuscles::test::simple::log::output::Set<
+								BrainMuscles::test::simple::log::output::format::Value, void, void>
+						{
+						public:
+							typedef BrainMuscles::test::simple::log::output::format::Value	ValueType;
+							typedef BrainMuscles::test::simple::log::output::Set<
+								ValueType, void, void>										BaseType;
+							typedef BrainMuscles::test::simple::log::output::set::Type		SetTypeType;
+						public:
+							template<typename TYPE>
+							using ArgumentsType = BrainMuscles::test::simple::log::output::Arguments<
+								Decimal, TYPE>;
+						public:
+							Decimal();
+						public:
+							ArgumentsType<unsigned char> operator()(const unsigned char& value);
+							ArgumentsType<char> operator()(const char& value);
+							ArgumentsType<unsigned short> operator()(const unsigned short& value);
+							ArgumentsType<short> operator()(const short& value);
+							ArgumentsType<unsigned int> operator()(const unsigned int& value);
+							ArgumentsType<int> operator()(const int& value);
+							ArgumentsType<unsigned long> operator()(const unsigned long& value);
+							ArgumentsType<long> operator()(const long& value);
+							ArgumentsType<unsigned long long> operator()(const unsigned long long& value);
+							ArgumentsType<long long> operator()(const long long& value);
+						};
+
+						Decimal::Decimal() :
+							BaseType(SetTypeType::local_value, &ValueType::DecimalEnable)
+						{}
+
+						typename Decimal::ArgumentsType<unsigned char> 
+							Decimal::operator()(const unsigned char& value)
+						{
+							return ArgumentsType<unsigned char>(value);
+						}
+
+						typename Decimal::ArgumentsType<char> 
+							Decimal::operator()(const char& value)
+						{
+							return ArgumentsType<char>(value);
+						}
+
+						typename Decimal::ArgumentsType<unsigned short> 
+							Decimal::operator()(const unsigned short& value)
+						{
+							return ArgumentsType<unsigned short>(value);
+						}
+
+						typename Decimal::ArgumentsType<short> 
+							Decimal::operator()(const short& value)
+						{
+							return ArgumentsType<short>(value);
+						}
+
+						typename Decimal::ArgumentsType<unsigned int> 
+							Decimal::operator()(const unsigned int& value)
+						{
+							return ArgumentsType<unsigned int>(value);
+						}
+
+						typename Decimal::ArgumentsType<int> 
+							Decimal::operator()(const int& value)
+						{
+							return ArgumentsType<int>(value);
+						}
+
+						typename Decimal::ArgumentsType<unsigned long> 
+							Decimal::operator()(const unsigned long& value)
+						{
+							return ArgumentsType<unsigned long>(value);
+						}
+
+						typename Decimal::ArgumentsType<long> 
+							Decimal::operator()(const long& value)
+						{
+							return ArgumentsType<long>(value);
+						}
+
+						typename Decimal::ArgumentsType<unsigned long long> 
+							Decimal::operator()(const unsigned long long& value)
+						{
+							return ArgumentsType<unsigned long long>(value);
+						}
+
+						typename Decimal::ArgumentsType<long long> 
+							Decimal::operator()(const long long& value)
+						{
+							return ArgumentsType<long long>(value);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_HANDLE_DECIMAL_H_
