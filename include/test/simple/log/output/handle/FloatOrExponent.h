eb18013fc191bfcf2ff323c7c48833937ac412ea
AnantaYudica <nta040689@gmail.com>
2017-08-08 19:47:28 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Added in macro if defined _USING_TEST_  - Add dependency with test\simple\log\output\format\Value.h  - Add dependency with test\simple\log\output\Arguments.h  - Add dependency with test\simple\log\output\Set.h  - Add class BrainMuscles::test::simple::log::output::handle::FloatOrExponent  - Added in class BrainMuscles::test::simple::log::output::handle::FloatOrExponent    - Add base class with BrainMuscles::test::simple::log::output::Set<BrainMuscles::test::simple::log::output::format::Value, void, void> and public inheritance    - Add aliases type with public to access      - Add alias type 'ValueType' with BrainMuscles::test::simple::log::output::format::Value for type      - Add alias type 'BaseType' with BrainMuscles::test::simple::log::output::Set<ValueType, void, void> for type      - Add alias type 'SetTypeType' with BrainMuscles::test::simple::log::output::set::Type for type      - Add alias type 'ArgumentsType' with parameter template 'TYPE' and BrainMuscles::test::simple::log::output::Arguments<FloatOrExponent, TYPE> for type    - Add default constructor with public to access    - Add functions member 'operator()' with ArgumentsType<float>, ArgumentsType<double> and ArgumentsType<long double> for return type and parameter function 'value' with const float&, const double& and const long double& for type

---
+++ include/test/simple/log/output/handle/FloatOrExponent.h
@@ -0,0 +1,76 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_HANDLE_FLOATOREXPONENT_H_
+#define TEST_SIMPLE_LOG_OUTPUT_HANDLE_FLOATOREXPONENT_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\log\output\format\Value.h"
+
+#include "test\simple\log\output\Arguments.h"
+#include "test\simple\log\output\Set.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace handle
+					{
+						class FloatOrExponent :
+							public BrainMuscles::test::simple::log::output::Set<
+								BrainMuscles::test::simple::log::output::format::Value, void, void>
+						{
+						public:
+							typedef BrainMuscles::test::simple::log::output::format::Value	ValueType;
+							typedef BrainMuscles::test::simple::log::output::Set<
+								ValueType, void, void>										BaseType;
+							typedef BrainMuscles::test::simple::log::output::set::Type		SetTypeType;
+						public:
+							template<typename TYPE>
+							using ArgumentsType = BrainMuscles::test::simple::log::output::Arguments<
+								FloatOrExponent, TYPE>;
+						public:
+							FloatOrExponent();
+						public:
+							ArgumentsType<float> operator()(const float& value);
+							ArgumentsType<double> operator()(const double& value);
+							ArgumentsType<long double> operator()(const long double& value);
+						};
+
+						FloatOrExponent::FloatOrExponent() :
+							BaseType(SetTypeType::local_value, &ValueType::FloatAndFloatExponentEnable)
+						{}
+
+						typename FloatOrExponent::ArgumentsType<float> 
+							FloatOrExponent::operator()(const float& value)
+						{
+							return ArgumentsType<float>(value);
+						}
+
+						typename FloatOrExponent::ArgumentsType<double> 
+							FloatOrExponent::operator()(const double& value)
+						{
+							return ArgumentsType<double>(value);
+						}
+
+						typename FloatOrExponent::ArgumentsType<long double> 
+							FloatOrExponent::operator()(const long double& value)
+						{ 
+							return ArgumentsType<long double>(value);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif 
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_HANDLE_FLOATOREXPONENT_H_
