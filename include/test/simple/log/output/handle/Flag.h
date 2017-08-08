045ac5e80540d5a922ccaa9ab0fa47f94f1bd0e5
AnantaYudica <nta040689@gmail.com>
2017-08-08 18:03:03 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Added macro if defined _USING_TEST_  - Add dependency with test\simple\log\output\format\Flags.h  - Add dependency with test\simple\log\output\Arguments.h  - Add class BrainMuscles::test::simple::log::output::handle::Flag  - Add in class BrainMuscles::test::simple::log::output::handle::Flag    - Add aliases type with public to access      - Add alias type 'FlagsType' with BrainMuscles::test::simple::log::output::format::Flags for type      - Add alias type 'ValueType' with BrainMuscles::test::simple::log::output::format::Value for type      - Add alias type 'SetType' with BrainMuscles::test::simple::log::output::Set<ValueType, void, const FlagsType&> for type      - Add alias type 'SetTypeType' with BrainMuscles::test::simple::log::output::set::Type    - Add variable member 'Negative', 'Positive', 'Space', 'Hash' and 'Zero' with const SetType for type and public to access    - Add default constructor with public to access

---
+++ include/test/simple/log/output/handle/Flag.h
@@ -0,0 +1,58 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_HANDLE_FLAG_H_
+#define TEST_SIMPLE_LOG_OUTPUT_HANDLE_FLAG_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\log\output\format\Flags.h"
+
+#include "test\simple\log\output\Arguments.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace handle
+					{
+						class Flag
+						{
+						public:
+							typedef BrainMuscles::test::simple::log::output::format::Flags	FlagsType;
+							typedef BrainMuscles::test::simple::log::output::format::Value	ValueType;
+							typedef BrainMuscles::test::simple::log::output::Set<
+								ValueType, void, const FlagsType&>							SetType;
+							typedef BrainMuscles::test::simple::log::output::set::Type		SetTypeType;
+						public:
+							const SetType Negative;
+							const SetType Positive;
+							const SetType Space;
+							const SetType Hash;
+							const SetType Zero;
+						public:
+							Flag();
+						};
+
+						Flag::Flag() :
+							Negative(SetTypeType::local_value, &ValueType::SetFlag, FlagsType::Negative()),
+							Positive(SetTypeType::local_value, &ValueType::SetFlag, FlagsType::Positive()),
+							Space(SetTypeType::local_value, &ValueType::SetFlag, FlagsType::Space()),
+							Hash(SetTypeType::local_value, &ValueType::SetFlag, FlagsType::Hash()),
+							Zero(SetTypeType::local_value, &ValueType::SetFlag, FlagsType::Zero())
+						{}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_HANDLE_FLAG_H_
