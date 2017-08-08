386b27328e0226f672d6b56c90bee436dd495977
AnantaYudica <nta040689@gmail.com>
2017-08-08 14:48:06 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Added in macro if defined _USING_TEST_  - Add dependency with test\simple\log\output\format\Value.h  - Add dependency with test\simple\log\output\Set.h  - Add class BrainMuscles::test::simple::log::output::handle::Clear  - Added in class BrainMuscles::test::simple::log::output::handle::Clear    - Add base class with BrainMuscles::test::simple::log::output::Set<BrainMuscles::test::simple::log::output::format::Value, void, void> and public inheritance    - Add aliases type with public to access      - Add alias 'ValueType' with BrainMuscles::test::simple::log::output::format::Value for type      - Add alias 'BaseType' with BrainMuscles::test::simple::log::output::Set<ValueType, void, void>      - Add alias 'SetType' with BrainMuscles::test::simple::log::output::set::Type for type    - Add default constructor with public to access

---
+++ include/test/simple/log/output/handle/Clear.h
@@ -0,0 +1,49 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_HANDLE_CLEAR_H_
+#define TEST_SIMPLE_LOG_OUTPUT_HANDLE_CLEAR_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\log\output\format\Value.h"
+
+#include "test\simple\log\output\Set.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace handle
+					{
+						class Clear :
+							public BrainMuscles::test::simple::log::output::Set<
+								BrainMuscles::test::simple::log::output::format::Value, void, void>
+						{
+						public:
+							typedef BrainMuscles::test::simple::log::output::format::Value	ValueType;
+							typedef BrainMuscles::test::simple::log::output::Set<
+								ValueType, void, void>										BaseType;
+							typedef BrainMuscles::test::simple::log::output::set::Type		SetType;
+						public:
+							Clear();
+						};
+
+						Clear::Clear() :
+							BaseType(SetType::local_and_global_value, &ValueType::Default)
+						{}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_HANDLE_CLEAR_H_
