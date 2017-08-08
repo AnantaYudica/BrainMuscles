cd016d4846ce1447b6b4e7c9f9eccd03fa323336
AnantaYudica <nta040689@gmail.com>
2017-08-08 19:56:48 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Added macro if defined _USING_TEST_  - Add dependency with test\simple\log\output\Arguments.h  - Add class BrainMuscles::test::simple::log::output::handle::Format  - Added in class BrainMuscles::test::simple::log::output::handle::Format    - Add alias type 'ArgumentsType' with parameter variadic template 'ARGS' and BrainMuscles::test::simple::log::output::Arguments<Format, const char*, ARGS...> for type    - Add function member 'operator()' with parameter variadic template 'ARGS', ArgumentsType<ARGS...> for return type and parameters function 'format' with const char* for type and 'args' with ARGS... for type

---
+++ include/test/simple/log/output/handle/Format.h
@@ -0,0 +1,47 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_HANDLE_FORMAT_H_
+#define TEST_SIMPLE_LOG_OUTPUT_HANDLE_FORMAT_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\log\output\Arguments.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace handle
+					{
+						class Format
+						{
+						public:
+							template<typename... ARGS>
+							using ArgumentsType = BrainMuscles::test::simple::log::output::Arguments<Format, const char*, ARGS...>;
+						public:
+							template<typename... ARGS>
+							ArgumentsType<ARGS...> operator()(const char* format, ARGS... args);
+						};
+
+						template<typename... ARGS>
+						typename Format::ArgumentsType<ARGS...>
+						Format::operator()(const char* format, ARGS... args)
+						{
+							return ArgumentsType<ARGS...>(format, args...);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_HANDLE_FORMAT_H_
