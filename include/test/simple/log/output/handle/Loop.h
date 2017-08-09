4a0182726a10da937b339e32532eda42a284e15b
AnantaYudica <nta040689@gmail.com>
2017-08-09 15:47:37 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Added in macro if defined _USING_TEST_  - Add dependency with cstddef  - Add dependency with test\simple\log\output\Constant.h  - Add dependency with test\simple\log\output\Arguments.h  - Add class BrainMuscles::test::simple::log::output::handle::Loop  - Added in class BrainMuscles::test::simple::log::output::handle::Loop    - Add aliases type with public to access      - Add alias type 'ConstantType' with BrainMuscles::test::simple::log::output::Constant for type      - Add alias type 'ArgumentsType' with parameters template 'ITERATOR_TYPE', 'CONJUNCTION_TYPE' with ConstantType::LoopConjunctionDefaultType for default type, 'BEFORE_PRINT_TYPE' with ConstantType::LoopBeforePrintDefaultType for default type and 'AFTER_PRINT_TYPE' with ConstantType::LoopAfterPrintDefaultType for default type and BrainMuscles::test::simple::log::output::Arguments<Loop, ITERATOR_TYPE, ITERATOR_TYPE, CONJUNCTION_TYPE, BEFORE_PRINT_TYPE, AFTER_PRINT_TYPE> for type    - Add function const member 'operator()' with public to access, parameters template 'TYPE', SIZE with std::size_t for type, 'CONJUNCTION_TYPE' with ConstantType::LoopConjunctionDefaultType for default type, 'BEFORE_PRINT_TYPE' with ConstantType::LoopBeforePrintDefaultType for default type and 'AFTER_PRINT_TYPE' with ConstantType::LoopAfterPrintDefaultType for default type and parameters function 'arr' with TYPE(&)[SIZE], 'conjunction' with CONJUNCTION_TYPE for type and ConstantType::LoopConjunctionDefault for default value, 'before_print' with BEFORE_PRINT_TYPE for type and ConstantType::LoopBeforePrintDefault for default value and 'after_print' with AFTER_PRINT_TYPE for type and ConstantType::LoopAfterPrintDefault for default value     - Add function const member 'operator()' with public to access parameters template 'ITERATOR_TYPE', 'CONJUNCTION_TYPE' with ConstantType::LoopConjunctionDefaultType for default type, 'BEFORE_PRINT_TYPE' with ConstantType::LoopBeforePrintDefaultType for default type and 'AFTER_PRINT_TYPE' with ConstantType::LoopAfterPrintDefaultType for default type and parameters function 'begin_iterator' and 'end_iterator' with ITERATOR_TYPE for type, 'conjunction' with CONJUNCTION_TYPE for type and ConstantType::LoopConjunctionDefault for default value, 'before_print' with BEFORE_PRINT_TYPE for type and ConstantType::LoopBeforePrintDefault for default value and 'after_print' with AFTER_PRINT_TYPE for type and ConstantType::LoopAfterPrintDefault for default value

---
+++ include/test/simple/log/output/handle/Loop.h
@@ -0,0 +1,81 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_HANDLE_LOOP_H_
+#define TEST_SIMPLE_LOG_OUTPUT_HANDLE_LOOP_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include <cstddef>
+
+#include "test\simple\log\output\Constant.h"
+
+#include "test\simple\log\output\Arguments.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace handle
+					{
+						class Loop
+						{
+						public:
+							typedef BrainMuscles::test::simple::log::output::Constant ConstantType;
+							template<typename ITERATOR_TYPE, 
+								typename CONJUNCTION_TYPE = ConstantType::LoopConjunctionDefaultType,
+								typename BEFORE_PRINT_TYPE = ConstantType::LoopBeforePrintDefaultType,
+								typename AFTER_PRINT_TYPE = ConstantType::LoopAfterPrintDefaultType>
+							using ArgumentsType = BrainMuscles::test::simple::log::output::Arguments<Loop, ITERATOR_TYPE, ITERATOR_TYPE, CONJUNCTION_TYPE, BEFORE_PRINT_TYPE, AFTER_PRINT_TYPE>;
+						public:
+							template<typename TYPE, 
+								std::size_t SIZE, 
+								typename CONJUNCTION_TYPE = ConstantType::LoopConjunctionDefaultType,
+								typename BEFORE_PRINT_TYPE = ConstantType::LoopBeforePrintDefaultType,
+								typename AFTER_PRINT_TYPE = ConstantType::LoopAfterPrintDefaultType>
+							ArgumentsType<TYPE*, CONJUNCTION_TYPE, BEFORE_PRINT_TYPE, AFTER_PRINT_TYPE> 
+								operator()(TYPE(&arr)[SIZE], 
+									CONJUNCTION_TYPE conjunction = ConstantType::LoopConjunctionDefault,
+									BEFORE_PRINT_TYPE before_print = ConstantType::LoopBeforePrintDefault,
+									AFTER_PRINT_TYPE after_print = ConstantType::LoopAfterPrintDefault) const;
+							
+							template<typename ITERATOR_TYPE, 
+								typename CONJUNCTION_TYPE = ConstantType::LoopConjunctionDefaultType,
+								typename BEFORE_PRINT_TYPE = ConstantType::LoopBeforePrintDefaultType,
+								typename AFTER_PRINT_TYPE = ConstantType::LoopAfterPrintDefaultType>
+							ArgumentsType<ITERATOR_TYPE, CONJUNCTION_TYPE, BEFORE_PRINT_TYPE, AFTER_PRINT_TYPE>
+								operator()(ITERATOR_TYPE begin_iterator, 
+									ITERATOR_TYPE end_iterator, 
+									CONJUNCTION_TYPE conjunction = ConstantType::LoopConjunctionDefault,
+									BEFORE_PRINT_TYPE before_print = ConstantType::LoopBeforePrintDefault,
+									AFTER_PRINT_TYPE after_print = ConstantType::LoopAfterPrintDefault) const;
+						};
+
+						template<typename TYPE, std::size_t SIZE, typename CONJUNCTION_TYPE, typename BEFORE_PRINT_TYPE, typename AFTER_PRINT_TYPE>
+						Loop::ArgumentsType<TYPE*, CONJUNCTION_TYPE, BEFORE_PRINT_TYPE, AFTER_PRINT_TYPE> 
+							Loop::operator()(TYPE(&arr)[SIZE], CONJUNCTION_TYPE conjunction, BEFORE_PRINT_TYPE before_print, AFTER_PRINT_TYPE after_print) const
+						{
+							return ArgumentsType<TYPE*, CONJUNCTION_TYPE, BEFORE_PRINT_TYPE, AFTER_PRINT_TYPE>(arr, arr + SIZE, conjunction, before_print, after_print);
+						}
+
+						template<typename ITERATOR_TYPE, typename CONJUNCTION_TYPE, typename BEFORE_PRINT_TYPE, typename AFTER_PRINT_TYPE>
+						Loop::ArgumentsType<ITERATOR_TYPE, CONJUNCTION_TYPE, BEFORE_PRINT_TYPE, AFTER_PRINT_TYPE>
+							Loop::operator()(ITERATOR_TYPE begin_iterator, ITERATOR_TYPE end_iterator, CONJUNCTION_TYPE conjunction , BEFORE_PRINT_TYPE before_print, AFTER_PRINT_TYPE after_print) const
+						{
+							return ArgumentsType<ITERATOR_TYPE, CONJUNCTION_TYPE, BEFORE_PRINT_TYPE, AFTER_PRINT_TYPE>(begin_iterator, end_iterator, conjunction, before_print, after_print);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_HANDLE_LOOP_H_
