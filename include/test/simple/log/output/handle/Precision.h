6549292b67915a6d7d97e01676d1aad63073018b
AnantaYudica <nta040689@gmail.com>
2017-08-09 19:13:47 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Added in macro if defined _USING_TEST_  - Add dependency with test\simple\log\output\format\Value.h  - Add dependency with test\simple\log\output\format\Precision.h  - Add dependency with test\simple\log\output\Arguments.h  - Add dependency with test\simple\log\output\Set.h  - Add class BrainMuscles::test::simple::log::output::handle::Precision  - Added in class BrainMuscles::test::simple::log::output::handle::Precision    - Add aliases type with public to access      - Add alias type 'PrecisionType' with BrainMuscles::test::simple::log::output::format::Precision for type      - Add alias type 'PrecisionInputType' with PrecisionType::PrecisionInputType for type      - Add alias type 'ValueType' with BrainMuscles::test::simple::log::output::format::Value for type      - Add alias type 'SetType' with BrainMuscles::test::simple::log::output::Set<ValueType, void, const PrecisionType&> for type      - Add alias type 'SetTypeType' with  BrainMuscles::test::simple::log::output::set::Type for type    - Add function const member 'operator()' with public to access, SetType for return type and parameter function 'value' with const PrecisionInputType& for type

---
+++ include/test/simple/log/output/handle/Precision.h
@@ -0,0 +1,57 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_HANDLE_PRECISION_H_
+#define TEST_SIMPLE_LOG_OUTPUT_HANDLE_PRECISION_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\log\output\format\Value.h"
+#include "test\simple\log\output\format\Precision.h"
+
+#include "test\simple\log\output\Arguments.h"
+#include "test\simple\log\output\Set.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace handle
+					{
+						class Precision
+						{
+						public:
+							typedef BrainMuscles::test::simple::log::output
+								::format::Precision										PrecisionType;
+							typedef typename PrecisionType::PrecisionInputType			PrecisionInputType;
+							typedef BrainMuscles::test::simple::log::output
+								::format::Value											ValueType;
+							typedef BrainMuscles::test::simple::log::output::Set<
+								ValueType, void, const PrecisionType&>					SetType;
+							typedef BrainMuscles::test::simple::log::output::set::Type	SetTypeType;
+						public:
+							SetType operator()(const PrecisionInputType& value) const;
+						};
+
+						typename Precision::SetType 
+							Precision::operator()(const PrecisionInputType& value) const
+						{
+							PrecisionType percision;
+							percision.SetValue(value);
+							return SetType(SetTypeType::local_value, &ValueType::SetPrecision, percision);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUT_CALL_HANDLE_PRECISION_H_
