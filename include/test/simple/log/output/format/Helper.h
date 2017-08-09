ce4f3b90d5d1ca4c5b5f9027c1c332414adf1102
AnantaYudica <nta040689@gmail.com>
2017-08-09 18:49:14 +0700

    MODIFIED FILE Add in macro if defined _USING_TEST_  - Add dependency with limits  - Add function constexpr BrainMuscles::test::simple:log::output::format::helper::Power    - Add parameters template 'RETURN_TYPE', 'BASE_TYPE' and 'EXPONENT_TYPE'    - Add return type with RETURN_TYPE    - Add parameters function 'base' with BASE_TYPE for type, 'exponent' with EXPONENT_TYPE for type and 'result' with RETURN_TYPE for type and 1 for default value  - Update function constexpr BrainMuscles::test::simple:log::output::format::helper::CastUnsignedIntegerToArrayOfChar with change algorithm

--- include/test/simple/log/output/format/Helper.h
+++ include/test/simple/log/output/format/Helper.h
@@ -5,6 +5,7 @@
 
 #if defined(_USING_TEST_)
 
+#include <limits>
 #include <type_traits>
 
 namespace BrainMuscles
@@ -21,15 +22,53 @@ namespace BrainMuscles
 					{
 						namespace helper
 						{
+							template<typename RETURN_TYPE, typename BASE_TYPE, typename EXPONENT_TYPE>
+							constexpr RETURN_TYPE Power(BASE_TYPE base, EXPONENT_TYPE exponent, RETURN_TYPE result = 1)
+							{
+								return exponent == 0 ? result : Power(base, exponent - 1, result * base);
+							}
+
 							template<typename SIZE_TYPE, typename INPUT_TYPE, typename ARRAY_TYPE>
 							SIZE_TYPE CastUnsignedIntegerToArrayOfChar(const INPUT_TYPE& in, ARRAY_TYPE& out)
 							{
 								static_assert(std::is_unsigned<INPUT_TYPE>::value, "INPUT_TYPE required unsigned");
+								constexpr SIZE_TYPE maxInputDigit10 = std::numeric_limits<INPUT_TYPE>::digits10;
+								constexpr INPUT_TYPE maxInputPower10 = Power<INPUT_TYPE>(10, maxInputDigit10);
 								SIZE_TYPE index = 0;
-								for (INPUT_TYPE val = in; val > 0; val /= 10, index++)
 								{
-									out[index] = 48 + (val % 10);
+									INPUT_TYPE value = in;
+									INPUT_TYPE power10 = maxInputPower10;
+									SIZE_TYPE i = 0;
+									while (i <= maxInputDigit10)
+									{
+										if (index != 0 && value == 0)
+										{
+											out[index++] = 48;
+										}
+										else if (value == 0)
+										{
+											break;
+										}
+										else
+										{
+											INPUT_TYPE valueMod10 = value % power10;
+											if (valueMod10 != value)
+											{
+												INPUT_TYPE number = value - valueMod10;
+												value -= number;
+												number /= power10;
+												out[index++] = 48 + (number % 10);
+											}
+											else if (index != 0)
+											{
+												out[index++] = 48;
+											}
+											power10 /= 10;
+										}
+										++i;
+									}
 								}
+								if (index == 0) out[index++] = 48;
 								return index;
 							}
 
