0847ed1e3f9b8311429b7f331f409103a2b4232c
AnantaYudica <nta040689@gmail.com>
2017-08-09 16:08:50 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Added macro if defined _USING_TEST_  - Add dependency with test\simple\log\output\format\Value.h  - Add dependency with test\simple\log\output\Set.h  - Add class BrainMuscles::test::simple::log::output::handle::Lower  - Added in class BrainMuscles::test::simple::log::output::handle::Lower    - Add base class with BrainMuscles::test::simple::log::output::Set<BrainMuscles::test::simple::log::output::format::Value, void, void> and public inheritance    - Add aliases type with public to access      - Add alias type 'ValueType' with BrainMuscles::test::simple::log::output::format::Value for type      - Add alias type 'BaseType' with BrainMuscles::test::simple::log::output::Set<ValueType, void, void> for type      - Add alias type 'SetTypeType' with BrainMuscles::test::simple::log::output::set::Type for type    - Add default constructor with public to access

---
+++ include/test/simple/log/output/handle/Lower.h
@@ -0,0 +1,50 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_HANDLE_LOWER_H_
+#define TEST_SIMPLE_LOG_OUTPUT_HANDLE_LOWER_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\log\output\format\Value.h"
+
+#include "test\simple\log\output\Set.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace handle
+					{
+						class Lower : 
+							public BrainMuscles::test::simple::log::output::Set<
+								BrainMuscles::test::simple::log::output::format::Value, void, void>
+						{
+						public:
+							typedef BrainMuscles::test::simple::log::output::format::Value	ValueType;
+							typedef BrainMuscles::test::simple::log::output::Set<
+								ValueType, void, void>										BaseType;
+							typedef BrainMuscles::test::simple::log::output::set::Type		SetTypeType;
+						public:
+							Lower();
+						};
+
+						Lower::Lower() :
+							BaseType(SetTypeType::local_value, &ValueType::LowerEnable)
+						{}
+					}
+				}
+			}
+		}
+	}
+}
+
+
+#endif 
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_HANDLE_LOWER_H_
