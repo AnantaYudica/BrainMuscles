5c8ac817312049207dd76f593ca5a9da62c2c6aa
AnantaYudica <nta040689@gmail.com>
2017-08-09 23:18:20 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Added in macro if defined _USING_TEST_  - Add dependency with test\simple\log\output\format\Value.h  - Add dependency with test\simple\log\output\Set.h  - Add class BrainMuscles::test::simple::log::output::handle::Upper  - Added in class BrainMuscles::test::simple::log::output::handle::Upper    - Add base class with BrainMuscles::test::simple::log::output::Set<BrainMuscles::test::simple::log::output::format::Value, void, void> and public inheritance    - Add aliases type with public to access      - Add alias type 'ValueType' with BrainMuscles::test::simple::log::output::format::Value for type      - Add alias type 'BaseType' with BrainMuscles::test::simple::log::output::Set<ValueType, void, void> for type      - Add alias type 'SetTypeType' with BrainMuscles::test::simple::log::output::set::Type for type    - Add default constructor with public to access

---
+++ include/test/simple/log/output/handle/Upper.h
@@ -0,0 +1,49 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_HANDLE_UPPER_H_
+#define TEST_SIMPLE_LOG_OUTPUT_HANDLE_UPPER_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\log\output\format\Value.h"
+
+#include "test\simple\log\output\Set.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace handle
+					{
+						class Upper :
+							public BrainMuscles::test::simple::log::output::Set<
+								BrainMuscles::test::simple::log::output::format::Value, void, void>
+						{
+						public:
+							typedef BrainMuscles::test::simple::log::output::format::Value	ValueType;
+							typedef BrainMuscles::test::simple::log::output::Set<
+								ValueType, void, void>										BaseType;
+							typedef BrainMuscles::test::simple::log::output::set::Type		SetTypeType;
+						public:
+							Upper();
+						};
+
+						Upper::Upper() :
+							BaseType(SetTypeType::local_value, &ValueType::UpperEnable)
+						{}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif 
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_HANDLE_UPPER_H_
