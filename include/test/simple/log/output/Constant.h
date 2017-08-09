d87df38e64baa5adf48c28fb573b0775267deea3
AnantaYudica <nta040689@gmail.com>
2017-08-09 15:22:55 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Added macro if defined _USING_TEST_  - Add class BrainMuscles::test::simple::log::output::Constant with final inheritance  - Added in class BrainMuscles::test::simple::log::output::Constant    - Delete default constructor    - Add aliases type 'LoopConjunctionDefaultType', 'LoopBeforePrintDefaultType' and 'LoopAfterPrintDefaultType' with const char * for type and public to access    - Add variables static constexpr member public to access      - Add variables 'BooleanLowerString' and 'BooleanUpperString' with array of const char * for type and size is 2      - Add variables 'LoopConjunctionDefault', 'LoopBeforePrintDefault' and 'LoopAfterPrintDefault' with LoopConjunctionDefaultType, LoopBeforePrintDefaultType and LoopAfterPrintDefaultType for type

---
+++ include/test/simple/log/output/Constant.h
@@ -0,0 +1,41 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_CONSTANT_H_
+#define TEST_SIMPLE_LOG_OUTPUT_CONSTANT_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					class Constant final
+					{
+						Constant() = delete;
+					public:
+						typedef const char * LoopConjunctionDefaultType;
+						typedef const char * LoopBeforePrintDefaultType;
+						typedef const char * LoopAfterPrintDefaultType;
+					public:
+						static constexpr const char * BooleanLowerString[2] = { "false", "true" };
+						static constexpr const char * BooleanUpperString[2] = { "FALSE", "TRUE" };
+
+						static constexpr LoopConjunctionDefaultType LoopConjunctionDefault = ", ";
+						static constexpr LoopBeforePrintDefaultType LoopBeforePrintDefault = "[";
+						static constexpr LoopAfterPrintDefaultType LoopAfterPrintDefault = "]";
+					};
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_CONSTANT_H_
