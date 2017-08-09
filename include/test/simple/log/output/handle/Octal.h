1e08a4c7485b98a2c2d85bab5658e01afb0524b1
AnantaYudica <nta040689@gmail.com>
2017-08-09 16:50:42 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Added in macro if defined _USING_TEST_  - Add dependency with test\simple\log\output\format\Value.h  - Add dependency with test\simple\log\output\Arguments.h  - Add dependency with test\simple\log\output\Set.h  - Add class BrainMuscles::test::simple::log::output::handle::Octal  - Added in class BrainMuscles::test::simple::log::output::handle::Octal    - Add base class with BrainMuscles::test::simple::log::output::Set<BrainMuscles::test::simple::log::output::format::Value, void, void> and public inheritance    - Add aliases type with public to access      - Add alias type 'ValueType' with BrainMuscles::test::simple::log::output::format::Value for type      - Add alias type 'BaseType' with BrainMuscles::test::simple::log::output::Set<ValueType, void, void>      - Add alias type 'SetTypeType' with BrainMuscles::test::simple::log::output::set::Type for type      - Add alias type 'ArgumentsType' with parameter template 'TYPE' and BrainMuscles::test::simple::log::output::Arguments<Octal, TYPE> for type    - Add default constructor with public to access    - Add function const member 'operator()' with ArgumentsType<unsigned char>, ArgumentsType<char>, ArgumentsType<unsigned short>, ArgumentsType<short>, ArgumentsType<unsigned int>, ArgumentsType<int>, ArgumentsType<unsigned long>, ArgumentsType<long>, ArgumentsType<unsigned long long> or ArgumentsType<long long> for return type, parameter function 'value' with const unsigned char&, const char&, const unsigned short&, const short&, const unsigned int&, const int&, const unsigned long&, const long&, const unsigned long long& or const long long& for type and public to access

---
+++ include/test/simple/log/output/handle/Octal.h
@@ -0,0 +1,125 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_HANDLE_OCTAL_H_
+#define TEST_SIMPLE_LOG_OUTPUT_HANDLE_OCTAL_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\log\output\format\Value.h"
+
+#include "test\simple\log\output\Arguments.h"
+#include "test\simple\log\output\Set.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace handle
+					{
+						class Octal :
+							public BrainMuscles::test::simple::log::output::Set<
+								BrainMuscles::test::simple::log::output::format::Value, void, void>
+						{
+						public:
+							typedef BrainMuscles::test::simple::log::output::format::Value	ValueType;
+							typedef BrainMuscles::test::simple::log::output::Set<
+								ValueType, void, void>										BaseType;
+							typedef BrainMuscles::test::simple::log::output::set::Type		SetTypeType;
+						public:
+							template<typename TYPE>
+							using ArgumentsType = BrainMuscles::test::simple::log::output::Arguments<
+								Octal, TYPE>;
+						public:
+							Octal();
+						public:
+							ArgumentsType<unsigned char> operator()(const unsigned char& value) const;
+							ArgumentsType<char> operator()(const char& value)  const;
+							ArgumentsType<unsigned short> operator()(const unsigned short& value)  const;
+							ArgumentsType<short> operator()(const short& value)  const;
+							ArgumentsType<unsigned int> operator()(const unsigned int& value)  const;
+							ArgumentsType<int> operator()(const int& value)  const;
+							ArgumentsType<unsigned long> operator()(const unsigned long& value) const;
+							ArgumentsType<long> operator()(const long& value) const;
+							ArgumentsType<unsigned long long> operator()(const unsigned long long& value) const;
+							ArgumentsType<long long> operator()(const long long& value) const;
+						};
+
+						Octal::Octal() :
+							BaseType(SetTypeType::local_value, &ValueType::OctalEnable)
+						{}
+
+						typename Octal::ArgumentsType<unsigned char> 
+							Octal::operator()(const unsigned char& value) const
+						{
+							return ArgumentsType<unsigned char>(value);
+						}
+
+						typename Octal::ArgumentsType<char> 
+							Octal::operator()(const char& value) const
+						{
+							return ArgumentsType<char>(value);
+						}
+
+						typename Octal::ArgumentsType<unsigned short> 
+							Octal::operator()(const unsigned short& value) const
+						{
+							return ArgumentsType<unsigned short>(value);
+						}
+
+						typename Octal::ArgumentsType<short> 
+							Octal::operator()(const short& value) const
+						{
+							return ArgumentsType<short>(value);
+						}
+
+						typename Octal::ArgumentsType<unsigned int> 
+							Octal::operator()(const unsigned int& value) const
+						{
+							return ArgumentsType<unsigned int>(value);
+						}
+
+						typename Octal::ArgumentsType<int> 
+							Octal::operator()(const int& value) const
+						{
+							return ArgumentsType<int>(value);
+						}
+
+						typename Octal::ArgumentsType<unsigned long> 
+							Octal::operator()(const unsigned long& value) const
+						{
+							return ArgumentsType<unsigned long>(value);
+						}
+
+						typename Octal::ArgumentsType<long> 
+							Octal::operator()(const long& value) const
+						{
+							return ArgumentsType<long>(value);
+						}
+
+						typename Octal::ArgumentsType<unsigned long long> 
+							Octal::operator()(const unsigned long long& value) const
+						{
+							return ArgumentsType<unsigned long long>(value);
+						}
+
+						typename Octal::ArgumentsType<long long> 
+							Octal::operator()(const long long& value) const
+						{
+							return ArgumentsType<long long>(value);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_HANDLE_OCTAL_H_
