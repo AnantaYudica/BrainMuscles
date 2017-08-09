baa9485bab0f774511bd062bfbe8c5e3c71f8b94
AnantaYudica <nta040689@gmail.com>
2017-08-09 23:31:40 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Added in macroi if defined _USING_TEST_  - Add dependency with test\simple\log\output\format\Value.h  - Add dependency with test\simple\log\output\format\Width.h  - Add dependency with test\simple\log\output\Arguments.h  - Add dependency with test\simple\log\output\Set.h  - Add class BrainMuscles::test::simple::log::output::handle::Width  - Added in class BrainMuscles::test::simple::log::output::handle::Width    - Add aliases type with public to access      - Add alias type 'WidthType' with BrainMuscles::test::simple::log::output::format::Width for type      - Add alias type 'WidthInputType' with WidthType::WidthInputType for type      - Add alias type 'ValueType' with BrainMuscles::test::simple::log::output::format::Value for type      - Add alias type 'SetType' with BrainMuscles::test::simple::log::output::Set<ValueType, void, const WidthType&> for type      - Add alias type 'SetTypeType' with BrainMuscles::test::simple::log::output::set::Type for type    - Add function const member 'operator()' with public to access, SetType for return type, parameter template 'value' with const WidthInputType&

---
+++ include/test/simple/log/output/handle/Width.h
@@ -0,0 +1,57 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_HANDLE_WIDTH_H_
+#define TEST_SIMPLE_LOG_OUTPUT_HANDLE_WIDTH_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\log\output\format\Value.h"
+#include "test\simple\log\output\format\Width.h"
+
+#include "test\simple\log\output\Arguments.h"
+#include "test\simple\log\output\Set.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace handle
+					{
+						class Width
+						{
+						public:
+							typedef BrainMuscles::test::simple::log::output
+								::format::Width											WidthType;
+							typedef typename WidthType::WidthInputType					WidthInputType;
+							typedef BrainMuscles::test::simple::log::output	
+								::format::Value											ValueType;
+							typedef BrainMuscles::test::simple::log::output::Set<
+								ValueType, void, const WidthType&>						SetType;
+							typedef BrainMuscles::test::simple::log::output::set::Type	SetTypeType;
+						public:
+							SetType operator()(const WidthInputType& value) const;
+						};
+
+						typename Width::SetType
+							Width::operator()(const WidthInputType& value) const
+						{
+							WidthType width;
+							width.SetValue(value);
+							return SetType(SetTypeType::local_value, &ValueType::SetWidth, width);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_HANDLE_WIDTH_H_
