09406a8747169804c83082c546e40ad7596d98c8
AnantaYudica <nta040689@gmail.com>
2017-08-09 14:38:56 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Added macro if defined _USING_TEST_  - Add dependency with test\simple\log\output\Arguments.h  - Add class BrainMuscles::test::simple::log::output::handle::Global with base class BrainMuscles::test::simple::log::output::Arguments<Global, void> and public inheritance

---
+++ include/test/simple/log/output/handle/Global.h
@@ -0,0 +1,34 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_HANDLE_GLOBAL_H_
+#define TEST_SIMPLE_LOG_OUTPUT_HANDLE_GLOBAL_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\log\output\Arguments.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace handle
+					{
+						class Global :
+							public BrainMuscles::test::simple::log::output::Arguments<Global, void>
+						{};
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_HANDLE_GLOBAL_H_
