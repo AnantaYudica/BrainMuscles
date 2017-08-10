6dcc6908e81473feba5ed95210bd003ad6eb2419
AnantaYudica <nta040689@gmail.com>
2017-08-10 15:32:36 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Added in macro if defined _USING_TEST_  - Add dependency with cstdio  - Add dependency with test\simple\log\output\format\Constant.h  - Add dependency with test\simple\log\output\format\Prototype.h  - Add dependency with test\simple\log\output\format\Debug.h  - Add class BrainMuscles::test::simple::log::output::Format  - Added in class BrainMuscles::test::simple::log::output::Format    - Add aliases type with private to access      - Add alias type 'ConstantType' with BrainMuscles::test::simple::log::output::format::Constant for type      - Add alias type 'SizeType' with ConstantType::SizeType for type    - Add aliases type with protected to access      - Add alias type 'FormatStringType' with ConstantType::FormatStringType for type      - Add alias type 'ValueType' with BrainMuscles::test::simple::log::output::format::Value for type    - Add aliases type with public to access      - Add alias type 'PrototypeType' with BrainMuscles::test::simple::log::output::format::Prototype for type      - Add alias type 'SpecifierType' with BrainMuscles::test::simple::log::output::format::Specifier for type      - Add alias type 'LengthType' with BrainMuscles::test::simple::log::output::format::Length for type    - Add variable member 'm_fileOutput' with std::FILE* for type and private to access    - Add constructor with parameter constructor 'file_output' with std::FILE* for type and protected for type    - Add destructor with public to access    - Add functions member with void for return type and protected to access      - Add function 'PrintBeginNewLine' with parameter variadic template ARGS and parameters function 'format' with const char * for type and 'args' with ARGS... for type      - Add function 'PrintNewline'      - Add function 'Print' with parameter variadic template ARGS and parameters function 'format' with const char * for type and 'args' with ARGS... for type      - Add function 'Output' with parameter template ARG and parameters function 'prototype' with PrototypeType for type and 'arg' with ARG for type

---
+++ include/test/simple/log/output/Format.h
@@ -0,0 +1,109 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_FORMAT_H_
+#define TEST_SIMPLE_LOG_OUTPUT_FORMAT_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include <cstdio>
+
+#include "test\simple\log\output\format\Constant.h"
+
+#include "test\simple\log\output\format\Prototype.h"
+
+#include "test\simple\log\output\format\Debug.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					class Format
+					{
+					private:
+						typedef BrainMuscles::test::simple::log::output
+							::format::Constant							ConstantType;
+						typedef typename ConstantType::SizeType			SizeType;
+					protected:
+						typedef typename ConstantType::FormatStringType	FormatStringType;
+					protected:
+						typedef BrainMuscles::test::simple::log
+							::output::format::Value						ValueType;
+					public:
+						typedef BrainMuscles::test::simple::log::output
+							::format::Prototype							PrototypeType;
+						typedef BrainMuscles::test::simple::log::output
+							::format::Specifier							SpecifierType;
+						typedef BrainMuscles::test::simple::log::output
+							::format::Length							LengthType;
+					private:
+						std::FILE* m_fileOutput;
+					protected:
+						Format(std::FILE* file_output);
+					public:
+						~Format();
+					protected:
+						template<typename... ARGS>
+						void PrintBeginNewLine(const char* format, ARGS... args);
+						void PrintNewline();
+						template<typename... ARGS>
+						void Print(const char* format, ARGS... args);
+						template<typename ARG>
+						void Output(PrototypeType prototype, ARG arg);
+					};
+
+					Format::Format(std::FILE* file_output) :
+						m_fileOutput(file_output)
+					{}
+
+					Format::~Format()
+					{}
+
+					template<typename... ARGS>
+					void Format::PrintBeginNewLine(const char* format, ARGS... args)
+					{
+						fprintf(m_fileOutput, format, args...);
+					}
+
+					void Format::PrintNewline()
+					{
+#if defined(_USING_TEST_SIMPLE_LOG_OUTPUT_FORMAT_DEBUG_)
+						BrainMuscles::test::simple::log::output::format::Debug::PrintNewline(m_fileOutput);
+#else
+						fprintf(m_fileOutput, "\n");
+#endif
+					}
+
+					template<typename... ARGS>
+					void Format::Print(const char* format, ARGS... args)
+					{
+#if defined(_USING_TEST_SIMPLE_LOG_OUTPUT_FORMAT_DEBUG_)
+						BrainMuscles::test::simple::log::output::format::Debug::PrintFormat(m_fileOutput, format, args...);
+#else
+						fprintf(m_fileOutput, format, args...);
+#endif
+					}
+
+					template<typename ARG>
+					void Format::Output(PrototypeType prototype, ARG arg)
+					{
+#if defined(_USING_TEST_SIMPLE_LOG_OUTPUT_FORMAT_DEBUG_)
+						BrainMuscles::test::simple::log::output::format::Debug::PrintFormat(m_fileOutput, prototype.GetFormat<ARG>(), arg);
+#else
+						fprintf(m_fileOutput, prototype.GetFormat<ARG>(), arg);
+#endif
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_FORMAT_H_
