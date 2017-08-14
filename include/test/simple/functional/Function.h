b6d4e072f30873dfc92cfd06ea551e073cda3f52
AnantaYudica <nta040689@gmail.com>
2017-08-14 19:16:20 +0700

    ADD FOLDER AND FILE Add folder include\test\simple\functional Add File include\test\simple\functional\Function.h  - Add dependency with test\Configure.h  - Add macro if defined _USING_TEST_    - Add dependency with functional    - Add class BrainMuscles::test::simple::functional::Function      - Add parameter template RETURN_TYPE      - Add parameter variadic template ARGS      - Add base class std::function<RETURN_TYPE(ARGS...)> with public inheritance      - Add aliases type with public to access        - Add alias type 'FunctionType' with Function<RETURN_TYPE, ARGS...> for type        - Add alias type 'PointerFunctionType' with RETURN_TYPE(*PointerFunctionType)(ARGS...) for type        - Add alias type 'BaseType' with std::function<RETURN_TYPE(ARGS...)> for type      - Add default constructor with public to access      - Add fill constructor with parameter constructor 'function' with PointerFunctionType for type and public to access      - Add fill constructor with parameter constructor 'function' with const BaseType& for type and public to access      - Add copy and move constructor with public to access

---
+++ include/test/simple/functional/Function.h
@@ -0,0 +1,64 @@
+#ifndef TEST_SIMPLE_FUNCTIONAL_FUNCTION_H_
+#define TEST_SIMPLE_FUNCTIONAL_FUNCTION_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include <functional>
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace functional
+			{
+				template<typename RETURN_TYPE, typename... ARGS>
+				class Function :
+					public std::function<RETURN_TYPE(ARGS...)>
+				{
+				public:
+					typedef Function<RETURN_TYPE, ARGS...> FunctionType;
+					typedef RETURN_TYPE(*PointerFunctionType)(ARGS...);
+					typedef std::function<RETURN_TYPE(ARGS...)> BaseType;
+				public:
+					Function();
+					Function(PointerFunctionType function);
+					Function(const BaseType& function);
+					Function(const FunctionType& copy);
+					Function(FunctionType&& move);
+				};
+
+				template<typename RETURN_TYPE, typename... ARGS>
+				Function<RETURN_TYPE, ARGS...>::Function()
+				{}
+
+				template<typename RETURN_TYPE, typename... ARGS>
+				Function<RETURN_TYPE, ARGS...>::Function(PointerFunctionType function) :
+					BaseType(function)
+				{}
+
+				template<typename RETURN_TYPE, typename... ARGS>
+				Function<RETURN_TYPE, ARGS...>::Function(const BaseType& function) :
+					BaseType(function)
+				{}
+
+				template<typename RETURN_TYPE, typename... ARGS>
+				Function<RETURN_TYPE, ARGS...>::Function(const FunctionType& copy) :
+					BaseType(static_cast<BaseType&>(const_cast<FunctionType&>(copy)))
+				{}
+
+				template<typename RETURN_TYPE, typename... ARGS>
+				Function<RETURN_TYPE, ARGS...>::Function(FunctionType&& move) :
+					BaseType(static_cast<BaseType&&>(move))
+				{}
+			}
+		}
+	}
+}
+
+#endif 
+
+#endif //!TEST_SIMPLE_FUNCTIONAL_FUNCTION_H_
