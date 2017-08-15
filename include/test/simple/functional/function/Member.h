5e9c22cf0550cd676aa77fe8c59475d1d0d06123
AnantaYudica <nta040689@gmail.com>
2017-08-15 23:49:06 +0700

    ADD FOLDER AND FILE Add folder include\test\simple\functional\function Add file  include\test\simple\functional\function\Member.h  - Add dependency with test\Configure.h  - Add macro if defined _USING_TEST_    - Add dependency with test\simple\functional\Function.h    - Add primary class BrainMuscles::test::simple::functional::function::Member      - Add parameters template 'RETURN_TYPE' and 'OBJECT_TYPE'      - Add parameter variadic template 'ARGS'      - Add base class BrainMuscles::test::simple::functional::Function<RETURN_TYPE, OBJECT_TYPE*, ARGS...> with public inheritance      - Add aliases type with public to access        - Add alias type 'ObjectType' with OBJECT_TYPE for type        - Add alias type 'ObjectPtrType' with ObjectType* for type        - Add alias type 'MemberType' with Member<RETURN_TYPE, ObjectType, ARGS...>        - Add alias type 'BaseType' with BrainMuscles::test::simple::functional::Function<RETURN_TYPE, ObjectPtrType, ARGS...> for type        - Add alias type 'FunctionMemberType' with RETURN_TYPE(OBJECT_TYPE::*)(ARGS...) for type      - Add variable member 'm_object' with private to access and ObjectPtrType for type      - Add default constructor with public to access      - Add fill constructor with public to access and parameters constructor 'function_member' with FunctionMemberType for type and 'object' with ObjectPtrType for type      - Add copy constructor with public to access      - Add move constructor with public to access      - Add function const member 'operator()' with RETURN_TYPE for return type and parameter 'args' with ARGS... for type    - Add specialize class BrainMuscles::test::simple::functional::function::Member where OBJECT_TYPE = const OBJECT_TYPE       - Add alias type 'FunctionMemberType' with RETURN_TYPE(OBJECT_TYPE::*)(ARGS...) const       - Other Add member same with primary class where OBJECT_TYPE = const OBJECT_TYPE    - Add specialize class BrainMuscles::test::simple::functional::function::Member where OBJECT_TYPE = volatile OBJECT_TYPE       - Add alias type 'FunctionMemberType' with RETURN_TYPE(OBJECT_TYPE::*)(ARGS...) volatile       - Other Add member same with primary class where OBJECT_TYPE = volatile OBJECT_TYPE    - Add specialize class BrainMuscles::test::simple::functional::function::Member where OBJECT_TYPE = const volatile OBJECT_TYPE       - Add alias type 'FunctionMemberType' with RETURN_TYPE(OBJECT_TYPE::*)(ARGS...) const volatile       - Other Add member same with primary class where OBJECT_TYPE = const volatile OBJECT_TYPE

---
+++ include/test/simple/functional/function/Member.h
@@ -0,0 +1,239 @@
+#ifndef TEST_SIMPLE_FUNCTIONAL_FUNCTION_MEMBER_H_
+#define TEST_SIMPLE_FUNCTIONAL_FUNCTION_MEMBER_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\functional\Function.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace functional
+			{
+				namespace function
+				{
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					class Member :
+						public BrainMuscles::test::simple::functional::Function<RETURN_TYPE, OBJECT_TYPE*, ARGS...>
+					{
+					public:
+						typedef OBJECT_TYPE ObjectType;
+						typedef ObjectType* ObjectPtrType;
+						typedef Member<RETURN_TYPE, ObjectType, ARGS...> MemberType;
+						typedef BrainMuscles::test::simple::functional::
+							Function<RETURN_TYPE, ObjectPtrType, ARGS...> BaseType;
+						typedef RETURN_TYPE(OBJECT_TYPE::*FunctionMemberType)(ARGS...);
+					private:
+						ObjectPtrType m_object;
+					public:
+						Member();
+						Member(FunctionMemberType function_member, ObjectPtrType object);
+						Member(const MemberType& copy);
+						Member(MemberType&& move);
+					public:
+						RETURN_TYPE operator()(ARGS... args) const;
+					};
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					class Member<RETURN_TYPE, const OBJECT_TYPE, ARGS...> :
+						public BrainMuscles::test::simple::functional::Function<RETURN_TYPE, const OBJECT_TYPE*, ARGS...>
+					{
+					public:
+						typedef const OBJECT_TYPE ObjectType;
+						typedef ObjectType* ObjectPtrType;
+						typedef Member<RETURN_TYPE, ObjectType, ARGS...> MemberType;
+						typedef BrainMuscles::test::simple::functional::
+							Function<RETURN_TYPE, ObjectPtrType, ARGS...> BaseType;
+						typedef RETURN_TYPE(OBJECT_TYPE::*FunctionMemberType)(ARGS...) const;
+					private:
+						ObjectPtrType m_object;
+					public:
+						Member();
+						Member(FunctionMemberType function_member, ObjectPtrType object);
+						Member(const MemberType& copy);
+						Member(MemberType&& move);
+					public:
+						RETURN_TYPE operator()(ARGS... args) const;
+					};
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					class Member<RETURN_TYPE, volatile OBJECT_TYPE, ARGS...> :
+						public BrainMuscles::test::simple::functional::Function<RETURN_TYPE, volatile OBJECT_TYPE*, ARGS...>
+					{
+					public:
+						typedef volatile OBJECT_TYPE ObjectType;
+						typedef ObjectType* ObjectPtrType;
+						typedef Member<RETURN_TYPE, ObjectType, ARGS...> MemberType;
+						typedef BrainMuscles::test::simple::functional::
+							Function<RETURN_TYPE, ObjectPtrType, ARGS...> BaseType;
+						typedef RETURN_TYPE(OBJECT_TYPE::*FunctionMemberType)(ARGS...) volatile;
+					private:
+						ObjectPtrType m_object;
+					public:
+						Member();
+						Member(FunctionMemberType function_member, ObjectPtrType object);
+						Member(const MemberType& copy);
+						Member(MemberType&& move);
+					public:
+						RETURN_TYPE operator()(ARGS... args) const;
+					};
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					class Member<RETURN_TYPE, const volatile OBJECT_TYPE, ARGS...> :
+						public BrainMuscles::test::simple::functional::Function<RETURN_TYPE, const volatile OBJECT_TYPE*, ARGS...>
+					{
+					public:
+						typedef const volatile OBJECT_TYPE ObjectType;
+						typedef ObjectType* ObjectPtrType;
+						typedef Member<RETURN_TYPE, ObjectType, ARGS...> MemberType;
+						typedef BrainMuscles::test::simple::functional::
+							Function<RETURN_TYPE, ObjectPtrType, ARGS...> BaseType;
+						typedef RETURN_TYPE(OBJECT_TYPE::*FunctionMemberType)(ARGS...) const volatile;
+					private:
+						ObjectPtrType m_object;
+					public:
+						Member();
+						Member(FunctionMemberType function_member, ObjectPtrType object);
+						Member(const MemberType& copy);
+						Member(MemberType&& move);
+					public:
+						RETURN_TYPE operator()(ARGS... args) const;
+					};
+
+					///////////////////////////////////////////////////////////////////////////////////////
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					Member<RETURN_TYPE, OBJECT_TYPE, ARGS...>::Member() :
+						m_object(0)
+					{}
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					Member<RETURN_TYPE, OBJECT_TYPE, ARGS...>::Member(FunctionMemberType function_member, ObjectPtrType object) :
+						BaseType(function_member),
+						m_object(object)
+					{}
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					Member<RETURN_TYPE, OBJECT_TYPE, ARGS...>::Member(const MemberType& copy) :
+						BaseType(copy),
+						m_object(copy.m_object)
+					{}
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					Member<RETURN_TYPE, OBJECT_TYPE, ARGS...>::Member(MemberType&& move) :
+						BaseType(move),
+						m_object(move.m_object)
+					{}
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					RETURN_TYPE Member<RETURN_TYPE, OBJECT_TYPE, ARGS...>::operator()(ARGS... args) const
+					{
+						return BaseType::operator()(m_object, args...);
+					}
+
+					///////////////////////////////////////////////////////////////////////////////////////
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					Member<RETURN_TYPE, const OBJECT_TYPE, ARGS...>::Member() :
+						m_object(0)
+					{}
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					Member<RETURN_TYPE, const OBJECT_TYPE, ARGS...>::Member(FunctionMemberType function_member, ObjectPtrType object) :
+						BaseType(function_member),
+						m_object(object)
+					{}
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					Member<RETURN_TYPE, const OBJECT_TYPE, ARGS...>::Member(const MemberType& copy) :
+						BaseType(copy),
+						m_object(copy.m_object)
+					{}
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					Member<RETURN_TYPE, const OBJECT_TYPE, ARGS...>::Member(MemberType&& move) :
+						BaseType(move),
+						m_object(move.m_object)
+					{}
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					RETURN_TYPE Member<RETURN_TYPE, const OBJECT_TYPE, ARGS...>::operator()(ARGS... args) const
+					{
+						return BaseType::operator()(m_object, args...);
+					}
+
+					///////////////////////////////////////////////////////////////////////////////////////
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					Member<RETURN_TYPE, volatile OBJECT_TYPE, ARGS...>::Member() :
+						m_object(0)
+					{}
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					Member<RETURN_TYPE, volatile OBJECT_TYPE, ARGS...>::Member(FunctionMemberType function_member, ObjectPtrType object) :
+						BaseType(function_member),
+						m_object(object)
+					{}
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					Member<RETURN_TYPE, volatile OBJECT_TYPE, ARGS...>::Member(const MemberType& copy) :
+						BaseType(copy),
+						m_object(copy.m_object)
+					{}
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					Member<RETURN_TYPE, volatile OBJECT_TYPE, ARGS...>::Member(MemberType&& move) :
+						BaseType(move),
+						m_object(move.m_object)
+					{}
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					RETURN_TYPE Member<RETURN_TYPE, volatile OBJECT_TYPE, ARGS...>::operator()(ARGS... args) const
+					{
+						return BaseType::operator()(m_object, args...);
+					}
+
+					///////////////////////////////////////////////////////////////////////////////////////
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					Member<RETURN_TYPE, const volatile OBJECT_TYPE, ARGS...>::Member() :
+						m_object(0)
+					{}
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					Member<RETURN_TYPE, const volatile OBJECT_TYPE, ARGS...>::Member(FunctionMemberType function_member, ObjectPtrType object) :
+						BaseType(function_member),
+						m_object(object)
+					{}
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					Member<RETURN_TYPE, const volatile OBJECT_TYPE, ARGS...>::Member(const MemberType& copy) :
+						BaseType(copy),
+						m_object(copy.m_object)
+					{}
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					Member<RETURN_TYPE, const volatile OBJECT_TYPE, ARGS...>::Member(MemberType&& move) :
+						BaseType(move),
+						m_object(move.m_object)
+					{}
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					RETURN_TYPE Member<RETURN_TYPE, const volatile OBJECT_TYPE, ARGS...>::operator()(ARGS... args) const
+					{
+						return BaseType::operator()(m_object, args...);
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_FUNCTIONAL_FUNCTION_MEMBER_H_
