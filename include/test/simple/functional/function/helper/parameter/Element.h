2973b205cc9b660bc9d8dcf0e3ad1ea1101bb6f3
AnantaYudica <nta040689@gmail.com>
2017-08-19 03:42:20 +0700

    ADD FILE AND FOLDER Add folder include\test\simple\functional\function\helper Add folder include\test\simple\functional\function\helper\parameter Add File include\test\simple\functional\function\helper\parameter\Element.h Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with test\simple\functional\Function.h  - Add forward declaration primary class BrainMuscles::test::simple::functional::function::helper::parameter::Element    - Add parameters template 'INDEX' with std::size_t for type and 'FUNCTION_TYPE'  - Add template specialize with INDEX = INDEX and FUNCTION_TYPE = BrainMuscles::test::simple::functional::Function<RETURN_TYPE, FUNCTION_ARG, FUNCTION_ARGS...>    - Add parameters template 'INDEX' with std::size_t for type, 'RETURN_TYPE' and 'FUNCTION_ARG'    - Add parameter variadic template 'FUNCTION_ARGS'    - Add base class with Element<INDEX - 1, BrainMuscles::test::simple::functional::Function<RETURN_TYPE, FUNCTION_ARGS...>>    - Delete default constructor    - Add alias type 'Type' with Element<INDEX - 1, BrainMuscles::test::simple::functional::Function<RETURN_TYPE, FUNCTION_ARGS...>>::Type for type  - Add template specialize with INDEX = 0 and FUNCTION_TYPE = BrainMuscles::test::simple::functional::Function<RETURN_TYPE, FUNCTION_ARG, FUNCTION_ARGS...>    - Add parameters template RETURN_TYPE and FUNCTION_ARGS    - Add parameter variadic template 'FUNCTION_ARGS'    - Delete default constructor    - Add alias 'Type' with FUNCTION_ARG for type

---
+++ include/test/simple/functional/function/helper/parameter/Element.h
@@ -0,0 +1,53 @@
+#ifndef TEST_SIMPLE_FUNCTIONAL_FUNCTION_HELPER_PARAMETER_ELEMENT_H_
+#define TEST_SIMPLE_FUNCTIONAL_FUNCTION_HELPER_PARAMETER_ELEMENT_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\functional\Function.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace functional
+			{
+				namespace function
+				{
+					namespace helper
+					{
+						namespace parameter
+						{
+							template<std::size_t INDEX, typename FUNCTION_TYPE>
+							class Element;
+
+							template<std::size_t INDEX, typename RETURN_TYPE, typename FUNCTION_ARG, typename... FUNCTION_ARGS>
+							class Element<INDEX, BrainMuscles::test::simple::functional::Function<RETURN_TYPE, FUNCTION_ARG, FUNCTION_ARGS...>> :
+								public Element<INDEX - 1, BrainMuscles::test::simple::functional::Function<RETURN_TYPE, FUNCTION_ARGS...>>
+							{
+								Element() = delete;
+							public:
+								typedef typename Element<INDEX - 1, BrainMuscles::test::simple::functional::Function<RETURN_TYPE, FUNCTION_ARGS...>>::Type Type;
+							};
+
+							template<typename RETURN_TYPE, typename FUNCTION_ARG, typename... FUNCTION_ARGS>
+							class Element<0, BrainMuscles::test::simple::functional::Function<RETURN_TYPE, FUNCTION_ARG, FUNCTION_ARGS...>>
+							{
+								Element() = delete;
+							public:
+								typedef FUNCTION_ARG Type;
+							};
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_FUNCTIONAL_FUNCTION_HELPER_PARAMETER_ELEMENT_H_
