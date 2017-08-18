0260b44045a77234f880491e41d70e8705baf430
AnantaYudica <nta040689@gmail.com>
2017-08-19 04:00:54 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with test\simple\functional\Function.h  - Add forward declaration primary class BrainMuscles::test::simple::functional::function::helper::parameter::Size    - Add parameter template 'FUNCTION_TYPE'  - Add specialize template class BrainMuscles::test::simple::functional::function::helper::parameter::Size with FUNCTION_TYPE = BrainMuscles::test::simple::functional::Function<RETURN_TYPE, FUNCTION_ARGS...>    - Add parameter template 'RETURN_TYPE'    - Add parameter variadic template 'FUNCTION_ARGS'    - Delete default constructor    - Add static constexpr 'Value' with std::size_t for type and  sizeof...(FUNCTION_ARGS) for value

---
+++ include/test/simple/functional/function/helper/parameter/Size.h
@@ -0,0 +1,44 @@
+#ifndef TEST_SIMPLE_FUNCTIONAL_FUNCTION_HELPER_PARAMETER_SIZE_H_
+#define TEST_SIMPLE_FUNCTIONAL_FUNCTION_HELPER_PARAMETER_SIZE_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\functional\Function.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace functional
+			{
+				namespace function
+				{
+					namespace helper
+					{
+						namespace parameter
+						{
+							template<typename FUNCTION_TYPE>
+							class Size;
+
+							template<typename RETURN_TYPE, typename... FUNCTION_ARGS>
+							class Size<BrainMuscles::test::simple::functional::Function<RETURN_TYPE, FUNCTION_ARGS...>>
+							{
+								Size() = delete;
+							public:
+								static constexpr std::size_t Value = sizeof...(FUNCTION_ARGS);
+							};
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_FUNCTIONAL_FUNCTION_HELPER_PARAMETER_SIZE_H_
