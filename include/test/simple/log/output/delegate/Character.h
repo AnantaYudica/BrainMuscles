ddeadd594cb0d5ba0f521115326a55dc2a4bedfd
AnantaYudica <nta040689@gmail.com>
2017-08-21 15:31:22 +0700

    MODIFIED FILE Update macro ifndef and define header Remove dependencies with test\simple\log\output\Arguments.h and test\simple\log\output\Set.h Add dependency with test\simple\log\output\call\value\Tag.h Add dependency with test\simple\log\output\call\Value.h Add dependency with test\simple\log\output\call\Handle.h Update namespace from handle to delegate Update in class BrainMuscles::test::simple::log::output::delegate::Character  - Update Base class with BrainMuscles::test::simple::log::output::call::Value  - Update aliases type    - Update alias type 'BaseType' with BrainMuscles::test::simple::log::output::call::Value for type    - Remove alias type 'SetType'    - Remove alias type 'ValueType'    - Remove alias type 'ArgumentsType'  - Add aliases type with public to access    - Add alias type 'ValueTagType' with BrainMuscles::test::simple::log::output::call::value::Tag for type    - Add alias type 'FormatValueType' with BrainMuscles::test::simple::log::output::format::Value for type    - Add alias type 'FunctionMemberFormatValueType' with BrainMuscles::test::simple::functional::Function<void, FormatValueType*> for type    - Add alias type 'HandleType' with BrainMuscles::test::simple::log::output::Handle for type    - Add alias type 'FunctionMemberHandlePrintType' with BrainMuscles::test::simple::functional::Function<void, HandleType*, const char*, int> for type    - Add alias type 'CallHandleType' with BrainMuscles::test::simple::log::output::call::Handle<> for type  - Update function member 'operator()' with CallHandleType for return type

--- include/test/simple/log/output/delegate/Character.h
+++ include/test/simple/log/output/delegate/Character.h
@@ -1,5 +1,5 @@
-#ifndef TEST_SIMPLE_LOG_OUTPUT_HANDLE_CHARACTER_H_
-#define TEST_SIMPLE_LOG_OUTPUT_HANDLE_CHARACTER_H_
+#ifndef TEST_SIMPLE_LOG_OUTPUT_DELEGATE_CHARACTER_H_
+#define TEST_SIMPLE_LOG_OUTPUT_DELEGATE_CHARACTER_H_
 
 #include "test\Configure.h"
 
@@ -7,8 +7,9 @@
 
 #include "test\simple\log\output\format\Value.h"
 
-#include "test\simple\log\output\Arguments.h"
-#include "test\simple\log\output\Set.h"
+#include "test\simple\log\output\call\value\Tag.h"
+#include "test\simple\log\output\call\Value.h"
+#include "test\simple\log\output\call\Handle.h"
 
 namespace BrainMuscles
 {
@@ -20,33 +21,39 @@ namespace BrainMuscles
 			{
 				namespace output
 				{
-					namespace handle
+					namespace delegate
 					{
 						class Character :
-							public BrainMuscles::test::simple::log::output::Set<
-								BrainMuscles::test::simple::log::output::format::Value, void, void>
+							public BrainMuscles::test::simple::log::output::call::Value
 						{
 						public:
-							typedef BrainMuscles::test::simple::log::output::Set<
-								BrainMuscles::test::simple::log::output::format
-								::Value, void, void>									BaseType;
-							typedef BrainMuscles::test::simple::log::output::set::Type	SetTypeType;
-							typedef BrainMuscles::test::simple::log::output::format
-								::Value													ValueType;
-							typedef BrainMuscles::test::simple::log::output::Arguments<
-								Character, int>											ArgumentsType;
+							typedef BrainMuscles::test::simple::log::output::call::Value		BaseType;
+							typedef BrainMuscles::test::simple::log::output::call::value::Tag	ValueTagType;
+							typedef BrainMuscles::test::simple::log::output::format::Value		FormatValueType;
+							typedef BrainMuscles::test::simple::functional::Function<void,
+								FormatValueType*>												FunctionMemberFormatValueType;
+							
+							typedef BrainMuscles::test::simple::log::output::Handle				HandleType;
+							typedef BrainMuscles::test::simple::functional::Function<void,
+								HandleType*, const char*, int>									FunctionMemberHandlePrintType;
+
+							typedef BrainMuscles::test::simple::log::output::call::Handle<>		CallHandleType;
 						public:
 							Character();
-							ArgumentsType operator()(const int& character) const;
+							CallHandleType operator()(const int& character) const;
 						};
 
 						Character::Character() :
-							BaseType(SetTypeType::local_value, &ValueType::CharacterEnable)
+							BaseType(ValueTagType::local_value, std::bind(&FormatValueType::CharacterEnable, std::placeholders::_1))
 						{}
 
-						typename Character::ArgumentsType Character::operator()(const int& character) const
+						typename Character::CallHandleType Character::operator()(const int& character) const
 						{
-							return ArgumentsType(character);
+							FunctionMemberFormatValueType functionMemberFormatValue = std::bind(&FormatValueType::CharacterEnable, std::placeholders::_1);
+							FunctionMemberHandlePrintType functionMemberHandlePrint = std::bind(&HandleType::Print<char>, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
+
+							return std::bind(static_cast<void(HandleType::*)(FunctionMemberHandlePrintType, FunctionMemberFormatValueType, int)>
+								(&HandleType::PrintDelegate), std::placeholders::_1, functionMemberHandlePrint, functionMemberFormatValue, character);
 						}
 					}
 				}
