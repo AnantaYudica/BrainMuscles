0f26febe815be4faa6354608a12d4014a1080759
AnantaYudica <nta040689@gmail.com>
2017-08-21 14:56:29 +0700

    ADD FILE AND FOLDER Add folder call Add folder call\value Add file call\value\Tag.h  - Add dependency with test\Configure.h  - Add macro if defined _USING_TEST_    - Add enum BrainMuscles::test::simple::log::output::call::value::Tag with unsigned char for type      - Add variable member 'local_value' with value is 1      - Add variable member 'global_value' with value is 1 << 1      - Add variable member 'local_and_global_value' with value is (local_value | global_value)    - Add function 'operator==' with parameters function 'a' with Tag for type and 'b' with Tag for type and bool for return type    - Add function 'operator!=' with parameters function 'a' with Tag for type and 'b' with Tag for type and bool for return type

---
+++ include/test/simple/log/output/call/value/Tag.h
@@ -0,0 +1,48 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_CALL_VALUE_TAG_H_
+#define TEST_SIMPLE_LOG_OUTPUT_CALL_VALUE_TAG_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace call
+					{
+						namespace value
+						{
+							enum Tag : unsigned char
+							{
+								local_value = 1,
+								global_value = 1 << 1,
+								local_and_global_value = (local_value | global_value)
+							};
+
+							bool operator==(Tag a, Tag b)
+							{
+								return (a & b);
+							}
+
+							bool operator!=(Tag a, Tag b)
+							{
+								return !(operator==(a, b));
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_CALL_VALUE_TAG_H_
