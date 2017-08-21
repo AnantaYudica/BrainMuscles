4e060539e15f1b2f3687e5615f708840370a423e
AnantaYudica <nta040689@gmail.com>
2017-08-21 14:44:05 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with test\simple\functional\Function.h  - Add dependency with test\simple\log\output\Signal.h  - Add dependency with test\simple\log\output\format\Value.h  - Add dependency with test\simple\log\output\Format.h  - Add dependency with test\simple\log\output\call\Value.h  - Add class BrainMuscles::test::simple::log::output::Handle    - Add base class with BrainMuscles::test::simple::log::output::Format and public inheritance    - Add aliases type      - with public to access        - Add alias type 'BaseType' with BrainMuscles::test::simple::log::output::Format for type        - Add alias type 'FormatValueType' with BrainMuscles::test::simple::log::output::format::Value for type        - Add alias type 'HandleType' with BrainMuscles::test::simple::log::output::Handle for type        - Add alias type 'CallValueType' with BrainMuscles::test::simple::log::output::call::Value        - Add alias type 'FunctionPrintType' with parameter variadic template 'ARGS' and BrainMuscles::test::simple::functional::Function<void, HandleType*, ARGS...> for type        - Add alias type 'FunctionMemberFormatValueType' with parameter variadic template 'ARGS' and BrainMuscles::test::simple::functional::Function<void, FormatValueType*, ARGS...>      - with protected to access        - Add alias type 'SignalType' with BrainMuscles::test::simple::log::output::Signal for type    - Add variables member with private to access      - Add variables member 'm_globalValue' and 'm_outputValue' with ValueType for type      - Add variable member 'm_outputSignal' with SignalType for type    - Add default constructor with public to access    - Add abstract destructor with oublic to access    - Add abstract function member 'OnBeginPrintOutput' and 'OnEndPrintOutput' with void for return type and protected to access    - Add functions member      - with public to access        - Add functions member 'SetGlobalValue', 'SetLocalValue' and 'EndPrintOutput' with void for return type        - Add function member 'PrintOutput' with parameter template 'ARG', parameter function 'arg' with ARG for type and void for return type        - Add function member 'PrintDelegate' with parameter function 'function_print' with FunctionPrintType<> for return type        - Add function member 'PrintDelegate' with parameter template 'ARG', parameters function 'function_print' with FunctionPrintType<const char*, ARG> for type, 'function_member_value' with FunctionMemberFormatValueType<> for type and 'value' with ARG for type and void to return type        - Add function member 'Print' with parameter variadic template 'ARGS', parameters function 'format' with const char* for type and 'args' with ARGS... for type and void for return type        - Add function member 'PrintLoop' with parameters template 'ITERATOR_TYPE', 'CONJUNCTION_TYPE' with ConstantType::LoopConjunctionDefaultType for default type, 'BEFORE_PRINT_TYPE' with ConstantType::LoopBeforePrintDefaultType for default type, 'AFTER_PRINT_TYPE' with ConstantType::LoopAfterPrintDefaultType for default type, parameters function 'iterator_begin' with ITERATOR_TYPE for type, 'iterator_end' with ITERATOR_TYPE for type, 'conjunction' with CONJUNCTION_TYPE for type and ConstantType::LoopConjunctionDefault for default value, 'before_print' with BEFORE_PRINT_TYPE for type and ConstantType::LoopBeforePrintDefault for default value and 'after_print' with AFTER_PRINT_TYPE for type and ConstantType::LoopAfterPrintDefault for default value and void for return type        - Add function member 'operator<<' with parameter function 'call_value' with const CallValueType& for type and Handle& for return type      - with protected to access        - Add functions member 'BeginPrintOutput' and 'NewlineOutput' with void for return type

---
+++ include/test/simple/log/output/Handle.h
@@ -0,0 +1,228 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_HANDLE_H_
+#define TEST_SIMPLE_LOG_OUTPUT_HANDLE_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\functional\Function.h"
+
+#include "test\simple\log\output\Signal.h"
+#include "test\simple\log\output\format\Value.h"
+#include "test\simple\log\output\Format.h"
+#include "test\simple\log\output\call\Value.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					class Handle :
+						public BrainMuscles::test::simple::log::output::Format
+					{
+					public:
+						typedef BrainMuscles::test::simple::log::output::Format			BaseType;
+						typedef BrainMuscles::test::simple::log::output::format::Value	FormatValueType;
+						typedef BrainMuscles::test::simple::log::output::Handle			HandleType;
+						typedef BrainMuscles::test::simple::log::output::call::Value	CallValueType;
+					protected:
+						typedef BrainMuscles::test::simple::log::output::Signal			SignalType;
+					public:
+						template<typename... ARGS>
+						using FunctionPrintType = BrainMuscles::test::simple::functional::Function<void, 
+							HandleType*, ARGS...>;
+					public:
+						template<typename... ARGS>
+						using FunctionMemberFormatValueType = BrainMuscles::test::simple::functional::Function<
+							void, FormatValueType*, ARGS...>;
+					private:
+						ValueType m_globalValue;
+						ValueType m_outputValue;
+						SignalType m_outputSignal;
+					protected:
+						Handle();
+					public:
+						virtual ~Handle();
+					protected:
+						virtual void OnBeginPrintOutput(std::FILE* outfile) = 0;
+						virtual void OnEndPrintOutput(std::FILE* outfile) = 0;
+					public:
+						void SetValueFormat(const CallValueType& call_value);
+					public:
+						void SetGlobalValue();
+						void SetLocalValue();
+					protected:
+						void BeginPrintOutput();
+						void NewlineOutput();
+					public:
+						void EndPrintOutput();
+					public:
+						template<typename ARG>
+						void PrintOutput(ARG arg);
+					public:
+						void PrintDelegate(FunctionPrintType<> function_print);
+						template<typename ARG>
+						void PrintDelegate(FunctionPrintType<const char*, ARG> function_print, FunctionMemberFormatValueType<> function_member_value, ARG value);
+					public:
+						template<typename... ARGS>
+						void Print(const char* format, ARGS... args);
+					public:
+						template<typename ITERATOR_TYPE,
+							typename CONJUNCTION_TYPE = ConstantType::LoopConjunctionDefaultType,
+							typename BEFORE_PRINT_TYPE = ConstantType::LoopBeforePrintDefaultType,
+							typename AFTER_PRINT_TYPE = ConstantType::LoopAfterPrintDefaultType>
+						void PrintLoop(ITERATOR_TYPE iterator_begin,
+							ITERATOR_TYPE iterator_end,
+							CONJUNCTION_TYPE conjunction = ConstantType::LoopConjunctionDefault,
+							BEFORE_PRINT_TYPE before_print = ConstantType::LoopBeforePrintDefault,
+							AFTER_PRINT_TYPE after_print = ConstantType::LoopAfterPrintDefault);
+					public:
+						Handle& operator<< (const CallValueType& call_value);
+					};
+
+					void Handle::SetValueFormat(const CallValueType& call_value)
+					{
+						if (call_value.IsSetGlobalValue())
+						{
+							call_value(&m_globalValue);
+						}
+						if (call_value.IsSetLocalValue())
+						{
+							call_value(&m_outputValue);
+						}
+					}
+
+					void Handle::SetGlobalValue()
+					{
+						m_globalValue = m_outputValue;
+					}
+
+					void Handle::SetLocalValue()
+					{
+						m_outputValue = m_globalValue;
+					}
+
+					void Handle::BeginPrintOutput()
+					{
+						if (m_outputSignal.IsPrintEnd())
+						{
+							SetLocalValue();
+							m_outputSignal.PrintStartSignal();
+						}
+						if (m_outputSignal.IsOutputEnd() || m_outputSignal.IsNewline())
+						{
+							OnBeginPrintOutput(m_fileOutput);
+							if (m_outputSignal.IsOutputEnd())
+							{
+								m_outputSignal.OutputStartSignal();
+							}
+							if (m_outputSignal.IsNewline())
+							{
+								m_outputSignal.CurrentlineSignal();
+							}
+						}
+					}
+
+					void Handle::NewlineOutput()
+					{
+						if (!m_outputSignal.IsNewline())
+						{
+							OnEndPrintOutput(m_fileOutput);
+							m_outputSignal.NewlineSignal();
+						}
+					}
+
+					void Handle::EndPrintOutput()
+					{
+						if (!m_outputSignal.IsOutputEnd())
+						{
+							OnEndPrintOutput(m_fileOutput);
+							m_outputSignal.OutputEndSignal();
+						}
+					}
+
+					template<typename ARG>
+					void Handle::PrintOutput(ARG arg)
+					{
+						BeginPrintOutput();
+						BaseType::Output(PrototypeType(m_outputValue), arg);
+						if (!m_outputSignal.IsPrintLoop())
+						{
+							m_outputSignal.PrintEndSignal();
+						}
+					}
+
+					void Handle::PrintDelegate(FunctionPrintType<> function_print)
+					{
+						m_outputSignal.PassiveNewlineSignal();
+						function_print(this);
+					}
+
+					template<typename ARG>
+					void Handle::PrintDelegate(FunctionPrintType<const char*, ARG> function_print, FunctionMemberFormatValueType<> function_member_value, ARG value)
+					{
+						m_outputSignal.PassiveNewlineSignal();
+						PrototypeType proto(m_outputValue);
+						function_member_value(&proto.GetValue());
+						function_print(this, proto.GetFormat<ARG>(), value);
+					}
+
+					template<typename... ARGS>
+					void Handle::Print(const char* format, ARGS... args)
+					{
+						if (m_outputSignal.IsForceNewline())
+						{
+							NewlineOutput();
+							BeginPrintOutput();
+							BaseType::Print(format, args...);
+							NewlineOutput();
+						}
+						else
+						{
+							BeginPrintOutput();
+							BaseType::Print(format, args...);
+							m_outputSignal.ForceNewlineSignal();
+						}
+					}
+
+					template<typename ITERATOR_TYPE, typename CONJUNCTION_TYPE, typename BEFORE_PRINT_TYPE, typename AFTER_PRINT_TYPE>
+					void Handle::PrintLoop(ITERATOR_TYPE iterator_begin, ITERATOR_TYPE iterator_end, CONJUNCTION_TYPE conjunction, BEFORE_PRINT_TYPE before_print, AFTER_PRINT_TYPE after_print)
+					{
+						BeginPrintOutput();
+						m_outputSignal.BeginPrintLoopSignal();
+						*this << before_print;
+						bool reverse = itrator_begin > iterator_end;
+						ITERATOR_TYPE it = itrator_begin;
+						while ((reverse ? it > iterator_end : it < iterator_end))
+						{
+							if (it != itrator_begin)
+							{
+								*this << conjunction;
+							}
+							*this << *it;
+							(reverse ? --it : ++it);
+						}
+						*this << after_print;
+						m_outputSignal.EndPrintLoopSignal();
+						m_outputSignal.PrintEndSignal();
+					}
+
+					Handle& Handle::operator<< (const CallValueType& call_value)
+					{
+						SetValueFormat(call_value);
+						return *this;
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_HANDLE_H_
