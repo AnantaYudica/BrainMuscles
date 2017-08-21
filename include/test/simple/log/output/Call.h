3a190c0dc98844b52cac163c0f5a3fe1860838a8
AnantaYudica <nta040689@gmail.com>
2017-08-21 14:17:34 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with test\simple\functional\Function.h  - Add primary class template BrainMuscles::test::simple::log::output::Call    - Add parameter template 'FUNCTION_TYPE'  - Add specialize class template template BrainMuscles::test::simple::log::output::Call with FUNCTION_TYPE = BrainMuscles::test::simple::functional::Function<RETURN_TYPE, OBJECT_TYPE, ARGS...>    - Add parameters template 'RETURN_TYPE' and 'OBJECT_TYPE'    - Add parameter variadic template 'ARGS'    - Add base class with BrainMuscles::test::simple::functional::Function<RETURN_TYPE, OBJECT_TYPE, ARGS...> and public inheritance    - Add alias type 'FunctionType' with BrainMuscles::test::simple::functional::Function<RETURN_TYPE, OBJECT_TYPE, ARGS...> for type    - Add default constructor with public to access

---
+++ include/test/simple/log/output/Call.h
@@ -0,0 +1,47 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_CALL_H_
+#define TEST_SIMPLE_LOG_OUTPUT_CALL_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\functional\Function.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					template<typename FUNCTION_TYPE>
+					class Call;
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					class Call<BrainMuscles::test::simple::functional::Function<RETURN_TYPE, OBJECT_TYPE, ARGS...>> :
+						public BrainMuscles::test::simple::functional::Function<RETURN_TYPE, OBJECT_TYPE, ARGS...>
+					{
+					public:
+						typedef BrainMuscles::test::simple::functional
+							::Function<RETURN_TYPE, OBJECT_TYPE, ARGS...> FunctionType;
+					protected:
+						Call(FunctionType function);
+					};
+
+					template<typename RETURN_TYPE, typename OBJECT_TYPE, typename... ARGS>
+					Call<BrainMuscles::test::simple::functional::Function<RETURN_TYPE, OBJECT_TYPE, ARGS...>>
+						::Call(FunctionType function) :
+						FunctionType(function)
+					{}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_CALL_H_
