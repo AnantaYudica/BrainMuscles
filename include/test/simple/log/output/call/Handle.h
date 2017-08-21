447708de72916e1c6ca64f52377281f8de8b2f2b
AnantaYudica <nta040689@gmail.com>
2017-08-21 15:20:17 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if _USING_TEST_  - Add dependency with test\simple\log\output\Call.h  - Add dependency with test\simple\log\output\Handle.h  - Add dependency with test\simple\log\output\format\Value.h  - Add class BrainMuscles::test::simple::log::output::call::Handle    - Add parameter variadic template 'ARGS'    - Add base class with BrainMuscles::test::simple::log::output::Call<BrainMuscles::test::simple::functional::Function<void, BrainMuscles::test::simple::log::output::Handle*, ARGS...>> and public inheritance    - Add aliases type with public to access      - Add alias type 'HandleType' with BrainMuscles::test::simple::log::output::Handle for type      - Add alias type 'FunctionType' with BrainMuscles::test::simple::functional::Function<void, HandleType*, ARGS...> for type      - Add alias type 'BaseType' with BrainMuscles::test::simple::log::output::Call<FunctionType> for type    - Add fill constructor with parameter constructor 'function' with FunctionType for type and public to access    - Add fill constructor with parameter constructor 'function' with std::function<void(HandleType*, ARGS...)> for type and public to access

---
+++ include/test/simple/log/output/call/Handle.h
@@ -0,0 +1,59 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_CALL_HANDLE_H_
+#define TEST_SIMPLE_LOG_OUTPUT_CALL_HANDLE_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\log\output\Call.h"
+#include "test\simple\log\output\Handle.h"
+#include "test\simple\log\output\format\Value.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace call
+					{
+						template<typename... ARGS>
+						class Handle :
+							public BrainMuscles::test::simple::log::output::Call<
+								BrainMuscles::test::simple::functional::Function<
+									void, BrainMuscles::test::simple::log::output::Handle*, ARGS...>>
+						{
+						public:
+							typedef BrainMuscles::test::simple::log::output::Handle			HandleType;
+							typedef BrainMuscles::test::simple::functional::Function<
+								void, HandleType*, ARGS...>									FunctionType;
+							typedef BrainMuscles::test::simple::log::output::Call<
+								FunctionType>												BaseType;
+						public:
+							Handle(FunctionType function);
+							Handle(std::function<void(HandleType*, ARGS...)> function);
+						};
+
+						template<typename... ARGS>
+						Handle<ARGS...>::Handle(FunctionType function) :
+							BaseType(function)
+						{}
+
+						template<typename... ARGS>
+						Handle<ARGS...>::Handle(std::function<void(HandleType*, ARGS...)> function) :
+							BaseType(function)
+						{}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif 
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_CALL_HANDLE_H_
