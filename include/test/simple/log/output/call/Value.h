6d5663e142d260493d932b1ef8e17f4473e571a2
AnantaYudica <nta040689@gmail.com>
2017-08-21 15:10:28 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with test\simple\log\output\Call.h  - Add dependency with test\simple\log\output\format\Value.h  - Add dependency with test\simple\log\output\call\value\Tag.h  - Add class BrainMuscles::test::simple::log::output::call::Value    - Add base class with BrainMuscles::test::simple::log::output::Call<BrainMuscles::test::simple::functional::Function<void, BrainMuscles::test::simple::log::output::format::Value*>>    - Add aliases type with public to access      - Add alias type 'FormatValueType' with BrainMuscles::test::simple::log::output::format::Value for type      - Add alias type 'FunctionType' with BrainMuscles::test::simple::functional::Function<void, FormatValueType*> for type      - Add alias type 'BaseType' with BrainMuscles::test::simple::log::output::Call<FunctionType> for type      - Add alias type 'TagType' with BrainMuscles::test::simple::log::output::call::value::Tag for type    - Add variable member 'm_tag' with private to access    - Add fill constructor with parameters constructor 'tag' with const TagType& for type and 'function' with std::function<void(FormatValueType*)> and public to access    - Add fill constructor with parameters constructor 'tag' with const TagType& for type and 'function' with const FunctionType& for type    - Add function const member 'IsSetGlobalValue' and 'IsSetLocalValue' with bool for return type

---
+++ include/test/simple/log/output/call/Value.h
@@ -0,0 +1,77 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_CALL_VALUE_H_
+#define TEST_SIMPLE_LOG_OUTPUT_CALL_VALUE_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\log\output\Call.h"
+#include "test\simple\log\output\format\Value.h"
+#include "test\simple\log\output\call\value\Tag.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace call
+					{
+						class Value :
+							public BrainMuscles::test::simple::log::output::Call<
+								BrainMuscles::test::simple::functional::Function<
+									void, BrainMuscles::test::simple::log::output::format::Value*>>
+						{
+						public:
+							typedef BrainMuscles::test::simple::log::output
+								::format::Value										FormatValueType;
+							typedef BrainMuscles::test::simple::functional
+								::Function<void, FormatValueType*>					FunctionType;
+							typedef BrainMuscles::test::simple::log::output::Call<
+								FunctionType>										BaseType;
+							typedef BrainMuscles::test::simple::log::output
+								::call::value::Tag									TagType;
+						private:
+							TagType m_tag;
+						public:
+							Value(const TagType& tag, std::function<void(FormatValueType*)> function);
+							Value(const TagType& tag, const FunctionType& function);
+						public:
+							bool IsSetGlobalValue() const;
+							bool IsSetLocalValue() const;
+						};
+
+						Value::Value(const TagType& tag, const FunctionType& function) :
+							BaseType(function),
+							m_tag(tag)
+						{}
+
+						
+						Value::Value(const TagType& tag, std::function<void(FormatValueType*)> function) :
+							BaseType(function),
+							m_tag(tag)
+						{}
+
+						bool Value::IsSetGlobalValue() const
+						{
+							return m_tag == TagType::global_value;
+						}
+
+						bool Value::IsSetLocalValue() const
+						{
+							return m_tag == TagType::local_value;
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_CALL_VALUE_H_
