be3c529a767319d775d4d36bdf163f3e8a13c406
AnantaYudica <nta040689@gmail.com>
2017-08-24 12:55:36 +0700

    MODIFIED FILE Update class in BrainMuscles::test::simple::log::output::delegate::FloatOrExponent  - Update base class with BrainMuscles::test::simple::log::output::call::Value<BrainMuscles::test::simple::log::output::delegate::FloatOrExponent<OUTPUT_TYPE>>  - Update aliases type    - Add alias type 'FloatOrExponentType' with BrainMuscles::test::simple::log::output::delegate::FloatOrExponent<OUTPUT_TYPE> for type and public to access    - Update alias type 'BaseType' with BrainMuscles::test::simple::log::output::call::Value<FloatOrExponentType> for type    - Update alias type 'CallHandleType' with BrainMuscles::test::simple::log::output::call::Handle<FloatOrExponentType, OUTPUT_TYPE> for type

--- include/test/simple/log/output/delegate/FloatOrExponent.h
+++ include/test/simple/log/output/delegate/FloatOrExponent.h
@@ -25,10 +25,14 @@ namespace BrainMuscles
 					{
 						template<typename OUTPUT_TYPE>
 						class FloatOrExponent :
-							public BrainMuscles::test::simple::log::output::call::Value
+							public BrainMuscles::test::simple::log::output::call::Value<
+								BrainMuscles::test::simple::log::output::delegate::FloatOrExponent<OUTPUT_TYPE>>
 						{
 						public:
-							typedef BrainMuscles::test::simple::log::output::call::Value		BaseType;
+							typedef BrainMuscles::test::simple::log::output::delegate
+								::FloatOrExponent<OUTPUT_TYPE>									FloatOrExponentType;
+							typedef BrainMuscles::test::simple::log::output::call::Value<
+								FloatOrExponentType>											BaseType;
 							typedef BrainMuscles::test::simple::log::output::call::value::Tag	ValueTagType;
 							typedef BrainMuscles::test::simple::log::output::format::Value		FormatValueType;
 							typedef BrainMuscles::test::simple::functional::Function<void,
@@ -37,7 +41,7 @@ namespace BrainMuscles
 							typedef BrainMuscles::test::simple::log::output::Handle<
 								OUTPUT_TYPE>													HandleType;
 							typedef BrainMuscles::test::simple::log::output::call::Handle<
-								OUTPUT_TYPE>													CallHandleType;
+								FloatOrExponentType, OUTPUT_TYPE>								CallHandleType;
 						public:
 							template<typename ARG>
 							using FunctionMemberHandlePrintType = BrainMuscles::test::simple::functional::Function<void,
