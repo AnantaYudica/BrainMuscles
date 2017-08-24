2976426654a6682780db3ca3595da44eb096b551
AnantaYudica <nta040689@gmail.com>
2017-08-24 08:35:46 +0700

    MODIFIED FILE Update macro ifndef and define header file Remove dependency with test\simple\log\output\Arguments.h Add dependency with test\simple\log\output\call\Handle.h Update namespace for header to delegate Update in class BrainMuscles::test::simple::log::output::delegate::Loop  - Add parameter template 'OUTPUT_TYPE'  - Update aliases type    - Remove alias type 'ArgumentsType'    - Add aliases type with public to access      - Add alias type 'HandleType' with BrainMuscles::test::simple::log::output::Handle<OUTPUT_TYPE> for type      - Add alias type 'CallHandleType' with BrainMuscles::test::simple::log::output::call::Handle<OUTPUT_TYPE> for type      - Add alias type 'FunctionMemberHandlePrintLoopType' with parameters template 'ITERATOR_TYPE', 'CONJUNCTION_TYPE' with ConstantType::LoopConjunctionDefaultType for default type, 'BEFORE_PRINT_TYPE' with ConstantType::LoopBeforePrintDefaultType for default type, 'AFTER_PRINT_TYPE' with ConstantType::LoopAfterPrintDefaultType for default type and BrainMuscles::test::simple::functional::Function<void, ITERATOR_TYPE, ITERATOR_TYPE, CONJUNCTION_TYPE, BEFORE_PRINT_TYPE, AFTER_PRINT_TYPE> for type  - Update function const member 'operator()' with CallHandleType for return type

--- include/test/simple/log/output/delegate/Loop.h
+++ include/test/simple/log/output/delegate/Loop.h
@@ -1,5 +1,5 @@
-#ifndef TEST_SIMPLE_LOG_OUTPUT_HANDLE_LOOP_H_
-#define TEST_SIMPLE_LOG_OUTPUT_HANDLE_LOOP_H_
+#ifndef TEST_SIMPLE_LOG_OUTPUT_DELEGATE_LOOP_H_
+#define TEST_SIMPLE_LOG_OUTPUT_DELEGATE_LOOP_H_
 
 #include "test\Configure.h"
 
@@ -9,7 +9,7 @@
 
 #include "test\simple\log\output\Constant.h"
 
-#include "test\simple\log\output\Arguments.h"
+#include "test\simple\log\output\call\Handle.h"
 
 namespace BrainMuscles
 {
@@ -21,25 +21,30 @@ namespace BrainMuscles
 			{
 				namespace output
 				{
-					namespace handle
+					namespace delegate
 					{
+						template<typename OUTPUT_TYPE>
 						class Loop
 						{
 						public:
-							typedef BrainMuscles::test::simple::log::output::Constant ConstantType;
+							typedef BrainMuscles::test::simple::log::output::Constant			ConstantType;
+							typedef BrainMuscles::test::simple::log::output::Handle<
+								OUTPUT_TYPE>													HandleType;
+							typedef BrainMuscles::test::simple::log::output::call::Handle<
+								OUTPUT_TYPE>													CallHandleType;
 							template<typename ITERATOR_TYPE, 
 								typename CONJUNCTION_TYPE = ConstantType::LoopConjunctionDefaultType,
 								typename BEFORE_PRINT_TYPE = ConstantType::LoopBeforePrintDefaultType,
 								typename AFTER_PRINT_TYPE = ConstantType::LoopAfterPrintDefaultType>
-							using ArgumentsType = BrainMuscles::test::simple::log::output::Arguments<Loop, ITERATOR_TYPE, ITERATOR_TYPE, CONJUNCTION_TYPE, BEFORE_PRINT_TYPE, AFTER_PRINT_TYPE>;
+							using FunctionMemberHandlePrintLoopType = BrainMuscles::test::simple::functional::Function<
+								void, ITERATOR_TYPE, ITERATOR_TYPE, CONJUNCTION_TYPE, BEFORE_PRINT_TYPE, AFTER_PRINT_TYPE>;
 						public:
 							template<typename TYPE, 
 								std::size_t SIZE, 
 								typename CONJUNCTION_TYPE = ConstantType::LoopConjunctionDefaultType,
 								typename BEFORE_PRINT_TYPE = ConstantType::LoopBeforePrintDefaultType,
 								typename AFTER_PRINT_TYPE = ConstantType::LoopAfterPrintDefaultType>
-							ArgumentsType<TYPE*, CONJUNCTION_TYPE, BEFORE_PRINT_TYPE, AFTER_PRINT_TYPE> 
-								operator()(TYPE(&arr)[SIZE], 
+							CallHandleType operator()(TYPE(&arr)[SIZE], 
 									CONJUNCTION_TYPE conjunction = ConstantType::LoopConjunctionDefault,
 									BEFORE_PRINT_TYPE before_print = ConstantType::LoopBeforePrintDefault,
 									AFTER_PRINT_TYPE after_print = ConstantType::LoopAfterPrintDefault) const;
@@ -48,26 +53,29 @@ namespace BrainMuscles
 								typename CONJUNCTION_TYPE = ConstantType::LoopConjunctionDefaultType,
 								typename BEFORE_PRINT_TYPE = ConstantType::LoopBeforePrintDefaultType,
 								typename AFTER_PRINT_TYPE = ConstantType::LoopAfterPrintDefaultType>
-							ArgumentsType<ITERATOR_TYPE, CONJUNCTION_TYPE, BEFORE_PRINT_TYPE, AFTER_PRINT_TYPE>
-								operator()(ITERATOR_TYPE begin_iterator, 
+							CallHandleType operator()(ITERATOR_TYPE begin_iterator, 
 									ITERATOR_TYPE end_iterator, 
 									CONJUNCTION_TYPE conjunction = ConstantType::LoopConjunctionDefault,
 									BEFORE_PRINT_TYPE before_print = ConstantType::LoopBeforePrintDefault,
 									AFTER_PRINT_TYPE after_print = ConstantType::LoopAfterPrintDefault) const;
 						};
 
+						template<typename OUTPUT_TYPE>
 						template<typename TYPE, std::size_t SIZE, typename CONJUNCTION_TYPE, typename BEFORE_PRINT_TYPE, typename AFTER_PRINT_TYPE>
-						Loop::ArgumentsType<TYPE*, CONJUNCTION_TYPE, BEFORE_PRINT_TYPE, AFTER_PRINT_TYPE> 
-							Loop::operator()(TYPE(&arr)[SIZE], CONJUNCTION_TYPE conjunction, BEFORE_PRINT_TYPE before_print, AFTER_PRINT_TYPE after_print) const
+						typename Loop<OUTPUT_TYPE>::CallHandleType
+							Loop<OUTPUT_TYPE>::operator()(TYPE(&arr)[SIZE], CONJUNCTION_TYPE conjunction, BEFORE_PRINT_TYPE before_print, AFTER_PRINT_TYPE after_print) const
 						{
-							return ArgumentsType<TYPE*, CONJUNCTION_TYPE, BEFORE_PRINT_TYPE, AFTER_PRINT_TYPE>(arr, arr + SIZE, conjunction, before_print, after_print);
+							return std::bind(static_cast<void (HandleType::*)(TYPE*, TYPE*, CONJUNCTION_TYPE, BEFORE_PRINT_TYPE, AFTER_PRINT_TYPE)>
+								(&HandleType::PrintLoop), std::placeholders::_1, arr, arr + SIZE, conjunction, before_print, after_print);
 						}
 
+						template<typename OUTPUT_TYPE>
 						template<typename ITERATOR_TYPE, typename CONJUNCTION_TYPE, typename BEFORE_PRINT_TYPE, typename AFTER_PRINT_TYPE>
-						Loop::ArgumentsType<ITERATOR_TYPE, CONJUNCTION_TYPE, BEFORE_PRINT_TYPE, AFTER_PRINT_TYPE>
-							Loop::operator()(ITERATOR_TYPE begin_iterator, ITERATOR_TYPE end_iterator, CONJUNCTION_TYPE conjunction , BEFORE_PRINT_TYPE before_print, AFTER_PRINT_TYPE after_print) const
+						typename Loop<OUTPUT_TYPE>::CallHandleType
+							Loop<OUTPUT_TYPE>::operator()(ITERATOR_TYPE begin_iterator, ITERATOR_TYPE end_iterator, CONJUNCTION_TYPE conjunction , BEFORE_PRINT_TYPE before_print, AFTER_PRINT_TYPE after_print) const
 						{
-							return ArgumentsType<ITERATOR_TYPE, CONJUNCTION_TYPE, BEFORE_PRINT_TYPE, AFTER_PRINT_TYPE>(begin_iterator, end_iterator, conjunction, before_print, after_print);
+							return std::bind(static_cast<void (HandleType::*)(ITERATOR_TYPE, ITERATOR_TYPE, CONJUNCTION_TYPE, BEFORE_PRINT_TYPE, AFTER_PRINT_TYPE)>
+								(&HandleType::PrintLoop), std::placeholders::_1, begin_iterator, end_iterator, conjunction, before_print, after_print);
 						}
 					}
 				}
@@ -78,4 +86,4 @@ namespace BrainMuscles
 
 #endif
 
-#endif //!TEST_SIMPLE_LOG_OUTPUT_HANDLE_LOOP_H_
+#endif //!TEST_SIMPLE_LOG_OUTPUT_DELEGATE_LOOP_H_
