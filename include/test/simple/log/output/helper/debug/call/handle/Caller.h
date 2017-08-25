020e40d1c281aea23142ff71936d78f3f9190950
AnantaYudica <nta040689@gmail.com>
2017-08-25 14:47:03 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add macro if defined _USING_TEST_SIMPLE_LOG_OUTPUT_DEBUG_    - Add dependency with utility    - Add dependency with test\simple\log\output\call\Handle.h    - Add class BrainMuscles::test::simple::log::output::helper::debug::call::handle::Caller      - Add parameter template 'TYPE'      - Add forward declaration static function 'CallerTypeImpl' with parameters template 'CALLER_TYPE' and 'OUTPUT_TYPE', const BrainMuscles::test::simple::log::output::call::Handle<CALLER_TYPE, OUTPUT_TYPE>& for parameter function type, CALLER_TYPE for return type and private to access      - Add forward declaration static function 'CallerTypeImpl' with parameter pack for parameter function type, void for return type and private to access   - Add alias type 'Type' with decltype(CallerTypeImpl(std::declval<TYPE>())) for type and public to access

---
+++ include/test/simple/log/output/helper/debug/call/handle/Caller.h
@@ -0,0 +1,57 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_HELPER_DEBUG_CALL_HANDLE_CALLER_H_
+#define TEST_SIMPLE_LOG_OUTPUT_HELPER_DEBUG_CALL_HANDLE_CALLER_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#if defined(_USING_TEST_SIMPLE_LOG_OUTPUT_DEBUG_)
+
+#include <utility>
+
+#include "test\simple\log\output\call\Handle.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace helper
+					{
+						namespace debug
+						{
+							namespace call
+							{
+								namespace handle
+								{
+									template<typename TYPE>
+									class Caller
+									{
+									private:
+										template<typename CALLER_TYPE, typename OUTPUT_TYPE>
+										static CALLER_TYPE CallerTypeImpl(const BrainMuscles::test::simple::log::output::call::Handle<CALLER_TYPE, OUTPUT_TYPE>&);
+
+										static void CallerTypeImpl(...);
+									public:
+										typedef decltype(CallerTypeImpl(std::declval<TYPE>())) Type;
+									};
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_HELPER_DEBUG_CALL_HANDLE_CALLER_H_
