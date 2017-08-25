88fdb132944e5ddca0a4cfce9ad7c3e18febd345
AnantaYudica <nta040689@gmail.com>
2017-08-25 10:49:13 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add macro if defined _USING_TEST_SIMPLE_LOG_OUTPUT_DEBUG_    - Add dependency with utility    - Add dependency with test\simple\log\output\Call.h    - Add class BrainMuscles::test::simple::log::output::helper::debug::call::Caller      - Add parameter template 'TYPE'      - Add forward declaration static function 'CallerTypeImpl' with parameter template 'CALLER_TYPE' and 'FUNCTION_TYPE', const BrainMuscles::test::simple::log::output::Call<CALLER_TYPE, FUNCTION_TYPE>& for parameter type, CALLER_TYPE for return type, private to access      - Add forward declaration static function 'CallerTypeImpl' with parameter pack for parameter type, void for return type and private to access      - Add alias type 'Type' with decltype(CallerTypeImpl(std::declval<TYPE>())) for type and public to access

---
+++ include/test/simple/log/output/helper/debug/call/Caller.h
@@ -0,0 +1,54 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_HELPER_DEBUG_CALL_CALLER_H_
+#define TEST_SIMPLE_LOG_OUTPUT_HELPER_DEBUG_CALL_CALLER_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#if defined(_USING_TEST_SIMPLE_LOG_OUTPUT_DEBUG_)
+
+#include <utility>
+
+#include "test\simple\log\output\Call.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace helper
+					{
+						namespace debug
+						{
+							namespace call
+							{
+								template<typename TYPE>
+								class Caller
+								{
+								private:
+									template<typename CALLER_TYPE, typename FUNCTION_TYPE>
+									static CALLER_TYPE CallerTypeImpl(const BrainMuscles::test::simple::log::output::Call<CALLER_TYPE, FUNCTION_TYPE>&);
+
+									static void CallerTypeImpl(...);
+								public:
+									typedef decltype(CallerTypeImpl(std::declval<TYPE>())) Type;
+								};
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif 
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_HELPER_DEBUG_CALL_CALLER_H_
