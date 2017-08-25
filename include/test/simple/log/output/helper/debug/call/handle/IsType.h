9a60b2c39de7bdc5168e84cf1a2cf4c8d7462de6
AnantaYudica <nta040689@gmail.com>
2017-08-25 14:22:50 +0700

    ADD FOLDER AND ADD FILE Add folder include\test\simple\log\output\helper\debug\call\handle Add file include\test\simple\log\output\helper\debug\call\handle\IsType.h  - Add dependency with test\Configure.h  - Add macro if defined _USING_TEST_    - Add macro if defined _USING_TEST_SIMPLE_LOG_OUTPUT_DEBUG_      - Add dependency with utility      - Add dependency with test\simple\log\output\call\Handle.h      - Add class BrainMuscles::test::simple::log::output::helper::debug::call::handle::IsType        - Add parameter template TYPE        - Add forward declaration 'IsTypeImpl' with parameters template 'CALLER_TYPE' and 'OUTPUT_TYPE', const BrainMuscles::test::simple::log::output::call::Handle<CALLER_TYPE, OUTPUT_TYPE>& for parameter function type, std::true_type for return type and private to access        - Add forward declaration 'IsTypeImpl' with parameter pack for parameter function type, std::false_type for return type and private to access        - Add static constexpr 'Value' with bool for type, decltype(IsTypeImpl(std::declval<TYPE>()))::value; for value and public to access

---
+++ include/test/simple/log/output/helper/debug/call/handle/IsType.h
@@ -0,0 +1,57 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_HELPER_DEBUG_CALL_HANDLE_ISTYPE_H_
+#define TEST_SIMPLE_LOG_OUTPUT_HELPER_DEBUG_CALL_HANDLE_ISTYPE_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#if defined(_USING_TEST_SIMPLE_LOG_OUTPUT_DEBUG_)
+
+#include <utility>
+
+#include "test\simple\log\output\call\Handle.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace helper
+					{
+						namespace debug
+						{
+							namespace call
+							{
+								namespace handle
+								{
+									template<typename TYPE>
+									class IsType
+									{
+									private:
+										template<typename CALLER_TYPE, typename OUTPUT_TYPE>
+										static std::true_type IsTypeImpl(const BrainMuscles::test::simple::log::output::call::Handle<CALLER_TYPE, OUTPUT_TYPE>&);
+
+										static std::false_type IsTypeImpl(...);
+									public:
+										static constexpr bool Value = decltype(IsTypeImpl(std::declval<TYPE>()))::value;
+									};
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif 
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_HELPER_DEBUG_CALL_HANDLE_ISTYPE_H_
