92395d0368081873a45575b5c4f3b7cecffa8acd
AnantaYudica <nta040689@gmail.com>
2017-08-25 12:34:46 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add macro if defined _USING_TEST_SIMPLE_LOG_OUTPUT_DEBUG_    - Add dependency with type_traits    - Add dependency with test\simple\log\output\Call.h    - Add dependency with test\simple\log\output\helper\debug\call\Caller.h    - Add dependency with test\simple\log\output\helper\debug\call\Function.h    - Add class BrainMuscles::test::simple::log::output::helper::debug::call::Cast      - Add parameter template TYPE      - Add aliases type with public to access        - Add alias type 'CallerType' with BrainMuscles::test::simple::log::output::helper::debug::call::Caller<TYPE>::Type for type        - Add alias type 'FunctionType' with typename BrainMuscles::test::simple::log::output::helper::debug::call::Function<TYPE>::Type	for type        - Add alias type 'Type' with std::conditional<std::is_void<CallerType>::value || std::is_void<FunctionType>::value, TYPE, BrainMuscles::test::simple::log::output::Call<CallerType, FunctionType>>::type for type    - Add static function 'Value' with parameter template 'type' with TYPE& for type, Type& for return type and public to access

---
+++ include/test/simple/log/output/helper/debug/call/Cast.h
@@ -0,0 +1,68 @@
+#ifndef TEST_SIMPLE_LOG_OUTPUT_HELPER_DEBUG_CALL_CAST_H_
+#define TEST_SIMPLE_LOG_OUTPUT_HELPER_DEBUG_CALL_CAST_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#if defined(_USING_TEST_SIMPLE_LOG_OUTPUT_DEBUG_)
+
+#include <type_traits>
+
+#include "test\simple\log\output\Call.h"
+
+#include "test\simple\log\output\helper\debug\call\Caller.h"
+#include "test\simple\log\output\helper\debug\call\Function.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace log
+			{
+				namespace output
+				{
+					namespace helper
+					{
+						namespace debug
+						{
+							namespace call
+							{
+								template<typename TYPE>
+								class Cast
+								{
+								public:
+									typedef typename BrainMuscles::test::simple::log::output::helper
+										::debug::call::Caller<TYPE>::Type								CallerType;
+									typedef typename BrainMuscles::test::simple::log::output::helper
+										::debug::call::Function<TYPE>::Type								FunctionType;
+									typedef typename std::conditional<std::is_void<CallerType>::value 
+										|| std::is_void<FunctionType>::value, TYPE,
+										BrainMuscles::test::simple::log::output::Call<
+										CallerType, FunctionType>>::type								Type;
+								public:
+									static Type& Value(TYPE& type);
+								};
+
+								template<typename TYPE>
+								typename Cast<TYPE>::Type&
+									Cast<TYPE>::Value(TYPE& type)
+								{
+									return static_cast<Type&>(type);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif
+
+#endif //!TEST_SIMPLE_LOG_OUTPUT_HELPER_DEBUG_CALL_CAST_H_
