3b3339675524f3da837b8084d628eab73bb5312c
AnantaYudica <nta040689@gmail.com>
2017-08-28 09:46:47 +0700

    ADD FOLDERS AND FILE Add folder /include/test/simple/output Add folder /include/test/simple/output/json Add file /include/test/simple/output/json/Type.h  - Add dependency with test\Configure.h  - Add macro if defined _USING_TEST_    - Add dependency with cstdio    - Add class BrainMuscles::test::output::json::Type      - Add variable member 'm_key' with const char * for type with private to access      - Add fill constructor with parameter constructor 'key' with const char * for type, protected to access      - Add copy and move constructor with public to access      - Add abstract destructor with public to access      - Add functions member 'PrintKey' and 'Print' with parameter function 'output_file' with std::FILE* for type, void for return type and public to access      - Add pure abstract function member 'PrintValue' with parameter function 'output_file' with std::FILE* for type, void for return type and public to access

---
+++ include/test/simple/output/json/Type.h
@@ -0,0 +1,66 @@
+#ifndef TEST_SIMPLE_OUTPUT_JSON_TYPE_H_
+#define TEST_SIMPLE_OUTPUT_JSON_TYPE_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include <cstdio>
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace output
+		{
+			namespace json
+			{
+				class Type
+				{
+				private:
+					const char * m_key;
+				protected:
+					Type(const char * key);
+				public:
+					Type(const Type& copy);
+					Type(Type&& move);
+					virtual ~Type();
+				public:
+					void PrintKey(std::FILE* output_file);
+					virtual void PrintValue(std::FILE* output_file) = 0;
+					void Print(std::FILE* output_file);
+				};
+
+				Type::Type(const char * key) :
+					m_key(key)
+				{}
+
+				Type::Type(const Type& copy) :
+					m_key(copy.m_key)
+				{}
+
+				Type::Type(Type&& move) :
+					m_key(move.m_key)
+				{}
+
+				Type::~Type()
+				{}
+
+				void Type::PrintKey(std::FILE* output_file)
+				{
+					fprintf(output_file, "\"%s\" : ", m_key);
+				}
+
+				void Type::Print(std::FILE* output_file)
+				{
+					PrintKey(output_file);
+					PrintValue(output_file);
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_OUTPUT_JSON_TYPE_H_
