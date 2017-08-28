339f2a75c0745f930fc3cbc0adcd24bb954c5fdc
AnantaYudica <nta040689@gmail.com>
2017-08-28 10:15:03 +0700

    MODIFIED FILE forget namespace simple

--- include/test/simple/output/json/Type.h
+++ include/test/simple/output/json/Type.h
@@ -11,50 +11,53 @@ namespace BrainMuscles
 {
 	namespace test
 	{
-		namespace output
+		namespace simple
 		{
-			namespace json
+			namespace output
 			{
-				class Type
+				namespace json
 				{
-				private:
-					const char * m_key;
-				protected:
-					Type(const char * key);
-				public:
-					Type(const Type& copy);
-					Type(Type&& move);
-					virtual ~Type();
-				public:
-					void PrintKey(std::FILE* output_file);
-					virtual void PrintValue(std::FILE* output_file) = 0;
-					void Print(std::FILE* output_file);
-				};
+					class Type
+					{
+					private:
+						const char * m_key;
+					protected:
+						Type(const char * key);
+					public:
+						Type(const Type& copy);
+						Type(Type&& move);
+						virtual ~Type();
+					public:
+						void PrintKey(std::FILE* output_file);
+						virtual void PrintValue(std::FILE* output_file) = 0;
+						void Print(std::FILE* output_file);
+					};
 
-				Type::Type(const char * key) :
-					m_key(key)
-				{}
+					Type::Type(const char * key) :
+						m_key(key)
+					{}
 
-				Type::Type(const Type& copy) :
-					m_key(copy.m_key)
-				{}
+					Type::Type(const Type& copy) :
+						m_key(copy.m_key)
+					{}
 
-				Type::Type(Type&& move) :
-					m_key(move.m_key)
-				{}
+					Type::Type(Type&& move) :
+						m_key(move.m_key)
+					{}
 
-				Type::~Type()
-				{}
+					Type::~Type()
+					{}
 
-				void Type::PrintKey(std::FILE* output_file)
-				{
-					fprintf(output_file, "\"%s\" : ", m_key);
-				}
+					void Type::PrintKey(std::FILE* output_file)
+					{
+						fprintf(output_file, "\"%s\" : ", m_key);
+					}
 
-				void Type::Print(std::FILE* output_file)
-				{
-					PrintKey(output_file);
-					PrintValue(output_file);
+					void Type::Print(std::FILE* output_file)
+					{
+						PrintKey(output_file);
+						PrintValue(output_file);
+					}
 				}
 			}
 		}
