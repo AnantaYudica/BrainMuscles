f09dffa93da394b48200a417e7991c14330fd9e5
AnantaYudica <nta040689@gmail.com>
2017-08-28 12:01:27 +0700

    ADD FOLDER AND FILE Add folder /include/test/simple/output/json/type/ Add file /include/test/simple/output/json/type/Array.h  - Add dependency with test\Configure.h  - Add macro if defined _USING_TEST_    - Add dependency with vector    - Add dependency with memory    - Add dependency with test\simple\output\json\Type.h    - Add class BrainMuscles::test::simple::output::json::type::Array      - Add base class with BrainMuscles::test::simple::output::json::Type and public inheritance      - Add alias type 'JsonType' with BrainMuscles::test::simple::output::json::Type for type and public to access      - Add alias type 'PtrJsonType' with std::shared_ptr<JsonType> for type and private to access      - Add variable member 'm_value' with std::vector<PtrJsonType> for type and private to access      - Add fill constructor with parameter constructor 'key' with const char* for type and public to access      - Add copy and move constructor with public to access      - Add destructor with public to access      - Add function member 'PrintValue' with parameter template 'output_file' with std::FILE* for type      - Add function member 'Add' with parameter template 'JSON_TYPE', parameter function 'json_type' with const JSON_TYPE& for type, std::enable_if<std::is_base_of<JsonType, JSON_TYPE>::value>::type for return type and public to access

---
+++ include/test/simple/output/json/type/Array.h
@@ -0,0 +1,93 @@
+#ifndef TEST_SIMPLE_OUTPUT_JSON_TYPE_ARRAY_H_
+#define TEST_SIMPLE_OUTPUT_JSON_TYPE_ARRAY_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include <vector>
+#include <memory>
+
+#include "test\simple\output\json\Type.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace output
+			{
+				namespace json
+				{
+					namespace type
+					{
+						class Array :
+							public BrainMuscles::test::simple::output::json::Type
+						{
+						public:
+							typedef BrainMuscles::test::simple::output::json::Type		JsonType;
+						private:
+							typedef std::shared_ptr<JsonType>							PtrJsonType;
+						private:
+							std::vector<PtrJsonType> m_value;
+						public:
+							Array(const char* key);
+							Array(const Array& copy);
+							Array(Array&& move);
+						public:
+							~Array();
+						public:
+							void PrintValue(std::FILE* output_file);
+							template<typename JSON_TYPE>
+							typename std::enable_if<std::is_base_of<JsonType, JSON_TYPE>::value>::type 
+								Add(const JSON_TYPE& json_type);
+						};
+
+						Array::Array(const char* key) :
+							JsonType(key)
+						{}
+
+						Array::Array(const Array& copy) :
+							JsonType(copy),
+							m_value(copy.m_value)
+						{}
+
+						Array::Array(Array&& move) :
+							JsonType(move),
+							m_value(move.m_value)
+						{}
+
+						Array::~Array()
+						{}
+
+						void Array::PrintValue(std::FILE* output_file)
+						{
+							fprintf(output_file, "[");
+							for (std::size_t i = 0; i < m_value.size(); ++i)
+							{
+								if (i != 0)
+								{
+									fprintf(output_file, ", ");
+								}
+								m_value[i]->PrintValue(output_file);
+							}
+							fprintf(output_file, "]");
+						}
+
+						template<typename JSON_TYPE>
+						typename std::enable_if<std::is_base_of<typename Array::JsonType, JSON_TYPE>::value>::type 
+							Array::Add(const JSON_TYPE& json_type)
+						{
+							m_value.push_back(PtrJsonType(new JSON_TYPE(json_type)));
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_OUTPUT_JSON_TYPE_ARRAY_H_
