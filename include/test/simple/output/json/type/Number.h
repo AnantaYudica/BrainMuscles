abf93d22773acd77b7fc1680ee1230c5f7854a0a
AnantaYudica <nta040689@gmail.com>
2017-08-28 12:41:53 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with vector  - Add dependency with test\simple\output\json\Type.h  - Add class BrainMuscles::test::simple::output::json::type::Number    - Add base class with BrainMuscles::test::simple::output::json::Type and public inheritance    - Add alias type 'JsonType' with BrainMuscles::test::simple::output::json::Type for type and protected to access    - Add fill constructor with parameter constructor 'key' with const char* for type and protected to access    - Add copy and move constructor with public to access    - Add abstract destructor with public to access    - Add pure abstract function 'PrintValue' with parameter function 'output_file' with std::FILE* for type, void for return type and public to access

---
+++ include/test/simple/output/json/type/Number.h
@@ -0,0 +1,62 @@
+#ifndef TEST_SIMPLE_OUTPUT_JSON_TYPE_NUMBER_H_
+#define TEST_SIMPLE_OUTPUT_JSON_TYPE_NUMBER_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include <vector>
+
+#include "test\simple\output\json\Type.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace output
+			{
+				namespace json
+				{
+					namespace type
+					{
+						class Number :
+							public BrainMuscles::test::simple::output::json::Type
+						{
+						protected:
+							typedef BrainMuscles::test::simple::output::json::Type		JsonType;
+						protected:
+							Number(const char* key);
+						public:
+							Number(const Number& copy);
+							Number(Number&& move);
+							virtual ~Number();
+						public:
+							virtual void PrintValue(std::FILE* output_file) = 0;
+						};
+
+						Number::Number(const char* key) :
+							JsonType(key)
+						{}
+
+						Number::Number(const Number& copy) :
+							JsonType(copy)
+						{}
+
+						Number::Number(Number&& move) :
+							JsonType(move)
+						{}
+
+						Number::~Number()
+						{}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_OUTPUT_JSON_TYPE_NUMBER_H_
