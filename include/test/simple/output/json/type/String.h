fa8268e8049a997c6173d2340e7df596f1a52619
AnantaYudica <nta040689@gmail.com>
2017-08-28 14:36:15 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with String  - Add dependency with test\simple\output\json\Type.h  - Add class BrainMuscles::test::simple::output::json::type::String    - Add base class with BrainMuscles::test::simple::output::json::Type and public inheritance    - Add alias type 'JsonType' with BrainMuscles::test::simple::output::json::Type for type and public to access    - Add variable member 'm_value' with std::string for type and private to access    - Add fill constructor with parameter constructor 'key' with const char* for type and public to access    - Add copy and move constructor with public to access    - Add destructor with public to access    - Add function member 'PrintValue' with parameter function 'output_file' with std::FILE* for type, void for return type and public to access    - Add function member 'operator=' with parameter function 'value' with const char* for type, String& for return type and public to access

---
+++ include/test/simple/output/json/type/String.h
@@ -0,0 +1,78 @@
+#ifndef TEST_SIMPLE_OUTPUT_JSON_TYPE_STRING_H_
+#define TEST_SIMPLE_OUTPUT_JSON_TYPE_STRING_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include <string>
+
+#include "test\simple\output\json\Type.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace output
+			{
+				namespace json
+				{
+					namespace type
+					{
+						class String :
+							public BrainMuscles::test::simple::output::json::Type
+						{
+						public:
+							typedef BrainMuscles::test::simple::output::json::Type		JsonType;
+						private:
+							std::string m_value;
+						public:
+							String(const char* key);
+							String(const String& copy);
+							String(String&& move);
+							~String();
+						public:
+							void PrintValue(std::FILE* output_file);
+							String& operator= (const char* value);
+						};
+
+						String::String(const char* key) :
+							JsonType(key)
+						{
+						}
+
+						String::String(const String& copy) :
+							JsonType(copy),
+							m_value(copy.m_value)
+						{}
+
+						String::String(String&& move) :
+							JsonType(move),
+							m_value(move.m_value)
+						{}
+
+						String::~String()
+						{}
+
+						void String::PrintValue(std::FILE* output_file)
+						{
+							fprintf(output_file, "\"%s\"", m_value.c_str());
+						}
+
+						String& String::operator= (const char* value)
+						{
+							m_value = value;
+							return *this;
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif 
+
+#endif //!TEST_SIMPLE_OUTPUT_JSON_TYPE_STRING_H_
