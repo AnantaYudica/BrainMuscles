63d7bf1fb545e288c819de782605d03c2da9635f
AnantaYudica <nta040689@gmail.com>
2017-08-30 12:04:45 +0700

    REPLACE FILE from Type.h to Value.h in file Value.h  - Add dependency with test\Configure.h  - Add macro if defined _USING_TEST_    - Add class BrainMuscles::test::simple::json::Value      - Add default constructor with protected to access      - Add copy and move constructor with public to access      - Add virtual destructor with public to access

---
+++ include/test/simple/json/Value.h
@@ -0,0 +1,44 @@
+#ifndef TEST_SIMPLE_JSON_VALUE_H_
+#define TEST_SIMPLE_JSON_VALUE_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				class Value
+				{
+				protected:
+					Value();
+				public:
+					Value(const Value& copy);
+					Value(Value&& move);
+					virtual ~Value();
+				};
+
+				Value::Value()
+				{}
+
+				Value::Value(const Value& copy)
+				{}
+
+				Value::Value(Value&& move)
+				{}
+
+				Value::~Value()
+				{}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_VALUE_H_
