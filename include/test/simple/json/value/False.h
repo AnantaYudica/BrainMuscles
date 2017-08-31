0851e81a5a67e5fab70deba48d0b08205db5e0f6
AnantaYudica <nta040689@gmail.com>
2017-08-31 16:59:18 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with test\simple\json\Constant.h  - Add class BrainMuscles::test::simple::json::value::False    - Add base class with BrainMuscles::test::simple::json::Constant::ValueType with public inheritance    - Add alias type 'ConstantType' with public to access    - Add default, copy and move constructor with public to access    - Add destructor with public to access    - Add functions const member 'operator const bool' and 'operator const char*' with public to access

---
+++ include/test/simple/json/value/False.h
@@ -0,0 +1,65 @@
+#ifndef TEST_SIMPLE_JSON_VALUE_FALSE_H_
+#define TEST_SIMPLE_JSON_VALUE_FALSE_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\json\Constant.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace value
+				{
+					class False :
+						public BrainMuscles::test::simple::json::Constant::ValueType
+					{
+					public:
+						typedef BrainMuscles::test::simple::json::Constant				ConstantType;
+					public:
+						False();
+						False(const False& copy);
+						False(False&& move);
+					public:
+						~False();
+					public:
+						operator const bool const();
+						operator const char* const();
+					};
+
+					False::False()
+					{}
+
+					False::False(const False& copy) 
+					{}
+
+					False::False(False&& move)
+					{}
+					
+					False::~False()
+					{}
+
+					False::operator const bool const()
+					{
+						return false;
+					}
+
+					False::operator const char* const()
+					{
+						return ConstantType::FalseCString;
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_VALUE_FALSE_H_
