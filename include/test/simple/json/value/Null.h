eddf67823003148105bc8b841d804c7ffbdb6cab
AnantaYudica <nta040689@gmail.com>
2017-08-31 17:40:47 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with test\simple\json\Constant.h  - Add class BrainMuscles::test::simple::json::value::Null    - Add base class with BrainMuscles::test::simple::json::Constant::ValueType    - Add alias type 'ConstantType' with BrainMuscles::test::simple::json::Constant for type and public to access    - Add default, copy and move constructor with public to access    - Add destructor with public to access    - Add function const member 'operator const char*' with public to access

---
+++ include/test/simple/json/value/Null.h
@@ -0,0 +1,59 @@
+#ifndef TEST_SIMPLE_JSON_VALUE_NULL_H_
+#define TEST_SIMPLE_JSON_VALUE_NULL_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\json\Constant.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace value
+				{
+					class Null :
+						public BrainMuscles::test::simple::json::Constant::ValueType
+					{
+					public:
+						typedef BrainMuscles::test::simple::json::Constant				ConstantType;
+					public:
+						Null();
+						Null(const Null& copy);
+						Null(Null&& move);
+					public:
+						~Null();
+					public:
+						operator const char* const();
+					};
+
+					Null::Null()
+					{}
+
+					Null::Null(const Null& copy)
+					{}
+
+					Null::Null(Null&& move)
+					{}
+
+					Null::~Null()
+					{}
+					
+					Null::operator const char* const()
+					{
+						return ConstantType::NullCString;
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_VALUE_NULL_H_
