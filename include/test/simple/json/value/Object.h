9d18264a4fae9d2e1614b2e561a5bf5011defd58
AnantaYudica <nta040689@gmail.com>
2017-08-31 16:04:53 +0700

    MODIFIED FILE Update macros ifndef and define header file Remove dependencies with vector, memory and test\simple\output\json\Type.h Add dependency with test\simple\json\Constant.h Remove namespace 'output' Add namespace 'json' in BrainMuscles::test::simple Update namespace from 'type' to 'value' Update BrainMuscles::test::simple::json::value::Object  - Update base class with BrainMuscles::test::simple::json::Constant::ValueType  - Add base class with BrainMuscles::test::simple::json::Constant::ContainerObjectType  - Update aliases type    - Remove aliases type 'JsonType' and 'PtrJsonType'    - Add aliases type with public to access      - Add alias type 'ConstantType' with BrainMuscles::test::simple::json::Constant for type      - Add alias type 'ValueType' with ConstantType::ValueType for type      - Add alias type 'ContainerType' with ConstantType::ContainerObjectType for type  - Remove variable member 'm_value'  - Remove fill constructor with parameter constructor 'key' with const char* for type  - Add default constructor with public to access  - Remove functions member 'PrintValue' and 'Add'

--- include/test/simple/json/value/Object.h
+++ include/test/simple/json/value/Object.h
@@ -1,14 +1,11 @@
-#ifndef TEST_SIMPLE_OUTPUT_JSON_TYPE_OBJECT_H_
-#define TEST_SIMPLE_OUTPUT_JSON_TYPE_OBJECT_H_
+#ifndef TEST_SIMPLE_JSON_VALUE_OBJECT_H_
+#define TEST_SIMPLE_JSON_VALUE_OBJECT_H_
 
 #include "test\Configure.h"
 
 #if defined(_USING_TEST_)
 
-#include <vector>
-#include <memory>
-
-#include "test\simple\output\json\Type.h"
+#include "test\simple\json\Constant.h"
 
 namespace BrainMuscles
 {
@@ -16,72 +13,39 @@ namespace BrainMuscles
 	{
 		namespace simple
 		{
-			namespace output
+			namespace json
 			{
-				namespace json
+				namespace value
 				{
-					namespace type
+					class Object :
+						public BrainMuscles::test::simple::json::Constant::ValueType,
+						public BrainMuscles::test::simple::json::Constant::ContainerObjectType
 					{
-						class Object :
-							public BrainMuscles::test::simple::output::json::Type
-						{
-						public:
-							typedef BrainMuscles::test::simple::output::json::Type		JsonType;
-						private:
-							typedef std::shared_ptr<JsonType>							PtrJsonType;
-						private:
-							std::vector<PtrJsonType> m_value;
-						public:
-							Object(const char* key);
-							Object(const Object& copy);
-							Object(Object&& move);
-						public:
-							~Object();
-						public:
-							void PrintValue(std::FILE* output_file);
-							template<typename JSON_TYPE>
-							typename std::enable_if<std::is_base_of<JsonType, JSON_TYPE>::value>::type 
-								Add(const JSON_TYPE& json_type);
-						};
+					public:
+						typedef BrainMuscles::test::simple::json::Constant				ConstantType;
+						typedef typename ConstantType::ValueType						ValueType;
+						typedef typename ConstantType::ContainerObjectType				ContainerType;
+					public:
+						Object();
+						Object(const Object& copy);
+						Object(Object&& move);
+					public:
+						~Object();
+					};
 
-						Object::Object(const char* key) :
-							JsonType(key)
-						{}
+					Object::Object()
+					{}
 
-						Object::Object(const Object& copy) :
-							JsonType(copy),
-							m_value(copy.m_value)
-						{}
+					Object::Object(const Object& copy) :
+						ContainerType(copy)
+					{}
 
-						Object::Object(Object&& move) :
-							JsonType(move),
-							m_value(move.m_value)
-						{}
+					Object::Object(Object&& move) :
+						ContainerType(move)
+					{}
 
-						Object::~Object()
-						{}
-
-						void Object::PrintValue(std::FILE* output_file)
-						{
-							fprintf(output_file, "{");
-							for (std::size_t i = 0; i < m_value.size(); ++i)
-							{
-								if (i != 0)
-								{
-									fprintf(output_file, ", ");
-								}
-								m_value[i]->Print(output_file);
-							}
-							fprintf(output_file, "}");
-						}
-
-						template<typename JSON_TYPE>
-						typename std::enable_if<std::is_base_of<typename Object::JsonType, JSON_TYPE>::value>::type
-							Object::Add(const JSON_TYPE& json_type)
-						{
-							m_value.push_back(PtrJsonType(new JSON_TYPE(json_type)));
-						}
-					}
+					Object::~Object()
+					{}
 				}
 			}
 		}
@@ -90,4 +54,4 @@ namespace BrainMuscles
 
 #endif
 
-#endif //!TEST_SIMPLE_OUTPUT_JSON_TYPE_OBJECT_H_
+#endif //!TEST_SIMPLE_JSON_VALUE_OBJECT_H_
