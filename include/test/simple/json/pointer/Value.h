80e48e6dbeaf58db3666bee531e7742ea4da407b
AnantaYudica <nta040689@gmail.com>
2017-08-31 13:20:13 +0700

    ADD FOLDER AND FILE Add folder /include/test/simple/json/pointer Add file /include/test/simple/json/pointer/Value.h  - Add dependency with test\Configure.h  - Add macro if defined _USING_TEST_    - Add dependency with type_traits    - Add dependency with memory    - Add dependency with test\simple\json\Value.h    - Add class BrainMuscles::test::simple::json::pointer::Value      - Add base class with std::shared_ptr<BrainMuscles::test::simple::json::Value> and public inheritance      - Add aliases type with public to access        - Add alias type 'ValueType' with BrainMuscles::test::simple::json::Value for type        - Add alias type 'BaseType' with std::shared_ptr<ValueType> for type      - Add default constructor with public to access      - Add fill constructor with parameter template 'VALUE_TYPE' with BrainMuscles::test::simple::json::Value for default type, parameter constructor 'value' with const typename std::enable_if<std::is_base_of<ValueType, VALUE_TYPE>::value, VALUE_TYPE>::type& for type and public to access      - Add copy constructor with public to access

---
+++ include/test/simple/json/pointer/Value.h
@@ -0,0 +1,56 @@
+#ifndef TEST_SIMPLE_JSON_POINTER_VALUE_H_
+#define TEST_SIMPLE_JSON_POINTER_VALUE_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include <type_traits>
+#include <memory>
+
+#include "test\simple\json\Value.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace pointer
+				{
+					class Value :
+						public std::shared_ptr<BrainMuscles::test::simple::json::Value>
+					{
+					public:
+						typedef BrainMuscles::test::simple::json::Value ValueType;
+						typedef std::shared_ptr<ValueType>				BaseType;
+					public:
+						Value();
+						template<typename VALUE_TYPE = BrainMuscles::test::simple::json::Value>
+						Value(const typename std::enable_if<std::is_base_of<ValueType, VALUE_TYPE>::value, VALUE_TYPE>::type& value);
+						Value(const Value& copy);
+					};
+
+					Value::Value()
+					{}
+
+					template<typename VALUE_TYPE>
+					Value::Value(const typename std::enable_if<std::is_base_of<ValueType, VALUE_TYPE>::value, VALUE_TYPE>::type& value) :
+						BaseType(new VALUE_TYPE(value))
+					{}
+
+					Value::Value(const Value& copy) :
+						BaseType(copy)
+					{
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_POINTER_VALUE_H_
