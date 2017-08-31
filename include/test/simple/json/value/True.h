1a244c9527e289631a3a343bef2ab649a719c91f
AnantaYudica <nta040689@gmail.com>
2017-08-31 17:45:52 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with test\simple\json\Constant.h  - Add class BrainMuscles::test::simple::json::value::True    - Add base class with BrainMuscles::test::simple::json::Constant::ValueType with public inheritance    - Add alias type 'ConstantType' with public to access    - Add default, copy and move constructor with public to access    - Add destructor with public to access    - Add functions const member 'operator const bool' and 'operator const char*' with public to access

---
+++ include/test/simple/json/value/True.h
@@ -0,0 +1,65 @@
+#ifndef TEST_SIMPLE_JSON_VALUE_TRUE_H_
+#define TEST_SIMPLE_JSON_VALUE_TRUE_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\json\Constant.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace value
+				{
+					class True :
+						public BrainMuscles::test::simple::json::Constant::ValueType
+					{
+					public:
+						typedef BrainMuscles::test::simple::json::Constant				ConstantType;
+					public:
+						True();
+						True(const True& copy);
+						True(True&& move);
+					public:
+						~True();
+					public:
+						operator const bool const();
+						operator const char* const();
+					};
+
+					True::True()
+					{}
+
+					True::True(const True& copy)
+					{}
+
+					True::True(True&& move)
+					{}
+
+					True::~True()
+					{}
+
+					True::operator const bool const()
+					{
+						return true;
+					}
+
+					True::operator const char* const()
+					{
+						return ConstantType::TrueCString;
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_VALUE_TRUE_H_
