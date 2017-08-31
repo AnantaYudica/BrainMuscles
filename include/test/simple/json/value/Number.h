41bb1b2ea0dcf7723358e0bf7c450d4ec5fd7a04
AnantaYudica <nta040689@gmail.com>
2017-08-31 15:50:35 +0700

    MODIFIED FILE Update macros ifndef and define header file Remove dependencies with vector and test\simple\output\json\Type.h Add dependency with test\simple\json\Constant.h Remove namespace 'output' Add namespace 'json' in BrainMuscles::Test::simple Update namespace from 'type' to 'value' Update class BrainMuscles::test::simple::json::value::Number  - Update base class with BrainMuscles::test::simple::json::Constant::ValueType  - Update aliases type    - Remove alias type 'JsonType'    - Add aliases type with protected to access      - Add alias type 'ConstantType' with BrainMuscles::test::simple::json::Constant for type      - Add alias type 'ValueType' with ConstantType::ValueType for type  - Remove fill constructor with parameter constructor 'key' with const char* for type  - Add default constructor with protected to access  - Remove pure virtual function member 'PrintValue'

--- include/test/simple/json/value/Number.h
+++ include/test/simple/json/value/Number.h
@@ -1,13 +1,11 @@
-#ifndef TEST_SIMPLE_OUTPUT_JSON_TYPE_NUMBER_H_
-#define TEST_SIMPLE_OUTPUT_JSON_TYPE_NUMBER_H_
+#ifndef TEST_SIMPLE_JSON_VALUE_NUMBER_H_
+#define TEST_SIMPLE_JSON_VALUE_NUMBER_H_
 
 #include "test\Configure.h"
 
 #if defined(_USING_TEST_)
 
-#include <vector>
-
-#include "test\simple\output\json\Type.h"
+#include "test\simple\json\Constant.h"
 
 namespace BrainMuscles
 {
@@ -15,42 +13,37 @@ namespace BrainMuscles
 	{
 		namespace simple
 		{
-			namespace output
+			namespace json
 			{
-				namespace json
+				namespace value
 				{
-					namespace type
+					class Number :
+						public BrainMuscles::test::simple::json::Constant::ValueType
 					{
-						class Number :
-							public BrainMuscles::test::simple::output::json::Type
-						{
-						protected:
-							typedef BrainMuscles::test::simple::output::json::Type		JsonType;
-						protected:
-							Number(const char* key);
-						public:
-							Number(const Number& copy);
-							Number(Number&& move);
-							virtual ~Number();
-						public:
-							virtual void PrintValue(std::FILE* output_file) = 0;
-						};
+					protected:
+						typedef BrainMuscles::test::simple::json::Constant				ConstantType;
+						typedef typename ConstantType::ValueType						ValueType;
+					protected:
+						Number();
+					public:
+						Number(const Number& copy);
+						Number(Number&& move);
+						virtual ~Number();
+					};
 
-						Number::Number(const char* key) :
-							JsonType(key)
-						{}
+					Number::Number()
+					{}
 
-						Number::Number(const Number& copy) :
-							JsonType(copy)
-						{}
+					Number::Number(const Number& copy) :
+						ValueType(copy)
+					{}
 
-						Number::Number(Number&& move) :
-							JsonType(move)
-						{}
+					Number::Number(Number&& move) :
+						ValueType(move)
+					{}
 
-						Number::~Number()
-						{}
-					}
+					Number::~Number()
+					{}
 				}
 			}
 		}
@@ -59,4 +52,4 @@ namespace BrainMuscles
 
 #endif
 
-#endif //!TEST_SIMPLE_OUTPUT_JSON_TYPE_NUMBER_H_
+#endif //!TEST_SIMPLE_JSON_VALUE_NUMBER_H_
