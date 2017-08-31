8d88fc8db060a714ec2b3ba0abf76796ee1efd46
AnantaYudica <nta040689@gmail.com>
2017-08-31 10:13:17 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with memory  - Add dependency with test\simple\json\Value.h  - Add class BrainMuscles::test::simple::json::Constant with final inheritance    - Delete default constructor    - Add alias type 'ValueType' with BrainMuscles::test::simple::json::Value for type    - Add alias type 'PointerValueType' with std::shared_ptr<ValueType> for type

---
+++ include/test/simple/json/Constant.h
@@ -0,0 +1,34 @@
+#ifndef TEST_SIMPLE_JSON_CONSTANT_H_
+#define TEST_SIMPLE_JSON_CONSTANT_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include <memory>
+
+#include "test\simple\json\Value.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				struct Constant final
+				{
+					Constant() = delete;
+				public:
+					typedef BrainMuscles::test::simple::json::Value		ValueType;
+					typedef std::shared_ptr<ValueType>					PointerValueType;
+				};
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_CONSTANT_H_
