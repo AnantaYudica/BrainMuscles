15473f0e4adbbe7176e75124efeb63c57d1c4264
AnantaYudica <nta040689@gmail.com>
2017-09-01 10:29:44 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with memory  - Add class BrainMuscles::test::simple::json::Memory    - Add parameter template 'VALUE_TYPE'    - Add alias type 'SharedPointerType' with std::shared_ptr<VALUE_TYPE>    - Add default constructor with default by compiler    - Add virtual desturctor with default by compiler    - Add pure virtual function const member 'MakeShared' with SharedPointerType for return type and public to access

---
+++ include/test/simple/json/Memory.h
@@ -0,0 +1,37 @@
+#ifndef TEST_SIMPLE_JSON_MEMORY_H_
+#define TEST_SIMPLE_JSON_MEMORY_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include <memory>
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				template<typename VALUE_TYPE>
+				class Memory
+				{
+				public:
+					typedef std::shared_ptr<VALUE_TYPE>			SharedPointerType;
+				protected:
+					Memory() = default;
+				public:
+					virtual ~Memory() = default;
+				public:
+					virtual SharedPointerType MakeShared() const = 0;
+				};
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_MEMORY_H_
