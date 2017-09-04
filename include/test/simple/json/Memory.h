b80bb760a1672ee9641ef920d72955bb608d1997
AnantaYudica <nta040689@gmail.com>
2017-09-04 12:51:29 +0700

    MODIFIED FILE Update class BrainMuscles::test::simple::json::Memory  - Add static functions 'DynamicPointerCast', 'StaticPointerCast' and 'ConstPointerCast' with    - Parameter template 'TO_TYPE'    - Parameter function 'pointer' with const SharedPointerType& for type    - Memory<TO_TYPE>::SharedPointerType for return type    - public to access

--- include/test/simple/json/Memory.h
+++ include/test/simple/json/Memory.h
@@ -26,7 +26,38 @@ namespace BrainMuscles
 					virtual ~Memory() = default;
 				public:
 					virtual SharedPointerType MakeShared() const = 0;
+				public:
+					template<typename TO_TYPE>
+					static typename Memory<TO_TYPE>::SharedPointerType DynamicPointerCast(const SharedPointerType& pointer);
+					template<typename TO_TYPE>
+					static typename Memory<TO_TYPE>::SharedPointerType StaticPointerCast(const SharedPointerType& pointer);
+					template<typename TO_TYPE>
+					static typename Memory<TO_TYPE>::SharedPointerType ConstPointerCast(const SharedPointerType& pointer);
 				};
+
+				template<typename VALUE_TYPE>
+				template<typename TO_TYPE>
+				static typename Memory<TO_TYPE>::SharedPointerType 
+					Memory<VALUE_TYPE>::DynamicPointerCast(const SharedPointerType& pointer)
+				{
+					return std::dynamic_pointer_cast<TO_TYPE>(pointer);
+				}
+				
+				template<typename VALUE_TYPE>
+				template<typename TO_TYPE>
+				static typename Memory<TO_TYPE>::SharedPointerType 
+					Memory<VALUE_TYPE>::StaticPointerCast(const SharedPointerType& pointer)
+				{
+					return std::static_pointer_cast<TO_TYPE>(pointer);
+				}
+				
+				template<typename VALUE_TYPE>
+				template<typename TO_TYPE>
+				static typename Memory<TO_TYPE>::SharedPointerType 
+					Memory<VALUE_TYPE>::ConstPointerCast(const SharedPointerType& pointer)
+				{
+					return std::const_pointer_cast<TO_TYPE>(pointer);
+				}
 			}
 		}
 	}
