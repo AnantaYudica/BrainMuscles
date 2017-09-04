5e560a1e5de4683406ae0404dc6e5bf59f8e9707
AnantaYudica <nta040689@gmail.com>
2017-09-04 18:42:35 +0700

    ADD FOLDER AND FILE Add folder /include/test/simple/json/value/number Add file /include/test/simple/json/value/number/Int.h  - Add dependency with test\Configure.h  - Add macro if defined _USING_TEST_    - Add dependency with test\simple\json\value\Number.h    - Add class BrainMuscles::test::simple::json::value::number::Int      - Add base class with BrainMuscles::test::simple::json::value::Number and public inheritance      - Add aliases type with public to access        - Add alias type 'NumberType' with BrainMuscles::test::simple::json::value::Number for type        - Add alias type 'ConstantType' with NumberType::ConstantType for type        - Add alias type 'ValueType' with NumberType::ValueType for type        - Add alias type 'SharedPointerType' with NumberType::SharedPointerType for type      - Add variable member 'm_value' with int for type and private to access      - Add default, copy and move constructor with public to access      - Add fill constructor with parameter constructor 'value' with const int& for type and public to access      - Add functions const member or member with public to access        - Add functions const member 'MakeShared' with SharedPointerType for return type, 'operator const int&' with const int& for return type        - Add function member 'operator=' with Int& for return type and parameter function 'value' with const int& for type        - Add function member 'operator int&' with int& for return type

---
+++ include/test/simple/json/value/number/Int.h
@@ -0,0 +1,90 @@
+#ifndef TEST_SIMPLE_JSON_VALUE_NUMBER_INT_H_
+#define TEST_SIMPLE_JSON_VALUE_NUMBER_INT_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\json\value\Number.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace value
+				{
+					namespace number
+					{
+						class Int :
+							public BrainMuscles::test::simple::json::value::Number
+						{
+						public:
+							typedef BrainMuscles::test::simple::json::value::Number	NumberType;
+							typedef typename NumberType::ConstantType				ConstantType;
+							typedef typename NumberType::ValueType					ValueType;
+							typedef typename NumberType::SharedPointerType			SharedPointerType;
+						private:
+							int m_value;
+						public:
+							Int();
+							Int(const int& value);
+							Int(const Int& copy);
+							Int(Int&& move);
+						public:
+							SharedPointerType MakeShared() const;
+						public:
+							Int& operator= (const int& value);
+							operator int&();
+							operator const int&() const;
+						};
+
+						Int::Int() :
+							m_value(0)
+						{}
+
+						Int::Int(const int& value) :
+							m_value(value)
+						{}
+
+						Int::Int(const Int& copy) :
+							m_value(copy.m_value)
+						{}
+
+						Int::Int(Int&& move) :
+							m_value(move.m_value)
+						{}
+
+						typename Int::SharedPointerType Int::MakeShared() const
+						{
+							return SharedPointerType(new Int(*this));
+						}
+
+						Int& Int::operator= (const int& value)
+						{
+							m_value = value;
+							return *this;
+						}
+
+						Int::operator int&()
+						{
+							return m_value;
+						}
+
+						Int::operator const int&() const
+						{
+							return m_value;
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_VALUE_NUMBER_INT_H_
