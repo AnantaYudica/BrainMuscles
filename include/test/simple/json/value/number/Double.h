d50122b8d2c488933f212b32064dac047bab12dd
AnantaYudica <nta040689@gmail.com>
2017-09-04 17:40:35 +0700

    ADD FOLDER AND FILE Add folder /include/test/simple/json/value/number Add file /include/test/simple/json/value/number/Double  - Add dependency with test\Configure.h  - Add macro if defined _USING_TEST_    - Add dependency with test\simple\json\value\Number.h    - Add class BrainMuscles::test::simple::json::value::number::Double      - Add base class with BrainMuscles::test::simple::json::value::Number and public inheritance      - Add aliases type with public to access        - Add alias type 'NumberType' with BrainMuscles::test::simple::json::value::Number for type        - Add alias type 'ConstantType' with NumberType::ConstantType for type        - Add alias type 'ValueType' with NumberType::ValueType for type        - Add alias type 'SharedPointerType' with NumberType::SharedPointerType for type      - Add variable member 'm_value' with double for type and private to access      - Add default, copy and move constructor with public to access      - Add fill constructor with parameter constructor 'value' with const double& for type and public to access      - Add functions const member or member with public to access        - Add functions const member 'MakeShared' with SharedPointerType for return type, 'operator const double&' with const double& for return type        - Add function member 'operator=' with Double& for return type and parameter function 'value' with const double& for type        - Add function member 'operator double&' with double& for return type

---
+++ include/test/simple/json/value/number/Double.h
@@ -0,0 +1,90 @@
+#ifndef TEST_SIMPLE_JSON_VALUE_NUMBER_DOUBLE_H_
+#define TEST_SIMPLE_JSON_VALUE_NUMBER_DOUBLE_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\json\value\Number.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace value
+				{
+					namespace number
+					{
+						class Double :
+							public BrainMuscles::test::simple::json::value::Number
+						{
+						public:
+							typedef BrainMuscles::test::simple::json::value::Number	NumberType;
+							typedef typename NumberType::ConstantType				ConstantType;
+							typedef typename NumberType::ValueType					ValueType;
+							typedef typename NumberType::SharedPointerType			SharedPointerType;
+						private:
+							double m_value;
+						public:
+							Double();
+							Double(const double& value);
+							Double(const Double& copy);
+							Double(Double&& move);
+						public:
+							SharedPointerType MakeShared() const;
+						public:
+							Double& operator= (const double& value);
+							operator double&();
+							operator const double&() const;
+						};
+
+						Double::Double() :
+							m_value(0)
+						{}
+
+						Double::Double(const double& value) :
+							m_value(value)
+						{}
+
+						Double::Double(const Double& copy) :
+							m_value(copy.m_value)
+						{}
+
+						Double::Double(Double&& move) :
+							m_value(move.m_value)
+						{}
+
+						typename Double::SharedPointerType Double::MakeShared() const
+						{
+							return SharedPointerType(new Double(*this));
+						}
+
+						Double& Double::operator= (const double& value)
+						{
+							m_value = value;
+							return *this;
+						}
+
+						Double::operator double&()
+						{
+							return m_value;
+						}
+
+						Double::operator const double&() const
+						{
+							return m_value;
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_VALUE_NUMBER_DOUBLE_H_
