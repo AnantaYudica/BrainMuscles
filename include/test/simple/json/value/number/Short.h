ae6c0eacd8b84105eb13ad3952747f1fa91c26e6
AnantaYudica <nta040689@gmail.com>
2017-09-04 19:22:52 +0700

    ADD FOLDER AND FILE Add folder /include/test/simple/json/value/number Add file /include/test/simple/json/value/number/Short.h  - Add dependency with test\Configure.h  - Add macro if defined _USING_TEST_    - Add dependency with test\simple\json\value\Number.h    - Add class BrainMuscles::test::simple::json::value::number::Short      - Add base class with BrainMuscles::test::simple::json::value::Number and public inheritance      - Add aliases type with public to access        - Add alias type 'NumberType' with BrainMuscles::test::simple::json::value::Number for type        - Add alias type 'ConstantType' with NumberType::ConstantType for type        - Add alias type 'ValueType' with NumberType::ValueType for type        - Add alias type 'SharedPointerType' with NumberType::SharedPointerType for type      - Add variable member 'm_value' with short for type and private to access      - Add default, copy and move constructor with public to access      - Add fill constructor with parameter constructor 'value' with const short& for type and public to access      - Add functions const member or member with public to access        - Add functions const member 'MakeShared' with SharedPointerType for return type, 'operator const short&' with const short& for return type        - Add function member 'operator=' with Short& for return type and parameter function 'value' with const short& for type        - Add function member 'operator short&' with short& for return type

---
+++ include/test/simple/json/value/number/Short.h
@@ -0,0 +1,90 @@
+#ifndef TEST_SIMPLE_JSON_VALUE_NUMBER_SHORT_H_
+#define TEST_SIMPLE_JSON_VALUE_NUMBER_SHORT_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\json\value\Number.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace value
+				{
+					namespace number
+					{
+						class Short :
+							public BrainMuscles::test::simple::json::value::Number
+						{
+						public:
+							typedef BrainMuscles::test::simple::json::value::Number	NumberType;
+							typedef typename NumberType::ConstantType				ConstantType;
+							typedef typename NumberType::ValueType					ValueType;
+							typedef typename NumberType::SharedPointerType			SharedPointerType;
+						private:
+							short m_value;
+						public:
+							Short();
+							Short(const short& value);
+							Short(const Short& copy);
+							Short(Short&& move);
+						public:
+							SharedPointerType MakeShared() const;
+						public:
+							Short& operator= (const short& value);
+							operator short&();
+							operator const short&() const;
+						};
+
+						Short::Short() :
+							m_value(0)
+						{}
+
+						Short::Short(const short& value) :
+							m_value(value)
+						{}
+
+						Short::Short(const Short& copy) :
+							m_value(copy.m_value)
+						{}
+
+						Short::Short(Short&& move) :
+							m_value(move.m_value)
+						{}
+
+						typename Short::SharedPointerType Short::MakeShared() const
+						{
+							return SharedPointerType(new Short(*this));
+						}
+
+						Short& Short::operator= (const short& value)
+						{
+							m_value = value;
+							return *this;
+						}
+
+						Short::operator short&()
+						{
+							return m_value;
+						}
+
+						Short::operator const short&() const
+						{
+							return m_value;
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_VALUE_NUMBER_SHORT_H_
