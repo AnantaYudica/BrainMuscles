16fbe6131ac5c3f1d123ffcab3d7520e268e81f8
AnantaYudica <nta040689@gmail.com>
2017-09-04 17:52:03 +0700

    ADD FOLDER AND FILE Add folder /include/test/simple/json/value/number Add file /include/test/simple/json/value/number/Float.h  - Add dependency with test\Configure.h  - Add macro if defined _USING_TEST_    - Add dependency with test\simple\json\value\Number.h    - Add class BrainMuscles::test::simple::json::value::number::Float      - Add base class with BrainMuscles::test::simple::json::value::Number and public inheritance      - Add aliases type with public to access        - Add alias type 'NumberType' with BrainMuscles::test::simple::json::value::Number for type        - Add alias type 'ConstantType' with NumberType::ConstantType for type        - Add alias type 'ValueType' with NumberType::ValueType for type        - Add alias type 'SharedPointerType' with NumberType::SharedPointerType for type      - Add variable member 'm_value' with float for type and private to access      - Add default, copy and move constructor with public to access      - Add fill constructor with parameter constructor 'value' with const float& for type and public to access      - Add functions const member or member with public to access        - Add functions const member 'MakeShared' with SharedPointerType for return type, 'operator const float&' with const float& for return type        - Add function member 'operator=' with Float& for return type and parameter function 'value' with const float& for type        - Add function member 'operator float&' with float& for return type

---
+++ include/test/simple/json/value/number/Float.h
@@ -0,0 +1,90 @@
+#ifndef TEST_SIMPLE_JSON_VALUE_NUMBER_FLOAT_H_
+#define TEST_SIMPLE_JSON_VALUE_NUMBER_FLOAT_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\json\value\Number.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace value
+				{
+					namespace number
+					{
+						class Float :
+							public BrainMuscles::test::simple::json::value::Number
+						{
+						public:
+							typedef BrainMuscles::test::simple::json::value::Number	NumberType;
+							typedef typename NumberType::ConstantType				ConstantType;
+							typedef typename NumberType::ValueType					ValueType;
+							typedef typename NumberType::SharedPointerType			SharedPointerType;
+						private:
+							float m_value;
+						public:
+							Float();
+							Float(const float& value);
+							Float(const Float& copy);
+							Float(Float&& move);
+						public:
+							SharedPointerType MakeShared() const;
+						public:
+							Float& operator= (const float& value);
+							operator float&();
+							operator const float&() const;
+						};
+
+						Float::Float() :
+							m_value(0)
+						{}
+
+						Float::Float(const float& value) :
+							m_value(value)
+						{}
+
+						Float::Float(const Float& copy) :
+							m_value(copy.m_value)
+						{}
+
+						Float::Float(Float&& move) :
+							m_value(move.m_value)
+						{}
+
+						typename Float::SharedPointerType Float::MakeShared() const
+						{
+							return SharedPointerType(new Float(*this));
+						}
+
+						Float& Float::operator= (const float& value)
+						{
+							m_value = value;
+							return *this;
+						}
+
+						Float::operator float&()
+						{
+							return m_value;
+						}
+
+						Float::operator const float&() const
+						{
+							return m_value;
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_VALUE_NUMBER_FLOAT_H_
