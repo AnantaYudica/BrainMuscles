99ccb3c56cbf10299ce9e68d1e9e58a7937a6c72
AnantaYudica <nta040689@gmail.com>
2017-09-05 14:28:51 +0700

    ADD FOLDER AND FILE Add folder /include/test/simple/json/value/number Add file /include/test/simple/json/value/number/UnsignedLong.h  - Add dependency with test\Configure.h  - Add macro if defined _USING_TEST_    - Add dependency with test\simple\json\value\Number.h    - Add class BrainMuscles::test::simple::json::value::number::UnsignedLong      - Add base class with BrainMuscles::test::simple::json::value::Number and public inheritance      - Add aliases type with public to access        - Add alias type 'NumberType' with BrainMuscles::test::simple::json::value::Number for type        - Add alias type 'ConstantType' with NumberType::ConstantType for type        - Add alias type 'ValueType' with NumberType::ValueType for type        - Add alias type 'SharedPointerType' with NumberType::SharedPointerType for type      - Add variable member 'm_value' with unsigned long for type and private to access      - Add default, copy and move constructor with public to access      - Add fill constructor with parameter constructor 'value' with const unsigned long& for type and public to access      - Add functions const member or member with public to access        - Add functions const member 'MakeShared' with SharedPointerType for return type, 'operator const unsigned long&' with const unsigned long& for return type        - Add function member 'operator=' with UnsignedLong& for return type and parameter function 'value' with const unsigned long& for type        - Add function member 'operator unsigned long&' with unsigned long& for return type

---
+++ include/test/simple/json/value/number/UnsignedLong.h
@@ -0,0 +1,90 @@
+#ifndef TEST_SIMPLE_JSON_VALUE_NUMBER_UNSIGNEDLONG_H_
+#define TEST_SIMPLE_JSON_VALUE_NUMBER_UNSIGNEDLONG_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\json\value\Number.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace value
+				{
+					namespace number
+					{
+						class UnsignedLong :
+							public BrainMuscles::test::simple::json::value::Number
+						{
+						public:
+							typedef BrainMuscles::test::simple::json::value::Number	NumberType;
+							typedef typename NumberType::ConstantType				ConstantType;
+							typedef typename NumberType::ValueType					ValueType;
+							typedef typename NumberType::SharedPointerType			SharedPointerType;
+						private:
+							unsigned long m_value;
+						public:
+							UnsignedLong();
+							UnsignedLong(const unsigned long& value);
+							UnsignedLong(const UnsignedLong& copy);
+							UnsignedLong(UnsignedLong&& move);
+						public:
+							SharedPointerType MakeShared() const;
+						public:
+							UnsignedLong& operator= (const unsigned long& value);
+							operator unsigned long&();
+							operator const unsigned long&() const;
+						};
+
+						UnsignedLong::UnsignedLong() :
+							m_value(0)
+						{}
+
+						UnsignedLong::UnsignedLong(const unsigned long& value) :
+							m_value(value)
+						{}
+
+						UnsignedLong::UnsignedLong(const UnsignedLong& copy) :
+							m_value(copy.m_value)
+						{}
+
+						UnsignedLong::UnsignedLong(UnsignedLong&& move) :
+							m_value(move.m_value)
+						{}
+
+						typename UnsignedLong::SharedPointerType UnsignedLong::MakeShared() const
+						{
+							return SharedPointerType(new UnsignedLong(*this));
+						}
+
+						UnsignedLong& UnsignedLong::operator= (const unsigned long& value)
+						{
+							m_value = value;
+							return *this;
+						}
+
+						UnsignedLong::operator unsigned long&()
+						{
+							return m_value;
+						}
+
+						UnsignedLong::operator const unsigned long&() const
+						{
+							return m_value;
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_VALUE_NUMBER_UNSIGNEDLONG_H_
