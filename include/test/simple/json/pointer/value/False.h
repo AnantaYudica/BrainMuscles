db490a325e757da23d9699c52dbf84a734d1be22
AnantaYudica <nta040689@gmail.com>
2017-09-06 14:54:48 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with test\simple\json\Memory.h  - Add dependency with test\simple\json\value\False.h  - Add class BrainMuscles::test::simple::json::pointer::value::False    - Add base class with BrainMuscles::test::simple::json::Memory<BrainMuscles::test::simple::json::value::False>::SharedPointerType with public inheritance    - Add aliases type with public to access      - Add alias type 'ValueFalseType' with BrainMuscles::test::simple::json::value::False for type      - Add alias type 'MemoryValueFalseType' with BrainMuscles::test::simple::json::Memory<ValueFalseType> for type      - Add alias type 'BaseType' with MemoryValueFalseType::SharedPointerType for type    - Add default constructor with default by compiler and public to access    - Add fill constructor with parameter constructor 'value' with const ValueFalseType& for type and public to access    - Add copy constructor with public to access

---
+++ include/test/simple/json/pointer/value/False.h
@@ -0,0 +1,53 @@
+#ifndef TEST_SIMPLE_JSON_POINTER_VALUE_FALSE_H_
+#define TEST_SIMPLE_JSON_POINTER_VALUE_FALSE_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\json\Memory.h"
+#include "test\simple\json\value\False.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace pointer
+				{
+					namespace value
+					{
+						class False :
+							public BrainMuscles::test::simple::json::Memory<
+								BrainMuscles::test::simple::json::value::False>::SharedPointerType
+						{
+						public:
+							typedef BrainMuscles::test::simple::json::value::False				ValueFalseType;
+							typedef BrainMuscles::test::simple::json::Memory<ValueFalseType>	MemoryValueFalseType;
+							typedef typename MemoryValueFalseType::SharedPointerType			BaseType;
+						public:
+							False() = default;
+							False(const ValueFalseType& value);
+							False(const False& copy);
+						};
+
+						False::False(const ValueFalseType& value) :
+							BaseType(new ValueFalseType(value))
+						{}
+
+						False::False(const False& copy) :
+							BaseType(copy)
+						{}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_POINTER_VALUE_FALSE_H_
