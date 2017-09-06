decea6253d3a76e641036ac44ad1e439fe5e9236
AnantaYudica <nta040689@gmail.com>
2017-09-06 16:36:54 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with test\simple\json\Memory.h  - Add dependency with test\simple\json\value\String.h  - Add class BrainMuscles::test::simple::json::pointer::value::String    - Add base class with BrainMuscles::test::simple::json::Memory<BrainMuscles::test::simple::json::value::String>::SharedPointerType with public inheritance    - Add aliases type with public to access      - Add alias type 'ValueStringType' with BrainMuscles::test::simple::json::value::String for type      - Add alias type 'MemoryValueStringType' with BrainMuscles::test::simple::json::Memory<ValueStringType> for type      - Add alias type 'BaseType' with MemoryValueStringType::SharedPointerType for type    - Add default constructor with default by compiler and public to access    - Add fill constructor with parameter constructor 'value' with const ValueStringType& for type and public to access    - Add copy constructor with public to access

---
+++ include/test/simple/json/pointer/value/String.h
@@ -0,0 +1,53 @@
+#ifndef TEST_SIMPLE_JSON_POINTER_VALUE_STRING_H_
+#define TEST_SIMPLE_JSON_POINTER_VALUE_STRING_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\json\Memory.h"
+#include "test\simple\json\value\String.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace pointer
+				{
+					namespace value
+					{
+						class String :
+							public BrainMuscles::test::simple::json::Memory<
+								BrainMuscles::test::simple::json::value::String>::SharedPointerType
+						{
+						public:
+							typedef BrainMuscles::test::simple::json::value::String				ValueStringType;
+							typedef BrainMuscles::test::simple::json::Memory<ValueStringType>	MemoryValueStringType;
+							typedef typename MemoryValueStringType::SharedPointerType			BaseType;
+						public:
+							String() = default;
+							String(const ValueStringType& value);
+							String(const String& copy);
+						};
+
+						String::String(const ValueStringType& value) :
+							BaseType(new ValueStringType(value))
+						{}
+
+						String::String(const String& copy) :
+							BaseType(copy)
+						{}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_POINTER_VALUE_STRING_H_
