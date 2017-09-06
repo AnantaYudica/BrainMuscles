f9425c3bdfd993b8011cacd5d286f90091a14f01
AnantaYudica <nta040689@gmail.com>
2017-09-06 17:02:05 +0700

    ADD FOLDER AND FILE Add folder /include/test/simple/json/pointer/value/number Add file /include/test/simple/json/pointer/value/number/Char.h  - Add dependency with test\Configure.h  - Add macro if defined _USING_TEST_    - Add dependency with test\simple\json\Memory.h    - Add dependency with test\simple\json\value\number\Char.h    - Add class BrainMuscles::Test::simple::json::pointer::value::number::Char      - Add base class with BrainMuscles::test::simple::json::Memory<BrainMuscles::test::simple::json::value::number::Char>::SharedPointerType and public inheritance      - Add aliases type with public to access        - Add alias type 'ValueNumberCharType' with BrainMuscles::test::simple::json::value::number::Char for type        - Add alias type 'MemoryValueNumberCharType' with BrainMuscles::test::simple::json::Memory<ValueNumberCharType> for type        - Add alias type 'BaseType' with MemoryValueNumberCharType::SharedPointerType for type      - Add default constructor with default by compiler and public to access      - Add fill constructor with parameter constructor 'value' with const ValueNumberCharType& for type and public to access      - Add copy constructor with public to access

---
+++ include/test/simple/json/pointer/value/number/Char.h
@@ -0,0 +1,59 @@
+#ifndef TEST_SIMPLE_JSON_POINTER_VALUE_NUMBER_CHAR_H_
+#define TEST_SIMPLE_JSON_POINTER_VALUE_NUMBER_CHAR_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\json\Memory.h"
+#include "test\simple\json\value\number\Char.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace pointer
+				{
+					namespace value
+					{
+						namespace number
+						{
+							class Char :
+								public BrainMuscles::test::simple::json::Memory<
+									BrainMuscles::test::simple::json::value::number::Char>::SharedPointerType
+							{
+							public:
+								typedef BrainMuscles::test::simple::json::value
+									::number::Char										ValueNumberCharType;
+								typedef BrainMuscles::test::simple::json::Memory<
+									ValueNumberCharType>								MemoryValueNumberCharType;
+								typedef typename MemoryValueNumberCharType
+									::SharedPointerType									BaseType;
+							public:
+								Char() = default;
+								Char(const ValueNumberCharType& value);
+								Char(const Char& copy);
+							};
+
+							Char::Char(const ValueNumberCharType& value) :
+								BaseType(new ValueNumberCharType(value))
+							{}
+
+							Char::Char(const Char& copy) :
+								BaseType(copy)
+							{}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_POINTER_VALUE_NUMBER_CHAR_H_
