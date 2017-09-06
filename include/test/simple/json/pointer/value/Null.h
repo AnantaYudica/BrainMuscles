13b08035223502e18a5b4c53b07bc89301839010
AnantaYudica <nta040689@gmail.com>
2017-09-06 14:59:37 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with test\simple\json\Memory.h  - Add dependency with test\simple\json\value\Null.h  - Add class BrainMuscles::test::simple::json::pointer::value::Null    - Add base class with BrainMuscles::test::simple::json::Memory<BrainMuscles::test::simple::json::value::Null>::SharedPointerType with public inheritance    - Add aliases type with public to access      - Add alias type 'ValueNullType' with BrainMuscles::test::simple::json::value::Null for type      - Add alias type 'MemoryValueNullType' with BrainMuscles::test::simple::json::Memory<ValueNullType> for type      - Add alias type 'BaseType' with MemoryValueNullType::SharedPointerType for type    - Add default constructor with default by compiler and public to access    - Add fill constructor with parameter constructor 'value' with const ValueNullType& for type and public to access    - Add copy constructor with public to access

---
+++ include/test/simple/json/pointer/value/Null.h
@@ -0,0 +1,53 @@
+#ifndef TEST_SIMPLE_JSON_POINTER_VALUE_NULL_H_
+#define TEST_SIMPLE_JSON_POINTER_VALUE_NULL_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\json\Memory.h"
+#include "test\simple\json\value\Null.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace pointer
+				{
+					namespace value
+					{
+						class Null :
+							public BrainMuscles::test::simple::json::Memory<
+							BrainMuscles::test::simple::json::value::Null>::SharedPointerType
+						{
+						public:
+							typedef BrainMuscles::test::simple::json::value::Null				ValueNullType;
+							typedef BrainMuscles::test::simple::json::Memory<ValueNullType>		MemoryValueNullType;
+							typedef typename MemoryValueNullType::SharedPointerType				BaseType;
+						public:
+							Null() = default;
+							Null(const ValueNullType& value);
+							Null(const Null& copy);
+						};
+
+						Null::Null(const ValueNullType& value) :
+							BaseType(new ValueNullType(value))
+						{}
+
+						Null::Null(const Null& copy) :
+							BaseType(copy)
+						{}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_POINTER_VALUE_NULL_H_
