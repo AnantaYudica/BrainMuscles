72d9737ec36f7c5a063041c1058bf1c347a12c76
AnantaYudica <nta040689@gmail.com>
2017-09-06 18:10:17 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with test\simple\json\Memory.h  - Add dependency with test\simple\json\value\number\LongDouble.h  - Add class BrainMuscles::Test::simple::json::pointer::value::number::LongDouble    - Add base class with BrainMuscles::test::simple::json::Memory<BrainMuscles::test::simple::json::value::number::LongDouble>::SharedPointerType and public inheritance    - Add aliases type with public to access      - Add alias type 'ValueNumberLongDoubleType' with BrainMuscles::test::simple::json::value::number::LongDouble for type      - Add alias type 'MemoryValueNumberLongDoubleType' with BrainMuscles::test::simple::json::Memory<ValueNumberLongDoubleType> for type      - Add alias type 'BaseType' with MemoryValueNumberLongDoubleType::SharedPointerType for type    - Add default constructor with default by compiler and public to access    - Add fill constructor with parameter constructor 'value' with const ValueNumberLongDoubleType& for type and public to access    - Add copy constructor with public to access

---
+++ include/test/simple/json/pointer/value/number/LongDouble.h
@@ -0,0 +1,59 @@
+#ifndef TEST_SIMPLE_JSON_POINTER_VALUE_NUMBER_LONGDOUBLE_H_
+#define TEST_SIMPLE_JSON_POINTER_VALUE_NUMBER_LONGDOUBLE_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\json\Memory.h"
+#include "test\simple\json\value\number\LongDouble.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace pointer
+				{
+					namespace value
+					{
+						namespace number
+						{
+							class LongDouble :
+								public BrainMuscles::test::simple::json::Memory<
+									BrainMuscles::test::simple::json::value::number::LongDouble>::SharedPointerType
+							{
+							public:
+								typedef BrainMuscles::test::simple::json::value
+									::number::LongDouble								ValueNumberLongDoubleType;
+								typedef BrainMuscles::test::simple::json::Memory<
+									ValueNumberLongDoubleType>							MemoryValueNumberLongDoubleType;
+								typedef typename MemoryValueNumberLongDoubleType
+									::SharedPointerType									BaseType;
+							public:
+								LongDouble() = default;
+								LongDouble(const ValueNumberLongDoubleType& value);
+								LongDouble(const LongDouble& copy);
+							};
+
+							LongDouble::LongDouble(const ValueNumberLongDoubleType& value) :
+								BaseType(new ValueNumberLongDoubleType(value))
+							{}
+
+							LongDouble::LongDouble(const LongDouble& copy) :
+								BaseType(copy)
+							{}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_POINTER_VALUE_NUMBER_LONGDOUBLE_H_
