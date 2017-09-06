09adc62680e20bcb1d0c567b123dabd36e31000a
AnantaYudica <nta040689@gmail.com>
2017-09-06 11:10:13 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add enum BrainMuscles::test::simple::json::helper::Tag with char for type  - Add function 'IsTagNumber' with parameter function 'tag' with const Tag& for type and bool for return type

---
+++ include/test/simple/json/helper/Tag.h
@@ -0,0 +1,54 @@
+#ifndef TEST_SIMPLE_JSON_HELPER_TAG_H_
+#define TEST_SIMPLE_JSON_HELPER_TAG_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace helper
+				{
+					enum Tag : char
+					{
+						value_array = 1,
+						value_false = 2,
+						value_null = 3,
+						value_number = 4,
+						value_object = 5,
+						value_string = 6,
+						value_true = 7,
+						value_number_char,
+						value_number_double,
+						value_number_float,
+						value_number_int,
+						value_number_long,
+						value_number_longdouble,
+						value_number_longlong,
+						value_number_short,
+						value_number_unsignedchar,
+						value_number_unsignedint,
+						value_number_unsignedlong,
+						value_number_unsignedlonglong,
+						value_number_unsignedshort
+					};
+
+					bool IsTagNumber(const Tag& tag)
+					{
+						return (tag >= Tag::value_number_char && tag <= value_number_unsignedshort) || tag == value_number;
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_H_
