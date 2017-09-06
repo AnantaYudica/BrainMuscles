eae8fb69aaa6952e52bc5b657c142b62ca8e5eee
AnantaYudica <nta040689@gmail.com>
2017-09-06 17:17:21 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with test\simple\json\Memory.h  - Add dependency with test\simple\json\value\number\Float.h  - Add class BrainMuscles::Test::simple::json::pointer::value::number::Float    - Add base class with BrainMuscles::test::simple::json::Memory<BrainMuscles::test::simple::json::value::number::Float>::SharedPointerType and public inheritance    - Add aliases type with public to access      - Add alias type 'ValueNumberFloatType' with BrainMuscles::test::simple::json::value::number::Float for type      - Add alias type 'MemoryValueNumberFloatType' with BrainMuscles::test::simple::json::Memory<ValueNumberFloatType> for type      - Add alias type 'BaseType' with MemoryValueNumberFloatType::SharedPointerType for type    - Add default constructor with default by compiler and public to access    - Add fill constructor with parameter constructor 'value' with const ValueNumberFloatType& for type and public to access    - Add copy constructor with public to access

---
+++ include/test/simple/json/pointer/value/number/Float.h
@@ -0,0 +1,59 @@
+#ifndef TEST_SIMPLE_JSON_POINTER_VALUE_NUMBER_FLOAT_H_
+#define TEST_SIMPLE_JSON_POINTER_VALUE_NUMBER_FLOAT_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\json\Memory.h"
+#include "test\simple\json\value\number\Float.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace pointer
+				{
+					namespace value
+					{
+						namespace number
+						{
+							class Float :
+								public BrainMuscles::test::simple::json::Memory<
+									BrainMuscles::test::simple::json::value::number::Float>::SharedPointerType
+							{
+							public:
+								typedef BrainMuscles::test::simple::json::value
+									::number::Float										ValueNumberFloatType;
+								typedef BrainMuscles::test::simple::json::Memory<
+									ValueNumberFloatType>								MemoryValueNumberFloatType;
+								typedef typename MemoryValueNumberFloatType
+									::SharedPointerType									BaseType;
+							public:
+								Float() = default;
+								Float(const ValueNumberFloatType& value);
+								Float(const Float& copy);
+							};
+
+							Float::Float(const ValueNumberFloatType& value) :
+								BaseType(new ValueNumberFloatType(value))
+							{}
+
+							Float::Float(const Float& copy) :
+								BaseType(copy)
+							{}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_POINTER_VALUE_NUMBER_FLOAT_H_
