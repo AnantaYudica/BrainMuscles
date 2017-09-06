b94166c5be1f89b4cbe8b4fe17cf365b79f5b797
AnantaYudica <nta040689@gmail.com>
2017-09-06 17:55:58 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with test\simple\json\Memory.h  - Add dependency with test\simple\json\value\number\Long.h  - Add class BrainMuscles::Test::simple::json::pointer::value::number::Long    - Add base class with BrainMuscles::test::simple::json::Memory<BrainMuscles::test::simple::json::value::number::Long>::SharedPointerType and public inheritance    - Add aliases type with public to access      - Add alias type 'ValueNumberLongType' with BrainMuscles::test::simple::json::value::number::Long for type      - Add alias type 'MemoryValueNumberLongType' with BrainMuscles::test::simple::json::Memory<ValueNumberLongType> for type      - Add alias type 'BaseType' with MemoryValueNumberLongType::SharedPointerType for type    - Add default constructor with default by compiler and public to access    - Add fill constructor with parameter constructor 'value' with const ValueNumberLongType& for type and public to access    - Add copy constructor with public to access

---
+++ include/test/simple/json/pointer/value/number/Long.h
@@ -0,0 +1,59 @@
+#ifndef TEST_SIMPLE_JSON_POINTER_VALUE_NUMBER_LONG_H_
+#define TEST_SIMPLE_JSON_POINTER_VALUE_NUMBER_LONG_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\json\Memory.h"
+#include "test\simple\json\value\number\Long.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace pointer
+				{
+					namespace value
+					{
+						namespace number
+						{
+							class Long :
+								public BrainMuscles::test::simple::json::Memory<
+									BrainMuscles::test::simple::json::value::number::Long>::SharedPointerType
+							{
+							public:
+								typedef BrainMuscles::test::simple::json::value
+									::number::Long										ValueNumberLongType;
+								typedef BrainMuscles::test::simple::json::Memory<
+									ValueNumberLongType>								MemoryValueNumberLongType;
+								typedef typename MemoryValueNumberLongType
+									::SharedPointerType									BaseType;
+							public:
+								Long() = default;
+								Long(const ValueNumberLongType& value);
+								Long(const Long& copy);
+							};
+
+							Long::Long(const ValueNumberLongType& value) :
+								BaseType(new ValueNumberLongType(value))
+							{}
+
+							Long::Long(const Long& copy) :
+								BaseType(copy)
+							{}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_POINTER_VALUE_NUMBER_LONG_H_
