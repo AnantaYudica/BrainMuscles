b37b956912dc84d778a758e1a01cebdcfe068092
AnantaYudica <nta040689@gmail.com>
2017-09-06 17:11:58 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with test\simple\json\Memory.h  - Add dependency with test\simple\json\value\number\Double.h  - Add class BrainMuscles::Test::simple::json::pointer::value::number::Double    - Add base class with BrainMuscles::test::simple::json::Memory<BrainMuscles::test::simple::json::value::number::Double>::SharedPointerType and public inheritance    - Add aliases type with public to access      - Add alias type 'ValueNumberDoubleType' with BrainMuscles::test::simple::json::value::number::Double for type      - Add alias type 'MemoryValueNumberDoubleType' with BrainMuscles::test::simple::json::Memory<ValueNumberDoubleType> for type      - Add alias type 'BaseType' with MemoryValueNumberDoubleType::SharedPointerType for type    - Add default constructor with default by compiler and public to access    - Add fill constructor with parameter constructor 'value' with const ValueNumberDoubleType& for type and public to access    - Add copy constructor with public to access

---
+++ include/test/simple/json/pointer/value/number/Double.h
@@ -0,0 +1,59 @@
+#ifndef TEST_SIMPLE_JSON_POINTER_VALUE_NUMBER_DOUBLE_H_
+#define TEST_SIMPLE_JSON_POINTER_VALUE_NUMBER_DOUBLE_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\json\Memory.h"
+#include "test\simple\json\value\number\Double.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace pointer
+				{
+					namespace value
+					{
+						namespace number
+						{
+							class Double :
+								public BrainMuscles::test::simple::json::Memory<
+									BrainMuscles::test::simple::json::value::number::Double>::SharedPointerType
+							{
+							public:
+								typedef BrainMuscles::test::simple::json::value
+									::number::Double									ValueNumberDoubleType;
+								typedef BrainMuscles::test::simple::json::Memory<
+									ValueNumberDoubleType>								MemoryValueNumberDoubleType;
+								typedef typename MemoryValueNumberDoubleType
+									::SharedPointerType									BaseType;
+							public:
+								Double() = default;
+								Double(const ValueNumberDoubleType& value);
+								Double(const Double& copy);
+							};
+
+							Double::Double(const ValueNumberDoubleType& value) :
+								BaseType(new ValueNumberDoubleType(value))
+							{}
+
+							Double::Double(const Double& copy) :
+								BaseType(copy)
+							{}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_POINTER_VALUE_NUMBER_DOUBLE_H_
