1ed7d3b24af583008bfc1d6da3e2a062dc738b56
AnantaYudica <nta040689@gmail.com>
2017-09-06 18:21:31 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with test\simple\json\Memory.h  - Add dependency with test\simple\json\value\number\Short.h  - Add class BrainMuscles::Test::simple::json::pointer::value::number::Short    - Add base class with BrainMuscles::test::simple::json::Memory<BrainMuscles::test::simple::json::value::number::Short>::SharedPointerType and public inheritance    - Add aliases type with public to access      - Add alias type 'ValueNumberShortType' with BrainMuscles::test::simple::json::value::number::Short for type      - Add alias type 'MemoryValueNumberShortType' with BrainMuscles::test::simple::json::Memory<ValueNumberShortType> for type      - Add alias type 'BaseType' with MemoryValueNumberShortType::SharedPointerType for type    - Add default constructor with default by compiler and public to access    - Add fill constructor with parameter constructor 'value' with const ValueNumberShortType& for type and public to access    - Add copy constructor with public to access

---
+++ include/test/simple/json/pointer/value/number/Short.h
@@ -0,0 +1,59 @@
+#ifndef TEST_SIMPLE_JSON_POINTER_VALUE_NUMBER_SHORT_H_
+#define TEST_SIMPLE_JSON_POINTER_VALUE_NUMBER_SHORT_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\json\Memory.h"
+#include "test\simple\json\value\number\Short.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace pointer
+				{
+					namespace value
+					{
+						namespace number
+						{
+							class Short :
+								public BrainMuscles::test::simple::json::Memory<
+									BrainMuscles::test::simple::json::value::number::Short>::SharedPointerType
+							{
+							public:
+								typedef BrainMuscles::test::simple::json::value
+									::number::Short										ValueNumberShortType;
+								typedef BrainMuscles::test::simple::json::Memory<
+									ValueNumberShortType>								MemoryValueNumberShortType;
+								typedef typename MemoryValueNumberShortType
+									::SharedPointerType									BaseType;
+							public:
+								Short() = default;
+								Short(const ValueNumberShortType& value);
+								Short(const Short& copy);
+							};
+
+							Short::Short(const ValueNumberShortType& value) :
+								BaseType(new ValueNumberShortType(value))
+							{}
+
+							Short::Short(const Short& copy) :
+								BaseType(copy)
+							{}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_POINTER_VALUE_NUMBER_SHORT_H_
