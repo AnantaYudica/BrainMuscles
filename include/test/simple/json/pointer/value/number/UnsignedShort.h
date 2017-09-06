b8666b403c8f4eca9361f10e657cf129c182689d
AnantaYudica <nta040689@gmail.com>
2017-09-06 18:49:31 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with test\simple\json\Memory.h  - Add dependency with test\simple\json\value\number\UnsignedShort.h  - Add class BrainMuscles::Test::simple::json::pointer::value::number::UnsignedShort    - Add base class with BrainMuscles::test::simple::json::Memory<BrainMuscles::test::simple::json::value::number::UnsignedShort>::SharedPointerType and public inheritance    - Add aliases type with public to access      - Add alias type 'ValueNumberUnsignedShortType' with BrainMuscles::test::simple::json::value::number::UnsignedShort for type      - Add alias type 'MemoryValueNumberUnsignedShortType' with BrainMuscles::test::simple::json::Memory<ValueNumberUnsignedShortType> for type      - Add alias type 'BaseType' with MemoryValueNumberUnsignedShortType::SharedPointerType for type    - Add default constructor with default by compiler and public to access    - Add fill constructor with parameter constructor 'value' with const ValueNumberUnsignedShortType& for type and public to access    - Add copy constructor with public to access

---
+++ include/test/simple/json/pointer/value/number/UnsignedShort.h
@@ -0,0 +1,59 @@
+#ifndef TEST_SIMPLE_JSON_POINTER_VALUE_NUMBER_UNSIGNEDSHORT_H_
+#define TEST_SIMPLE_JSON_POINTER_VALUE_NUMBER_UNSIGNEDSHORT_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\json\Memory.h"
+#include "test\simple\json\value\number\UnsignedShort.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace pointer
+				{
+					namespace value
+					{
+						namespace number
+						{
+							class UnsignedShort :
+								public BrainMuscles::test::simple::json::Memory<
+									BrainMuscles::test::simple::json::value::number::UnsignedShort>::SharedPointerType
+							{
+							public:
+								typedef BrainMuscles::test::simple::json::value
+									::number::UnsignedShort								ValueNumberUnsignedShortType;
+								typedef BrainMuscles::test::simple::json::Memory<
+									ValueNumberUnsignedShortType>						MemoryValueNumberUnsignedShortType;
+								typedef typename MemoryValueNumberUnsignedShortType
+									::SharedPointerType									BaseType;
+							public:
+								UnsignedShort() = default;
+								UnsignedShort(const ValueNumberUnsignedShortType& value);
+								UnsignedShort(const UnsignedShort& copy);
+							};
+
+							UnsignedShort::UnsignedShort(const ValueNumberUnsignedShortType& value) :
+								BaseType(new ValueNumberUnsignedShortType(value))
+							{}
+
+							UnsignedShort::UnsignedShort(const UnsignedShort& copy) :
+								BaseType(copy)
+							{}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_POINTER_VALUE_NUMBER_UNSIGNEDSHORT_H_
