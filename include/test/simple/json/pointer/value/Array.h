c87c1cb56bf211fb7fd0029961786d669ca76e9c
AnantaYudica <nta040689@gmail.com>
2017-09-06 14:14:41 +0700

    ADD FOLDER AND FILE Add folder /include/test/simple/json/pointer/value Add file /include/test/simple/json/pointer/value/Array.h  - Add dependency with test\Configure.h  - Add macro if defined _USING_TEST_    - Add dependency with test\simple\json\Memory.h    - Add dependency with test\simple\json\value\Array.h    - Add class BrainMuscles::test::simple::json::pointer::value::Array      - Add base class with BrainMuscles::test::simple::json::Memory<BrainMuscles::test::simple::json::value::Array>::SharedPointerType with public inheritance      - Add aliases type with public to access        - Add alias type 'ValueArrayType' with BrainMuscles::test::simple::json::value::Array for type        - Add alias type 'MemoryValueArrayType' with BrainMuscles::test::simple::json::Memory<ValueArrayType> for type        - Add alias type 'BaseType' with MemoryValueArrayType::SharedPointerType for type      - Add default constructor with default by compiler and public to access      - Add fill constructor with parameter constructor 'value' with const ValueArrayType& for type and public to access      - Add copy constructor with public to access

---
+++ include/test/simple/json/pointer/value/Array.h
@@ -0,0 +1,53 @@
+#ifndef TEST_SIMPLE_JSON_POINTER_VALUE_ARRAY_H_
+#define TEST_SIMPLE_JSON_POINTER_VALUE_ARRAY_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include "test\simple\json\Memory.h"
+#include "test\simple\json\value\Array.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace pointer
+				{
+					namespace value
+					{
+						class Array :
+							public BrainMuscles::test::simple::json::Memory<
+								BrainMuscles::test::simple::json::value::Array>::SharedPointerType
+						{
+						public:
+							typedef BrainMuscles::test::simple::json::value::Array				ValueArrayType;
+							typedef BrainMuscles::test::simple::json::Memory<ValueArrayType>	MemoryValueArrayType;
+							typedef typename MemoryValueArrayType::SharedPointerType			BaseType;
+						public:
+							Array() = default;
+							Array(const ValueArrayType& value);
+							Array(const Array& copy);
+						};
+
+						Array::Array(const ValueArrayType& value) :
+							BaseType(new ValueArrayType(value))
+						{}
+
+						Array::Array(const Array& copy) :
+							BaseType(copy)
+						{}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_POINTER_VALUE_ARRAY_H_
