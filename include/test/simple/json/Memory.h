85e840830f71dbee4ffa08f29579a334f59104d1
AnantaYudica <nta040689@gmail.com>
2017-09-07 16:33:24 +0700

    MODIFIED FILE Add dependency with type_traits Update class BrainMuscles::test::simple::json::Memory  - Add forward declaration static functions 'IsBaseOfSharedPointerImpl' with {parameter template 'TYPE', parameter function with const SharedPointerType<TYPE>& for type and std::true_type for return type}, {parameter function with paramater pack and std::false_type for return type} and private to access  - Add alias type 'IsBaseOfSharedPointer' with parameter template 'TYPE', decltype(IsBaseOfSharedPointerImpl(std::declval<TYPE>())) for type and private to access  - Add static functions 'DynamicPointerCastImpl', 'StaticPointerCastImpl' and 'ConstPointerCastImpl' with parameters template 'FROM_SHAREDPOINTER_TYPE' and 'TO_TYPE', parameters function 'from_sharedpointer' with const FROM_SHAREDPOINTER_TYPE& for type and 'ref_pointer_to_sharedpointer' with SharedPointerType<TO_TYPE>& for type, void for return type and private to access  - Add static functions 'DynamicPointerCast', 'StaticPointerCast' and 'ConstPointerCast' with parameters type {['FROM_SHAREDPOINTER_TYPE' and 'TO_SHAREDPOINTER_TYPE'], ['FROM_TYPE', 'TO_SHAREDPOINTER_TYPE'] and ['FROM_SHAREDPOINTER_TYPE', 'TO_TYPE']}, {'TO_SHAREDPOINTER_TYPE' and 'SharedPointerType<TO_TYPE>'} for return type, parameter function 'pointer' with {'const FROM_SHAREDPOINTER_TYPE&', const FROM_SHAREDPOINTER_TYPE&} for type and public to access  - Update static function 'DynamicPointerCast', 'StaticPointerCast' and 'ConstPointerCast' with parameters type 'FROM_TYPE' and 'TO_TYPE', 'SharedPointerType<TO_TYPE>' for return type and parameter function 'pointer' with const SharedPointerType<FROM_TYPE>& for type

--- include/test/simple/json/Memory.h
+++ include/test/simple/json/Memory.h
@@ -5,6 +5,7 @@
 
 #if defined(_USING_TEST_)
 
+#include <type_traits>
 #include <memory>
 
 namespace BrainMuscles
@@ -21,35 +22,195 @@ namespace BrainMuscles
 					template<typename TYPE>
 					using SharedPointerType = std::shared_ptr<TYPE>;
 				private:
+					template<typename TYPE>
+					static std::true_type IsBaseOfSharedPointerImpl(const SharedPointerType<TYPE>&);
+					static std::false_type IsBaseOfSharedPointerImpl(...);
+				private:
+					template<typename TYPE>
+					using IsBaseOfSharedPointer = decltype(IsBaseOfSharedPointerImpl(std::declval<TYPE>()));
+				private:
 					Memory() = delete;
+				private:
+					template<typename FROM_SHAREDPOINTER_TYPE, typename TO_TYPE>
+					static void DynamicPointerCastImpl(const FROM_SHAREDPOINTER_TYPE& from_sharedpointer, SharedPointerType<TO_TYPE>& ref_pointer_to_sharedpointer);
+					template<typename FROM_SHAREDPOINTER_TYPE, typename TO_TYPE>
+					static void StaticPointerCastImpl(const FROM_SHAREDPOINTER_TYPE& from_sharedpointer, SharedPointerType<TO_TYPE>& ref_pointer_to_sharedpointer);
+					template<typename FROM_SHAREDPOINTER_TYPE, typename TO_TYPE>
+					static void ConstPointerCastImpl(const FROM_SHAREDPOINTER_TYPE& from_sharedpointer, SharedPointerType<TO_TYPE>& ref_pointer_to_sharedpointer);
 				public:
+					template<typename FROM_SHAREDPOINTER_TYPE, typename TO_SHAREDPOINTER_TYPE>
+					static typename std::enable_if<IsBaseOfSharedPointer<TO_SHAREDPOINTER_TYPE>::value, TO_SHAREDPOINTER_TYPE>::type
+						DynamicPointerCast(const FROM_SHAREDPOINTER_TYPE& pointer);
+
+					template<typename FROM_TYPE, typename TO_SHAREDPOINTER_TYPE>
+					static typename std::enable_if<IsBaseOfSharedPointer<TO_SHAREDPOINTER_TYPE>::value, TO_SHAREDPOINTER_TYPE>::type
+						DynamicPointerCast(const SharedPointerType<FROM_TYPE>& pointer);
+
+					template<typename FROM_SHAREDPOINTER_TYPE, typename TO_TYPE>
+					static typename std::enable_if<!IsBaseOfSharedPointer<TO_TYPE>::value, SharedPointerType<TO_TYPE>>::type
+						DynamicPointerCast(const FROM_SHAREDPOINTER_TYPE& pointer);
+
 					template<typename FROM_TYPE, typename TO_TYPE>
-					static SharedPointerType<TO_TYPE> DynamicPointerCast(const SharedPointerType<FROM_TYPE>& pointer);
+					static typename std::enable_if<!IsBaseOfSharedPointer<TO_TYPE>::value, SharedPointerType<TO_TYPE>>::type
+						DynamicPointerCast(const SharedPointerType<FROM_TYPE>& pointer);
+				public:
+					template<typename FROM_SHAREDPOINTER_TYPE, typename TO_SHAREDPOINTER_TYPE>
+					static typename std::enable_if<IsBaseOfSharedPointer<TO_SHAREDPOINTER_TYPE>::value, TO_SHAREDPOINTER_TYPE>::type
+						StaticPointerCast(const FROM_SHAREDPOINTER_TYPE& pointer);
+
+					template<typename FROM_TYPE, typename TO_SHAREDPOINTER_TYPE>
+					static typename std::enable_if<IsBaseOfSharedPointer<TO_SHAREDPOINTER_TYPE>::value, TO_SHAREDPOINTER_TYPE>::type
+						StaticPointerCast(const SharedPointerType<FROM_TYPE>& pointer);
+
+					template<typename FROM_SHAREDPOINTER_TYPE, typename TO_TYPE>
+					static typename std::enable_if<!IsBaseOfSharedPointer<TO_TYPE>::value, SharedPointerType<TO_TYPE>>::type
+						StaticPointerCast(const FROM_SHAREDPOINTER_TYPE& pointer);
+
 					template<typename FROM_TYPE, typename TO_TYPE>
-					static SharedPointerType<TO_TYPE> StaticPointerCast(const SharedPointerType<FROM_TYPE>& pointer);
+					static typename std::enable_if<!IsBaseOfSharedPointer<TO_TYPE>::value, SharedPointerType<TO_TYPE>>::type
+						StaticPointerCast(const SharedPointerType<FROM_TYPE>& pointer);
+				public:
+					template<typename FROM_SHAREDPOINTER_TYPE, typename TO_SHAREDPOINTER_TYPE>
+					static typename std::enable_if<IsBaseOfSharedPointer<TO_SHAREDPOINTER_TYPE>::value, TO_SHAREDPOINTER_TYPE>::type
+						ConstPointerCast(const FROM_SHAREDPOINTER_TYPE& pointer);
+
+					template<typename FROM_TYPE, typename TO_SHAREDPOINTER_TYPE>
+					static typename std::enable_if<IsBaseOfSharedPointer<TO_SHAREDPOINTER_TYPE>::value, TO_SHAREDPOINTER_TYPE>::type
+						ConstPointerCast(const SharedPointerType<FROM_TYPE>& pointer);
+
+					template<typename FROM_SHAREDPOINTER_TYPE, typename TO_TYPE>
+					static typename std::enable_if<!IsBaseOfSharedPointer<TO_TYPE>::value, SharedPointerType<TO_TYPE>>::type
+						ConstPointerCast(const FROM_SHAREDPOINTER_TYPE& pointer);
+
 					template<typename FROM_TYPE, typename TO_TYPE>
-					static SharedPointerType<TO_TYPE> ConstPointerCast(const SharedPointerType<FROM_TYPE>& pointer);
+					static typename std::enable_if<!IsBaseOfSharedPointer<TO_TYPE>::value, SharedPointerType<TO_TYPE>>::type
+						ConstPointerCast(const SharedPointerType<FROM_TYPE>& pointer);
 				};
 
-				template<typename FROM_TYPE, typename TO_TYPE>
-				typename Memory::SharedPointerType<TO_TYPE>
+				template<typename FROM_SHAREDPOINTER_TYPE, typename TO_TYPE>
+				void Memory::DynamicPointerCastImpl(const FROM_SHAREDPOINTER_TYPE& from_sharedpointer, SharedPointerType<TO_TYPE>& ref_pointer_to_sharedpointer)
+				{
+					ref_pointer_to_sharedpointer = std::dynamic_pointer_cast<TO_TYPE>(from_sharedpointer);
+				}
+
+				template<typename FROM_SHAREDPOINTER_TYPE, typename TO_TYPE>
+				void Memory::StaticPointerCastImpl(const FROM_SHAREDPOINTER_TYPE& from_sharedpointer, SharedPointerType<TO_TYPE>& ref_pointer_to_sharedpointer)
+				{
+					ref_pointer_to_sharedpointer = std::static_pointer_cast<TO_TYPE>(from_sharedpointer);
+				}
+
+				template<typename FROM_SHAREDPOINTER_TYPE, typename TO_TYPE>
+				void Memory::ConstPointerCastImpl(const FROM_SHAREDPOINTER_TYPE& from_sharedpointer, SharedPointerType<TO_TYPE>& ref_pointer_to_sharedpointer)
+				{
+					ref_pointer_to_sharedpointer = std::const_pointer_cast<TO_TYPE>(from_sharedpointer);
+				}
+
+				template<typename FROM_SHAREDPOINTER_TYPE, typename TO_SHAREDPOINTER_TYPE>
+				typename std::enable_if<Memory::IsBaseOfSharedPointer<TO_SHAREDPOINTER_TYPE>::value, TO_SHAREDPOINTER_TYPE>::type
+					Memory::DynamicPointerCast(const FROM_SHAREDPOINTER_TYPE& pointer)
+				{
+					TO_SHAREDPOINTER_TYPE pointer_to_sharedpointer;
+					DynamicPointerCastImpl(pointer, pointer_to_sharedpointer);
+					return pointer_to_sharedpointer;
+				}
+
+				template<typename FROM_TYPE, typename TO_SHAREDPOINTER_TYPE>
+				typename std::enable_if<Memory::IsBaseOfSharedPointer<TO_SHAREDPOINTER_TYPE>::value, TO_SHAREDPOINTER_TYPE>::type
 					Memory::DynamicPointerCast(const SharedPointerType<FROM_TYPE>& pointer)
 				{
-					return std::dynamic_pointer_cast<TO_TYPE>(pointer);
+					TO_SHAREDPOINTER_TYPE pointer_to_sharedpointer;
+					DynamicPointerCastImpl(pointer, pointer_to_sharedpointer);
+					return pointer_to_sharedpointer;
+				}
+
+				template<typename FROM_SHAREDPOINTER_TYPE, typename TO_TYPE>
+				typename std::enable_if<!Memory::IsBaseOfSharedPointer<TO_TYPE>::value, Memory::SharedPointerType<TO_TYPE>>::type
+					Memory::DynamicPointerCast(const FROM_SHAREDPOINTER_TYPE& pointer)
+				{
+					Memory::SharedPointerType<TO_TYPE> pointer_to_sharedpointer;
+					DynamicPointerCastImpl(pointer, pointer_to_sharedpointer);
+					return pointer_to_sharedpointer;
+				}
+
+				template<typename FROM_TYPE, typename TO_TYPE>
+				typename std::enable_if<!Memory::IsBaseOfSharedPointer<TO_TYPE>::value, Memory::SharedPointerType<TO_TYPE>>::type
+					Memory::DynamicPointerCast(const SharedPointerType<FROM_TYPE>& pointer)
+				{
+					Memory::SharedPointerType<TO_TYPE> pointer_to_sharedpointer;
+					DynamicPointerCastImpl(pointer, pointer_to_sharedpointer);
+					return pointer_to_sharedpointer;
 				}
 				
-				template<typename FROM_TYPE, typename TO_TYPE>
-				typename Memory::SharedPointerType<TO_TYPE>
+				template<typename FROM_SHAREDPOINTER_TYPE, typename TO_SHAREDPOINTER_TYPE>
+				typename std::enable_if<Memory::IsBaseOfSharedPointer<TO_SHAREDPOINTER_TYPE>::value, TO_SHAREDPOINTER_TYPE>::type
+					Memory::StaticPointerCast(const FROM_SHAREDPOINTER_TYPE& pointer)
+				{
+					TO_SHAREDPOINTER_TYPE pointer_to_sharedpointer;
+					StaticPointerCastImpl(pointer, pointer_to_sharedpointer);
+					return pointer_to_sharedpointer;
+				}
+
+				template<typename FROM_TYPE, typename TO_SHAREDPOINTER_TYPE>
+				typename std::enable_if<Memory::IsBaseOfSharedPointer<TO_SHAREDPOINTER_TYPE>::value, TO_SHAREDPOINTER_TYPE>::type
 					Memory::StaticPointerCast(const SharedPointerType<FROM_TYPE>& pointer)
 				{
-					return std::static_pointer_cast<TO_TYPE>(pointer);
+					TO_SHAREDPOINTER_TYPE pointer_to_sharedpointer;
+					StaticPointerCastImpl(pointer, pointer_to_sharedpointer);
+					return pointer_to_sharedpointer;
 				}
-				
+
+				template<typename FROM_SHAREDPOINTER_TYPE, typename TO_TYPE>
+				typename std::enable_if<!Memory::IsBaseOfSharedPointer<TO_TYPE>::value, Memory::SharedPointerType<TO_TYPE>>::type
+					Memory::StaticPointerCast(const FROM_SHAREDPOINTER_TYPE& pointer)
+				{
+					Memory::SharedPointerType<TO_TYPE> pointer_to_sharedpointer;
+					StaticPointerCastImpl(pointer, pointer_to_sharedpointer);
+					return pointer_to_sharedpointer;
+				}
+
 				template<typename FROM_TYPE, typename TO_TYPE>
-				typename Memory::SharedPointerType<TO_TYPE>
+				typename std::enable_if<!Memory::IsBaseOfSharedPointer<TO_TYPE>::value, Memory::SharedPointerType<TO_TYPE>>::type
+					Memory::StaticPointerCast(const SharedPointerType<FROM_TYPE>& pointer)
+				{
+					Memory::SharedPointerType<TO_TYPE> pointer_to_sharedpointer;
+					StaticPointerCastImpl(pointer, pointer_to_sharedpointer);
+					return pointer_to_sharedpointer;
+				}
+
+				template<typename FROM_SHAREDPOINTER_TYPE, typename TO_SHAREDPOINTER_TYPE>
+				typename std::enable_if<Memory::IsBaseOfSharedPointer<TO_SHAREDPOINTER_TYPE>::value, TO_SHAREDPOINTER_TYPE>::type
+					Memory::ConstPointerCast(const FROM_SHAREDPOINTER_TYPE& pointer)
+				{
+					TO_SHAREDPOINTER_TYPE pointer_to_sharedpointer;
+					ConstPointerCastImpl(pointer, pointer_to_sharedpointer);
+					return pointer_to_sharedpointer;
+				}
+
+				template<typename FROM_TYPE, typename TO_SHAREDPOINTER_TYPE>
+				typename std::enable_if<Memory::IsBaseOfSharedPointer<TO_SHAREDPOINTER_TYPE>::value, TO_SHAREDPOINTER_TYPE>::type
 					Memory::ConstPointerCast(const SharedPointerType<FROM_TYPE>& pointer)
 				{
-					return std::const_pointer_cast<TO_TYPE>(pointer);
+					TO_SHAREDPOINTER_TYPE pointer_to_sharedpointer;
+					ConstPointerCastImpl(pointer, pointer_to_sharedpointer);
+					return pointer_to_sharedpointer;
+				}
+
+				template<typename FROM_SHAREDPOINTER_TYPE, typename TO_TYPE>
+				typename std::enable_if<!Memory::IsBaseOfSharedPointer<TO_TYPE>::value, Memory::SharedPointerType<TO_TYPE>>::type
+					Memory::ConstPointerCast(const FROM_SHAREDPOINTER_TYPE& pointer)
+				{
+					Memory::SharedPointerType<TO_TYPE> pointer_to_sharedpointer;
+					ConstPointerCastImpl(pointer, pointer_to_sharedpointer);
+					return pointer_to_sharedpointer;
+				}
+
+				template<typename FROM_TYPE, typename TO_TYPE>
+				typename std::enable_if<!Memory::IsBaseOfSharedPointer<TO_TYPE>::value, Memory::SharedPointerType<TO_TYPE>>::type
+					Memory::ConstPointerCast(const SharedPointerType<FROM_TYPE>& pointer)
+				{
+					Memory::SharedPointerType<TO_TYPE> pointer_to_sharedpointer;
+					ConstPointerCastImpl(pointer, pointer_to_sharedpointer);
+					return pointer_to_sharedpointer;
 				}
 			}
 		}
