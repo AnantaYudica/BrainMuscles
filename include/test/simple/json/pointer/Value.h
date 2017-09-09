550816a3376a145b6b4df8ca3d47338973f80bf3
AnantaYudica <nta040689@gmail.com>
2017-09-09 19:08:00 +0700

    MODIFIED FILE Update class BrainMuscles::test::simple::json::pointer::Value  - Remove fill constructor with parameter constructor const ValueType&  - Add fill constructor with parameter template 'VALUE_TYPE', parameter constructor 'value' with const VALUE_TYPE& for type and public to access  - add static function 'MakeShared' with parameter template 'VALUE_TYPE', BaseType for return type, parameter function 'value' with const VALUE_TYPE& for type and public to access

--- include/test/simple/json/pointer/Value.h
+++ include/test/simple/json/pointer/Value.h
@@ -29,17 +29,29 @@ namespace BrainMuscles
 						typedef typename MemoryType::SharedPointerType<ValueType>		BaseType;
 					public:
 						Value() = default;
-						Value(const ValueType& value);
+						template<typename VALUE_TYPE>
+						Value(const VALUE_TYPE& value);
 						Value(const Value& copy);
+					private:
+						template<typename VALUE_TYPE>
+						static BaseType MakeShared(const VALUE_TYPE& value);
 					};
 
-					Value::Value(const ValueType& value) :
-						BaseType(value.MakeShared())
+					template<typename VALUE_TYPE>
+					Value::Value(const VALUE_TYPE& value) :
+						BaseType(MakeShared<VALUE_TYPE>(value))
 					{}
 
 					Value::Value(const Value& copy) :
 						BaseType(copy)
 					{}
+
+					template<typename VALUE_TYPE>
+					typename Value::BaseType Value::MakeShared(const VALUE_TYPE& value)
+					{
+						static_assert(std::is_base_of<ValueType, VALUE_TYPE>::value, "VALUE_TYPE is not base of BrainMuscles::test::simple::json::Value");
+						return BaseType(new VALUE_TYPE(value));
+					}
 				}
 			}
 		}
