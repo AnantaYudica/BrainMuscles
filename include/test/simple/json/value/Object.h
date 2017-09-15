850437f4a23288e7c6fa338b755f391ecf7a8f16
AnantaYudica <nta040689@gmail.com>
2017-09-16 06:59:38 +0700

    MODIFIED FILE Add dependency with test\simple\json\Container.h Update class BrainMuscles::test::simple::json::value::Object  - Remove base class with BrainMuscles::test::simple::json::Constant::ContainerObjectType  - Update alias type 'ValueType' with BrainMuscles::test::simple::json::Container::ObjectValueType for type  - Add aliases type with public to access    - Add alias type 'ElementType' with BrainMuscles::test::simple::json::Container::ObjectElementType for type    - Add alias type 'IteratorType' with BrainMuscles::test::simple::json::Container::ObjectIteratorType for type    - Add alias type 'ConstIteratorType' with BrainMuscles::test::simple::json::Container::ObjectConstIteratorType for type    - Add alias type 'SizeType' with BrainMuscles::test::simple::json::Container::ObjectSizeType for type    - Add alias type 'KeyType' with BrainMuscles::test::simple::json::Container::ObjectKeyType for type  - Add variable member 'm_container' with ContainerType for type and private to access  - Update copy and move constructor  - Add static functions 'Value' with {ValueType&, const ValueType&} for type, parameter function 'element' with {ElementType&, ElementType} for type and public to access  - Add function member 'Key' with parameter template 'IS_OBJECT_HAS_KEY_IN_ELEMENT' with bool for type and BrainMuscles::test::simple::json::Container::ObjectHasKeyInElement for default value, KeyType for return type, parameter function 'element' with const ElementType& for type and public to access  - Add functions member and const member 'At' and 'operator[]' with {ValueType&, const ValueType&} for return type, parameter function 'key' with const KeyType& for type and public to access  - Add functions member and const member 'Begin', 'End', 'ConstBegin', 'ConstEnd' with {IteratorType, ConstIteratorType} for return type and public to access  - Add function member 'Insert' with parameter template 'INPUT_ITERATOR_TYPE', {IteratorType, void} for return type, parameter function {'value' with const ElementType& for type, {{'first', 'last'} with INPUT_ITERATOR_TYPE for type} and public to access  - Add function member 'Emplace' with parameter variadic template 'ARGS', IteratorType for return type, parameter function 'args' with ARGS... for type, and public to access  - Add functions member 'Erase' with IteratorType for return type, parameter function {'position' with ConstIteratorType for type, {'first', 'last'} with ConstIteratorType for type} and public to access  - Add function member 'Clear' with void for return type and public to access  - Add function const member 'Size' with SizeType for return type and public to access  - Add functions member and const member 'Find' with {IteratorType, ConstIteratorType} for return type, parameter function 'key' with const KeyType& for type and public to access  - Add function member 'Swap' with void return type , parameter function 'object' with Object& for type and public to access  - Add function member 'operator=' with Object& for return type, parameter function 'object' with const Object& for type and public to access  - Add functions member and const member 'Container' with {ContainerType&, const ContainerType&} for return type and public to access

--- include/test/simple/json/value/Object.h
+++ include/test/simple/json/value/Object.h
@@ -6,6 +6,7 @@
 #if defined(_USING_TEST_)
 
 #include "test\simple\json\Constant.h"
+#include "test\simple\json\Container.h"
 
 namespace BrainMuscles
 {
@@ -18,37 +19,240 @@ namespace BrainMuscles
 				namespace value
 				{
 					class Object :
-						public BrainMuscles::test::simple::json::Constant::ValueType,
-						public BrainMuscles::test::simple::json::Constant::ContainerObjectType
+						public BrainMuscles::test::simple::json::Constant::ValueType
 					{
 					public:
 						typedef BrainMuscles::test::simple::json::Constant				ConstantType;
-						typedef typename ConstantType::ValueType						ValueType;
 						typedef typename ConstantType::ContainerObjectType				ContainerType;
+						typedef BrainMuscles::test::simple::json::Container
+							::ObjectElementType											ElementType;
+						typedef BrainMuscles::test::simple::json::Container
+							::ObjectIteratorType										IteratorType;
+						typedef BrainMuscles::test::simple::json::Container
+							::ObjectConstIteratorType									ConstIteratorType;
+						typedef BrainMuscles::test::simple::json::Container
+							::ObjectSizeType											SizeType;
+						typedef BrainMuscles::test::simple::json::Container
+							::ObjectKeyType												KeyType;
+						typedef BrainMuscles::test::simple::json::Container
+							::ObjectValueType											ValueType;
+					private:
+						ContainerType m_container;
 					public:
 						Object();
 						Object(const Object& copy);
 						Object(Object&& move);
 					public:
 						~Object();
+					public:
+						static ValueType& Value(ElementType& element);
+						static const ValueType& Value(const ElementType& element);
+					public:
+						template<bool IS_OBJECT_HAS_KEY_IN_ELEMENT = BrainMuscles::test::simple::json::Container::ObjectHasKeyInElement>
+						KeyType Key(const ElementType& element);
+						ValueType& At(const KeyType& key);
+						const ValueType& At(const KeyType& key) const;
+						IteratorType Begin();
+						ConstIteratorType Begin() const;
+						ConstIteratorType ConstBegin();
+						IteratorType End();
+						ConstIteratorType End() const;
+						ConstIteratorType ConstEnd();
+						IteratorType Insert(const ElementType& value);
+						template<typename INPUT_ITERATOR_TYPE>
+						void Insert(INPUT_ITERATOR_TYPE first, INPUT_ITERATOR_TYPE last);
+						template<typename... ARGS>
+						IteratorType Emplace(ARGS... args);
+						IteratorType Erase(ConstIteratorType position);
+						IteratorType Erase(ConstIteratorType first, ConstIteratorType last);
+						void Clear();
+						SizeType Size() const;
+						IteratorType Find(const KeyType& key);
+						ConstIteratorType Find(const KeyType& key) const;
+						void Swap(Object& object);
+					public:
+						ValueType& operator[](const KeyType& key);
+						const ValueType& operator[](const KeyType& key) const;
+					public:
+						Object& operator=(const Object& object);
+					public:
+						ContainerType& Container();
+						const ContainerType& Container() const;
 					};
 
 					Object::Object()
 					{}
 
 					Object::Object(const Object& copy) :
-						ContainerType(copy)
+						m_container(copy.m_container)
 					{}
 
 					Object::Object(Object&& move) :
-						ContainerType(move)
+						m_container(move.m_container)
 					{}
 
 					Object::~Object()
 					{}
+
+					typename Object::ValueType& Object::Value(ElementType& element)
+					{
+						return BrainMuscles::test::simple::json::Container::ValueImpl(element);
+					}
+
+					const typename Object::ValueType& Object::Value(const ElementType& element)
+					{
+						return BrainMuscles::test::simple::json::Container::ValueImpl(element);
+					}
+
+					template<bool IS_OBJECT_HAS_KEY_IN_ELEMENT>
+					typename Object::KeyType Object::Key(const ElementType& element)
+					{
+						return BrainMuscles::test::simple::json::Container::KeyImpl<IS_OBJECT_HAS_KEY_IN_ELEMENT>(m_container, element);
+					}
+
+					typename Object::ValueType& Object::At(const KeyType& key)
+					{
+						return BrainMuscles::test::simple::json::Container::AtImpl(m_container, key);
+					}
+
+					const typename Object::ValueType& Object::At(const KeyType& key) const
+					{
+						return BrainMuscles::test::simple::json::Container::AtImpl(m_container, key);
+					}
+
+					typename Object::IteratorType Object::Begin()
+					{
+						return BrainMuscles::test::simple::json::Container::BeginImpl(m_container);
+					}
+
+					typename Object::ConstIteratorType Object::Begin() const
+					{
+						return BrainMuscles::test::simple::json::Container::BeginImpl(m_container);
+					}
+
+					typename Object::ConstIteratorType Object::ConstBegin()
+					{
+						return BrainMuscles::test::simple::json::Container::ConstBeginImpl(m_container);
+					}
+
+					typename Object::IteratorType Object::End()
+					{
+						return BrainMuscles::test::simple::json::Container::EndImpl(m_container);
+					}
+
+					typename Object::ConstIteratorType Object::End() const
+					{
+						return BrainMuscles::test::simple::json::Container::EndImpl(m_container);
+					}
+
+					typename Object::ConstIteratorType Object::ConstEnd()
+					{
+						return BrainMuscles::test::simple::json::Container::ConstEndImpl(m_container);
+					}
+
+					typename Object::IteratorType Object::Insert(const ElementType& value)
+					{
+						return BrainMuscles::test::simple::json::Container::InsertImpl(m_container, value);
+					}
+
+					template<typename INPUT_ITERATOR_TYPE>
+					void Object::Insert(INPUT_ITERATOR_TYPE first, INPUT_ITERATOR_TYPE last)
+					{
+						return BrainMuscles::test::simple::json::Container::InsertImpl(m_container, first, last);
+					}
+
+					template<typename... ARGS>
+					typename Object::IteratorType Object::Emplace(ARGS... args)
+					{
+						return BrainMuscles::test::simple::json::Container::EmplaceImpl(m_container, args...);
+					}
+
+					typename Object::IteratorType Object::Erase(ConstIteratorType position)
+					{
+						return BrainMuscles::test::simple::json::Container::EraseImpl(m_container, position);
+					}
+
+					typename Object::IteratorType Object::Erase(ConstIteratorType first, ConstIteratorType last)
+					{
+						return BrainMuscles::test::simple::json::Container::EraseImpl(m_container, first, last);
+					}
+
+					void Object::Clear()
+					{
+						return BrainMuscles::test::simple::json::Container::ClearImpl(m_container);
+					}
+
+					typename Object::SizeType Object::Size() const
+					{
+						return BrainMuscles::test::simple::json::Container::SizeImpl(m_container);
+					}
+
+					typename Object::IteratorType Object::Find(const KeyType& key)
+					{
+						return BrainMuscles::test::simple::json::Container::FindImpl(m_container, key);
+					}
+
+					typename Object::ConstIteratorType Object::Find(const KeyType& key) const
+					{
+						return BrainMuscles::test::simple::json::Container::FindImpl(m_container, key);
+					}
+
+					void Object::Swap(Object& object)
+					{
+						return BrainMuscles::test::simple::json::Container::SwapImpl(m_container, object.m_container);
+					}
+
+					typename Object::ValueType& Object::operator[](const KeyType& key)
+					{
+						return BrainMuscles::test::simple::json::Container::ArraySubscriptImpl(m_container, key);
+					}
+
+					const typename Object::ValueType& Object::operator[](const KeyType& key) const
+					{
+						return BrainMuscles::test::simple::json::Container::ArraySubscriptImpl(m_container, key);;
+					}
+
+					Object& Object::operator=(const Object& object)
+					{
+						m_container.operator=(object.m_container);
+						return *this;
+					}
+
+					typename Object::ContainerType& Object::Container()
+					{
+						return m_container;
+					}
+
+					const typename Object::ContainerType& Object::Container() const
+					{
+						return m_container;
+					}
 				}
 			}
 		}
+	}
+}
+
+namespace std
+{
+	auto inline begin(BrainMuscles::test::simple::json::value::Object& _Cont) -> decltype(_Cont.Begin())
+	{
+		return _Cont.Begin();
+	}
+
+	auto inline end(BrainMuscles::test::simple::json::value::Object& _Cont) -> decltype(_Cont.End())
+	{
+		return _Cont.End();
+	}
+
+	auto inline begin(const BrainMuscles::test::simple::json::value::Object& _Cont) -> decltype(_Cont.Begin())
+	{
+		return _Cont.Begin();
+	}
+
+	auto inline end(const BrainMuscles::test::simple::json::value::Object& _Cont) -> decltype(_Cont.End())
+	{
+		return _Cont.End();
 	}
 }
 
