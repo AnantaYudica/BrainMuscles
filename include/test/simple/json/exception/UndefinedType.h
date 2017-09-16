ff748703a2acbc846799ace7952662d0b92e8a94
AnantaYudica <nta040689@gmail.com>
2017-09-17 00:04:19 +0700

    MODIFIED FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add dependency with exception  - Add dependency with test\simple\type\Name.h  - Add class BrainMuscles::test::simple::json::exception::UndefinedType    - Add parameter template 'TYPE'    - Add base class with std::exception    - Add static variable 'Message' with const std::string for type and public to access    - Add default constructor with default by compiler and public to access    - Add destructor with default by compiler and public to access    - Add function const member 'what' with const char* for return type and public to access    - Add static function 'InstanceMessage' with const std::string for return and public to access

---
+++ include/test/simple/json/exception/UndefinedType.h
@@ -0,0 +1,61 @@
+#ifndef TEST_SIMPLE_JSON_EXCEPTION_UNDEFINEDTYPE_H_
+#define TEST_SIMPLE_JSON_EXCEPTION_UNDEFINEDTYPE_H_
+
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#include <exception>
+
+#include "test\simple\type\Name.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace exception
+				{
+					template<typename TYPE>
+					class UndefinedType :
+						public std::exception
+					{
+					public:
+						static const std::string Message;
+					public:
+						UndefinedType() = default;
+						~UndefinedType() = default;
+					public:
+						const char* what() const;
+					private:
+						static const std::string InstanceMessage();
+					};
+
+					template<typename TYPE>
+					const std::string UndefinedType<TYPE>::Message = InstanceMessage();
+
+					template<typename TYPE>
+					const char* UndefinedType<TYPE>::what() const
+					{
+						return Message.c_str();
+					}
+
+					template<typename TYPE>
+					const std::string UndefinedType<TYPE>::InstanceMessage()
+					{
+						std::string message = "Undefined Type with ";
+						message += BrainMuscles::test::simple::type::Name<TYPE>::Value();
+						return message;
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
+
+#endif //!TEST_SIMPLE_JSON_EXCEPTION_UNDEFINEDTYPE_H_
