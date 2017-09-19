c2fe957d9a80b3f26a0ec115aa048b7bc2b929ba
AnantaYudica <nta040689@gmail.com>
2017-09-19 15:35:10 +0700

    MODIFIED FILE Add dependency with string Update primary template class BrainMuscles::test::simple::json::exception::UndefinedType  - Add parameter template 'VALUE_TYPE' with void for default type  - Add variable member 'Value' with const VALUE_TYPE for type and public to access  - remove static keyword in variable 'Message'  - Remove default constructor  - Add fill constructor with parameter constructor 'value' with VALUE for type and public to access  - Update static function 'InstanceMessage' with parameter function 'value' with const VALUE_TYPE& for type Add specialize template class BrainMuscles::test::simple::json::exception::UndefinedType with VALUE_TYPE = void  - Add base class with std::exception with public inheritance  - Add static variable 'Message' with const std::string for type and public to access  - Add default constructor with default by complier and public to access  - Add destructor with default by compiler and public to access  - Add function const member 'what' with const char * for return type and public to access  - Add static function 'InstanceMessage' with const std::string for return type and private to access

--- include/test/simple/json/exception/UndefinedType.h
+++ include/test/simple/json/exception/UndefinedType.h
@@ -6,6 +6,7 @@
 #if defined(_USING_TEST_)
 
 #include <exception>
+#include <string>
 
 #include "test\simple\type\Name.h"
 
@@ -19,8 +20,24 @@ namespace BrainMuscles
 			{
 				namespace exception
 				{
-					template<typename TYPE>
+					template<typename TYPE, typename VALUE_TYPE = void>
 					class UndefinedType :
+						public std::exception
+					{
+					public:
+						const VALUE_TYPE Value;
+						const std::string Message;
+					public:
+						UndefinedType(VALUE_TYPE value);
+						~UndefinedType() = default;
+					public:
+						const char* what() const;
+					private:
+						static const std::string InstanceMessage(const VALUE_TYPE& value);
+					};
+
+					template<typename TYPE>
+					class UndefinedType<TYPE, void> :
 						public std::exception
 					{
 					public:
@@ -34,17 +51,37 @@ namespace BrainMuscles
 						static const std::string InstanceMessage();
 					};
 
-					template<typename TYPE>
-					const std::string UndefinedType<TYPE>::Message = InstanceMessage();
+					template<typename TYPE, typename VALUE_TYPE>
+					UndefinedType<TYPE, VALUE_TYPE>::UndefinedType(VALUE_TYPE value) :
+						Value(value),
+						Message(InstanceMessage(value))
+					{}
+
+					template<typename TYPE, typename VALUE_TYPE>
+					const char* UndefinedType<TYPE, VALUE_TYPE>::what() const
+					{
+						return Message.c_str();
+					}
+
+					template<typename TYPE, typename VALUE_TYPE>
+					const std::string UndefinedType<TYPE, VALUE_TYPE>::InstanceMessage(const VALUE_TYPE& value)
+					{
+						std::string message = "Undefined Type with ";
+						message += BrainMuscles::test::simple::type::Name<TYPE>::Value(value);
+						return message;
+					}
 
 					template<typename TYPE>
-					const char* UndefinedType<TYPE>::what() const
+					const std::string UndefinedType<TYPE, void>::Message = InstanceMessage();
+
+					template<typename TYPE>
+					const char* UndefinedType<TYPE, void>::what() const
 					{
 						return Message.c_str();
 					}
 
 					template<typename TYPE>
-					const std::string UndefinedType<TYPE>::InstanceMessage()
+					const std::string UndefinedType<TYPE, void>::InstanceMessage()
 					{
 						std::string message = "Undefined Type with ";
 						message += BrainMuscles::test::simple::type::Name<TYPE>::Value();
