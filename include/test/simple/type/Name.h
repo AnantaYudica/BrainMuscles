f44f593e1f817febcc806a2c7861dea05bbbcd06
AnantaYudica <nta040689@gmail.com>
2017-09-19 14:21:08 +0700

    MODIFIED FILE Add dependency with type_traits Add variable BrainMuscles::test::simple::type::name::DefaultAliasName with char[] for type and "TYPE" for value Add variable BrainMuscles::test::simple::type::name::DefaultBeginAliasName with char[] for type and "[" for value Add variable BrainMuscles::test::simple::type::name::DefaultEndAliasName with char[] for type and "]" for value Update primary template class BrainMuscles::test::simple::type::Name  - Add parameter template ALIAS_NAME with const char* for type and BrainMuscles::test::simple::type::name::DefaultAliasName for default value  - Add alias type 'FunctionTypeNameInstanceType' with void(*)(std::string&, std::string, std::string) for type and protected to access  - Add static function 'AliasNameInstance' with void for return type and parameter function 'out' with std::string& for type and private to access  - Add static functions 'ValueOfTypeInstance' with void for return type, parameters function 'out' with std::string& and 'value' with VALUE_TYPE and private to access    - enable if {std::string, VALUE_TYPE} is same or {VALUE_TYPE is pointer and {VALUE_TYPE, char} is same}    - enable if {std::string, VALUE_TYPE} is not same and not {VALUE_TYPE is pointer and {VALUE_TYPE, char} is same}  - Delete static function 'NameInstance'  - Add static function 'TypeNameInstance' with void for return type, parameters function 'out' with std::string& for type, {'pre_name' and 'post_name'} with std::string for type and "" for default value and protected to access  - Add static functions 'ValueInstance' with parameter template 'VALUE_TYPE', void for return type, parameters function {'typenameinstance' with FunctionTypeNameInstanceType for type, 'out' with std::string& for type} or 'value' with VALUE_TYPE for type and protected to access  - Add static function 'Value' with parameter template 'VALUE_TYPE', std::string for return type, parameter function 'value' with VALUE_TYPE for type and protected to access Update in specialize template class BrainMuscles::test::simple::type::Name with TYPE = {const TYPE, volatile TYPE, const volatile TYPE, TYPE*, TYPE* const, TYPE* volatile, TYPE* const volatile, TYPE&, TYPE&&}  - Add parameter template ALIAS_NAME with const char* for type  - Remove static function 'NameInstance'  - Add static function 'TypeNameInstance' with void for return type, parameters function 'out' with std::string& for type, {'pre_name' and 'post_name'} with std::string for type and "" for default value and protected to access  - Add static function 'Value' with parameter template 'VALUE_TYPE', std::string for return type, parameter function 'value' with VALUE_TYPE for type and protected to access

--- include/test/simple/type/Name.h
+++ include/test/simple/type/Name.h
@@ -6,6 +6,7 @@
 #if defined(_USING_TEST_)
 
 #include <string>
+#include <type_traits>
 #include <typeindex>
 
 namespace BrainMuscles
@@ -16,229 +17,422 @@ namespace BrainMuscles
 		{
 			namespace type
 			{
-				template<typename TYPE>
+				namespace name
+				{
+					char DefaultAliasName[] = "TYPE";
+					char DefaultBeginAliasName[] = "[";
+					char DefaultEndAliasName[] = "]";
+				}
+
+				template<typename TYPE, const char* ALIAS_NAME = BrainMuscles::test::simple::type::name::DefaultAliasName>
 				class Name
 				{
 				protected:
-					static std::string NameInstance(std::string pre_name = "", std::string post_name = "");
+					typedef void(*FunctionTypeNameInstanceType)(std::string&, std::string, std::string);
+				private:
+					static void AliasNameInstance(std::string& out);
+					template<typename VALUE_TYPE>
+					static typename std::enable_if<std::is_same<std::string, typename std::remove_cv<typename std::remove_reference<VALUE_TYPE>::type>::type>::value
+						|| (std::is_pointer<VALUE_TYPE>::value && std::is_same<typename std::remove_cv<typename std::remove_pointer<VALUE_TYPE>::type>::type, char>::value)>::type
+						ValueOfTypeInstance(std::string& out, VALUE_TYPE value);
+					template<typename VALUE_TYPE>
+					static typename std::enable_if<!std::is_same<std::string, VALUE_TYPE>::value && !(std::is_pointer<VALUE_TYPE>::value && std::is_same<typename std::remove_cv<typename std::remove_pointer<VALUE_TYPE>::type>::type, char>::value)>::type
+						ValueOfTypeInstance(std::string& out, VALUE_TYPE value);
+				protected:
+					static void TypeNameInstance(std::string& out, std::string pre_name = "", std::string post_name = "");
+					static void ValueInstance(FunctionTypeNameInstanceType typenameinstance ,std::string& out);
+					template<typename VALUE_TYPE>
+					static void ValueInstance(FunctionTypeNameInstanceType typenameinstance, std::string& out, VALUE_TYPE value);
 				public:
 					static std::string Value();
+					template<typename VALUE_TYPE>
+					static std::string Value(VALUE_TYPE value);
 				};
 
-				template<typename TYPE>
-				class Name<const TYPE> :
-					public Name<TYPE>
+				template<typename TYPE, const char* ALIAS_NAME>
+				class Name<const TYPE, ALIAS_NAME> :
+					public Name<TYPE, ALIAS_NAME>
 				{
 				protected:
-					static std::string NameInstance(std::string pre_name = "", std::string post_name = "");
+					static void TypeNameInstance(std::string& out, std::string pre_name = "", std::string post_name = "");
 				public:
 					static std::string Value();
+					template<typename VALUE_TYPE>
+					static std::string Value(VALUE_TYPE value);
 				};
 
-				template<typename TYPE>
-				class Name<volatile TYPE> :
-					public Name<TYPE>
+				template<typename TYPE, const char* ALIAS_NAME>
+				class Name<volatile TYPE, ALIAS_NAME> :
+					public Name<TYPE, ALIAS_NAME>
 				{
 				protected:
-					static std::string NameInstance(std::string pre_name = "", std::string post_name = "");
+					static void TypeNameInstance(std::string& out, std::string pre_name = "", std::string post_name = "");
 				public:
 					static std::string Value();
+					template<typename VALUE_TYPE>
+					static std::string Value(VALUE_TYPE value);
 				};
 
-				template<typename TYPE>
-				class Name<const volatile TYPE> :
-					public Name<TYPE>
+				template<typename TYPE, const char* ALIAS_NAME>
+				class Name<const volatile TYPE, ALIAS_NAME> :
+					public Name<TYPE, ALIAS_NAME>
 				{
 				protected:
-					static std::string NameInstance(std::string pre_name = "", std::string post_name = "");
+					static void TypeNameInstance(std::string& out, std::string pre_name = "", std::string post_name = "");
 				public:
 					static std::string Value();
+					template<typename VALUE_TYPE>
+					static std::string Value(VALUE_TYPE value);
 				};
 
-				template<typename TYPE>
-				class Name<TYPE*> :
-					public Name<TYPE>
+				template<typename TYPE, const char* ALIAS_NAME>
+				class Name<TYPE*, ALIAS_NAME> :
+					public Name<TYPE, ALIAS_NAME>
 				{
 				protected:
-					static std::string NameInstance(std::string pre_name = "", std::string post_name = "");
+					static void TypeNameInstance(std::string& out, std::string pre_name = "", std::string post_name = "");
 				public:
 					static std::string Value();
+					template<typename VALUE_TYPE>
+					static std::string Value(VALUE_TYPE value);
 				};
 
-				template<typename TYPE>
-				class Name<TYPE* const> :
-					public Name<TYPE>
+				template<typename TYPE, const char* ALIAS_NAME>
+				class Name<TYPE* const, ALIAS_NAME> :
+					public Name<TYPE, ALIAS_NAME>
 				{
 				protected:
-					static std::string NameInstance(std::string pre_name = "", std::string post_name = "");
+					static void TypeNameInstance(std::string& out, std::string pre_name = "", std::string post_name = "");
 				public:
 					static std::string Value();
+					template<typename VALUE_TYPE>
+					static std::string Value(VALUE_TYPE value);
 				};
 
-				template<typename TYPE>
-				class Name<TYPE* volatile> :
-					public Name<TYPE>
+				template<typename TYPE, const char* ALIAS_NAME>
+				class Name<TYPE* volatile, ALIAS_NAME> :
+					public Name<TYPE, ALIAS_NAME>
 				{
 				protected:
-					static std::string NameInstance(std::string pre_name = "", std::string post_name = "");
+					static void TypeNameInstance(std::string& out, std::string pre_name = "", std::string post_name = "");
 				public:
 					static std::string Value();
+					template<typename VALUE_TYPE>
+					static std::string Value(VALUE_TYPE value);
 				};
 
-				template<typename TYPE>
-				class Name<TYPE* const volatile> :
-					public Name<TYPE>
+				template<typename TYPE, const char* ALIAS_NAME>
+				class Name<TYPE* const volatile, ALIAS_NAME> :
+					public Name<TYPE, ALIAS_NAME>
 				{
 				protected:
-					static std::string NameInstance(std::string pre_name = "", std::string post_name = "");
+					static void TypeNameInstance(std::string& out, std::string pre_name = "", std::string post_name = "");
 				public:
 					static std::string Value();
+					template<typename VALUE_TYPE>
+					static std::string Value(VALUE_TYPE value);
 				};
 
-				template<typename TYPE>
-				class Name<TYPE&> :
-					public Name<TYPE>
+				template<typename TYPE, const char* ALIAS_NAME>
+				class Name<TYPE&, ALIAS_NAME> :
+					public Name<TYPE, ALIAS_NAME>
 				{
 				protected:
-					static std::string NameInstance(std::string pre_name = "", std::string post_name = "");
+					static void TypeNameInstance(std::string& out, std::string pre_name = "", std::string post_name = "");
 				public:
 					static std::string Value();
+					template<typename VALUE_TYPE>
+					static std::string Value(VALUE_TYPE value);
 				};
 
-				template<typename TYPE>
-				class Name<TYPE&&> :
-					public Name<TYPE>
+				template<typename TYPE, const char* ALIAS_NAME>
+				class Name<TYPE&&, ALIAS_NAME> :
+					public Name<TYPE, ALIAS_NAME>
 				{
 				protected:
-					static std::string NameInstance(std::string pre_name = "", std::string post_name = "");
+					static void TypeNameInstance(std::string& out, std::string pre_name = "", std::string post_name = "");
 				public:
 					static std::string Value();
+					template<typename VALUE_TYPE>
+					static std::string Value(VALUE_TYPE value);
 				};
 
-				template<typename TYPE>
-				std::string Name<TYPE>::NameInstance(std::string pre_name, std::string post_name)
+				template<typename TYPE, const char* ALIAS_NAME>
+				template<typename VALUE_TYPE>
+				static typename std::enable_if<std::is_same<std::string, typename std::remove_cv<typename std::remove_reference<VALUE_TYPE>::type>::type>::value
+					|| (std::is_pointer<VALUE_TYPE>::value && std::is_same<typename std::remove_cv<typename std::remove_pointer<VALUE_TYPE>::type>::type, char>::value)>::type
+					Name<TYPE, ALIAS_NAME>::ValueOfTypeInstance(std::string& out, VALUE_TYPE value)
 				{
-					std::string name = pre_name;
-					name += "TYPE";
-					name += post_name;
-					name += " [TYPE = ";
-					name += typeid(TYPE).name();
-					name += "]";
-					return name;
+					out += " = ";
+					out += value;
 				}
 
-				template<typename TYPE>
-				std::string Name<TYPE>::Value()
+				template<typename TYPE, const char* ALIAS_NAME>
+				template<typename VALUE_TYPE>
+				static typename std::enable_if<!std::is_same<std::string, VALUE_TYPE>::value && !(std::is_pointer<VALUE_TYPE>::value && std::is_same<typename std::remove_cv<typename std::remove_pointer<VALUE_TYPE>::type>::type, char>::value)>::type
+					Name<TYPE, ALIAS_NAME>::ValueOfTypeInstance(std::string& out, VALUE_TYPE value)
 				{
-					return NameInstance();
+					out += " = ";
+					out += std::to_string(value);
 				}
 
-				template<typename TYPE>
-				std::string Name<const TYPE>::NameInstance(std::string pre_name, std::string post_name)
+				template<typename TYPE, const char* ALIAS_NAME>
+				void Name<TYPE, ALIAS_NAME>::AliasNameInstance(std::string& out)
 				{
-					return Name<TYPE>::NameInstance("const ", post_name);
+					out += " ";
+					out += BrainMuscles::test::simple::type::name::DefaultBeginAliasName;
+					out += ALIAS_NAME;
+					out += " = ";
+					out += typeid(TYPE).name();
+					out += BrainMuscles::test::simple::type::name::DefaultEndAliasName;
 				}
 
-				template<typename TYPE>
-				std::string Name<const TYPE>::Value()
+				template<typename TYPE, const char* ALIAS_NAME>
+				void Name<TYPE, ALIAS_NAME>::TypeNameInstance(std::string& out, std::string pre_name, std::string post_name)
 				{
-					return NameInstance();
+					out += pre_name;
+					out += ALIAS_NAME;
+					out += post_name;
 				}
 
-				template<typename TYPE>
-				std::string Name<volatile TYPE>::NameInstance(std::string pre_name, std::string post_name)
+				template<typename TYPE, const char* ALIAS_NAME>
+				void Name<TYPE, ALIAS_NAME>::ValueInstance(FunctionTypeNameInstanceType typenameinstance, std::string& out)
 				{
-					return Name<TYPE>::NameInstance("volatile ", post_name);
+					typenameinstance(out, "", "");
+					AliasNameInstance(out);
 				}
 
-				template<typename TYPE>
-				std::string Name<volatile TYPE>::Value()
+				template<typename TYPE, const char* ALIAS_NAME>
+				template<typename VALUE_TYPE>
+				void Name<TYPE, ALIAS_NAME>::ValueInstance(FunctionTypeNameInstanceType typenameinstance, std::string& out, VALUE_TYPE value)
 				{
-					return NameInstance();
+					typenameinstance(out, "", "");
+					ValueOfTypeInstance(out, value);
+					AliasNameInstance(out);
 				}
 
-				template<typename TYPE>
-				std::string Name<const volatile TYPE>::NameInstance(std::string pre_name, std::string post_name)
+				template<typename TYPE, const char* ALIAS_NAME>
+				template<typename VALUE_TYPE>
+				std::string Name<TYPE, ALIAS_NAME>::Value(VALUE_TYPE value)
 				{
-					return Name<TYPE>::NameInstance("const volatile ", post_name);
+					std::string out;
+					ValueInstance(&TypeNameInstance, out, value);
+					return out;
 				}
 
-				template<typename TYPE>
-				std::string Name<const volatile TYPE>::Value()
+				template<typename TYPE, const char* ALIAS_NAME>
+				std::string Name<TYPE, ALIAS_NAME>::Value()
 				{
-					return NameInstance();
+					std::string out;
+					ValueInstance(&TypeNameInstance, out);
+					return out;
 				}
 
-				template<typename TYPE>
-				std::string Name<TYPE*>::NameInstance(std::string pre_name, std::string post_name)
+				template<typename TYPE, const char* ALIAS_NAME>
+				void Name<const TYPE, ALIAS_NAME>::TypeNameInstance(std::string& out, std::string pre_name, std::string post_name)
 				{
-					return Name<TYPE>::NameInstance(pre_name, std::string("*") + post_name);
+					return Name<TYPE, ALIAS_NAME>::TypeNameInstance(out, "const ", post_name);
 				}
 
-				template<typename TYPE>
-				std::string Name<TYPE*>::Value()
+				template<typename TYPE, const char* ALIAS_NAME>
+				template<typename VALUE_TYPE>
+				std::string Name<const TYPE, ALIAS_NAME>::Value(VALUE_TYPE value)
 				{
-					return NameInstance();
+					std::string out;
+					ValueInstance(&TypeNameInstance, out, value);
+					return out;
 				}
 
-				template<typename TYPE>
-				std::string Name<TYPE* const>::NameInstance(std::string pre_name, std::string post_name)
+				template<typename TYPE, const char* ALIAS_NAME>
+				std::string Name<const TYPE, ALIAS_NAME>::Value()
 				{
-					return Name<TYPE>::NameInstance(pre_name, "* const");
+					std::string out;
+					ValueInstance(&TypeNameInstance, out);
+					return out;
 				}
 
-				template<typename TYPE>
-				std::string Name<TYPE* const>::Value()
+				template<typename TYPE, const char* ALIAS_NAME>
+				void Name<volatile TYPE, ALIAS_NAME>::TypeNameInstance(std::string& out, std::string pre_name, std::string post_name)
 				{
-					return NameInstance();
+					return Name<TYPE, ALIAS_NAME>::TypeNameInstance(out, "volatile ", post_name);
 				}
 
-				template<typename TYPE>
-				std::string Name<TYPE* volatile>::NameInstance(std::string pre_name, std::string post_name)
+				template<typename TYPE, const char* ALIAS_NAME>
+				template<typename VALUE_TYPE>
+				std::string Name<volatile TYPE, ALIAS_NAME>::Value(VALUE_TYPE value)
 				{
-					return Name<TYPE>::NameInstance(pre_name, "* volatile");
+					std::string out;
+					ValueInstance(&TypeNameInstance, out, value);
+					return out;
 				}
 
-				template<typename TYPE>
-				std::string Name<TYPE* volatile>::Value()
+				template<typename TYPE, const char* ALIAS_NAME>
+				std::string Name<volatile TYPE, ALIAS_NAME>::Value()
 				{
-					return NameInstance();
+					std::string out;
+					ValueInstance(&TypeNameInstance, out);
+					return out;
 				}
 
-				template<typename TYPE>
-				std::string Name<TYPE* const volatile>::NameInstance(std::string pre_name, std::string post_name)
+				template<typename TYPE, const char* ALIAS_NAME>
+				void Name<const volatile TYPE, ALIAS_NAME>::TypeNameInstance(std::string& out, std::string pre_name, std::string post_name)
 				{
-					return Name<TYPE>::NameInstance(pre_name, "* const volatile");
+					return Name<TYPE, ALIAS_NAME>::TypeNameInstance(out, "const volatile ", post_name);
 				}
 
-				template<typename TYPE>
-				std::string Name<TYPE* const volatile>::Value()
+				template<typename TYPE, const char* ALIAS_NAME>
+				template<typename VALUE_TYPE>
+				std::string Name<const volatile TYPE, ALIAS_NAME>::Value(VALUE_TYPE value)
 				{
-					return NameInstance();
+					std::string out;
+					ValueInstance(&TypeNameInstance, out, value);
+					return out;
 				}
 
-				template<typename TYPE>
-				std::string Name<TYPE&>::NameInstance(std::string pre_name, std::string post_name)
+				template<typename TYPE, const char* ALIAS_NAME>
+				std::string Name<const volatile TYPE, ALIAS_NAME>::Value()
 				{
-					return Name<TYPE>::NameInstance(pre_name, "&");
+					std::string out;
+					ValueInstance(&TypeNameInstance, out);
+					return out;
 				}
 
-				template<typename TYPE>
-				std::string Name<TYPE&>::Value()
+				template<typename TYPE, const char* ALIAS_NAME>
+				void Name<TYPE*, ALIAS_NAME>::TypeNameInstance(std::string& out, std::string pre_name, std::string post_name)
 				{
-					return NameInstance();
+					return Name<TYPE, ALIAS_NAME>::TypeNameInstance(out, pre_name, std::string("*") + post_name);
 				}
 
-				template<typename TYPE>
-				std::string Name<TYPE&&>::NameInstance(std::string pre_name, std::string post_name)
+				template<typename TYPE, const char* ALIAS_NAME>
+				template<typename VALUE_TYPE>
+				std::string Name<TYPE*, ALIAS_NAME>::Value(VALUE_TYPE value)
 				{
-					return Name<TYPE>::NameInstance(pre_name, "&&");
+					std::string out;
+					ValueInstance(&TypeNameInstance, out, value);
+					return out;
 				}
 
-				template<typename TYPE>
-				std::string Name<TYPE&&>::Value()
+				template<typename TYPE, const char* ALIAS_NAME>
+				std::string Name<TYPE*, ALIAS_NAME>::Value()
 				{
-					return NameInstance();
+					std::string out;
+					ValueInstance(&TypeNameInstance, out);
+					return out;
+				}
+
+				template<typename TYPE, const char* ALIAS_NAME>
+				void Name<TYPE* const, ALIAS_NAME>::TypeNameInstance(std::string& out, std::string pre_name, std::string post_name)
+				{
+					return Name<TYPE, ALIAS_NAME>::TypeNameInstance(out, pre_name, "* const");
+				}
+
+				template<typename TYPE, const char* ALIAS_NAME>
+				template<typename VALUE_TYPE>
+				std::string Name<TYPE* const, ALIAS_NAME>::Value(VALUE_TYPE value)
+				{
+					std::string out;
+					ValueInstance(&TypeNameInstance, out, value);
+					return out;
+				}
+
+				template<typename TYPE, const char* ALIAS_NAME>
+				std::string Name<TYPE* const, ALIAS_NAME>::Value()
+				{
+					std::string out;
+					ValueInstance(&TypeNameInstance, out);
+					return out;
+				}
+
+				template<typename TYPE, const char* ALIAS_NAME>
+				void Name<TYPE* volatile, ALIAS_NAME>::TypeNameInstance(std::string& out, std::string pre_name, std::string post_name)
+				{
+					return Name<TYPE, ALIAS_NAME>::TypeNameInstance(out, pre_name, "* volatile");
+				}
+
+				template<typename TYPE, const char* ALIAS_NAME>
+				template<typename VALUE_TYPE>
+				std::string Name<TYPE* volatile, ALIAS_NAME>::Value(VALUE_TYPE value)
+				{
+					std::string out;
+					ValueInstance(&TypeNameInstance, out, value);
+					return out;
+				}
+
+				template<typename TYPE, const char* ALIAS_NAME>
+				std::string Name<TYPE* volatile, ALIAS_NAME>::Value()
+				{
+					std::string out;
+					ValueInstance(&TypeNameInstance, out);
+					return out;
+				}
+
+				template<typename TYPE, const char* ALIAS_NAME>
+				void Name<TYPE* const volatile, ALIAS_NAME>::TypeNameInstance(std::string& out, std::string pre_name, std::string post_name)
+				{
+					return Name<TYPE, ALIAS_NAME>::TypeNameInstance(out, pre_name, "* const volatile");
+				}
+
+				template<typename TYPE, const char* ALIAS_NAME>
+				template<typename VALUE_TYPE>
+				std::string Name<TYPE* const volatile, ALIAS_NAME>::Value(VALUE_TYPE value)
+				{
+					std::string out;
+					ValueInstance(&TypeNameInstance, out, value);
+					return out;
+				}
+
+				template<typename TYPE, const char* ALIAS_NAME>
+				std::string Name<TYPE* const volatile, ALIAS_NAME>::Value()
+				{
+					std::string out;
+					ValueInstance(&TypeNameInstance, out);
+					return out;
+				}
+
+				template<typename TYPE, const char* NAME>
+				void Name<TYPE&, NAME>::TypeNameInstance(std::string& out, std::string pre_name, std::string post_name)
+				{
+					return Name<TYPE, NAME>::TypeNameInstance(out, pre_name, "&");
+				}
+
+				template<typename TYPE, const char* ALIAS_NAME>
+				template<typename VALUE_TYPE>
+				std::string Name<TYPE&, ALIAS_NAME>::Value(VALUE_TYPE value)
+				{
+					std::string out;
+					ValueInstance(&TypeNameInstance, out, value);
+					return out;
+				}
+
+				template<typename TYPE, const char* ALIAS_NAME>
+				std::string Name<TYPE&, ALIAS_NAME>::Value()
+				{
+					std::string out;
+					ValueInstance(&TypeNameInstance, out);
+					return out;
+				}
+
+				template<typename TYPE, const char* ALIAS_NAME>
+				void Name<TYPE&&, ALIAS_NAME>::TypeNameInstance(std::string& out, std::string pre_name, std::string post_name)
+				{
+					return Name<TYPE, ALIAS_NAME>::TypeNameInstance(out, pre_name, "&&");
+				}
+
+				template<typename TYPE, const char* ALIAS_NAME>
+				template<typename VALUE_TYPE>
+				std::string Name<TYPE&&, ALIAS_NAME>::Value(VALUE_TYPE value)
+				{
+					std::string out;
+					ValueInstance(&TypeNameInstance, out, value);
+					return out;
+				}
+
+				template<typename TYPE, const char* ALIAS_NAME>
+				std::string Name<TYPE&&, ALIAS_NAME>::Value()
+				{
+					std::string out;
+					ValueInstance(&TypeNameInstance, out);
+					return out;
 				}
 			}
 		}
