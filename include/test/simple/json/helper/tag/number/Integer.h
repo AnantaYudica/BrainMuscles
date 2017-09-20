86d501bc5c6f5ad5e30a73f0deb3d62cb82abe4b
AnantaYudica <nta040689@gmail.com>
2017-09-20 16:30:19 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Add macro ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_H___DEFINITION__ - Add macro ifndef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_INTEGER_H___DEFINITION__   - Add macro define TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_INTEGER_H___DEFINITION__   - Add definition struct BrainMuscles::test::simple::json::helper::tag::number::Integer 	- Add base class with BrainMuscles::test::simple::json::helper::tag::Number and public inheritance 	- Add aliases type with public to access 	  - Add alias type 'BaseType' with BrainMuscles::test::simple::json::helper::tag::Number for type 	  - Add alias type 'EnumType' with BaseType::EnumType for type 	- Add default constructor with public to protected Add macro else from macro ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_H___DEFINITION__ - Add dependency with test\simple\json\helper\Tag.h Add macro ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_INTEGER_H___DEFINITION__ - Add macro ifndef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_INTEGER_H___IMPLEMENTATION__   - Add macro define TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_INTEGER_H___IMPLEMENTATION__   - Add implementation of struct BrainMuscles::test::simple::json::helper::tag::number::Integer

---
+++ include/test/simple/json/helper/tag/number/Integer.h
@@ -0,0 +1,83 @@
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_H___DEFINITION__
+
+#ifndef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_INTEGER_H___DEFINITION__
+#define TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_INTEGER_H___DEFINITION__
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace helper
+				{
+					namespace tag
+					{
+						namespace number
+						{
+							struct Integer :
+								public BrainMuscles::test::simple::json::helper::tag::Number
+							{
+							public:
+								typedef BrainMuscles::test::simple::json::helper
+									::tag::Number										BaseType;
+								typedef typename BaseType::EnumType						EnumType;
+							protected:
+								Integer(const EnumType& value);
+							};
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_INTEGER_H___DEFINITION__
+
+#else //ELSE TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_H___DEFINITION__
+#include "test\simple\json\helper\Tag.h"
+
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_H___DEFINITION__
+
+#ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_INTEGER_H___DEFINITION__
+
+#ifndef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_INTEGER_H___IMPLEMENTATION__
+#define TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_INTEGER_H___IMPLEMENTATION__
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace helper
+				{
+					namespace tag
+					{
+						namespace number
+						{
+							Integer::Integer(const EnumType& value) :
+								BaseType(value)
+							{}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_INTEGER_H___IMPLEMENTATION__
+
+#endif
+
+#endif
