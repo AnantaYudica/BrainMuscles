7fcea0cc2657fdde5f26fb980763796019c2d65b
AnantaYudica <nta040689@gmail.com>
2017-09-20 09:55:49 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Add macro ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_H___DEFINITION__ - Add macro ifndef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_DOUBLE_H___DEFINITION__   - Add macro define TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_DOUBLE_H___DEFINITION__   - Add definition struct BrainMuscles::test::simple::json::helper::tag::number::Double 	- Add base class with BrainMuscles::test::simple::json::helper::tag::Number and public inheritance 	- Add aliases type with public to access 	  - Add alias type 'BaseType' with BrainMuscles::test::simple::json::helper::tag::Number for type 	  - Add alias type 'EnumType' with BaseType::EnumType for type 	- Add default constructor with public to access Add macro else from macro ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_H___DEFINITION__ - Add dependency with test\simple\json\helper\Tag.h Add macro ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_DOUBLE_H___DEFINITION__ - Add macro ifndef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_DOUBLE_H___IMPLEMENTATION__   - Add macro define TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_DOUBLE_H___IMPLEMENTATION__   - Add implementation of struct BrainMuscles::test::simple::json::helper::tag::number::Double

---
+++ include/test/simple/json/helper/tag/number/Double.h
@@ -0,0 +1,83 @@
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_H___DEFINITION__
+
+#ifndef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_DOUBLE_H___DEFINITION__
+#define TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_DOUBLE_H___DEFINITION__
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace helper
+				{
+					namespace tag
+					{
+						namespace number
+						{
+							struct Double : 
+								public BrainMuscles::test::simple::json::helper::tag::Number
+							{
+							public:
+								typedef BrainMuscles::test::simple::json::helper
+									::tag::Number										BaseType;
+								typedef typename BaseType::EnumType						EnumType;
+							public:
+								Double();
+							};
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_DOUBLE_H___DEFINITION__
+
+#else //ELSE TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_H___DEFINITION__
+#include "test\simple\json\helper\Tag.h"
+
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_H___DEFINITION__
+
+#ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_DOUBLE_H___DEFINITION__
+
+#ifndef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_DOUBLE_H___IMPLEMENTATION__
+#define TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_DOUBLE_H___IMPLEMENTATION__
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace helper
+				{
+					namespace tag
+					{
+						namespace number
+						{
+							Double::Double() :
+								BaseType(EnumType::value_number_double)
+							{}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_DOUBLE_H___IMPLEMENTATION__
+
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_DOUBLE_H___DEFINITION__
+
+#endif 
