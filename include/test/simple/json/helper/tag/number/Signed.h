13464bf2b2e1fa54c8c818cc96f266ab9de16363
AnantaYudica <nta040689@gmail.com>
2017-09-20 16:38:21 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_ Add macro ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_INTEGER_H___DEFINITION__ - Add macro ifndef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_SIGNED_H___DEFINITION__   - Add macro define TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_SIGNED_H___DEFINITION__   - Add definition struct BrainMuscles::test::simple::json::helper::tag::number::Signed 	- Add base class with BrainMuscles::test::simple::json::helper::tag::number::Integer and public inheritance 	- Add aliases type with public to access 	  - Add alias type 'BaseType' with BrainMuscles::test::simple::json::helper::tag::number::Integer for type 	  - Add alias type 'EnumType' with BaseType::EnumType for type 	- Add default constructor with public to protected Add macro else from macro ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_INTEGER_H___DEFINITION__ - Add dependency with test\simple\json\helper\Tag.h Add macro ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_SIGNED_H___DEFINITION__ - Add macro ifndef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_SIGNED_H___IMPLEMENTATION__   - Add macro define TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_SIGNED_H___IMPLEMENTATION__   - Add implementation of struct BrainMuscles::test::simple::json::helper::tag::number::Signed

---
+++ include/test/simple/json/helper/tag/number/Signed.h
@@ -0,0 +1,83 @@
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_INTEGER_H___DEFINITION__
+
+#ifndef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_SIGNED_H___DEFINITION__
+#define TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_SIGNED_H___DEFINITION__
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace helper
+				{
+					namespace tag
+					{
+						namespace number
+						{
+							struct Signed :
+								public BrainMuscles::test::simple::json::helper::tag::number::Integer
+							{
+							public:
+								typedef BrainMuscles::test::simple::json::helper
+									::tag::number::Integer								BaseType;
+								typedef typename BaseType::EnumType						EnumType;
+							protected:
+								Signed(const EnumType& value);
+							};
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_SIGNED_H___DEFINITION__
+
+#else //ELSE TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_INTEGER_H___DEFINITION__
+#include "test\simple\json\helper\Tag.h"
+
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_INTEGER_H___DEFINITION__
+
+#ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_SIGNED_H___DEFINITION__
+
+#ifndef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_SIGNED_H___IMPLEMENTATION__
+#define TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_SIGNED_H___IMPLEMENTATION__
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace helper
+				{
+					namespace tag
+					{
+						namespace number
+						{
+							Signed::Signed(const EnumType& value) :
+								BaseType(value)
+							{}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_SIGNED_H___IMPLEMENTATION__
+
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_SIGNED_H___DEFINITION__
+
+#endif
