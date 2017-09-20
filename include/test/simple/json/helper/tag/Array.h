e1d2376da7ab94bb43340c60dfa90a99c1359d89
AnantaYudica <nta040689@gmail.com>
2017-09-20 07:32:43 +0700

    ADD FOLDER AND FILE Add folder /include/test/simple/json/helper/tag Add File /include/test/simple/json/helper/tag/Array.h  - Add dependency with test\Configure.h  - Add macro if defined _USING_TEST_    - Add macro ifdef TEST_SIMPLE_JSON_HELPER_TAG_H___DEFINITION__      - Add macro ifndef TEST_SIMPLE_JSON_HELPER_TAG_ARRAY_H___DEFINITION__        - Add macro define TEST_SIMPLE_JSON_HELPER_TAG_ARRAY_H___DEFINITION__        - Add definition struct BrainMuscles::test::simple::json::helper::tag::Array          - Add base class with BrainMuscles::test::simple::json::helper::Tag and public inheritance          - Add aliases type with public to access            - Add alias type 'BaseType' with BrainMuscles::test::simple::json::helper::Tag for type            - Add alias type 'EnumType' with BaseType::EnumType for type          - Add default constructor with public to access    - Add macro else from macro ifdef TEST_SIMPLE_JSON_HELPER_TAG_H___DEFINITION__      - Add dependency with test\simple\json\helper\Tag.h    - Add macro ifdef TEST_SIMPLE_JSON_HELPER_TAG_ARRAY_H___DEFINITION__      - Add macro ifndef TEST_SIMPLE_JSON_HELPER_TAG_ARRAY_H___IMPLEMENTATION___        - Add macro define TEST_SIMPLE_JSON_HELPER_TAG_ARRAY_H___IMPLEMENTATION___        - Add implementation of struct BrainMuscles::test::simple::json::helper::tag::Array

---
+++ include/test/simple/json/helper/tag/Array.h
@@ -0,0 +1,76 @@
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#ifdef TEST_SIMPLE_JSON_HELPER_TAG_H___DEFINITION__
+
+#ifndef TEST_SIMPLE_JSON_HELPER_TAG_ARRAY_H___DEFINITION__
+#define TEST_SIMPLE_JSON_HELPER_TAG_ARRAY_H___DEFINITION__
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace helper
+				{
+					namespace tag
+					{
+						struct Array : 
+							public BrainMuscles::test::simple::json::helper::Tag
+						{
+						public:
+							typedef BrainMuscles::test::simple::json::helper::Tag	BaseType;
+							typedef typename BaseType::EnumType						EnumType;
+						public:
+							Array();
+						};
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_ARRAY_H___DEFINITION__
+
+#else //ELSE TEST_SIMPLE_JSON_HELPER_TAG_H___DEFINITION__
+#include "test\simple\json\helper\Tag.h"
+
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_H___DEFINITION__
+
+#ifdef TEST_SIMPLE_JSON_HELPER_TAG_ARRAY_H___DEFINITION__
+
+#ifndef TEST_SIMPLE_JSON_HELPER_TAG_ARRAY_H___IMPLEMENTATION___
+#define TEST_SIMPLE_JSON_HELPER_TAG_ARRAY_H___IMPLEMENTATION___
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace helper
+				{
+					namespace tag
+					{
+						Array::Array() :
+							BaseType(EnumType::value_array)
+						{}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_ARRAY_H___IMPLEMENTATION___
+
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_ARRAY_H___DEFINITION__
+
+#endif
