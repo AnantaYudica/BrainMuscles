44b6d71702d8c3fd75392667aa659970484d7dbd
AnantaYudica <nta040689@gmail.com>
2017-09-20 17:28:51 +0700

    MODIFIED FILE Update file /include/test/simple/json/helper/tag/number/UnsignedChar.h and Update file /include/test/simple/json/helper/tag/number/UnsignedInt.h and Update file /include/test/simple/json/helper/tag/number/UnsignedLong.h and Update file /include/test/simple/json/helper/tag/number/UnsignedLongLong.h and Update file /include/test/simple/json/helper/tag/number/UnsignedShort.h and  - Update macro ifdef from TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_H___DEFINITION__ to TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_UNSIGNED_H___DEFINITION__  - Update struct BrainMuscles::test::simple::json::helper::tag::number::UnsignedChar and    Update struct BrainMuscles::test::simple::json::helper::tag::number::UnsignedInt and    Update struct BrainMuscles::test::simple::json::helper::tag::number::UnsignedLong and    Update struct BrainMuscles::test::simple::json::helper::tag::number::UnsignedLongLong and    Update struct BrainMuscles::test::simple::json::helper::tag::number::UnsignedShort    - Update base class from BrainMuscles::test::simple::json::helper::tag::Number to BrainMuscles::test::simple::json::helper::tag::number::Unsigned    - Update alias type 'BaseType' from BrainMuscles::test::simple::json::helper::tag::Number to BrainMuscles::test::simple::json::helper::tag::number::Unsigned for type

--- include/test/simple/json/helper/tag/number/UnsignedChar.h
+++ include/test/simple/json/helper/tag/number/UnsignedChar.h
@@ -2,7 +2,7 @@
 
 #if defined(_USING_TEST_)
 
-#ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_H___DEFINITION__
+#ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_UNSIGNED_H___DEFINITION__
 
 #ifndef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_UNSIGNEDCHAR_H___DEFINITION__
 #define TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_UNSIGNEDCHAR_H___DEFINITION__
@@ -22,11 +22,11 @@ namespace BrainMuscles
 						namespace number
 						{
 							struct UnsignedChar : 
-								public BrainMuscles::test::simple::json::helper::tag::Number
+								public BrainMuscles::test::simple::json::helper::tag::number::Unsigned
 							{
 							public:
 								typedef BrainMuscles::test::simple::json::helper
-									::tag::Number										BaseType;
+									::tag::number::Unsigned								BaseType;
 								typedef typename BaseType::EnumType						EnumType;
 							public:
 								UnsignedChar();
@@ -41,10 +41,10 @@ namespace BrainMuscles
 
 #endif //!TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_UNSIGNEDCHAR_H___DEFINITION__
 
-#else //ELSE TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_H___DEFINITION__
+#else //ELSE TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_UNSIGNED_H___DEFINITION__
 #include "test\simple\json\helper\Tag.h"
 
-#endif //!TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_H___DEFINITION__
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_UNSIGNED_H___DEFINITION__
 
 #ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_UNSIGNEDCHAR_H___DEFINITION__
 
--- include/test/simple/json/helper/tag/number/UnsignedLongLong.h
+++ include/test/simple/json/helper/tag/number/UnsignedLongLong.h
@@ -2,7 +2,7 @@
 
 #if defined(_USING_TEST_)
 
-#ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_H___DEFINITION__
+#ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_UNSIGNED_H___DEFINITION__
 
 #ifndef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_UNSIGNEDLONGLONG_H___DEFINITION__
 #define TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_UNSIGNEDLONGLONG_H___DEFINITION__
@@ -23,11 +23,11 @@ namespace BrainMuscles
 						namespace number
 						{
 							struct UnsignedLongLong : 
-								public BrainMuscles::test::simple::json::helper::tag::Number
+								public BrainMuscles::test::simple::json::helper::tag::number::Unsigned
 							{
 							public:
 								typedef BrainMuscles::test::simple::json::helper
-									::tag::Number										BaseType;
+									::tag::number::Unsigned								BaseType;
 								typedef typename BaseType::EnumType						EnumType;
 							public:
 								UnsignedLongLong();
@@ -42,10 +42,10 @@ namespace BrainMuscles
 
 #endif //!TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_UNSIGNEDLONGLONG_H___DEFINITION__
 
-#else //ELSE TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_H___DEFINITION__
+#else //ELSE TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_UNSIGNED_H___DEFINITION__
 #include "test\simple\json\helper\Tag.h"
 
-#endif //!TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_H___DEFINITION__
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_UNSIGNED_H___DEFINITION__
 
 #ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_UNSIGNEDLONGLONG_H___DEFINITION__
 
