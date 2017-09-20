f415a3a7178d79669025c3a3dcb093d180db71bc
AnantaYudica <nta040689@gmail.com>
2017-09-20 08:32:39 +0700

    ADD FILE Add dependency with test\Configure.h Add macro if defined _USING_TEST_  - Add macro ifdef TEST_SIMPLE_JSON_HELPER_TAG_H___DEFINITION__    - Add macro ifndef TEST_SIMPLE_JSON_HELPER_TAG_TRUE_H___DEFINITION__      - Add macro define TEST_SIMPLE_JSON_HELPER_TAG_TRUE_H___DEFINITION__      - Add definition struct BrainMuscles::test::simple::json::helper::tag::True        - Add base class with BrainMuscles::test::simple::json::helper::Tag and public inheritance        - Add aliases type with public to access          - Add alias type 'BaseType' with BrainMuscles::test::simple::json::helper::Tag for type          - Add alias type 'EnumType' with BaseType::EnumType for type        - Add default constructor with public to access  - Add macro else from ifdef TEST_SIMPLE_JSON_HELPER_TAG_H___DEFINITION__    - Add dependency with test\simple\json\helper\Tag.h  - Add macro ifdef TEST_SIMPLE_JSON_HELPER_TAG_TRUE_H___DEFINITION__    - Add macro ifndef TEST_SIMPLE_JSON_HELPER_TAG_TRUE_H___IMPELEMTATION__      - Add macro define TEST_SIMPLE_JSON_HELPER_TAG_TRUE_H___IMPELEMTATION__      - Add implementation of struct BrainMuscles::test::simple::json::helper::tag::True

---
+++ include/test/simple/json/helper/tag/True.h
@@ -0,0 +1,75 @@
+#include "test\Configure.h"
+
+#if defined(_USING_TEST_)
+
+#ifdef TEST_SIMPLE_JSON_HELPER_TAG_H___DEFINITION__
+
+#ifndef TEST_SIMPLE_JSON_HELPER_TAG_TRUE_H___DEFINITION__
+#define TEST_SIMPLE_JSON_HELPER_TAG_TRUE_H___DEFINITION__
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace helper
+				{
+					namespace tag
+					{
+						struct True : public BrainMuscles::test::simple::json::helper::Tag
+						{
+						public:
+							typedef BrainMuscles::test::simple::json::helper::Tag	BaseType;
+							typedef typename BaseType::EnumType						EnumType;
+						public:
+							True();
+						};
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_TRUE_H___DEFINITION__
+
+#else //ELSE TEST_SIMPLE_JSON_HELPER_TAG_H___DEFINITION__
+#include "test\simple\json\helper\Tag.h"
+
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_H___DEFINITION__
+
+#ifdef TEST_SIMPLE_JSON_HELPER_TAG_TRUE_H___DEFINITION__
+
+#ifndef TEST_SIMPLE_JSON_HELPER_TAG_TRUE_H___IMPELEMTATION__
+#define TEST_SIMPLE_JSON_HELPER_TAG_TRUE_H___IMPELEMTATION__
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace helper
+				{
+					namespace tag
+					{
+						True::True() :
+							BaseType(EnumType::value_true)
+						{}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_TRUE_H___IMPELEMTATION__
+
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_TRUE_H___DEFINITION__
+
+#endif
