36d206e969ba6bde37d15fc99242cee50ebd9d7d
AnantaYudica <nta040689@gmail.com>
2017-09-20 17:13:56 +0700

    MODIFIED FILES Update in file /include/test/simple/json/helper/tag/number/Double.h and Update in file /include/test/simple/json/helper/tag/number/Float.h and Update in file /include/test/simple/json/helper/tag/number/LongDouble.h  - Update macro ifdef from TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_H___DEFINITION__ to TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_FLOATINGPOINT_H___DEFINITION__  - Update struct BrainMuscles::test::simple::json::helper::tag::number::Double and    Update struct BrainMuscles::test::simple::json::helper::tag::number::Float and    Update struct BrainMuscles::test::simple::json::helper::tag::number::LongDouble    - Update base class from BrainMuscles::test::simple::json::helper::tag::Number to BrainMuscles::test::simple::json::helper::tag::number::FloatingPoint    - Update alias type 'BaseType' from BrainMuscles::test::simple::json::helper::tag::Number to BrainMuscles::test::simple::json::helper::tag::number::FloatingPoint for type

--- include/test/simple/json/helper/tag/number/LongDouble.h
+++ include/test/simple/json/helper/tag/number/LongDouble.h
@@ -2,7 +2,7 @@
 
 #if defined(_USING_TEST_)
 
-#ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_H___DEFINITION__
+#ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_FLOATINGPOINT_H___DEFINITION__
 
 #ifndef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_LONGDOUBLE_H___DEFINITION__
 #define TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_LONGDOUBLE_H___DEFINITION__
@@ -22,11 +22,11 @@ namespace BrainMuscles
 						namespace number
 						{
 							struct LongDouble : 
-								public BrainMuscles::test::simple::json::helper::tag::Number
+								public BrainMuscles::test::simple::json::helper::tag::number::FloatingPoint
 							{
 							public:
 								typedef BrainMuscles::test::simple::json::helper
-									::tag::Number										BaseType;
+									::tag::number::FloatingPoint						BaseType;
 								typedef typename BaseType::EnumType						EnumType;
 							public:
 								LongDouble();
@@ -41,10 +41,10 @@ namespace BrainMuscles
 
 #endif //!TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_LONGDOUBLE_H___DEFINITION__
 
-#else //ELSE TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_H___DEFINITION__
+#else //ELSE TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_FLOATINGPOINT_H___DEFINITION__
 #include "test\simple\json\helper\Tag.h"
 
-#endif //!TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_H___DEFINITION__
+#endif //!TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_FLOATINGPOINT_H___DEFINITION__
 
 #ifdef TEST_SIMPLE_JSON_HELPER_TAG_NUMBER_LONGDOUBLE_H___DEFINITION__
 
