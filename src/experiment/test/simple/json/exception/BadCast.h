c8e8f068b1c406266a7bf0a512e53661770411ee
AnantaYudica <nta040689@gmail.com>
2017-09-25 19:32:48 +0700

    ADD FOLDERS AND FILE Add folder /src/experiment Add folder /src/experiment/test Add folder /src/experiment/test/simple Add folder /src/experiment/test/simple/json Add folder /src/experiment/test/simple/json/exception Add file /src/experiment/test/simple/json/exception/BadCast.h  - Add dependency with test\Source.h  - Add struct experiment::test::simple::json::exception::BadCast with final inheritance    - Add base class with BrainMuscles::test::Source<BadCast> and public inheritance    - Add declare friend with class BrainMuscles::test::Source<BadCast>    - Add default constructor with default by complier and private to access    - Add static function member 'Test' with void for return type and public to access

---
+++ src/experiment/test/simple/json/exception/BadCast.h
@@ -0,0 +1,31 @@
+#ifndef EXCEPTION_BADCAST_H_
+#define EXCEPTION_BADCAST_H_
+
+#include "test\Source.h"
+
+namespace experiment
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace exception
+				{
+					struct BadCast final :
+						public BrainMuscles::test::Source<BadCast>
+					{
+						friend class BrainMuscles::test::Source<BadCast>;
+					private:
+						BadCast() = default;
+					public:
+						static void Test();
+					};
+				}
+			}
+		}
+	}
+}
+
+#endif //EXCEPTION_BADCAST_H_
