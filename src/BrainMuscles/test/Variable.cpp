e4b8c0e4dee05c300adacfba98b6fb0eb25d5214
AnantaYudica <nta040689@gmail.com>
2017-09-26 05:00:46 +0700

    ADD FOLDER AND FILE Add folder /src/BrainMuscles/test Add file /src/BrainMuscles/test/Variable.cpp  - Add dependency with test\Variable.h  - Add implementation extern variables BrainMuscles::test::variable::simple::type::name::DefaultAliasName, BrainMuscles::test::variable::simple::type::name::DefaultBeginAliasName and BrainMuscles::test::variable::simple::type::name::DefaultEndAliasName

---
+++ src/BrainMuscles/test/Variable.cpp
@@ -0,0 +1,5 @@
+#include "test\Variable.h"
+
+extern char BrainMuscles::test::variable::simple::type::name::DefaultAliasName[] = "TYPE";
+extern char BrainMuscles::test::variable::simple::type::name::DefaultBeginAliasName[] = "[";
+extern char BrainMuscles::test::variable::simple::type::name::DefaultEndAliasName[] = "]";
