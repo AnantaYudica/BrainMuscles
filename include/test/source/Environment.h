017394c100da053dbdcc9bd88586f627ec1cb373
AnantaYudica <nta040689@gmail.com>
2017-09-25 18:13:48 +0700

    ADD FILE Add macro ifdef _USING_TEST_SOURCE_  - Add dependency with cstdio  - Add dependency with test\soruce\Result.h  - Add class BrainMuscles::test::source::Environment with final inheritance    - Add alias type 'ResultType' with BrainMuscles::test::source::Result for type and public to access    - Add variable member 'm_result' with ResultType for type and private to access    - Add variable member 'm_file' with std::FILE* for type and private to access    - Add inline default constructor and public to access    - Add inline static function 'Instance' with Environment& for return type and private to access    - Add inline static function 'GetInstance' with const Environment& for return type and public to access    - Add inline static function 'Result' with const ResultType& for return type and public to access    - Add inline static function 'SetError' with void for return type, parameters function 'message' with const char* for type, 'file' with const char* for type and 'line' with const std::size_t& for type and public to access

---
+++ include/test/source/Environment.h
@@ -0,0 +1,68 @@
+#ifndef TEST_SOURCE_ENVIRONMENT_H_
+#define TEST_SOURCE_ENVIRONMENT_H_
+
+#ifdef _USING_TEST_SOURCE_
+
+#include <cstdio>
+
+#include "test\soruce\Result.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace source
+		{
+			class Environment final
+			{
+			public:
+				typedef BrainMuscles::test::source::Result ResultType;
+			private:
+				ResultType m_result;
+				std::FILE* m_file;
+			private:
+				inline Environment();
+			private:
+				static inline Environment& Instance();
+			public:
+				static inline const Environment& GetInstance();
+				static inline const ResultType& Result();
+				static inline void SetError(const char* message, const char* file, const std::size_t& line);
+			};
+
+			inline Environment& Environment::Instance()
+			{
+				static Environment instance = Environment();
+				return instance;
+			}
+
+			inline Environment::Environment() :
+				m_result(ResultType::pass),
+				m_file(stderr)
+			{}
+
+			inline const Environment& Environment::GetInstance()
+			{
+				return Instance();
+			}
+
+			inline const typename Environment::ResultType& Environment::Result()
+			{
+				return GetInstance().m_result;
+			}
+
+			inline void Environment::SetError(const char* message, const char* file, const std::size_t& line)
+			{
+				if (Result() != ResultType::error)
+				{
+					Instance().m_result = ResultType::error;
+					std::fprintf(Instance().m_file, "Assertion failed: %s, file %s, line %zu\n", message, file, line);
+				}
+			}
+		}
+	}
+}
+
+#endif //!_USING_TEST_SOURCE_
+
+#endif //!TEST_SOURCE_ENVIRONMENT_H_
