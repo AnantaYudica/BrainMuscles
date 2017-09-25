7b44964f92bcf7c65f4814c9ef31f561df679f4b
AnantaYudica <nta040689@gmail.com>
2017-09-26 04:50:41 +0700

    ADD FILE Add declaration extern variables BrainMuscles::test::variable::simple::type::name::DefaultAliasName, BrainMuscles::test::variable::simple::type::name::DefaultBeginAliasName and BrainMuscles::test::variable::simple::type::name::DefaultEndAliasName with array of char

---
+++ include/test/Variable.h
@@ -0,0 +1,26 @@
+#ifndef TEST_VARIABLE_H_
+#define TEST_VARIABLE_H_
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace variable
+		{
+			namespace simple
+			{
+				namespace type
+				{
+					namespace name
+					{
+						extern char DefaultAliasName[];
+						extern char DefaultBeginAliasName[];
+						extern char DefaultEndAliasName[];
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif //!TEST_VARIABLE_H_
