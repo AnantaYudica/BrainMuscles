36aa0bfb35cabcdb48910223d5fa8d3383f88a50
AnantaYudica <nta040689@gmail.com>
2017-09-25 17:50:21 +0700

    ADD FOLDER AND FILE Add folder /include/test/soruce Add file /include/test/source/Result.h  - Add macro ifdef _USING_TEST_SOURCE_    - Add Enum BrainMuscles::test::source::Result      - Add list {pass ,error, not_test}

---
+++ include/test/soruce/Result.h
@@ -0,0 +1,24 @@
+#ifndef TEST_SOURCE_RESULT_H_
+#define TEST_SOURCE_RESULT_H_
+
+#ifdef _USING_TEST_SOURCE_
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace source
+		{
+			enum Result
+			{
+				pass,
+				error,
+				not_test
+			};
+		}
+	}
+}
+
+#endif //!_USING_TEST_SOURCE_
+
+#endif //!TEST_SOURCE_RESULT_H_
