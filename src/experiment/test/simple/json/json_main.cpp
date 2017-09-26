9031618a8760f5af85579be46d211941b48b9fbc
AnantaYudica <nta040689@gmail.com>
2017-09-26 16:47:30 +0700

    MODIFIED FILE Add dependency with exception\UndefinedType.h Add call static function 'RunTest()' from class experiment::test::simple::json::exception::UndefinedType in main function

--- src/experiment/test/simple/json/json_main.cpp
+++ src/experiment/test/simple/json/json_main.cpp
@@ -1,11 +1,13 @@
 #define _USING_TEST_SOURCE_
 
 #include "exception\BadCast.h"
+#include "exception\UndefinedType.h"
 
 using namespace experiment::test::simple::json;
 
 int main(int argc, char *argv[])
 {
 	exception::BadCast::RunTest();
+	exception::UndefinedType::RunTest();
 	return 1;
 }
