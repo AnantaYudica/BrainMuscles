9768ef2d7fa145ca4b4effe257a58d7571d6ce76
AnantaYudica <nta040689@gmail.com>
2017-09-26 20:19:39 +0700

    MODIFIED FILE Add dependency with 'Value.h' Add declare call static function member 'RunTest()' from class experiment::test::simple::json::Value in main function

--- src/experiment/test/simple/json/json_main.cpp
+++ src/experiment/test/simple/json/json_main.cpp
@@ -1,7 +1,9 @@
+#define _USING_TEST_
 #define _USING_TEST_SOURCE_
 
 #include "exception\BadCast.h"
 #include "exception\UndefinedType.h"
+#include "Value.h"
 
 using namespace experiment::test::simple::json;
 
@@ -9,5 +11,6 @@ int main(int argc, char *argv[])
 {
 	exception::BadCast::RunTest();
 	exception::UndefinedType::RunTest();
+	Value::RunTest();
 	return 1;
 }
