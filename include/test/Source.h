6e8b2ed9076be16004f6fba5a7a56aa5cd4b4339
AnantaYudica <nta040689@gmail.com>
2017-09-27 15:22:47 +0700

    MODIFIED FILE Add dependency with utility Add dependency with string Add dependency with type_traits Update class BrainMuscles::test::Source  - Add prototype static function member 'IsBaseOfSourceImpl' with parameter template 'TYPE', {std::true_type and std::false_type} for return type, parameter function with {const Source<TYPE>& and ...} for type and private to access  - Add static function member 'RunPreTest' with void for return type and private to access  - Add static function member 'SetStatus' with const ResultType& for return type, parameter function 'status' with const ResultType& for type and private to access  - Add static function member 'Requirement' with parameter template 'OTHER_SOURCE', enable if OTHER_SOURCE is not same type with DERIVED_TYPE, void for return type, parameters function 'file' with const char* for type and 'line' with const std::size_t& for type and protected to access  - Add static functions member 'HasTest', 'HasStaticTest', 'HasPreTest', 'IsPass', 'IsError' and 'IsNotTest' with bool for return type and public to access  - Add static function member 'PreTest' with void for return type and public to access  - Update static functions member 'RunStaticTest' and 'RunTest' Add macro function 'SourceRequirement'

--- include/test/Source.h
+++ include/test/Source.h
@@ -3,6 +3,10 @@
 
 #ifdef _USING_TEST_SOURCE_
 
+#include <utility>
+#include <string>
+#include <type_traits>
+
 #include "test\source\Result.h"
 #include "test\source\Environment.h"
 
@@ -18,6 +22,10 @@ namespace BrainMuscles
 		private:
 			static DERIVED_TYPE ms_instance;
 		private:
+			template<typename TYPE>
+			static std::true_type IsBaseOfSourceImpl(const Source<TYPE>&);
+			static std::false_type IsBaseOfSourceImpl(...);
+		private:
 			ResultType m_status;
 		public:
 			static const ResultType ResultStaticTest;
@@ -25,15 +33,29 @@ namespace BrainMuscles
 			Source();
 		public:
 			~Source();
+		private:
+			static ResultType RunStaticTest();
+			static void RunPreTest();
+			static const ResultType& SetStatus(const ResultType& status);
 		protected:
 			static void Assert(const bool& condition, const char* file, const std::size_t& line, const char* message, const char* condition_str = NULL);
+			template<typename OTHER_SOURCE>
+			static typename std::enable_if<!std::is_same<DERIVED_TYPE, OTHER_SOURCE>::value, void>::type 
+				Requirement(const char* file, const std::size_t& line);
+		public:
+			static bool HasTest();
+			static bool HasStaticTest();
+			static bool HasPreTest();
+			static bool IsPass();
+			static bool IsError();
+			static bool IsNotTest();
 		public:
 			static const ResultType& Status();
 			static ResultType RunTest();
 			static const DERIVED_TYPE& GetInstance();
 			static void Test();
-			static ResultType RunStaticTest();
 			static void StaticTest();
+			static void PreTest();
 		};
 
 		template<typename DERIVED_TYPE>
@@ -52,6 +74,36 @@ namespace BrainMuscles
 		{}
 
 		template<typename DERIVED_TYPE>
+		typename Source<DERIVED_TYPE>::ResultType Source<DERIVED_TYPE>::RunStaticTest()
+		{
+			if (BrainMuscles::test::source::Environment::IsPass() && HasStaticTest())
+			{
+				DERIVED_TYPE::StaticTest();
+				return SetStatus(BrainMuscles::test::source::Environment::Result())
+			}
+			return ResultType::not_test;
+		}
+
+		template<typename DERIVED_TYPE>
+		void Source<DERIVED_TYPE>::RunPreTest()
+		{
+			if (BrainMuscles::test::source::Environment::IsPass() && HasPreTest() && IsNotTest())
+			{
+				DERIVED_TYPE::PreTest();
+			}
+		}
+
+		template<typename DERIVED_TYPE>
+		typename const Source<DERIVED_TYPE>::ResultType& Source<DERIVED_TYPE>::SetStatus(const ResultType& status)
+		{
+			if (IsNotTest())
+			{
+				ms_instance.m_status = status;
+			}
+			return ms_instance.m_status;
+		}
+
+		template<typename DERIVED_TYPE>
 		void Source<DERIVED_TYPE>::Assert(const bool& condition, const char* file, const std::size_t& line, const char* message, const char* condition_str)
 		{
 			if (BrainMuscles::test::source::Environment::Result() == ResultType::pass)
@@ -64,6 +116,65 @@ namespace BrainMuscles
 		}
 
 		template<typename DERIVED_TYPE>
+		template<typename OTHER_SOURCE>
+		typename std::enable_if<!std::is_same<DERIVED_TYPE, OTHER_SOURCE>::value, void>::type 
+			Source<DERIVED_TYPE>::Requirement(const char* file, const std::size_t& line)
+		{
+			static_assert(decltype(IsBaseOfSourceImpl(std::declval<OTHER_SOURCE>()))::value,
+				"OTHER_SOURCE not base of BrainMuscles::test::Source<?>");
+			if (BrainMuscles::test::source::Environment::IsPass())
+			{
+				if (!OTHER_SOURCE::HasTest())
+				{
+					std::string msg = typeid(OTHER_SOURCE).name();
+					msg += " not has static function member 'Test'";
+					Assert(false, file, line, msg.c_str());
+					return;
+				}
+				if (OTHER_SOURCE::IsNotTest())
+				{
+					OTHER_SOURCE::RunTest();
+				}
+			}
+		}
+
+		template<typename DERIVED_TYPE>
+		bool Source<DERIVED_TYPE>::HasTest()
+		{
+			return &DERIVED_TYPE::Test != &Source<DERIVED_TYPE>::Test;
+		}
+
+		template<typename DERIVED_TYPE>
+		bool Source<DERIVED_TYPE>::HasStaticTest()
+		{
+			return &DERIVED_TYPE::StaticTest != &Source<DERIVED_TYPE>::StaticTest;
+		}
+
+		template<typename DERIVED_TYPE>
+		bool Source<DERIVED_TYPE>::HasPreTest()
+		{
+			return &DERIVED_TYPE::PreTest != &Source<DERIVED_TYPE>::PreTest;
+		}
+
+		template<typename DERIVED_TYPE>
+		bool Source<DERIVED_TYPE>::IsPass()
+		{
+			return Status() == ResultType::pass;
+		}
+
+		template<typename DERIVED_TYPE>
+		bool Source<DERIVED_TYPE>::IsError()
+		{
+			return Status() == ResultType::error;
+		}
+
+		template<typename DERIVED_TYPE>
+		bool Source<DERIVED_TYPE>::IsNotTest()
+		{
+			return Status() == ResultType::not_test;
+		}
+
+		template<typename DERIVED_TYPE>
 		const typename Source<DERIVED_TYPE>::ResultType& Source<DERIVED_TYPE>::Status()
 		{
 			return ms_instance.m_status;
@@ -72,13 +183,14 @@ namespace BrainMuscles
 		template<typename DERIVED_TYPE>
 		typename Source<DERIVED_TYPE>::ResultType Source<DERIVED_TYPE>::RunTest()
 		{
-			if (BrainMuscles::test::source::Environment::Result() == ResultType::pass 
-				&& ms_instance.m_status == ResultType::not_test
-				&& &DERIVED_TYPE::Test != &Source<DERIVED_TYPE>::Test)
+			if (HasTest())
 			{
-				DERIVED_TYPE::Test();
-				ms_instance.m_status = BrainMuscles::test::source::Environment::Result();
-				return ms_instance.m_status;
+				RunPreTest();
+				if (BrainMuscles::test::source::Environment::IsPass() && IsNotTest())
+				{
+					DERIVED_TYPE::Test();
+					return SetStatus(BrainMuscles::test::source::Environment::Result());
+				}
 			}
 			return ResultType::not_test;
 		}
@@ -94,20 +206,11 @@ namespace BrainMuscles
 		{}
 
 		template<typename DERIVED_TYPE>
-		typename Source<DERIVED_TYPE>::ResultType Source<DERIVED_TYPE>::RunStaticTest()
-		{
-			if (BrainMuscles::test::source::Environment::Result() == ResultType::pass
-				&& &DERIVED_TYPE::StaticTest != &Source<DERIVED_TYPE>::StaticTest)
-			{
-				DERIVED_TYPE::StaticTest();
-				ms_instance.m_status = BrainMuscles::test::source::Environment::Result();
-				return ms_instance.m_status;
-			}
-			return ResultType::not_test;
-		}
+		void Source<DERIVED_TYPE>::StaticTest()
+		{}
 
 		template<typename DERIVED_TYPE>
-		void Source<DERIVED_TYPE>::StaticTest()
+		void Source<DERIVED_TYPE>::PreTest()
 		{}
 	}
 }
@@ -121,6 +224,16 @@ do{\
 }while(false)
 
 #endif //!SourceAssert
+
+#ifndef SourceRequirement
+
+//SourceRequirement(SOURCE_TYPE)
+#define SourceRequirement(SOURCE_TYPE)\
+do{\
+	Requirement<SOURCE_TYPE>(__FILE__, __LINE__);\
+}while(false)
+
+#endif //!SourceRequirement
 
 #endif //!_USING_TEST_SOURCE_
 
