e770a410709357f3a9702b8372b0a822e3c6d201
AnantaYudica <nta040689@gmail.com>
2017-09-29 20:16:15 +0700

    ADD FOLDER AND FILE Add folder /scr/experiment/test/simple/json/value Add file /scr/experiment/test/simple/json/value/Array.h  - Add dependency with 'test\Source.h'  - Add definition class experiment::test::simple::json::value::Array with final inheritance    - Add base class with BrainMuscles::test::Source<Array>    - Add declare friend with BrainMuscles::test::Source<Array>    - Add default constructor with default by compiler and public to access    - Add static function member 'PreTest' and 'Test' with void for return type and public to access

---
+++ src/experiment/test/simple/json/value/Array.h
@@ -0,0 +1,32 @@
+#ifndef EXPERIMENT_TEST_SIMPLE_JSON_VALUE_ARRAY_H_
+#define EXPERIMENT_TEST_SIMPLE_JSON_VALUE_ARRAY_H_
+
+#include "test\Source.h"
+
+namespace experiment
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace value
+				{
+					struct Array final :
+						public BrainMuscles::test::Source<Array>
+					{
+						friend class BrainMuscles::test::Source<Array>;
+					private:
+						Array() = default;
+					public:
+						static void PreTest();
+						static void Test();
+					};
+				}
+			}
+		}
+	}
+}
+
+#endif //!EXPERIMENT_TEST_SIMPLE_JSON_VALUE_ARRAY_H_
