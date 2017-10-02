61ce83811de2b91afd2973f5288fd4c324acb245
AnantaYudica <nta040689@gmail.com>
2017-10-02 18:22:29 +0700

    ADD FILE Add dependency with 'test\Source.h' Add definition struct experiment::test::simple::json::value::number::Short with final inheritance  - Add base class with BrainMuscles::test::Source<Short>  - Add declare friend with class BrainMuscles::test::Source<Short>  - Add default constructor with default by compiler and public to access  - Add static function member 'Test' with public to access and void for return type

---
+++ src/experiment/test/simple/json/value/number/Short.h
@@ -0,0 +1,34 @@
+#ifndef EXPERIMENT_TEST_SIMPLE_JSON_VALUE_NUMBER_SHORT_H_
+#define EXPERIMENT_TEST_SIMPLE_JSON_VALUE_NUMBER_SHORT_H_
+
+#include "test\Source.h"
+
+namespace experiment
+{
+	namespace test
+	{
+		namespace simple
+		{
+			namespace json
+			{
+				namespace value
+				{
+					namespace number
+					{
+						struct Short final :
+							public BrainMuscles::test::Source<Short>
+						{
+							friend class BrainMuscles::test::Source<Short>;
+						private:
+							Short() = default;
+						public:
+							static void Test();
+						};
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif //!EXPERIMENT_TEST_SIMPLE_JSON_VALUE_NUMBER_SHORT_H_
