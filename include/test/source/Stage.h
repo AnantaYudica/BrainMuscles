9b8fee962669787b856674302396fa01ab571f81
AnantaYudica <nta040689@gmail.com>
2017-10-03 21:03:28 +0700

    MODIFIED FILE Add dependency with 'test\source\Environment.h' Remove enum BrainMuscles::Test::source::Stage Add class BrainMuscles::test::source::Stage  - Add variable member 'm_value' with char for type and private to access  - Add inline default constructor with public to access  - Add inline functions member 'IsHasPreTest', 'IsHasTest' and 'IsHasPostTest' with bool for return type and private to access  - Add inline functions member 'IsNotTest', 'IsPreTest', 'IsTest' and 'IsPostTest' with bool for return type and public to access  - Add inline functions member 'SetPreTest', 'SetTest' and 'SetPostTest' with void for return type and public to access

--- include/test/source/Stage.h
+++ include/test/source/Stage.h
@@ -3,19 +3,108 @@
 
 #ifdef _USING_TEST_SOURCE_
 
+#include "test\source\Environment.h"
+
 namespace BrainMuscles
 {
 	namespace test
 	{
 		namespace source
 		{
-			enum Stage
+			class Stage
 			{
-				not_test,
-				pre_test,
-				test,
-				post_test
+			private:
+				char m_value;
+			public:
+				inline Stage();
+			private:
+				inline bool IsHasPreTest();
+				inline bool IsHasTest();
+				inline bool IsHasPostTest();
+			public:
+				inline bool IsNotTest();
+				inline bool IsPreTest();
+				inline bool IsTest();
+				inline bool IsPostTest();
+				inline void SetPreTest();
+				inline void SetTest();
+				inline void SetPostTest();
 			};
+
+			inline Stage::Stage() :
+				m_value(0)
+			{}
+
+			inline bool Stage::IsHasPreTest()
+			{
+				return m_value & 1;
+			}
+
+			inline bool Stage::IsHasTest()
+			{
+				return m_value & 2;
+			}
+
+			inline bool Stage::IsHasPostTest()
+			{
+				return m_value & 4;
+			}
+
+			inline bool Stage::IsNotTest()
+			{
+				return m_value == 0;
+			}
+
+			inline bool Stage::IsPreTest()
+			{
+				return m_value == 1;
+			}
+
+			inline bool Stage::IsTest()
+			{
+				return m_value >= 2 && m_value < 4;
+			}
+
+			inline bool Stage::IsPostTest()
+			{
+				return m_value >= 4 && m_value < 8;
+			}
+
+			inline void Stage::SetPreTest()
+			{
+				if (IsNotTest())
+				{
+					m_value |= 1;
+				}
+				else
+				{
+					//todo error
+				}
+			}
+
+			inline void Stage::SetTest()
+			{
+				if (IsNotTest() || IsPreTest())
+				{
+					m_value |= 2;
+				}
+				else
+				{
+					//todo error	
+				}
+			}
+
+			inline void Stage::SetPostTest()
+			{
+				if (IsTest())
+				{
+					m_value |= 4;
+				}
+				else
+				{
+					//todo error	
+				}
+			}
 		}
 	}
 }
