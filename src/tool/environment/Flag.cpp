9331caa71f5f5030a536413cba898d7bd8b855e3
AnantaYudica <nta040689@gmail.com>
2017-10-14 18:23:14 +0700

    ADD FILE Add dependency with 'environment\Flag.h' Add implementation member class tool::environment::Flag  - Add implementation default constructor  - Add implementation functions member 'EnableDebug', 'DisableDebug' and 'IsDebug'

---
+++ src/tool/environment/Flag.cpp
@@ -0,0 +1,23 @@
+#include "environment\Flag.h"
+
+tool::environment::Flag::Flag() :
+	m_value(0)
+{}
+
+void tool::environment::Flag::EnableDebug()
+{
+	m_value |= BitDebug;
+}
+
+void tool::environment::Flag::DisableDebug()
+{
+	if (IsDebug())
+	{
+		m_value ^= BitDebug;
+	}
+}
+
+bool tool::environment::Flag::IsDebug()
+{
+	return m_value & BitDebug;
+}
