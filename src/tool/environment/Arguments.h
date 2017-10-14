1230c19bf2e80fd2da6b46d9a414f6fe8c9eb592
AnantaYudica <nta040689@gmail.com>
2017-10-14 15:20:58 +0700

    ADD FOLDERS AND FILE Add folder /src/tool Add folder /src/tool/environment Add file /src/tool/environment/Arguments.h  - Add dependencies with <cstdio>, <cstddef>, <string>, <vector>, <map>, <functional>  - Add prototype class tool::environment::Arguments  - Add definition class tool::environment::Arguments    - Add aliases type with public to access      - Add alias type 'SizeType' with std::size_t for type      - Add alias type 'LineType' with std::size_t for type      - Add alias type 'FunctionDefinition' with std::function<void(std::string)> for type      - Add alias type 'FunctionCaller' with std::function<void()> for type      - Add variable static member 'ms_instance' with Arguments for type and private to access      - Add variable member 'm_programName' with const char* for type and private to access      - Add variable member 'm_callersKey' with std::map<std::string, FunctionCaller> for type and private to access      - Add variable member 'm_definitionsKey' with std::map<std::string, FunctionDefinition> for type and private to access      - Add variable member 'm_argumentsKey' with std::map<std::string, std::string> for type and private to access      - Add variable member 'm_arguments' with std::vector<std::string> for type and private to access      - Add default constructor with private to access      - Add functions member 'IsKeyAliasArgument' and 'IsKeyNameArgument' with bool for return type, parameters function 'argument' with const char* for type and private to access      - Add function member 'KeyValueArgument' with std::pair<std::string, std::string> for return type, parameter function 'argument' with const char* for type and private to access      - Add function static member 'GetInstance' with Arguments& for return type and public to acces      - Add function member 'Initialization' with void for return type, parameters function 'argc' with int for type and 'argv' with char*[] for type and public to access      - Add function member 'Definition' with void for return type, parameters function 'key' with std::string for type and 'function_definition' with FunctionDefinition for type and public to access      - Add function member 'Caller' with void for return type, parameters function 'key' with std::string for type and 'function_caller' with FunctionCaller for type and public to access      - Add function member 'Size' with SizeType for return type and public to access      - Add function const member 'ProgramName' with const char* for return type and public to access      - Add functions member 'operator[]' with std::string for return type and {'index' with SizeType for type or 'key' with std::string for type} and public to access

---
+++ src/tool/environment/Arguments.h
@@ -0,0 +1,53 @@
+#ifndef TOOL_ENVIRONMENT_ARGUMENTS_H_
+#define TOOL_ENVIRONMENT_ARGUMENTS_H_
+
+#include <cstdio>
+#include <cstddef>
+#include <string>
+#include <vector>
+#include <map>
+#include <functional>
+
+namespace tool
+{
+	namespace environment
+	{
+		class Arguments;
+	}
+}
+
+class tool::environment::Arguments final
+{
+public:
+	typedef std::size_t							SizeType;
+	typedef std::size_t							LineType;
+	typedef std::function<void(std::string)>	FunctionDefinition;
+	typedef std::function<void()>				FunctionCaller;
+private:
+	static Arguments ms_instance;
+private:
+	const char* m_programName;
+	std::map<std::string, FunctionCaller> m_callersKey;
+	std::map<std::string, FunctionDefinition> m_definitionsKey;
+	std::map<std::string, std::string> m_argumentsKey;
+	std::vector<std::string> m_arguments;
+private:
+	Arguments();
+private:
+	bool IsKeyAliasArgument(const char* argument);
+	bool IsKeyNameArgument(const char* argument);
+	std::pair<std::string, std::string> KeyValueArgument(const char* argument);
+public:
+	static Arguments& GetInstance();
+public:
+	void Initialization(int argc, char *argv[]);
+	void Definition(std::string key, FunctionDefinition function_definition);
+	void Caller(std::string key, FunctionCaller function_caller);
+	SizeType Size();
+	const char* ProgramName() const;
+public:
+	std::string operator[](SizeType index);
+	std::string operator[](std::string key);
+};
+
+#endif //!TOOL_ENVIRONMENT_ARGUMENTS_H_
