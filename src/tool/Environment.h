9cf335186850490ddc8fac74ff97ce85d432e3e0
AnantaYudica <nta040689@gmail.com>
2017-10-15 07:22:57 +0700

    MODIFIED FILE Add dependency with <cstring> Add define macro 'ERROR_MESSAGE_BUFFER_SIZE' with 1024 for definition Update definition class tool::Environment  - Add functions inline member 'ErrorNumber' with void for return type, parameter function {'error_number' with int for type or {'title' with const char* for type and 'error_number' with int for type}} and public to access Add implementation functions inline member 'ErrorNumber' from class tool::Environment Update implementation function inline member 'Info' Add define macro '_Environment' with tool::Environment::GetInstance() for definition Add define function macro '__Error' Add define function macro '__ErrorNumber' Add define function macro '__Info'

--- src/tool/Environment.h
+++ src/tool/Environment.h
@@ -2,7 +2,10 @@
 #define TOOL_ENVIRONMENT_H_
 
 #include <cstdio>
+#include <cstring>
 #include "environment\Flag.h"
+
+#define ERROR_MESSAGE_BUFFER_SIZE 1024
 
 namespace tool
 {
@@ -22,13 +25,40 @@ private:
 public:
 	static Environment& GetInstance();
 public:
-	FlagType& Flag();
+	inline void ErrorNumber(int error_number);
+	inline void ErrorNumber(const char* title, int error_number);
 	template<typename... ARGS>
 	inline void Error(const char* format, ARGS... args);
 	template<typename... ARGS>
 	inline void Info(const char* format, ARGS... args);
+public:
+	FlagType& Flag();
 	int Result();
 };
+
+inline void tool::Environment::ErrorNumber(int error_number)
+{
+	m_result = 1;
+	char msg[ERROR_MESSAGE_BUFFER_SIZE];
+#if ((defined(_WIN32) || defined(_WIN64)) && !defined(_CRT_SECURE_NO_WARNINGS))
+	strerror_s(msg, errno);
+	std::printf("%s\n", msg);
+#elif (__GNUC__ \
+	|| (defined(_WIN32) || defined(_WIN64)) \
+		&& defined(_CRT_SECURE_NO_WARNINGS)))
+	strncpy(msg, std::strerror(error_number), ERROR_MESSAGE_BUFFER_SIZE);
+	msg[ERROR_MESSAGE_BUFFER_SIZE - 1] = NULL;
+	std::printf("%s\n", msg);
+#else
+#error "Undefined Compiler"
+#endif
+}
+
+inline void tool::Environment::ErrorNumber(const char* title, int error_number)
+{
+	std::printf("%s: ", title);
+	ErrorNumber(error_number);
+}
 
 template<typename... ARGS>
 inline void tool::Environment::Error(const char* format, ARGS... args)
@@ -44,8 +74,16 @@ inline void tool::Environment::Info(const char* format, ARGS... args)
 	if (m_flag.IsDebug())
 	{
 		std::printf(format, args...);
+		std::printf("\n");
 	}
-	std::printf("\n");
 }
+
+#define _Environment tool::Environment::GetInstance()
+
+#define __Error(...) _Environment.Error(__VA_ARGS__)
+
+#define __ErrorNumber(...) _Environment.ErrorNumber(__VA_ARGS__)
+
+#define __Info(...) _Environment.Info(__VA_ARGS__)
 
 #endif //!TOOL_ENVIRONMENT_H_
