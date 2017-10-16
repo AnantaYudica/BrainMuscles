ca55d1a8fb2abe1d01ed3e826188de30e3beae86
AnantaYudica <nta040689@gmail.com>
2017-10-16 09:16:19 +0700

    MODIFIED FILE Update in class tool::file::Read  - Remove aliases type 'LineType' and 'ColumnType'  - Remove variables member 'm_line' and 'm_column'  - Update function member 'SeekPosition' with change parameter function name from off to pos  - Update functions member 'GetLine' with change function name from 'GetLine' to 'GetString'  - Update functions member 'Find' with add prefix 'end_' at function parameter name  - Update function member 'IsEnd' with change function name from 'IsEnd' to IsEOF

--- src/tool/file/Read.h
+++ src/tool/file/Read.h
@@ -18,8 +18,6 @@ class tool::file::Read
 {
 private:
 	typedef std::ifstream	StreamType;
-	typedef std::size_t		LineType;
-	typedef std::size_t		ColumnType;
 public:
 	typedef std::size_t						SizeType;
 	typedef typename StreamType::off_type	OffType;
@@ -27,8 +25,6 @@ public:
 	typedef std::ios_base::seekdir			SeekDirType;
 	typedef std::function<bool(int)>		FunctionCondition;
 private:
-	LineType m_line;
-	ColumnType m_column;
 	std::string m_pathname;
 	StreamType m_filestream;
 public:
@@ -42,24 +38,23 @@ private:
 public:
 	bool Open(std::string pathname);
 public:
-	void SeekPosition(PosType off);
+	void SeekPosition(PosType pos);
 	void SeekPosition(OffType off, SeekDirType dir);
 	PosType TellPosition();
 public:
 	int GetCharacter();
-	std::string GetString(SizeType buffer_size);
-	std::string GetString(char end_character, SizeType buffer_size);
-	std::string GetString(std::string end_string, SizeType buffer_size);
-	std::string GetString(FunctionCondition end_condition,
+	std::string GetLine(SizeType buffer_size);
+	std::string GetLine(char end_character, SizeType buffer_size);
+	std::string GetLine(std::string end_string, SizeType buffer_size);
+	std::string GetLine(FunctionCondition end_condition,
 		SizeType buffer_size);
 public:
-	bool Find(char character);
-	bool Find(std::string string);
-	bool Find(FunctionCondition condition);
+	bool Find(char end_character);
+	bool Find(std::string end_string);
+	bool Find(FunctionCondition end_condition);
 public:
 	bool IsOpen();
-	bool IsEnd();
+	bool IsEOF();
 };
-
 
 #endif //!TOOL_FILE_READ_H_
