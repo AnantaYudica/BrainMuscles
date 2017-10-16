b2e2349a0aef365b8c57fc8eda05ac7b7e279ebf
AnantaYudica <nta040689@gmail.com>
2017-10-16 15:08:01 +0700

    ADD FOLDER AND FILE Add folder /src/tool/source Add file /src/tool/source/Column.h  - Add dependencies with <cstdio>, <cctype>, <cstddef> and <string>  - Add dependency with 'file\Read.h'  - Add prototype class tool::source::Column  - Add definition class tool::source::Column    - Add aliases type with private to access      - Add alias type 'FlagType' with unsigned char for type      - Add alias type 'ReadType' with tool::file::Read for type    - Add aliases type with public to access      - Add alias type 'SizeType' and 'LineType' with std::size_t for type    - Add variable static constexpr member 'BitLineFeed' with FlagType for type, 1 for value and private to access    - Add variable static constexpr member 'BitCarriageReturn' with FlagType for type, 2 for value and private to access    - Add variable member 'm_flag' with FlagType for type and private to access    - Add variable member 'm_maximum' with SizeType for type and private to access    - Add variable member 'm_line' with LineType for type and private to access    - Add variable member 'm_read' with ReadType for type and private to access    - Add fill constructor with parameter function 'pathname' with std::string for type and public to access    - Add function member 'ResetNewline' with void for return type and private to access    - Add functions member 'SetLineFeed' and 'SetCarriageReturn' with bool for return type, parameter function 'character' with const int& for type and private to access    - Add function member 'IsLineFeed', 'IsCarriageReturn', 'IsNewLine' and 'IsSkip' with bool for return type and private to access    - Add functions static member 'IsLineFeed' and 'IsCarriageReturn' with bool for return type, parameter function 'character' with const int& for type and private to access    - Add function member 'Newline' with void return type, parameter function 'column' with const SizeType& for type and 'string' with std::string for type and private to access    - Add function member 'Maximum' with void for return type, parameter function 'value' with SizeType for type and public to access    - Add function member 'Check' with int for return type and public to access

---
+++ src/tool/source/Column.h
@@ -0,0 +1,53 @@
+#ifndef TOOL_SOURCE_COLUMN_H_
+#define TOOL_SOURCE_COLUMN_H_
+
+#include <cstdio>
+#include <cctype>
+#include <cstddef>
+#include <string>
+
+#include "file\Read.h"
+
+namespace tool
+{
+	namespace source
+	{
+		class Column;
+	}
+}
+
+class tool::source::Column
+{
+private:
+	typedef unsigned char		FlagType;
+	typedef tool::file::Read	ReadType;
+public:
+	typedef std::size_t SizeType;
+	typedef std::size_t LineType;
+private:
+	static constexpr FlagType BitLineFeed = 1;
+	static constexpr FlagType BitCarriageReturn = 2;
+private:
+	FlagType m_flag;
+	SizeType m_maximum;
+	LineType m_line;
+	ReadType m_read;
+public:
+	Column(std::string pathname);
+private:
+	void ResetNewline();
+	bool SetLineFeed(const int& character);
+	bool SetCarriageReturn(const int& character);
+	bool IsLineFeed();
+	bool IsCarriageReturn();
+	static bool IsLineFeed(const int& character);
+	static bool IsCarriageReturn(const int& character);
+	bool IsNewLine(const int& character);
+	bool IsSkip(const int& character);
+	void Newline(const SizeType& column, std::string string);
+public:
+	void Maximum(SizeType value);
+	int Check();
+};
+
+#endif //!TOOL_SOURCE_COLUMN_H_
