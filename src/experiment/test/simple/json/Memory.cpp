2d1230f9ed62b00953cb1e177cf678ac5c1c7b0f
AnantaYudica <nta040689@gmail.com>
2017-10-17 12:03:04 +0700

    MODIFIED FILE Update implementation function member 'Test' of class experiment::test::simple::json::Memory

--- src/experiment/test/simple/json/Memory.cpp
+++ src/experiment/test/simple/json/Memory.cpp
@@ -5,133 +5,242 @@
 
 #include "experiment\test\simple\json\Memory.h"
 
-namespace experiment
+void experiment::test::simple::json::Memory::Test()
 {
-	namespace test
-	{
-		namespace simple
-		{
-			namespace json
-			{
-				void Memory::Test()
-				{
-					//declare namespace from BrainMuscles::test::simple to BMTestSimple for simplify
-					namespace BMTestSimple = BrainMuscles::test::simple;
+	//	Declare namespace from 
+	//		'BrainMuscles::test::simple'
+	//	to 
+	//		'BMTestSimple'
+	namespace BMTestSimple = BrainMuscles::test::simple;
 
-					//declare variable of class BrainMuscles::test::simple::json::Memory::SharedPointerType<int> 
-					// with direct initialization from direct initialization of int
-					BMTestSimple::json::Memory::SharedPointerType<int> sharedpointer_int(new int(123));
+	//	Declare alias type 'Memory' 
+	//	with prototype class
+	//		'BrainMuscles::test::simple::json::Memory'
+	typedef BMTestSimple::json::Memory Memory;
 
-					//declare variable of class BrainMuscles::test::simple::json::Memory::SharedPointerType<const int> 
-					// with direct initialization from class BrainMuscles::test::simple::json::Memory::SharedPointerType<int> 
-					BMTestSimple::json::Memory::SharedPointerType<const int> sharedpointer_const_int(sharedpointer_int);
-					
-					//check value between value of pointer and value of initialization from class BrainMuscles::test::simple::json::Memory::SharedPointerType<int> is same
-					SourceAssert(*sharedpointer_int == 123, 
-						"value between value of pointer and value of initialization from class BrainMuscles::test::simple::json::Memory::SharedPointerType<int> is not same");
-					
-					//check value between value of pointer from class BrainMuscles::test::simple::json::Memory::SharedPointerType<int> 
-					// and value of pointer from BrainMuscles::test::simple::json::Memory::SharedPointerType<const int> is same
-					SourceAssert(*sharedpointer_const_int == *sharedpointer_int, 
-						"value between value of pointer from class BrainMuscles::test::simple::json::Memory::SharedPointerType<int>"
-						" and value of pointer from BrainMuscles::test::simple::json::Memory::SharedPointerType<const int> is not same");
+	//	Declare variable of
+	//		class 'BrainMuscles::test::simple::json
+	//			::Memory::SharedPointerType<int>'
+	//	with direct initialization from direct initialization of int
+	Memory::SharedPointerType<int> sharedpointer_int(new int(123));
 
-					//check return value from static function member 'IsBaseOfSharedPointer()' of class BrainMuscles::test::simple::json::Memory
-					// and argument template with class BrainMuscles::test::simple::json::Memory::SharedPointerType<int> is true
-					SourceAssert(BMTestSimple::json::Memory::IsBaseOfSharedPointer<BMTestSimple::json::Memory::SharedPointerType<int>>() == true,
-						"return value from static function member 'IsBaseOfSharedPointer()' of class BrainMuscles::test::simple::json::Memory"
-						" and argument template with class BrainMuscles::test::simple::json::Memory::SharedPointerType<int> is false");
-					
-					//declare variable of class BrainMuscles::test::simple::json::Memory::SharedPointerType<const ExampleBase> 
-					// with direct initialization from default initialization of class experiment::test::simple::json::Memory::ExampleDerivedA
-					BMTestSimple::json::Memory::SharedPointerType<const ExampleBase> pointer_const_examplebase1(new ExampleDerivedA);
-					
-					//declare variable of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleBase> 
-					// with copy initalization from return static function member 'ConstPointerCast<const ExampleBase, ExampleBase>()' 
-					// of class BrainMuscles::test::simple::json::Memory and argument with variable of class BrainMuscles::test::simple::json::Memory::SharedPointerType<const ExampleBase> 
-					BMTestSimple::json::Memory::SharedPointerType<ExampleBase> pointer_examplebase1 = 
-						BMTestSimple::json::Memory::ConstPointerCast<const ExampleBase, ExampleBase>(pointer_const_examplebase1);
+	//	Declare variable of 
+	//		class 'BrainMuscles::test::simple::json
+	//			::Memory::SharedPointerType<const int>'
+	//	with direct initialization from 
+	//		class 'BrainMuscles::test::simple::json
+	//			::Memory::SharedPointerType<int>'
+	Memory::SharedPointerType<const int> 
+		sharedpointer_const_int(sharedpointer_int);
 
-					//assignment variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleBase> 
-					// with copy initialization of int
-					pointer_examplebase1->Value = 4;
+	//	Check value between 
+	//		pointer value of variable 'sharedpointer_int' 
+	//	and 
+	//		123 from int
+	//	is same
+	SourceAssert(*sharedpointer_int == 123,
+		"value between pointer value of variable 'sharedpointer_int' "
+		"and 123 from int is not same");
 
-					//check value between variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<const ExampleBase> 
-					// and variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleBase> is same
-					SourceAssert(pointer_const_examplebase1->Value == pointer_examplebase1->Value,
-						"value between variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<const ExampleBase> "
-						"and variable member 'value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleBase> is not same");
+	//	Check value between 
+	//		pointer value of variable 'sharedpointer_const_int'
+	//	and 
+	//		pointer value of variable  'sharedpointer_int' 
+	//	is same
+	SourceAssert(*sharedpointer_const_int == *sharedpointer_int,
+		"value between pointer value of variable 'sharedpointer_const_int' "
+		" and pointer value of variable  'sharedpointer_int' is not same");
 
-					//check address between variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<const ExampleBase> 
-					// and variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleBase> is same
-					SourceAssert(&pointer_const_examplebase1->Value == &pointer_examplebase1->Value, 
-						"address between variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<const ExampleBase> "
-						"and variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleBase> is not same");
-					
-					//declare variable of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleBase> 
-					// with direct initialize from default initialize of class experiment::test::simple::json::Memory::ExampleDerivedA
-					BMTestSimple::json::Memory::SharedPointerType<ExampleBase> pointer_examplebase2(new ExampleDerivedA);
-					
-					//declare variable of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleDerivedA>
-					// with copy initalization from static function member 'StaticPointerCast<ExampleBase, ExampleDerivedA>'
-					// of class BrainMuscles::test::simple::json::Memory and argument with variable of BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleBase>
-					BMTestSimple::json::Memory::SharedPointerType<ExampleDerivedA> pointer_examplederivedA1 =
-						BMTestSimple::json::Memory::StaticPointerCast<ExampleBase, ExampleDerivedA>(pointer_examplebase2);
+	//	Check return value from 
+	//		function static member 'IsBaseOfSharedPointer<
+	//				BMTestSimple::json::Memory::SharedPointerType<int>>' of 
+	//			class 'BrainMuscles::test::simple::json::Memory'
+	//	is true
+	SourceAssert(Memory::IsBaseOfSharedPointer<
+		Memory::SharedPointerType<int>>() == true,
+		"return value from function static member 'IsBaseOfSharedPointer<"
+		"BrainMuscles::test::simple::json::Memory::SharedPointerType<int>>' "
+		"of class BrainMuscles::test::simple::json::Memory is false");
 
-					//assignment variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleDerivedA>
-					// with copy initialization of int
-					pointer_examplederivedA1->Value = 8;
+	//	Declare variable of 
+	//		class 'BrainMuscles::test::simple::json::Memory
+	//			::SharedPointerType<const ExampleBase> '
+	//	with direct initialization from default initialization of 
+	//		class 'experiment::test::simple::json::Memory::ExampleDerivedA'
+	Memory::SharedPointerType<const ExampleBase>
+		pointer_const_examplebase1(new ExampleDerivedA);
 
-					//check value between variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleBase> 
-					// and variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleDerivedA> is same
-					SourceAssert(pointer_examplebase2->Value == pointer_examplederivedA1->Value,
-						"value between variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleBase> "
-						"and variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleDerivedA> is not same");
+	//	Declare variable of 
+	//		class 'BrainMuscles::test::simple::json
+	//			::Memory::SharedPointerType<ExampleBase>'
+	//	with copy initalization from 
+	//		return function static member 'ConstPointerCast<
+	//			const ExampleBase, ExampleBase>' of 
+	//				class 'BrainMuscles::test::simple::json::Memory'
+	//	where variable 'pointer_const_examplebase1' as first argument to 
+	//		function static member 'ConstPointerCast<
+	//			const ExampleBase, ExampleBase>'
+	Memory::SharedPointerType<ExampleBase> pointer_examplebase1 
+		= Memory::ConstPointerCast<const ExampleBase, 
+			ExampleBase>(pointer_const_examplebase1);
 
-					//check address between variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleBase> 
-					// and variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleDerivedA> is same
-					SourceAssert(&pointer_examplebase2->Value == &pointer_examplederivedA1->Value,
-						"address between variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleBase> "
-						"and variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleDerivedA> is not same");
+	//	Assignment to
+	//		variable member 'Value' of 
+	//			variable 'pointer_examplebase1'
+	//	with copy initialization of int
+	pointer_examplebase1->Value = 4;
+
+	//	Check value between 
+	//		variable member 'Value' of
+	//			variable 'pointer_const_examplebase1'
+	//	and 
+	//		variable member 'Value' of 
+	//			variable 'pointer_examplebase1'
+	//	is same
+	SourceAssert(pointer_const_examplebase1->Value 
+		== pointer_examplebase1->Value,
+		"value between variable member 'Value' of "
+		"variable 'pointer_const_examplebase1' and variable member 'value' of "
+		"variable 'pointer_examplebase1' is not same");
+
+	//	Check address between 
+	//		variable member 'Value' of 
+	//			variable 'pointer_const_examplebase1'
+	//	and 
+	//		variable member 'Value' of 
+	//			variable 'pointer_examplebase1' 
+	//	is same
+	SourceAssert(&pointer_const_examplebase1->Value 
+		== &pointer_examplebase1->Value,
+		"address between variable member 'Value' of "
+		"variable 'pointer_const_examplebase1' and variable member 'Value' of "
+		"variable 'pointer_examplebase1' is not same");
+
+	//	Declare variable of 
+	//		class 'BrainMuscles::test::simple::json
+	//			::Memory::SharedPointerType<ExampleBase>'
+	//	with direct initialize from default initialize of 
+	//		class 'experiment::test::simple::json::Memory::ExampleDerivedA'
+	Memory::SharedPointerType<ExampleBase> 
+		pointer_examplebase2(new ExampleDerivedA);
+
+	//	Declare variable of 
+	//		class BrainMuscles::test::simple::json
+	//			::Memory::SharedPointerType<ExampleDerivedA>
+	//	with copy initalization from 
+	//		return function static member 'StaticPointerCast<
+	//			ExampleBase, ExampleDerivedA>' of 
+	//				class BrainMuscles::test::simple::json::Memory 
+	//	where variable 'pointer_examplebase2' as first argument to
+	//		function static member 'StaticPointerCast<
+	//			ExampleBase, ExampleDerivedA>'
+	Memory::SharedPointerType<ExampleDerivedA> pointer_examplederivedA1
+		= Memory::StaticPointerCast<ExampleBase, 
+			ExampleDerivedA>(pointer_examplebase2);
+
+	//	Assignment to 
+	//		variable member 'Value' of 
+	//			variable 'pointer_examplederivedA1'
+	//	with copy initialization of int
+	pointer_examplederivedA1->Value = 8;
+
+	//	Check value between 
+	//		variable member 'Value' of 
+	//			variable 'pointer_examplebase2'
+	//	and 
+	//		variable member 'Value' of 
+	//			variable 'pointer_examplederivedA1' 
+	//	is same
+	SourceAssert(pointer_examplebase2->Value 
+		== pointer_examplederivedA1->Value,
+		"value between variable member 'Value' of "
+		"variable 'pointer_examplebase2' and variable member 'Value' of "
+		"variable 'pointer_examplederivedA1' is not same");
+
+	//	Check address between 
+	//		variable member 'Value' of 
+	//			variable 'pointer_examplebase2'
+	//	and variable member 'Value' of 
+	//		variable 'pointer_examplederivedA1' 
+	//	is same
+	SourceAssert(&pointer_examplebase2->Value 
+		== &pointer_examplederivedA1->Value,
+		"address between variable member 'Value' "
+		"of variable 'pointer_examplebase2' and variable member 'Value' of "
+		"variable 'pointer_examplederivedA1'  is not same");
 
 
-					//declare variable of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleBase> 
-					// with direct initialize from default initalization of class experiment::test::simple::json::Memory::ExampleDerivedA
-					BMTestSimple::json::Memory::SharedPointerType<ExampleBase> pointer_examplebase3(new ExampleDerivedA);
+	//	Declare variable of 
+	//		class 'BrainMuscles::test::simple::json
+	//			::Memory::SharedPointerType<ExampleBase>'
+	//	with direct initialize from default initalization of 
+	//		class 'experiment::test::simple::json::Memory::ExampleDerivedA'
+	Memory::SharedPointerType<ExampleBase> 
+		pointer_examplebase3(new ExampleDerivedA);
 
-					//declare variable of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleDerivedA> 
-					// with copy initalization from static function member 'DynamicPointerCast<ExampleBase, ExampleDerivedA>'
-					// of class BrainMuscles::test::simple::json::Memory and argument with variable of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleBase> 
-					BMTestSimple::json::Memory::SharedPointerType<ExampleDerivedA> pointer_examplederivedA2
-						= BMTestSimple::json::Memory::DynamicPointerCast<ExampleBase, ExampleDerivedA>(pointer_examplebase3);
+	//	Declare variable of 
+	//		class 'BrainMuscles::test::simple::json
+	//			::Memory::SharedPointerType<ExampleDerivedA> '
+	//	with copy initalization from 
+	//		function static member 'DynamicPointerCast<
+	//			ExampleBase, ExampleDerivedA>' of 
+	//				class 'BrainMuscles::test::simple::json::Memory' 
+	//	where variable 'pointer_examplebase3' as first argument to
+	//		function static member 'DynamicPointerCast<
+	//			ExampleBase, ExampleDerivedA>'
+	Memory::SharedPointerType<ExampleDerivedA> pointer_examplederivedA2
+		= Memory::DynamicPointerCast<ExampleBase, 
+			ExampleDerivedA>(pointer_examplebase3);
 
-					//assignment variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleDerivedA>
-					// with copy initialization of int
-					pointer_examplederivedA2->Value = 16;
+	//	Assignment to
+	//		variable member 'Value' of 
+	//			variable 'pointer_examplederivedA2'
+	//	 with copy initialization of int
+	pointer_examplederivedA2->Value = 16;
 
-					//check value between variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleBase> 
-					// and variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleDerivedA> is same
-					SourceAssert(pointer_examplebase3->Value == pointer_examplederivedA2->Value,
-						"value between variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleBase> "
-						"and variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleDerivedA> is not same");
+	//	Check value between 
+	//		variable member 'Value' of 
+	//			variable 'pointer_examplebase3'
+	//	and 
+	//		variable member 'Value' of 
+	//			variable 'pointer_examplederivedA2' 
+	//	is same
+	SourceAssert(pointer_examplebase3->Value
+		== pointer_examplederivedA2->Value,
+		"value between variable member 'Value' of "
+		"variable 'pointer_examplebase3' and variable member 'Value' of "
+		"variable 'pointer_examplederivedA2' is not same");
 
-					//check address between variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleBase> 
-					// and variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleDerivedA> is same
-					SourceAssert(&pointer_examplebase3->Value == &pointer_examplederivedA2->Value,
-						"address between variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleBase> "
-						"and variable member 'Value' of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleDerivedA> is not same");
+	//	Check address between 
+	//		variable member 'Value' of 
+	//			variable 'pointer_examplebase3'
+	//	and 
+	//		variable member 'Value' of 
+	//			variable 'pointer_examplederivedA2' 
+	//	is same
+	SourceAssert(&pointer_examplebase3->Value 
+		== &pointer_examplederivedA2->Value,
+		"address between variable member 'Value' of "
+		"variable 'pointer_examplebase3' and variable member 'Value' of "
+		"variable 'pointer_examplederivedA2' is not same");
 
-					//declare variable of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleDerivedB> 
-					// with copy initalization from static function member 'DynamicPointerCast<ExampleBase, ExampleDerivedB>' 
-					// of class BrainMuscles::test::simple::json::Memory and argument with variable of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleBase> 
-					BMTestSimple::json::Memory::SharedPointerType<ExampleDerivedB> pointer_examplederivedB2 = 
-						BMTestSimple::json::Memory::DynamicPointerCast<ExampleBase, ExampleDerivedB>(pointer_examplebase3);
-					
-					//check variable of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleDerivedB> is null pointer
-					SourceAssert(pointer_examplederivedB2 == nullptr, 
-						"variable of class BrainMuscles::test::simple::json::Memory::SharedPointerType<ExampleDerivedB> is not null pointer");
-				}
-			}
-		}
-	}
+	//	Declare variable of 
+	//		class 'BrainMuscles::test::simple::json
+	//			::Memory::SharedPointerType<ExampleDerivedB>'
+	//	with copy initalization from 
+	//		function static member 'DynamicPointerCast<
+	//			ExampleBase, ExampleDerivedB>' of 
+	//				class BrainMuscles::test::simple::json::Memory
+	//	where variable 'pointer_examplebase3' as first argument to 
+	//		function static member 'DynamicPointerCast<
+	//			ExampleBase, ExampleDerivedB>'
+	Memory::SharedPointerType<ExampleDerivedB> pointer_examplederivedB2 =
+		Memory::DynamicPointerCast<ExampleBase, 
+			ExampleDerivedB>(pointer_examplebase3);
+
+	//	Check variable
+	//		'pointer_examplederivedB2'
+	//	is null pointer
+	SourceAssert(pointer_examplederivedB2 == nullptr,
+		"variable 'pointer_examplederivedB2' is not null pointer");
 }
