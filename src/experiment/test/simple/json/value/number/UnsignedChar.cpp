05dde1e6608375e76812032592750cab75530e2e
AnantaYudica <nta040689@gmail.com>
2017-10-19 16:51:56 +0700

    MODIFIED FILE Update implementation functions static member 'PreTest' and 'Test' from class experiment::test::simple::json::value::number::UnsignedChar

--- src/experiment/test/simple/json/value/number/UnsignedChar.cpp
+++ src/experiment/test/simple/json/value/number/UnsignedChar.cpp
@@ -7,77 +7,106 @@
 
 #include "experiment\test\simple\json\value\number\UnsignedChar.h"
 
-namespace experiment
+void experiment::test::simple::json::value::number::UnsignedChar::PreTest()
 {
-	namespace test
-	{
-		namespace simple
-		{
-			namespace json
-			{
-				namespace value
-				{
-					namespace number
-					{
-						void UnsignedChar::PreTest()
-						{
-							SourceRequirement(experiment::test::simple::json::value::Number);
-						}
+	//	Call function static member 'Requirement' from
+	//		class 'experiment::test::simple::json::value::Number'
+	SourceRequirement(experiment::test::simple::json::value::Number);
+}
 
-						void UnsignedChar::Test()
-						{
-							//declare namespace from BrainMuscles::test::simple to BMTestSimple for simplify
-							namespace BMTestSimple = BrainMuscles::test::simple;
+void experiment::test::simple::json::value::number::UnsignedChar::Test()
+{
+	//	Declare namespace from 
+	//		'BrainMuscles::test::simple'
+	//	to 
+	//		'BMTestSimple'
+	namespace BMTestSimple = BrainMuscles::test::simple;
 
-							//declare variable of class BrainMuscles::test::simple::json::value::number::UnsignedChar
-							// with default initialization 
-							BMTestSimple::json::value::number::UnsignedChar value_number_unsginedchar1;
+	//	Declare namespace from 
+	//		'BrainMuscles::test::simple::json'
+	//	to 
+	//		'Json'
+	namespace Json = BMTestSimple::json;
 
-							//check value between variable 'value_number_unsginedchar1' and 0 from int is same
-							SourceAssert(value_number_unsginedchar1 == 0,
-								"value variable 'value_number_unsginedchar1' is not equal with 0 from int");
+	//	Declare namespace from 
+	//		'BrainMuscles::test::simple::json::value'
+	//	to 
+	//		'JsonValue'
+	namespace JsonValue = Json::value;
 
-							//declare variable of class BrainMuscles::test::simple::json::value::number::UnsignedChar
-							// with direct initialization from 15 from unsigned int
-							BMTestSimple::json::value::number::UnsignedChar value_number_unsignedchar2(15U);
+	//	Declare namespace from 
+	//		'BrainMuscles::test::simple::json::value::number'
+	//	to 
+	//		'JsonValueNumber'
+	namespace JsonValueNumber = JsonValue::number;
 
-							//check value between variable 'value_number_unsignedchar2' and 15 from unsigned int is same
-							SourceAssert(value_number_unsignedchar2 == 15U,
-								"value variable 'value_number_unsignedchar2' is not equal with 15 from unsigned int");
+	//	Declare variable of 
+	//		class 'BrainMuscles::test::simple::json::value
+	//			::number::UnsignedChar'
+	// with default initialization 
+	JsonValueNumber::UnsignedChar value_number_unsginedchar1;
 
-							//declare variable of class BrainMuscles::test::simple::json::value::number::UnsignedChar
-							// with copy initialization from variable of class BrainMuscles::test::simple::json::value::number::UnsignedChar
-							BMTestSimple::json::value::number::UnsignedChar value_number_unsignedchar3(value_number_unsignedchar2);
+	//	Check variable 'value_number_unsginedchar1' 
+	//	is 0 of int
+	SourceAssert(value_number_unsginedchar1 == 0,
+		"variable 'value_number_unsginedchar1' is not 0 of int");
 
-							//check value between variable 'value_number_unsignedchar3' and variable 'value_number_unsignedchar2' is same 
-							SourceAssert(value_number_unsignedchar3 == value_number_unsignedchar2,
-								"value variable 'value_number_unsignedchar3' and variable 'value_number_unsignedchar2' is not same");
+	//	Declare variable of 
+	//		class 'BrainMuscles::test::simple::json::value
+	//			::number::UnsignedChar'
+	//	with direct initialization from 15 of unsigned int
+	JsonValueNumber::UnsignedChar value_number_unsignedchar2(15U);
 
-							//assignment variable 'value_number_unsginedchar1' with unsigned int
-							value_number_unsginedchar1 = 12U;
+	//	Check variable 'value_number_unsignedchar2' 
+	//	is 15 of unsigned int
+	SourceAssert(value_number_unsignedchar2 == 15U,
+		"variable 'value_number_unsignedchar2' is not 15 of unsigned int");
 
-							//check value between variable 'value_number_unsginedchar1' and 12 from unsigned int is same
-							SourceAssert(value_number_unsginedchar1 == 12U,
-								"value variable 'value_number_unsginedchar1' is not equal with 12 from unsigned int");
+	//	Declare variable of 
+	//		class 'BrainMuscles::test::simple::json::value
+	//			::number::UnsignedChar'
+	//	with copy initialization from variable 'value_number_unsignedchar2'
+	JsonValueNumber::UnsignedChar 
+		value_number_unsignedchar3(value_number_unsignedchar2);
 
-							//declare reference variable of unsigned char with reference initialization 
-							// from class BrainMuscles::test::simple::json::value::number::UnsignedChar
-							unsigned char &reference_unsignedchar1 = value_number_unsginedchar1;
+	//	Check compare variable between 
+	//		variable 'value_number_unsignedchar3' 
+	//	and variable 'value_number_unsignedchar2' 
+	//	is same 
+	SourceAssert(value_number_unsignedchar3 == value_number_unsignedchar2,
+		"compare variable between variable 'value_number_unsignedchar3' and "
+		"variable 'value_number_unsignedchar2' is not same");
 
-							//assignment reference variable 'reference_unsignedchar1' with unsigned int
-							reference_unsignedchar1 = 16U;
+	//	Assignment variable 'value_number_unsginedchar1'
+	//	with 12 of unsigned int
+	value_number_unsginedchar1 = 12U;
 
-							//check value between reference variable 'reference_unsignedchar1' and 16 from unsigned int is same
-							SourceAssert(reference_unsignedchar1 == 16U,
-								"value of reference variable 'reference_unsignedchar1' is not equal with 16 from unsigned int");
+	//	Check variable 'value_number_unsginedchar1' 
+	//	is 12 of unsigned int
+	SourceAssert(value_number_unsginedchar1 == 12U,
+		"variable 'value_number_unsginedchar1' is not 12 of unsigned int");
 
-							//check value between reference variable 'reference_unsignedchar1' and variable 'value_number_unsginedchar1' is same
-							SourceAssert(reference_unsignedchar1 == value_number_unsginedchar1,
-								"value of reference variable 'reference_unsignedchar1' and variable 'value_number_unsginedchar1' is not same");
-						}
-					}
-				}
-			}
-		}
-	}
+	//	Declare reference variable of unsigned char 
+	//		with reference initialization from
+	//			variable 'value_number_unsginedchar1'
+	unsigned char &reference_unsignedchar1 = value_number_unsginedchar1;
+
+	//	Assignment reference variable 'reference_unsignedchar1' 
+	//	with 16 of unsigned int
+	reference_unsignedchar1 = 16U;
+
+	//	Check reference variable 'reference_unsignedchar1' 
+	//	is 16 of unsigned int
+	SourceAssert(reference_unsignedchar1 == 16U,
+		"reference variable 'reference_unsignedchar1' "
+		"is not 16 of unsigned int");
+
+	//	Check compare variable between 
+	//		reference variable 'reference_unsignedchar1' 
+	//	and variable 'value_number_unsginedchar1' 
+	//	is same
+	SourceAssert(reference_unsignedchar1 == value_number_unsginedchar1,
+		"compare variable between "
+		"reference variable 'reference_unsignedchar1' "
+		"and variable 'value_number_unsginedchar1' is not same");
 }
