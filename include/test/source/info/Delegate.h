37cb943e47378623a1bd9752125f72233f800202
AnantaYudica <nta040689@gmail.com>
2017-11-06 05:43:56 +0700

    ADD FILE Add macro ifdef '_USING_TEST_SOURCE_'  - Add macro define '_USING_TEST_SOURCE_INFO_DELEGATE_DEFAULT_BUFFER_SIZE_' with 1024 for replacement  - Add macro ifndef _USING_TEST_SOURCE_INFO_DELEGATE_BUFFER_SIZE_    - Add macro define '_USING_TEST_SOURCE_INFO_DELEGATE_BUFFER_SIZE_' with '_USING_TEST_SOURCE_INFO_DELEGATE_DEFAULT_BUFFER_SIZE_' for replacement  - Add dependencies <cstdio> and <type_traits>  - Add dependency with 'test\source\Info.h'  - Add dependency with 'test\source\info\Flags.h'  - Add prototype template class 'BrainMuscles::test::source::info::Delegate' with parameter template 'ENVIRONMENT_TYPE'  - Add definition template class 'BrainMuscles::test::source::info::Delegate'    - Add aliases type with public to access      - Add alias type 'DelegateType' with Delegate<ENVIRONMENT_TYPE> for type      - Add alias type 'InfoType' with BrainMuscles::test::source::Info for type      - Add alias type 'FlagsType' with BrainMuscles::test::source::info::Flags for type      - Add alias type 'FlagsIntegerType' with BrainMuscles::test::source::info::FlagsType for type    - Add variables member with private to access      - Add variable member 'm_flag' with FlagsIntegerType for type      - Add variable member 'm_buffer' with char * for type      - Add variable member 'm_bufferSize' with const std::size_t for type      - Add variable member 'm_file' with const char * for type      - Add variable member 'm_line' with const std::size_t for type      - Add variable member 'm_info' with InfoType * const for type    - Add fill constructor with parameters constructor 'info' with InfoType * const  for type, 'flag' with FlagsIntegerType for type, 'file' with const char *  for type, 'line' with const std::size_t& for type, 'buffer_size' with std::size_t for type and {_USING_TEST_SOURCE_INFO_DELEGATE_BUFFER_SIZE_ or _USING_TEST_SOURCE_INFO_DELEGATE_DEFAULT_BUFFER_SIZE_} for default value    - Add destructor    - Add template functions member 'Out' with parameter template variadic 'ARGS', DelegateType& for return type, parameters function ['flag' with FlagsType for type], 'format' with const char* for type and 'args' with ARGS... for type and public to access    - Add function member 'End' with void for return type and public to access  - Add implementation constructor, destructor, template function member and function member of template class 'BrainMuscles::test::source::info::Delegate'

---
+++ include/test/source/info/Delegate.h
@@ -0,0 +1,139 @@
+#ifndef TEST_SOURCE_INFO_DELEGATE_H_
+#define TEST_SOURCE_INFO_DELEGATE_H_
+
+#ifdef _USING_TEST_SOURCE_
+
+#define _USING_TEST_SOURCE_INFO_DELEGATE_DEFAULT_BUFFER_SIZE_ 1024
+
+#ifndef _USING_TEST_SOURCE_INFO_DELEGATE_BUFFER_SIZE_
+
+#define _USING_TEST_SOURCE_INFO_DELEGATE_BUFFER_SIZE_ \
+	_USING_TEST_SOURCE_INFO_DELEGATE_DEFAULT_BUFFER_SIZE_
+
+#endif //!_USING_TEST_SOURCE_INFO_DELEGATE_BUFFER_SIZE_
+
+#include <cstdio>
+#include <type_traits>
+
+#include "test\source\Info.h"
+#include "test\source\info\Flags.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace source
+		{
+			namespace info
+			{
+				template<typename ENVIRONMENT_TYPE>
+				class Delegate;
+			}
+		}
+	}
+}
+
+template<typename ENVIRONMENT_TYPE>
+class BrainMuscles::test::source::info::Delegate
+{
+public:
+	typedef Delegate<ENVIRONMENT_TYPE> DelegateType;
+public:
+	typedef BrainMuscles::test::source::Info InfoType;
+	typedef BrainMuscles::test::source::info::Flags FlagsType;
+	typedef BrainMuscles::test::source::info::FlagsType	FlagsIntegerType;
+private:
+	FlagsIntegerType m_flag;
+	char * m_buffer;
+	const std::size_t m_bufferSize;
+	const char * m_file;
+	const std::size_t m_line;
+	InfoType * const m_info;
+public:
+	Delegate(InfoType * const info, FlagsIntegerType flag, const char * file,
+		const std::size_t& line, std::size_t buffer_size = (std::is_unsigned<
+			decltype(_USING_TEST_SOURCE_INFO_DELEGATE_BUFFER_SIZE_)>::value ? 
+				(std::size_t)_USING_TEST_SOURCE_INFO_DELEGATE_BUFFER_SIZE_ 
+				: _USING_TEST_SOURCE_INFO_DELEGATE_DEFAULT_BUFFER_SIZE_));
+	~Delegate();
+public:
+	template<typename... ARGS>
+	DelegateType& Out(FlagsType flag, const char* format, ARGS... args);
+	template<typename... ARGS>
+	DelegateType& Out(const char* format, ARGS... args);
+	void End();
+};
+
+template<typename ENVIRONMENT_TYPE>
+BrainMuscles::test::source::info::Delegate<ENVIRONMENT_TYPE>
+	::Delegate(InfoType * const info, FlagsIntegerType flag, const char * file,
+		const std::size_t& line, std::size_t buffer_size) :
+	m_flag(flag),
+	m_buffer(nullptr),
+	m_bufferSize(buffer_size),
+	m_file(file),
+	m_line(line),
+	m_info(info)
+{
+	if (m_info->IsEnable(flag))
+	{
+		m_buffer = new char[m_bufferSize + 1];
+		m_buffer[0] = m_buffer[m_bufferSize] = NULL;
+	}
+}
+
+template<typename ENVIRONMENT_TYPE>
+BrainMuscles::test::source::info::Delegate<ENVIRONMENT_TYPE>::~Delegate()
+{
+	if (m_buffer)
+	{
+		delete[] m_buffer;
+	}
+}
+
+template<typename ENVIRONMENT_TYPE>
+template<typename... ARGS>
+typename BrainMuscles::test::source::info
+	::Delegate<ENVIRONMENT_TYPE>::DelegateType&
+BrainMuscles::test::source::info::Delegate<ENVIRONMENT_TYPE>
+	::Out(FlagsType flag, const char* format, ARGS... args)
+{
+	if (m_buffer)
+	{
+		if (m_info->IsEnable(m_flag & flag))
+		{
+			return Out(format, args...);
+		}
+	}
+	return *this;
+}
+
+template<typename ENVIRONMENT_TYPE>
+template<typename... ARGS>
+typename BrainMuscles::test::source::info
+	::Delegate<ENVIRONMENT_TYPE>::DelegateType&
+BrainMuscles::test::source::info::Delegate<ENVIRONMENT_TYPE>
+	::Out(const char* format, ARGS... args)
+{
+	if (m_buffer)
+	{
+		std::snprintf(m_buffer, m_bufferSize, format, args...);
+	}
+	return *this;
+}
+
+template<typename ENVIRONMENT_TYPE>
+void BrainMuscles::test::source::info::Delegate<ENVIRONMENT_TYPE>::End()
+{
+	if (m_buffer)
+	{
+		if (m_buffer[0] != NULL)
+		{
+			ENVIRONMENT_TYPE::Info(m_file, m_line, "%s", m_buffer);
+		}
+	}
+}
+
+#endif //!_USING_TEST_SOURCE_
+
+#endif //!TEST_SOURCE_INFO_DELEGATE_H_
