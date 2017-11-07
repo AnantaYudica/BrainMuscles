796aff01b5ddd96e645061c661ec3b4b70bb0666
AnantaYudica <nta040689@gmail.com>
2017-11-08 03:41:45 +0700

    ADD FOLDER AND FILE Add folder /include/test/source/task Add file /include/test/source/task/Flags.h  - Add macro ifdef _USING_TEST_SOURCE_    - Add alias type 'BrainMuscles::test::source::task::FlagsIntegerType' with unsigned char for type    - Add prototype enum BrainMuscles::test::source::task::Flags    - Add definition enum BrainMuscles::test::source::task::Flags with BrainMuscles::test::source::task::FlagsIntegerType for type

---
+++ include/test/source/task/Flags.h
@@ -0,0 +1,30 @@
+#ifndef TEST_SOURCE_TASK_FLAGS_H_
+#define TEST_SOURCE_TASK_FLAGS_H_
+
+#ifdef _USING_TEST_SOURCE_
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace source
+		{
+			namespace task
+			{
+				typedef unsigned char FlagsIntegerType;
+				enum Flags;
+			}
+		}
+	}
+}
+
+enum BrainMuscles::test::source::task::Flags :
+	BrainMuscles::test::source::task::FlagsIntegerType
+{
+	test = 1,
+	static_test = 2
+};
+
+#endif //!_USING_TEST_SOURCE_
+
+#endif //!TEST_SOURCE_INTERFACE_FLAGS_H_
