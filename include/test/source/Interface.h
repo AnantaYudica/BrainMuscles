9804130c823e3fb812f9def6cf5f569f54741573
AnantaYudica <nta040689@gmail.com>
2017-11-07 23:00:58 +0700

    ADD FILE Add macro ifdef _USING_TEST_SOURCE_  - Add dependency with 'test\source\interface\Flags.h'  - Add prototype template class BrainMuscles::test::source::Interface with parameters template 'SOURCE_TYPE' and 'DERIVED_TYPE'  - Add definition template class BrainMuscles::test::source::Interface    - Add alias type 'InterfaceType' with BrainMuscles::test::source::Interface<SOURCE_TYPE<DERIVED_TYPE>, DERIVED_TYPE> with private to access    - Add alias type 'FlagsIntegerType' with BrainMuscles::test::source::interface::FlagsIntegerType for type and public to access    - Add alias type 'FlagsType' with BrainMuscles::test::source::interface::Flags for type and public to access    - Add variable member 'm_flags' with FlagsIntegerType for type and private to access    - Add variable static member 'ms_instance' with InterfaceType for type and public to access    - Add default constructor with private to access    - Add function member 'Set' with void to access, parameter function 'flag' with FlagsType for type and private to access    - Add constexpr functions static member 'HasPostTest', 'HasPreTest', 'HasStaticTest' and 'HasTest' with bool for return type and public to access    - Add constexpr function static member 'Has' with bool for return type, parameter function 'flag' with FlagsType for type and public to access    - Add function static member 'IsCalled' with bool for return type, parameter function 'flag' with FlagsType for type and public to access    - Add functions static member 'CallPostTest', 'CallPreTest', 'CallStaticTest' and 'CallTest' with void for return type and public to access    - Add function static member 'Call' with void for return type, parameter function 'flag' with FlagsType for type and public to access  - Add implementation variable member 'ms_instace', default constructor, functions member {'Set', 'HasPostTest', 'HasStaticTest', 'HasTest', 'Has', 'IsCalled', 'CallPostTest', 'CallPreTest', 'CallStaticTest', 'CallTest', 'Call'} from class BrainMuscles::test::source::Interface

---
+++ include/test/source/Interface.h
@@ -0,0 +1,174 @@
+#ifndef TEST_SOURCE_INTERFACE_H_
+#define TEST_SOURCE_INTERFACE_H_
+
+#ifdef _USING_TEST_SOURCE_
+
+#include "test\source\interface\Flags.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace source
+		{
+			template<typename SOURCE_TYPE, typename DERIVED_TYPE>
+			class Interface;
+		}
+	}
+}
+
+template<template<typename> class SOURCE_TYPE, typename DERIVED_TYPE>
+class BrainMuscles::test::source::Interface<SOURCE_TYPE<DERIVED_TYPE>,
+	DERIVED_TYPE>
+{
+private:
+	typedef BrainMuscles::test::source::Interface<SOURCE_TYPE<DERIVED_TYPE>,
+		DERIVED_TYPE> InterfaceType;
+public:
+	typedef BrainMuscles::test::source::interface::FlagsIntegerType 
+		FlagsIntegerType;
+	typedef BrainMuscles::test::source::interface::Flags FlagsType;
+private:
+	FlagsIntegerType m_flags;
+	static InterfaceType ms_instance;
+private:
+	Interface();
+private:
+	void Set(FlagsType flag);
+public:
+	static constexpr bool HasPostTest();
+	static constexpr bool HasPreTest();
+	static constexpr bool HasStaticTest();
+	static constexpr bool HasTest();
+	static constexpr bool Has(FlagsType flag);
+public:
+	static bool IsCalled(FlagsType flag);
+public:
+	static void CallPostTest();
+	static void CallPreTest();
+	static void CallStaticTest();
+	static void CallTest();
+	static void Call(FlagsType flag);
+};
+
+template<template<typename> class SOURCE_TYPE, typename DERIVED_TYPE>
+typename BrainMuscles::test::source::Interface<SOURCE_TYPE<DERIVED_TYPE>,
+	DERIVED_TYPE>::InterfaceType 
+	BrainMuscles::test::source::Interface<SOURCE_TYPE<DERIVED_TYPE>,
+	DERIVED_TYPE>::ms_instance;
+
+template<template<typename> class SOURCE_TYPE, typename DERIVED_TYPE>
+BrainMuscles::test::source::Interface<SOURCE_TYPE<DERIVED_TYPE>,
+	DERIVED_TYPE>::Interface() :
+	m_flags(0)
+{}
+
+template<template<typename> class SOURCE_TYPE, typename DERIVED_TYPE>
+void BrainMuscles::test::source::Interface<SOURCE_TYPE<DERIVED_TYPE>,
+	DERIVED_TYPE>::Set(FlagsType flag)
+{
+	ms_instance.m_flags |= flag;
+}
+
+template<template<typename> class SOURCE_TYPE, typename DERIVED_TYPE>
+constexpr bool BrainMuscles::test::source::Interface<SOURCE_TYPE<DERIVED_TYPE>,
+	DERIVED_TYPE>::HasPostTest()
+{
+	return &SOURCE_TYPE<DERIVED_TYPE>::PostTest != &DERIVED_TYPE::PostTest;
+}
+
+template<template<typename> class SOURCE_TYPE, typename DERIVED_TYPE>
+constexpr bool BrainMuscles::test::source::Interface<SOURCE_TYPE<DERIVED_TYPE>,
+	DERIVED_TYPE>::HasPreTest()
+{
+	return &SOURCE_TYPE<DERIVED_TYPE>::PreTest != &DERIVED_TYPE::PreTest;
+}
+
+template<template<typename> class SOURCE_TYPE, typename DERIVED_TYPE>
+constexpr bool BrainMuscles::test::source::Interface<SOURCE_TYPE<DERIVED_TYPE>,
+	DERIVED_TYPE>::HasStaticTest()
+{
+	return &SOURCE_TYPE<DERIVED_TYPE>::StaticTest != &DERIVED_TYPE::StaticTest;
+}
+
+template<template<typename> class SOURCE_TYPE, typename DERIVED_TYPE>
+constexpr bool BrainMuscles::test::source::Interface<SOURCE_TYPE<DERIVED_TYPE>,
+	DERIVED_TYPE>::HasTest()
+{
+	return &SOURCE_TYPE<DERIVED_TYPE>::Test != &DERIVED_TYPE::Test;
+}
+
+template<template<typename> class SOURCE_TYPE, typename DERIVED_TYPE>
+constexpr bool BrainMuscles::test::source::Interface<SOURCE_TYPE<DERIVED_TYPE>,
+	DERIVED_TYPE>::Has(FlagsType flag)
+{
+	return flag == FlagsType::post_test ? HasPostTest() : 
+		flag == FlagsType::pre_test ? HasPreTest() :
+		flag == FlagsType::static_test ? HasStaticTest() :
+		flag == FlagsType::test ? HasTest() : false;
+}
+
+template<template<typename> class SOURCE_TYPE, typename DERIVED_TYPE>
+bool BrainMuscles::test::source::Interface<SOURCE_TYPE<DERIVED_TYPE>,
+	DERIVED_TYPE>::IsCalled(FlagsType flag)
+{
+	return m_flags & flag;
+}
+
+template<template<typename> class SOURCE_TYPE, typename DERIVED_TYPE>
+void BrainMuscles::test::source::Interface<SOURCE_TYPE<DERIVED_TYPE>,
+	DERIVED_TYPE>::CallPostTest()
+{
+	if (HasPostTest())
+	{
+		DERIVED_TYPE::PostTest();
+		ms_instance.Set(FlagsType::post_test);
+	}
+}
+
+template<template<typename> class SOURCE_TYPE, typename DERIVED_TYPE>
+void BrainMuscles::test::source::Interface<SOURCE_TYPE<DERIVED_TYPE>,
+	DERIVED_TYPE>::CallPreTest()
+{
+	if (HasPreTest())
+	{
+		DERIVED_TYPE::PreTest();
+		ms_instance.Set(FlagsType::pre_test);
+	}
+}
+
+template<template<typename> class SOURCE_TYPE, typename DERIVED_TYPE>
+void BrainMuscles::test::source::Interface<SOURCE_TYPE<DERIVED_TYPE>,
+	DERIVED_TYPE>::CallStaticTest()
+{
+	if (HasStaticTest())
+	{
+		DERIVED_TYPE::StaticTest();
+		ms_instance.Set(FlagsType::static_test);
+	}
+}
+
+template<template<typename> class SOURCE_TYPE, typename DERIVED_TYPE>
+void BrainMuscles::test::source::Interface<SOURCE_TYPE<DERIVED_TYPE>,
+	DERIVED_TYPE>::CallTest()
+{
+	if (HasTest())
+	{
+		DERIVED_TYPE::Test();
+		ms_instance.Set(FlagsType::test);
+	}
+}
+
+template<template<typename> class SOURCE_TYPE, typename DERIVED_TYPE>
+void BrainMuscles::test::source::Interface<SOURCE_TYPE<DERIVED_TYPE>,
+	DERIVED_TYPE>::Call(FlagsType flag)
+{
+	flag == FlagsType::post_test ? CallPostTest() :
+		flag == FlagsType::pre_test ? CallPreTest() :
+		flag == FlagsType::static_test ? CallStaticTest() :
+		flag == FlagsType::test ? CallTest() : void(0);
+}
+
+#endif //!_USING_TEST_SOURCE_
+
+#endif //!TEST_SOURCE_INTERFACE_H_
