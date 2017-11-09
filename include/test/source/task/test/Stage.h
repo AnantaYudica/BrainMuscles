f2032e578142851f66331146b020edf64ac6902f
AnantaYudica <nta040689@gmail.com>
2017-11-10 01:33:02 +0700

    ADD FOLDER AND FILE add folder /include/test/source/task/test add file /include/test/source/task/test/Stage.h  - Add macro ifdef '_USING_TEST_SOURCE_'    - Add dependency with 'test\source\Environment.h'    - Add prototype class BrainMuscles::test::source::task::test::Stage    - Add definition class BrainMuscles::test::source::task::test::Stage      - Add alias type 'ValueType' with unsigned char for type and private to access      - Add constexpr variables static member 'BitNotTest', 'BitPreTest', 'BitTest', 'BitPostTest' and 'BitEnd' with ValueType for type and private to access      - Add variable member 'm_value' with ValueType for type and private to access      - Add inline default constructor with public to access      - Add inline functions member 'IsHasNotTest', 'IsHasPreTest', 'IsHasTest', 'IsHasPostTest' and 'IsHasEnd' with bool for return type, private to access      - Add inline functions member 'SetNotTest', 'SetPreTest', 'SetTest', 'SetPostTest' and 'SetEnd' with void for return type and private to access      - Add inline functions const member 'IsBegin', 'IsNotTest', 'IsPreTest', 'IsTest', 'IsPostTest' and 'IsEnd' with bool for return type and public to access      - Add inline functions member 'CanBeginStage', 'CanPreTestStage', 'CanTEstStage', 'CanPostTestStage' and 'CanEndStage' with bool for return type and public to access    - Add implementation inline default constructor, functions member or const member {'IsHasNotTest', 'IsHasPreTest', 'IsHasTest', 'IsHasPostTest', 'IsHasEnd', 'SetNotTest', 'SetPreTest', 'SetTest', 'SetPostTest', 'SetEnd', 'IsBegin', 'IsNotTest', 'IsPreTest', 'IsTest', 'IsPostTest', 'IsEnd', CanBeginStage', 'CanPreTestStage', 'CanTEstStage', 'CanPostTestStage' and 'CanEndStage'} from class BrainMuscles::test::source::task::test::Stage

---
+++ include/test/source/task/test/Stage.h
@@ -0,0 +1,202 @@
+#ifndef TEST_SOURCE_TASK_TEST_STAGE_H_
+#define TEST_SOURCE_TASK_TEST_STAGE_H_
+
+#ifdef _USING_TEST_SOURCE_
+
+#include "test\source\Environment.h"
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace source
+		{
+			namespace task
+			{
+				namespace test
+				{
+					class Stage;
+				}
+			}
+		}
+	}
+}
+
+class BrainMuscles::test::source::task::test::Stage
+{
+private:
+	typedef unsigned char ValueType;
+private:
+	static constexpr ValueType BitNotTest = 1;
+	static constexpr ValueType BitPreTest = 2;
+	static constexpr ValueType BitTest = 4;
+	static constexpr ValueType BitPostTest = 8;
+	static constexpr ValueType BitEnd = 16;
+private:
+	ValueType m_value;
+public:
+	inline Stage();
+private:
+	inline bool IsHasNotTest();
+	inline bool IsHasPreTest();
+	inline bool IsHasTest();
+	inline bool IsHasPostTest();
+	inline bool IsHasEnd();
+private:
+	inline void SetNotTest();
+	inline void SetPreTest();
+	inline void SetTest();
+	inline void SetPostTest();
+	inline void SetEnd();
+public:
+	inline bool IsBegin() const;
+	inline bool IsNotTest() const;
+	inline bool IsPreTest() const;
+	inline bool IsTest() const;
+	inline bool IsPostTest() const;
+	inline bool IsEnd() const;
+public:
+	inline bool CanBeginStage();
+	inline bool CanPreTestStage();
+	inline bool CanTestStage();
+	inline bool CanPostTestStage();
+	inline bool CanEndStage();
+};
+
+inline BrainMuscles::test::source::task::test::Stage::Stage() :
+	m_value(0)
+{}
+
+inline bool BrainMuscles::test::source::task::test::Stage::IsHasNotTest()
+{
+	return m_value & BitNotTest;
+}
+
+inline bool BrainMuscles::test::source::task::test::Stage::IsHasPreTest()
+{
+	return m_value & BitPreTest;
+}
+
+inline bool BrainMuscles::test::source::task::test::Stage::IsHasTest()
+{
+	return m_value & BitTest;
+}
+
+inline bool BrainMuscles::test::source::task::test::Stage::IsHasPostTest()
+{
+	return m_value & BitPostTest;
+}
+
+inline bool BrainMuscles::test::source::task::test::Stage::IsHasEnd()
+{
+	return m_value & BitEnd;
+}
+
+inline void BrainMuscles::test::source::task::test::Stage::SetNotTest()
+{
+	m_value |= BitNotTest;
+}
+
+inline void BrainMuscles::test::source::task::test::Stage::SetPreTest()
+{
+	m_value |= BitPreTest;
+}
+
+inline void BrainMuscles::test::source::task::test::Stage::SetTest()
+{
+	m_value |= BitTest;
+}
+
+inline void BrainMuscles::test::source::task::test::Stage::SetPostTest()
+{
+	m_value |= BitPostTest;
+}
+
+inline void BrainMuscles::test::source::task::test::Stage::SetEnd()
+{
+	m_value |= BitEnd;
+}
+
+inline bool BrainMuscles::test::source::task::test::Stage::IsBegin() const
+{
+	return m_value == 0;
+}
+
+inline bool BrainMuscles::test::source::task::test::Stage::IsNotTest() const
+{
+	return m_value == BitNotTest;
+}
+
+inline bool BrainMuscles::test::source::task::test::Stage::IsPreTest() const
+{
+	return m_value >= BitPreTest && m_value < BitTest;
+}
+
+inline bool BrainMuscles::test::source::task::test::Stage::IsTest() const
+{
+	return m_value >= BitTest && m_value < BitPostTest;
+}
+
+inline bool BrainMuscles::test::source::task::test::Stage::IsPostTest() const
+{
+	return m_value >= BitPostTest && m_value < BitEnd;
+}
+
+inline bool BrainMuscles::test::source::task::test::Stage::IsEnd() const
+{
+	return m_value >= BitEnd;
+}
+
+inline bool BrainMuscles::test::source::task::test::Stage::CanBeginStage()
+{
+	if (IsBegin())
+	{
+		SetNotTest();
+		return true;
+	}
+	return false;
+}
+
+inline bool BrainMuscles::test::source::task::test::Stage::CanPreTestStage()
+{
+	if (IsNotTest())
+	{
+		SetPreTest();
+		return true;
+	}
+	return false;
+}
+
+inline bool BrainMuscles::test::source::task::test::Stage::CanTestStage()
+{
+	if (IsNotTest() || IsPreTest())
+	{
+		SetTest();
+		return true;
+	}
+	return false;
+}
+
+inline bool BrainMuscles::test::source::task::test::Stage::CanPostTestStage()
+{
+	if (IsTest())
+	{
+		SetPostTest();
+		return true;
+	}
+	return false;
+}
+
+inline bool BrainMuscles::test::source::task::test::Stage::CanEndStage()
+{
+	if (!IsEnd())
+	{
+		SetPostTest();
+		return true;
+	}
+	return false;
+}
+
+#endif //!_USING_TEST_SOURCE_
+
+#endif //!TEST_SOURCE_TASK_TEST_STAGE_H_
