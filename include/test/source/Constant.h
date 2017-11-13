f5268a74926b2f8b44ba2e6ec0211e86b85172e5
AnantaYudica <nta040689@gmail.com>
2017-11-13 17:32:29 +0700

    MODIFIED FILE Add dependency with 'test\source\interface\Flags.h' Update class BrainMuscles::test::source::Constant  - Add alias type 'InterfaceFlagsType' with BrainMuscles::test::source::interface::Flags  - Update static variables member with change name {'InStr' to 'CstringIn'}, {'WhenCalledStr' to 'CstringWhenCalled'}, {'FromStr' to 'CstringFrom'}, {'FailedStr' to 'CstringFailed'}, {'FunctionStaticTestStr' to 'CstringInterfaceStaticTest'}, {'FunctionPreTestStr' to 'CstringInterfacePreTest'}, {'FunctionTestStr' to 'CstringInterfaceTest'}, {'FunctionPostTestStr' to 'CstringInterfacePostTest'}, {'FunctionRequirementStr' to 'CstringFunctionRequirement'}, {'FunctionCallStr' to 'CstringFunctionCall'}, {'TitleAssertionStr' to 'CstringTitleAssertion'}, {'TitleRequirmentStr' to 'CstringTitleRequirement'}, {'TitleCallStr' to 'CstringTitleCall'}, {'BaseOfSourceMsg' to 'CstringMessageBaseOfSource'}, {'SourceHasTestMsg' to 'CstringMessageSourceHasTest'}, {'SourceIsErrorMsg' to 'CstringMessageSourceHasError'} and {'SourceIsNotCompletedMsg' to 'CstringMessageSourceIsNotCompleted'}  - Add static constexpr variable member 'CstringFunctionUnknown' with const char* for type and "function unknown" for value and private to access  - Add static inline template functions member 'StringInterface' with parameter template 'IMPLEMENT_TYPE', std::string for return type    - Add function parameter with 'interface' with const char * for type and private to access    - Add function parameter with 'flag' with InterfaceFlagsType for type and public to access  - Add static inline template functions member 'StringInterfacePostTest', 'StringInterfacePreTest', 'StringInterfaceStaticTest' and 'StringInterfaceTest' with parameter template 'IMPLEMENT_TYPE', std::string for return type and public to access Update implementation static inline functions member 'Caller', 'Failed', 'CallerStaticTest', 'CallerPreTest', 'CallerTest', 'CallerPostTest', 'CallerRequirement', 'CallerCall', 'AssertionFailed', 'RequirementFailed', 'CallFailed' and 'AppendFileLine' from class BrainMuscles::test::source::Constant Add implementation static inline template function member 'StringInterface', 'StringInterfacePostTest', 'StringInterfacePreTest', 'StringInterfaceStaticTest' and 'StringInterfaceTest'

--- include/test/source/Constant.h
+++ include/test/source/Constant.h
@@ -6,6 +6,7 @@
 #include <string>
 
 #include "test\source\Environment.h"
+#include "test\source\interface\Flags.h"
 
 namespace BrainMuscles
 {
@@ -18,27 +19,29 @@ namespace BrainMuscles
 				Constant() = delete;
 			public:
 				typedef BrainMuscles::test::source::Environment	EnvironmentType;
+				typedef BrainMuscles::test::source::interface::Flags InterfaceFlagsType;
 			private:
-				static constexpr const char* InStr = "in";
-				static constexpr const char* WhenCalledStr = "when called";
-				static constexpr const char* FromStr = "from";
-				static constexpr const char* FailedStr = "failed";
+				static constexpr const char* CstringIn = "in";
+				static constexpr const char* CstringWhenCalled = "when called";
+				static constexpr const char* CstringFrom = "from";
+				static constexpr const char* CstringFailed = "failed";
 			private:
-				static constexpr const char* FunctionStaticTestStr = "static function member 'StaticTest()'";
-				static constexpr const char* FunctionPreTestStr = "static function member 'PreTest()'";
-				static constexpr const char* FunctionTestStr = "static function member 'Test()'";
-				static constexpr const char* FunctionPostTestStr = "static function member 'PostTest()'";
-				static constexpr const char* FunctionRequirementStr = "static function member 'Requirement(const char* file, const std::size_t& line)'";
-				static constexpr const char* FunctionCallStr = "static function member 'Call(const char* file, const std::size_t& line)'";
+				static constexpr const char* CstringFunctionUnknown = "function unknown";
+				static constexpr const char* CstringInterfaceStaticTest = "static function member 'StaticTest()'";
+				static constexpr const char* CstringInterfacePreTest = "static function member 'PreTest()'";
+				static constexpr const char* CstringInterfaceTest = "static function member 'Test()'";
+				static constexpr const char* CstringInterfacePostTest = "static function member 'PostTest()'";
+				static constexpr const char* CstringFunctionRequirement = "static function member 'Requirement(const char* file, const std::size_t& line)'";
+				static constexpr const char* CstringFunctionCall = "static function member 'Call(const char* file, const std::size_t& line)'";
 			private:
-				static constexpr const char* TitleAssertionStr = "Assertion";
-				static constexpr const char* TitleRequirementStr = "Requirement";
-				static constexpr const char* TitleCallStr = "Call";
+				static constexpr const char* CstringTitleAssertion = "Assertion";
+				static constexpr const char* CstringTitleRequirement = "Requirement";
+				static constexpr const char* CstringTitleCall = "Call";
 			public:
-				static constexpr const char* BaseOfSourceMsg = "%s is not base class with class BrainMuscles::test::Source";
-				static constexpr const char* SourceHasTestMsg = "%s is not has static function member 'Test'";
-				static constexpr const char* SourceIsErrorMsg = "%s has error";
-				static constexpr const char* SourceIsNotCompletedMsg = "test in %s is not completed";
+				static constexpr const char* CstringMessageBaseOfSource = "%s is not base class with class BrainMuscles::test::Source";
+				static constexpr const char* CstringMessageSourceHasTest = "%s is not has static function member 'Test'";
+				static constexpr const char* CstringMessageSourceHasError = "%s has error";
+				static constexpr const char* CstringMessageSourceIsNotCompleted = "test in %s is not completed";
 			private:
 				template<typename SOURCE_TYPE>
 				static inline std::string Caller(const char* function_str);
@@ -62,26 +65,40 @@ namespace BrainMuscles
 				static inline std::string CallFailed();
 			public:
 				static inline std::string& AppendFileLine(std::string& out, const char * file, const std::size_t& line);
+			private:
+				template<typename IMPLEMENT_TYPE>
+				static inline std::string StringInterface(const char * interface);
+			public:
+				template<typename IMPLEMENT_TYPE>
+				static inline std::string StringInterfacePostTest();
+				template<typename IMPLEMENT_TYPE>
+				static inline std::string StringInterfacePreTest();
+				template<typename IMPLEMENT_TYPE>
+				static inline std::string StringInterfaceStaticTest();
+				template<typename IMPLEMENT_TYPE>
+				static inline std::string StringInterfaceTest();
+				template<typename IMPLEMENT_TYPE>
+				static inline std::string StringInterface(InterfaceFlagsType flag);
 			};
 
 			template<typename SOURCE_TYPE>
 			inline std::string Constant::Caller(const char* function_str)
 			{
 				std::string caller_function = "";
-				if (function_str == FunctionRequirementStr || function_str == FunctionCallStr)
+				if (function_str == CstringFunctionRequirement || function_str == CstringFunctionCall)
 				{
-					caller_function += WhenCalledStr;
+					caller_function += CstringWhenCalled;
 				}
-				else if (function_str == FunctionStaticTestStr || function_str == FunctionPreTestStr || function_str == FunctionTestStr || function_str == FunctionPostTestStr)
+				else if (function_str == CstringInterfaceStaticTest || function_str == CstringInterfacePreTest || function_str == CstringInterfaceTest || function_str == CstringInterfacePostTest)
 				{
-					caller_function += InStr;
+					caller_function += CstringIn;
 				}
 				if (!caller_function.empty())
 				{
 					caller_function += " ";
 					caller_function += function_str;
 					caller_function += " ";
-					caller_function += FromStr;
+					caller_function += CstringFrom;
 					caller_function += " ";
 					caller_function += typeid(SOURCE_TYPE).name();
 				}
@@ -92,31 +109,31 @@ namespace BrainMuscles
 			{
 				std::string title_failed = title_str;
 				title_failed += " ";
-				title_failed += FailedStr;
+				title_failed += CstringFailed;
 				return title_failed;
 			}
 
 			template<typename SOURCE_TYPE>
 			inline std::string Constant::CallerStaticTest()
 			{
-				return Caller<SOURCE_TYPE>(FunctionStaticTestStr);
+				return Caller<SOURCE_TYPE>(CstringInterfaceStaticTest);
 			}
 
 			template<typename SOURCE_TYPE>
 			inline std::string Constant::CallerPreTest()
 			{
-				return Caller<SOURCE_TYPE>(FunctionPreTestStr);
+				return Caller<SOURCE_TYPE>(CstringInterfacePreTest);
 			}
 			template<typename SOURCE_TYPE>
 			inline std::string Constant::CallerTest()
 			{
-				return Caller<SOURCE_TYPE>(FunctionTestStr);
+				return Caller<SOURCE_TYPE>(CstringInterfaceTest);
 			}
 
 			template<typename SOURCE_TYPE>
 			inline std::string Constant::CallerPostTest()
 			{
-				return Caller<SOURCE_TYPE>(FunctionPostTestStr);
+				return Caller<SOURCE_TYPE>(CstringInterfacePostTest);
 			}
 
 			template<typename SOURCE_TYPE>
@@ -124,7 +141,7 @@ namespace BrainMuscles
 			{
 				if (EnvironmentType::CallerFunction().empty())
 				{
-					return Caller<SOURCE_TYPE>(FunctionRequirementStr);
+					return Caller<SOURCE_TYPE>(CstringFunctionRequirement);
 				}
 				return EnvironmentType::CallerFunction();
 			}
@@ -134,24 +151,24 @@ namespace BrainMuscles
 			{
 				if (EnvironmentType::CallerFunction().empty())
 				{
-					return Caller<SOURCE_TYPE>(FunctionCallStr);
+					return Caller<SOURCE_TYPE>(CstringFunctionCall);
 				}
 				return EnvironmentType::CallerFunction();
 			}
 
 			inline std::string Constant::AssertionFailed()
 			{
-				return Failed(TitleAssertionStr);
+				return Failed(CstringTitleAssertion);
 			}
 
 			inline std::string Constant::RequirementFailed()
 			{
-				return Failed(TitleRequirementStr);
+				return Failed(CstringTitleRequirement);
 			}
 
 			inline std::string Constant::CallFailed()
 			{
-				return Failed(TitleCallStr);
+				return Failed(CstringTitleCall);
 			}
 
 			inline std::string& Constant::AppendFileLine(std::string& out, const char * file, const std::size_t& line)
@@ -162,15 +179,79 @@ namespace BrainMuscles
 					{
 						out += ",";
 					}
-					str += " file ";
-					str += file;
-					str += ", line ";
-					str += std::to_string(line);
+					out += " file ";
+					out += file;
+					out += ", line ";
+					out += std::to_string(line);
 				}
 				return out;
 			}
 		}
 	}
+}
+
+
+template<typename IMPLEMENT_TYPE>
+inline std::string BrainMuscles::test
+::source::Constant::StringInterface(const char * cstring_interface)
+{
+	std::string string_result = "";
+	if (cstring_interface == CstringInterfaceStaticTest
+		|| cstring_interface == CstringInterfacePreTest 
+		|| cstring_interface == CstringInterfaceTest 
+		|| cstring_interface == CstringInterfacePostTest)
+	{
+		string_result += CstringIn;
+	}
+	if (!string_result.empty())
+	{
+		string_result += " ";
+		string_result += cstring_interface;
+		string_result += " ";
+		string_result += CstringFrom;
+		string_result += " ";
+		string_result += typeid(IMPLEMENT_TYPE).name();
+	}
+	return string_result;
+}
+
+template<typename IMPLEMENT_TYPE>
+inline std::string
+BrainMuscles::test::source::Constant::StringInterfacePostTest()
+{
+	return  StringInterface<IMPLEMENT_TYPE>(CstringInterfacePostTest);
+}
+
+template<typename IMPLEMENT_TYPE>
+inline std::string 
+BrainMuscles::test::source::Constant::StringInterfacePreTest()
+{
+	return StringInterface<IMPLEMENT_TYPE>(CstringInterfacePreTest);
+}
+
+template<typename IMPLEMENT_TYPE>
+inline std::string
+BrainMuscles::test::source::Constant::StringInterfaceStaticTest()
+{
+	return StringInterface<IMPLEMENT_TYPE>(CstringInterfaceStaticTest);
+}
+
+template<typename IMPLEMENT_TYPE>
+inline std::string 
+BrainMuscles::test::source::Constant::StringInterfaceTest()
+{
+	return StringInterface<IMPLEMENT_TYPE>(CstringInterfaceTest);
+}
+
+template<typename IMPLEMENT_TYPE>
+inline std::string 
+BrainMuscles::test::source::Constant::StringInterface(InterfaceFlagsType flag)
+{
+	return flag == InterfaceFlagsType::post_test ?
+		StringInterfacePostTest() : flag == InterfaceFlagsType::pre_test ?
+		StringInterfacePreTest() : flag == InterfaceFlagsType::static_test ?
+		StringInterfaceStaticTest() : flag == InterfaceFlagsType::test ?
+		StringInterfaceTest() : CstringFunctionUnknown;
 }
 
 #endif //!_USING_TEST_SOURCE_
