21b5587fe43f72a2e6ce5e8a294545afdd8a3487
AnantaYudica <nta040689@gmail.com>
2017-11-13 14:07:35 +0700

    MODIFIED FILE Add inline specifier

--- include/test/source/environment/Trace.h
+++ include/test/source/environment/Trace.h
@@ -27,22 +27,24 @@ private:
 	std::string m_default;
 	std::stack<std::string> m_stack;
 protected:
-	Trace(std::string default = "");
+	inline Trace(std::string default = "");
 protected:
-	void Push(std::string value);
+	inline void Push(std::string value);
 public:
-	void Pop();
-	std::string Top(std::string default = "");
-	std::stack<std::string> Get();
+	inline void Pop();
+	inline std::string Top(std::string default = "");
+	inline std::stack<std::string> Get();
 };
 
-BrainMuscles::test::source::environment::Trace::Trace(std::string default) :
+inline BrainMuscles::test::source
+	::environment::Trace::Trace(std::string default) :
 	m_counter(0),
 	m_default(default)
 {
 }
 
-void BrainMuscles::test::source::environment::Trace::Push(std::string value)
+inline void 
+BrainMuscles::test::source::environment::Trace::Push(std::string value)
 {
 	if (!value.empty())
 	{
@@ -54,7 +56,7 @@ void BrainMuscles::test::source::environment::Trace::Push(std::string value)
 	}
 }
 
-void BrainMuscles::test::source::environment::Trace::Pop()
+inline void BrainMuscles::test::source::environment::Trace::Pop()
 {
 	if (m_counter == 0)
 	{
@@ -69,7 +71,7 @@ void BrainMuscles::test::source::environment::Trace::Pop()
 	}
 }
 
-std::string 
+inline std::string
 BrainMuscles::test::source::environment::Trace::Top(std::string default)
 {
 	if (!m_stack.empty())
@@ -79,7 +81,7 @@ BrainMuscles::test::source::environment::Trace::Top(std::string default)
 	return default.empty() ? m_default : default;
 }
 
-std::stack<std::string> 
+inline std::stack<std::string>
 BrainMuscles::test::source::environment::Trace::Get()
 {
 	return m_stack;
