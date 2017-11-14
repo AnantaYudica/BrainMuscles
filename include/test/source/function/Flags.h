251fbce7a51652f3c90906c51b08ac0f8a6367a4
AnantaYudica <nta040689@gmail.com>
2017-11-14 11:49:36 +0700

    ADD FOLDER AND FILE Add folder /include/test/source/function Add file /include/test/source/function/Flags.h  - Add macro ifdef '_USING_TEST_SOURCE_'    - Add alias type 'BrainMuscles::test::source::function::FlagsType' with unsigned short for type    - Add prototype enum 'BrainMuscles::test::source::function::FlagsType'    - Add definition enum 'BrainMuscles::test::source::function::FlagsType' with BrainMuscles::test::source::function::FlagsType for type

---
+++ include/test/source/function/Flags.h
@@ -0,0 +1,39 @@
+#ifndef TEST_SOURCE_FUNCTION_FLAGS_H_
+#define TEST_SOURCE_FUNCTION_FLAGS_H_
+
+#ifdef _USING_TEST_SOURCE_
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace source
+		{
+			namespace function
+			{
+				typedef unsigned short FlagsType;
+				enum Flags;
+			}
+		}
+	}
+}
+
+enum BrainMuscles::test::source::function::Flags :
+	BrainMuscles::test::source::function::FlagsType
+{
+	unknown = 0,
+	pre_test = 1,
+	test = 2,
+	post_test = 4,
+	static_test = 8,
+	call = 16,
+	requirement = 32,
+	info = 64,
+	error = 128,
+	assert = 256
+};
+
+
+#endif //!_USING_TEST_SOURCE_
+
+#endif //!TEST_SOURCE_FUNCTION_FLAGS_H_
