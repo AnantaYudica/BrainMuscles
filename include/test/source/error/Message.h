4cc6e07426d753c0331edd63063baf24fa5713a7
AnantaYudica <nta040689@gmail.com>
2017-11-15 14:51:41 +0700

    MODIFIED FILE Remove define '_USING_TEST_SOURCE_' Add macro define '_USING_TEST_SOURCE_ERROR_MESSAGE_DEFAULT_BUFFER_ALLOCATION_' with 1024 for replacement Add macro ifdef '_USING_TEST_SOURCE_ERROR_MESSAGE_BUFFER_ALLOCATION_'  - Add macro define '_USING_TEST_SOURCE_ERROR_MESSAGE_BUFFER_ALLOCATION_' with _USING_TEST_SOURCE_ERROR_MESSAGE_DEFAULT_BUFFER_ALLOCATION_ for replacement Add dependency with 'test\source\Constant.h' Add dependency with 'test\source\function\Flags.h' Update prototype type 'BrainMuscles::test::source::error::Message' from struct to class Update definition class 'BrainMuscles::test::source::error::Message'  - Add alias type 'ConstantType' with BrainMuscles::test::source::Constant for type and private to access  - Add alias type 'FunctionFlagsType' with BrainMuscles::test::source::function::Flags for type and public to access  - Add static constexpr variable member 'BufferAllocation' with std::size_t for type, _USING_TEST_SOURCE_ERROR_MESSAGE_BUFFER_ALLOCATION_ or _USING_TEST_SOURCE_ERROR_MESSAGE_DEFAULT_BUFFER_ALLOCATION_ for value and private to access  - Update variables member 'Cause', 'Information' and 'Trace' with public to access  - Update fill inline constructor with public to access  - Add static inline template functions member 'Instance' with parameter template {'ENVIRONMENT_TYPE' or '...ARGS'}, Message for return type, parameters function 'file' with const char* for type, 'line' with const std::size_t& for type, 'flag' with FunctionFlagsType for type and {'message' with std::string for type or {'format' with const char* for type and 'args' with ARGS..s for type}} and public to access Add implementation function member 'Instance' from class BrainMuscles::test::source::error::Message Update inline function std::to_string

--- include/test/source/error/Message.h
+++ include/test/source/error/Message.h
@@ -1,10 +1,22 @@
 #ifndef TEST_SOURCE_ERROR_MESSAGE_H_
 #define TEST_SOURCE_ERROR_MESSAGE_H_
-#define _USING_TEST_SOURCE_
+
 #ifdef _USING_TEST_SOURCE_
+
+#define _USING_TEST_SOURCE_ERROR_MESSAGE_DEFAULT_BUFFER_ALLOCATION_ 1024
+
+#ifndef _USING_TEST_SOURCE_ERROR_MESSAGE_BUFFER_ALLOCATION_
+
+#define _USING_TEST_SOURCE_ERROR_MESSAGE_BUFFER_ALLOCATION_ \
+	_USING_TEST_SOURCE_ERROR_MESSAGE_DEFAULT_BUFFER_ALLOCATION_
+
+#endif //!_USING_TEST_SOURCE_ERROR_MESSAGE_BUFFER_ALLOCATION_
 
 #include <string>
 #include <stack>
+
+#include "test\source\Constant.h"
+#include "test\source\function\Flags.h"
 
 namespace BrainMuscles
 {
@@ -14,19 +26,37 @@ namespace BrainMuscles
 		{
 			namespace error
 			{
-				struct Message;
+				class Message;
 			}
 		}
 	}
 }
 
-struct BrainMuscles::test::source::error::Message
+class BrainMuscles::test::source::error::Message
 {
+private:
+	typedef BrainMuscles::test::source::Constant ConstantType;
+public:
+	typedef BrainMuscles::test::source::function::Flags FunctionFlagsType;
+private:
+	static constexpr std::size_t BufferAllocation = (std::is_unsigned<
+		decltype(_USING_TEST_SOURCE_ERROR_MESSAGE_BUFFER_ALLOCATION_)>::value ?
+			(std::size_t)_USING_TEST_SOURCE_ERROR_MESSAGE_BUFFER_ALLOCATION_
+			: _USING_TEST_SOURCE_ERROR_MESSAGE_DEFAULT_BUFFER_ALLOCATION_);
+public:
 	const std::string Cause;
 	const std::string Information;
 	const std::stack<std::string> Trace;
+public:
 	inline Message(const std::string& cause, const std::string& information,
 		const std::stack<std::string>& trace);
+public:
+	template<typename ENVIRONMENT_TYPE>
+	static inline Message Instance(const char* file, const std::size_t& line,
+		FunctionFlagsType flag, std::string message);
+	template<typename ENVIRONMENT_TYPE, typename... ARGS>
+	static inline Message Instance(const char* file, const std::size_t& line,
+		FunctionFlagsType flag, const char* format, ARGS... args);
 };
 
 inline BrainMuscles::test::source
@@ -37,22 +67,46 @@ inline BrainMuscles::test::source
 	Trace(trace)
 {}
 
+template<typename ENVIRONMENT_TYPE>
+inline BrainMuscles::test::source::error::Message 
+BrainMuscles::test::source::error::Message::Instance(const char* file,
+	const std::size_t& line, FunctionFlagsType flag, std::string message)
+{
+	return Instance<ENVIRONMENT_TYPE, const char*>(file, line, flag, 
+		"%s", message.c_str());
+}
+
+template<typename ENVIRONMENT_TYPE, typename... ARGS>
+inline BrainMuscles::test::source::error::Message 
+BrainMuscles::test::source::error::Message::Instance(const char* file,
+	const std::size_t& line, FunctionFlagsType flag, const char* format,
+	ARGS... args)
+{
+	char buffer[BufferAllocation];
+#if ((defined(_WIN32) || defined(_WIN64)) && !defined(_CRT_SECURE_NO_WARNINGS))
+	sprintf_s(buffer, format, args...);
+#else
+	std::sprintf(buffer, format, args...);
+#endif
+	std::string string_cause = ConstantType::StringErrorTitle(flag);
+	string_cause += buffer;
+	std::string string_information = "";
+	ConstantType::AppendFileLine(string_information, file, line);
+	return Message(string_cause, string_information, 
+		ENVIRONMENT_TYPE::TraceFunction().Get());
+}
+
 namespace std
 {
 	inline std::string 
 		to_string(const BrainMuscles::test::source::error::Message&
 			error_message)
 	{
-		std::string out = error_message.Cause;
-		if (error_message.Trace.size() == 0)
-		{
-			out += ", ";
-			out += error_message.Information;
-		}
-		return out;
+		return BrainMuscles::test::source
+			::Constant::StringErrorMessage(error_message.Cause,
+				error_message.Information, error_message.Trace);
 	}
 }
-
 
 #endif //!_USING_TEST_SOURCE_
 
