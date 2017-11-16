6d170a8170da56d271f16dc4b741c9612cd7f349
AnantaYudica <nta040689@gmail.com>
2017-11-16 07:39:04 +0700

    ADD FILE Add macro ifdef _USING_TEST_SOURCE_ Add alias type 'BrainMuscles::test::source::error::NumbersType' with unsigned char for type Add prototype enum 'BrainMuscles::test::source::error::Numbers' Add implementation enum 'BrainMuscles::test::source::error::Numbers' with BrainMuscles::test::source::error::NUmbersType for type

---
+++ include/test/source/error/Numbers.h
@@ -0,0 +1,33 @@
+#ifndef TEST_SOURCE_ERROR_NUMBERS_H_
+#define TEST_SOURCE_ERROR_NUMBERS_H_
+
+#ifdef _USING_TEST_SOURCE_
+
+namespace BrainMuscles
+{
+	namespace test
+	{
+		namespace source
+		{
+			namespace error
+			{
+				typedef unsigned char NumbersType;
+				enum Numbers;
+			}
+		}
+	}
+}
+
+enum BrainMuscles::test::source::error::Numbers :
+	BrainMuscles::test::source::error::NumbersType
+{
+	not_base_of_source = 1,
+	has_not_impl_test,
+	has_error,
+	has_not_completed
+};
+
+
+#endif //!_USING_TEST_SOURCE_
+
+#endif //!TEST_SOURCE_ERROR_NUMBERS_H_
