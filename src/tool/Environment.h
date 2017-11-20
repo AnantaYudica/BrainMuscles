2219704205fda1ab86893acdbd219a4f7024ed71
AnantaYudica <nta040689@gmail.com>
2017-11-20 17:59:00 +0700

    MODIFIED FILE Update with remove incomplete curly brackets

--- src/tool/Environment.h
+++ src/tool/Environment.h
@@ -49,7 +49,7 @@ inline void tool::Environment::ErrorNumber(int error_number)
 	std::printf("%s\n", msg);
 #elif (__GNUC__ \
 	|| (defined(_WIN32) || defined(_WIN64)) \
-		&& defined(_CRT_SECURE_NO_WARNINGS)))
+		&& defined(_CRT_SECURE_NO_WARNINGS))
 	strncpy(msg, std::strerror(error_number), ERROR_MESSAGE_BUFFER_SIZE);
 	msg[ERROR_MESSAGE_BUFFER_SIZE - 1] = NULL;
 	std::printf("%s\n", msg);
