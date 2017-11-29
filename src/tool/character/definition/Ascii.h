b25b09e50a12a66b8ef3e59079960df88aba98c8
AnantaYudica <nta040689@gmail.com>
2017-11-30 03:53:25 +0700

    MODIFIED FILE Update in struct tool::character::definition::Ascii  - Update name alias type from 'BitsType' to 'BinaryDigitType'  - Update name variables member from 'MaximumBits' to 'MaximumBinaryDigit' and from 'MinimumBits' to 'MinimumBinaryDigit'  - Update type variables member 'MaximumBinaryDigit' and 'MinimumBinaryDigit' from BitsType to BinaryDigitType

--- src/tool/character/definition/Ascii.h
+++ src/tool/character/definition/Ascii.h
@@ -15,14 +15,14 @@ namespace tool
 struct tool::character::definition::Ascii final
 {
 	Ascii() = delete;
-	typedef unsigned char BitsType;
+	typedef unsigned char BinaryDigitType;
 	typedef unsigned char ValueType;
 	typedef unsigned char RawValueType;
 	static constexpr const char* Name = "ascii";
 	static constexpr ValueType MaximumValue = 127;
 	static constexpr ValueType MinimumValue = 0;
-	static constexpr BitsType MaximumBits = 7;
-	static constexpr BitsType MinimumBits = 7;
+	static constexpr BinaryDigitType MaximumBinaryDigit = 7;
+	static constexpr BinaryDigitType MinimumBinaryDigit = 7;
 };
 
 #endif //!TOOL_CHARACTER_DEFINITION_ASCII_H_
