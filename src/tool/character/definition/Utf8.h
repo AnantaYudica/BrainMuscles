060f0dd141679b46298db32943e05d6686094eaf
AnantaYudica <nta040689@gmail.com>
2017-12-04 10:23:52 +0700

    MODIFIED FILE Update in struct tool::character::definition::Utf8  - Update name static constexpr variables member from 'LengthRead' to 'Size', from 'MaximumBinaryDigit' to 'MaximumValueBinaryDigit' and from 'MinimumBinaryDigit' to 'MinimumValueBinaryDigit'

--- src/tool/character/definition/Utf8.h
+++ src/tool/character/definition/Utf8.h
@@ -22,9 +22,9 @@ struct tool::character::definition::Utf8 final
 	typedef std::uint32_t ValueType;
 	typedef std::uint32_t RawValueType;
 	static constexpr const char* Name = "utf-8";
-	static constexpr SizeByteType LengthRead = 1;
-	static constexpr SizeBinaryDigitType MaximumBinaryDigit = 21;
-	static constexpr SizeBinaryDigitType MinimumBinaryDigit = 7;
+	static constexpr SizeByteType SizeRead = 1;
+	static constexpr SizeBinaryDigitType MaximumValueBinaryDigit = 21;
+	static constexpr SizeBinaryDigitType MinimumValueBinaryDigit = 7;
 	static constexpr ValueType MaximumValue = 2097151;
 	static constexpr ValueType MinimumValue = 0;
 };
