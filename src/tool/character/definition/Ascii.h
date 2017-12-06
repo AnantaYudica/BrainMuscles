ce3f296268272021125674cdaf9afc4466cd7133
AnantaYudica <nta040689@gmail.com>
2017-12-06 08:09:43 +0700

    MODIFIED FILE Add dependency with <cstddef> Update in struct tool::character::definition::Ascii  - Update alias type 'ValueType' from unsigned char to char  - Update alias type 'RawValueType' from unsigned char to char  - Add static inline functions member 'Encode' with unsigned int for return type    - Add function member with parameters function 'input' with const ValueType& for type, 'output' with RawValueType& for type    - Add template function member with parameter template 'ITERATOR_TYPE' and parameters function 'input' with const ValueType& for type, 'begin_output' with ITERATOR_TYPE for type and 'end_output' with ITERATOR_TYPE for type    - Add template function member with parameter template 'N' with std::size_t for default type and parameters function 'input' with const ValueType& for type, 'output' with char(&)[N] for type and 'index_output' with const size_t& for type and 0 for default value - Add static inline functions member 'Decode' with unsigned int for return type    - Add function member with parameters function 'input' with const RawValueType& for type, 'output' with ValueType& for type    - Add template function member with parameter template 'ITERATOR_TYPE' and parameters function 'begin_input' with ITERATOR_TYPE for type, 'end_input' with ITERATOR_TYPE for type and 'output' with ValueType& for type,    - Add template function member with parameter template 'N' with std::size_t for default type and parameters function 'output' with ValueType& for type, 'input' with char(&)[N] for type and 'index_input' with const size_t& for type and 0 for default value Add implementation inline function member 'Encode' and 'Decode' from struct tool::character::definition::Ascii

--- src/tool/character/definition/Ascii.h
+++ src/tool/character/definition/Ascii.h
@@ -1,6 +1,8 @@
 #ifndef TOOL_CHARACTER_DEFINITION_ASCII_H_
 #define TOOL_CHARACTER_DEFINITION_ASCII_H_
 
+#include <cstddef>
+
 namespace tool
 {
 	namespace character
@@ -17,14 +19,94 @@ struct tool::character::definition::Ascii final
 	Ascii() = delete;
 	typedef unsigned char SizeBinaryDigitType;
 	typedef unsigned char SizeByteType;
-	typedef unsigned char ValueType;
-	typedef unsigned char RawValueType;
+	typedef char ValueType;
+	typedef char RawValueType;
 	static constexpr const char* Name = "ascii";
 	static constexpr SizeByteType SizeRead = 1;
 	static constexpr SizeBinaryDigitType MaximumBinaryDigit = 7;
 	static constexpr SizeBinaryDigitType MinimumBinaryDigit = 7;
 	static constexpr ValueType MaximumValue = 127;
 	static constexpr ValueType MinimumValue = 0;
+
+	static inline unsigned int Encode(const ValueType& input, RawValueType& output);
+	template<typename ITERATOR_TYPE>
+	static inline unsigned int Encode(const ValueType& input,
+		ITERATOR_TYPE begin_output, ITERATOR_TYPE end_output);
+	template<std::size_t N>
+	static inline unsigned int Encode(const ValueType& input, char(&output)[N],
+		const std::size_t& index_output = 0);
+
+	static inline unsigned int Decode(const RawValueType& input, ValueType& output);
+	template<typename ITERATOR_TYPE>
+	static inline unsigned int Decode(ITERATOR_TYPE begin_input,
+		ITERATOR_TYPE end_input, ValueType& output);
+	template<std::size_t N>
+	static inline unsigned int Decode(const char(&input)[N], ValueType& output,
+		const std::size_t& index_input = 0);
 };
+
+inline unsigned int 
+tool::character::definition::Ascii::Encode(const ValueType& input, 
+	RawValueType& output)
+{
+	if (input > MaximumValue || input < MinimumValue)
+	{
+		return 0;
+	}
+	output = input;
+	return 1;
+}
+
+template<typename ITERATOR_TYPE>
+inline unsigned int 
+tool::character::definition::Ascii::Encode(const ValueType& input,
+	ITERATOR_TYPE begin_output, ITERATOR_TYPE end_output)
+{
+	if (begin_output >= end_output)
+	{
+		return 0;
+	}
+	return Encode(input, *begin_output);
+}
+
+template<std::size_t N>
+inline unsigned int 
+tool::character::definition::Ascii::Encode(const ValueType& input,
+	char(&output)[N], const std::size_t& index_output)
+{
+	return Encode(input, output + index_output, output + N);
+}
+
+inline unsigned int 
+tool::character::definition::Ascii::Decode(const RawValueType& input,
+	ValueType& output)
+{
+	if (input > MaximumValue || input < MinimumValue)
+	{
+		return 0;
+	}
+	output = input;
+	return 1;
+}
+
+template<typename ITERATOR_TYPE>
+inline unsigned int
+tool::character::definition::Ascii::Decode(ITERATOR_TYPE begin_input,
+	ITERATOR_TYPE end_input, ValueType& output)
+{
+	if (begin_input >= end_input)
+	{
+		return 0;
+	}
+	return Encode(*begin_input, output);
+}
+
+template<std::size_t N>
+inline unsigned int 
+tool::character::definition::Ascii::Decode(const char(&input)[N],
+	ValueType& output, const std::size_t& index_input)
+{
+	return Decode(input + index_input, input + N, output);
+}
 
 #endif //!TOOL_CHARACTER_DEFINITION_ASCII_H_
