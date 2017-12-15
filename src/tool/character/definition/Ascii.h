6d2dfba88823eee3384f154639c6725a05578016
AnantaYudica <nta040689@gmail.com>
2017-12-15 14:06:09 +0700

    MODIFIED FILE Update class tool::character::definition::Ascii  - Add alias type 'SizeType' with std::size_t for type  - Remove static inline function member 'ValidationPointer'  - Update static inline functions member 'Encode' and 'Decode' with parameter function 'input' and 'output'    - Change return type from std::size_t to SizeType  - Remove static inline functions member 'Encode' and 'Decode'    - With parameter function 'input_type_size', 'input_size', 'input_offset', 'output', 'output_type_size', 'output_size' and 'output_offset'    - With parameter function 'input', 'output', 'output_type_size', 'output_size' and 'output_offset'    - With parameter function 'input', 'input_type_size', 'input_size', 'input_offset', 'output' Remove and update implementation static inline functions member 'Encode' and 'Decode' from class tool::character::definition::Ascii

--- src/tool/character/definition/Ascii.h
+++ src/tool/character/definition/Ascii.h
@@ -28,38 +28,14 @@ public:
 	static constexpr SizeBinaryDigitType MinimumBinaryDigit = 7;
 	static constexpr ValueType MaximumValue = 127;
 	static constexpr ValueType MinimumValue = 0;
+	typedef std::size_t SizeType;
 private:
 	static inline bool ValidationValue(const ValueType& value);
-	static inline bool ValidationPointer(const void* pointer,
-		const std::size_t& pointer_type_size, const std::size_t& pointer_size,
-		const std::size_t& pointer_offset);
 public:
-	static inline std::size_t Encode(const ValueType& input, 
+	static inline SizeType Encode(const ValueType& input,
 		RawValueType& output);
-	static inline std::size_t Encode(const void* input,
-		const std::size_t& input_type_size, const std::size_t& input_size,
-		const std::size_t& input_offset, void* output,
-		const std::size_t& output_type_size, const std::size_t& output_size,
-		const std::size_t& output_offset);
-	static inline std::size_t Encode(const ValueType& input, void* output,
-		const std::size_t& output_type_size, const std::size_t& output_size,
-		const std::size_t& output_offset);
-	static inline std::size_t Encode(const void* input, 
-		const std::size_t& input_type_size, const std::size_t& input_size,
-		const std::size_t& input_offset, RawValueType& output);
-	static inline std::size_t Decode(const RawValueType& input, 
+	static inline SizeType Decode(const RawValueType& input,
 		ValueType& output);
-	static inline std::size_t Decode(const void* input,
-		const std::size_t& input_type_size, const std::size_t& input_size,
-		const std::size_t& input_offset, void* output,
-		const std::size_t& output_type_size, const std::size_t& output_size,
-		const std::size_t& output_offset);
-	static inline std::size_t Decode(const RawValueType& input, void* output,
-		const std::size_t& output_type_size, const std::size_t& output_size,
-		const std::size_t& output_offset);
-	static inline std::size_t Decode(const void* input,
-		const std::size_t& input_type_size, const std::size_t& input_size,
-		const std::size_t& input_offset, ValueType& output);
 };
 
 inline bool 
@@ -68,16 +44,7 @@ tool::character::definition::Ascii::ValidationValue(const ValueType& value)
 	return value <= MaximumValue && value >= MinimumValue;
 }
 
-inline bool 
-tool::character::definition::Ascii::ValidationPointer(const void* pointer,
-	const std::size_t& pointer_type_size, const std::size_t& pointer_size,
-	const std::size_t& pointer_offset)
-{
-	return pointer != NULL && pointer_type_size != 0 && pointer_size != 0
-		&& pointer_offset < pointer_type_size;
-}
-
-inline std::size_t
+inline typename tool::character::definition::Ascii::SizeType
 tool::character::definition::Ascii::Encode(const ValueType& input, 
 	RawValueType& output)
 {
@@ -87,48 +54,6 @@ tool::character::definition::Ascii::Encode(const ValueType& input,
 		return 1;
 	}
 	return 0;
-}
-
-inline std::size_t 
-tool::character::definition::Ascii::Encode(const void* input,
-	const std::size_t& input_type_size, const std::size_t& input_size,
-	const std::size_t& input_offset, void* output, 
-	const std::size_t& output_type_size,
-	const std::size_t& output_size, const std::size_t& output_offset)
-{
-	if (ValidationPointer(input, input_type_size, input_size, input_offset)
-		&& ValidationPointer(output, output_type_size, output_size,
-			output_offset))
-	{
-		ValueType value;
-		RawValueType raw_value;
-		memcpy_s(&value, SizeRead, (const char*)input + input_offset, SizeRead);
-		if (Encode(value, raw_value))
-		{
-			memcpy_s((char*)output + output_offset, SizeRead, &raw_value,
-				SizeRead);
-			return output_type_size;
-		}
-	}
-	return 0;
-}
-
-inline std::size_t 
-tool::character::definition::Ascii::Encode(const ValueType& input, void* output,
-	const std::size_t& output_type_size, const std::size_t& output_size,
-	const std::size_t& output_offset)
-{
-	return Encode(&input, sizeof(ValueType), 1, 0, output,
-		output_type_size, output_size, output_offset);
-}
-
-inline std::size_t
-tool::character::definition::Ascii::Encode(const void* input,
-	const std::size_t& input_type_size, const std::size_t& input_size,
-	const std::size_t& input_offset, RawValueType& output)
-{
-	return Encode(input, input_type_size, input_size, input_offset, &output,
-		sizeof(RawValueType), 1, 0);
 }
 
 inline std::size_t
@@ -142,50 +67,5 @@ tool::character::definition::Ascii::Decode(const RawValueType& input,
 	}
 	return 0;
 }
-
-inline std::size_t 
-tool::character::definition::Ascii::Decode(const void* input,
-	const std::size_t& input_type_size, const std::size_t& input_size,
-	const std::size_t& input_offset, void* output,
-	const std::size_t& output_type_size, const std::size_t& output_size,
-	const std::size_t& output_offset)
-{
-	if (ValidationPointer(input, input_type_size, input_size, input_offset)
-		&& ValidationPointer(output, output_type_size, output_size,
-			output_offset))
-	{
-		ValueType value;
-		RawValueType raw_value;
-		memcpy_s(&raw_value, SizeRead, (const char*)input + input_offset,
-			SizeRead);
-		if (Encode(raw_value, value))
-		{
-			memcpy_s((char*)output + output_offset, SizeRead,
-				&value, SizeRead);
-			return output_type_size;
-		}
-	}
-	return 0;
-}
-
-inline std::size_t
-tool::character::definition::Ascii::Decode(const RawValueType& input, 
-	void* output, const std::size_t& output_type_size, 
-	const std::size_t& output_size, const std::size_t& output_offset)
-{
-	return Decode(&input, sizeof(ValueType), 1, 0, output,
-		output_type_size, output_size, output_offset);
-}
-
-inline std::size_t 
-tool::character::definition::Ascii::Decode(const void* input,
-	const std::size_t& input_type_size, const std::size_t& input_size,
-	const std::size_t& input_offset, ValueType& output)
-{
-	return Encode(input, input_type_size, input_size, input_offset, &output,
-		sizeof(ValueType), 1, 0);
-}
-
-
 
 #endif //!TOOL_CHARACTER_DEFINITION_ASCII_H_
