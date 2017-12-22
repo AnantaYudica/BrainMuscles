ab9aef3a9fdcdf9c34198413d3dbe010c2d8fc40
AnantaYudica <nta040689@gmail.com>
2017-12-22 23:58:58 +0700

    ADD FILE Add dependencies with <cstddef> and <cstdint> Add prototype class tool::definition Add definition class tool::definition with final inheritance  - Delete default constructor  - Add aliases type with public to access    - Add 'SizeBinaryDigitType' with std::uint8_t for type    - Add 'SizeByteType' with std::uint8_t for type    - Add 'ByteType' with std::uint8_t for type    - Add 'SizeType' with std::size_t for type  - Add static constexpr variable member 'SizeByteBinaryDigit' with SizeBinaryDigitType for type and 8 for value

---
+++ src/tool/Definition.h
@@ -0,0 +1,23 @@
+#ifndef TOOL_DEFINITION_H_
+#define TOOL_DEFINITION_H_
+
+#include <cstddef>
+#include <cstdint>
+
+namespace tool
+{
+	class Definition;
+}
+
+class tool::Definition final
+{
+public:
+	Definition() = delete;
+	typedef std::uint8_t SizeBinaryDigitType;
+	typedef std::uint8_t SizeByteType;
+	typedef std::uint8_t ByteType;
+	typedef std::size_t SizeType;
+	static constexpr SizeBinaryDigitType SizeByteBinaryDigit = 8;
+};
+
+#endif //!TOOL_DEFINITION_H_
