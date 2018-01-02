c5a6507cc2af30a53e8ddfa76d2250f98e0328fd
AnantaYudica <nta040689@gmail.com>
2018-01-02 13:31:11 +0700

    ADD FILE Add prototype template class tool::character::Value with 'CARACTER_DEFINITION_TYPE' for name template parameter

---
+++ src/tool/character/Value.h
@@ -0,0 +1,13 @@
+#ifndef TOOL_CHARACTER_VALUE_H_
+#define TOOL_CHARACTER_VALUE_H_
+
+namespace tool
+{
+	namespace character
+	{
+		template<typename CHARACTER_DEFINITION_TYPE>
+		class Value;
+	}
+}
+
+#endif //!TOOL_CHARACTER_VALUE_H_
