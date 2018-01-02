e24e2214288984740451629c2d140334c4ba2ece
AnantaYudica <nta040689@gmail.com>
2018-01-02 19:49:36 +0700

    MODIFIED FILE Add definition template class tool::character::Value  - Add aliases type with public to access    - 'CharacterDefinitionType' with CHARACTER_DEFINITION_TYPE for type    - 'ValueType' with CharacterDefinitionType::ValueType for type    - 'RawValueType' with CharacterDefinitionType::RawValueType for type    - 'SizeType' with CharacterDefinitionType::SizeType for type  - Add variable member 'm_value' with ValueType for type and private to access  - Add default constructor and public to access  - Add fill constructor with parameter constructor 'value' with const ValueType& for type and public to access  - Add destructor with public to access  - Add copy constructor and assignment with pubic to access  - Add functions member with public to access    - 'operator CHARACTER_DEFINITION_TYPE::ValueType&'    - 'Encode' with SizeType for return type and parameter function 'output' with RawValueType& for type    - 'Decode' with SizeType for return type and parameter function 'input' with const RawValueType& for type  - Add function const member 'IsGood' with public to access and bool for return type Add implementation default constructor, fill constructor, destructor, copy constructor, copy assignment, operator CHARACTER_DEFINITION_TYPE::ValueType&, 'Encode', 'Decode' and 'IsGood'

--- src/tool/character/Value.h
+++ src/tool/character/Value.h
@@ -10,4 +10,85 @@ namespace tool
 	}
 }
 
+template<typename CHARACTER_DEFINITION_TYPE>
+class tool::character::Value
+{
+public:
+	typedef CHARACTER_DEFINITION_TYPE CharacterDefinitionType;
+	typedef typename CharacterDefinitionType::ValueType ValueType;
+	typedef typename CharacterDefinitionType::RawValueType RawValueType;
+	typedef typename CharacterDefinitionType::SizeType SizeType;
+private:
+	ValueType m_value;
+public:
+	Value();
+	Value(const ValueType& value);
+	Value(const Value& copy);
+	~Value();
+public:
+	Value& operator=(const Value& copy);
+public:
+	operator typename CHARACTER_DEFINITION_TYPE::ValueType&();
+public:
+	bool IsGood() const;
+	SizeType Encode(RawValueType& output);
+	SizeType Decode(const RawValueType& input);
+};
+
+template<typename CHARACTER_DEFINITION_TYPE>
+tool::character::Value<CHARACTER_DEFINITION_TYPE>::Value() :
+	m_value(0)
+{}
+
+template<typename CHARACTER_DEFINITION_TYPE>
+tool::character::Value<
+	CHARACTER_DEFINITION_TYPE>::Value(const ValueType& value) :
+	m_value(value)
+{}
+
+template<typename CHARACTER_DEFINITION_TYPE>
+tool::character::Value<CHARACTER_DEFINITION_TYPE>::Value(const Value& copy) :
+	m_value(copy.m_value)
+{}
+
+template<typename CHARACTER_DEFINITION_TYPE>
+tool::character::Value<CHARACTER_DEFINITION_TYPE>::~Value()
+{}
+
+template<typename CHARACTER_DEFINITION_TYPE>
+tool::character::Value<CHARACTER_DEFINITION_TYPE>& 
+tool::character::Value<CHARACTER_DEFINITION_TYPE>::operator=(const Value& copy)
+{
+	this->m_value = copy.m_value;
+	return *this;
+}
+
+template<typename CHARACTER_DEFINITION_TYPE>
+tool::character::Value<CHARACTER_DEFINITION_TYPE>::operator 
+typename CHARACTER_DEFINITION_TYPE::ValueType&()
+{
+	return m_value;
+}
+
+template<typename CHARACTER_DEFINITION_TYPE>
+bool tool::character::Value<CHARACTER_DEFINITION_TYPE>::IsGood() const
+{
+	return CharacterDefinitionType::ValidationValue(m_value);
+}
+
+template<typename CHARACTER_DEFINITION_TYPE>
+typename tool::character::Value<CHARACTER_DEFINITION_TYPE>::SizeType 
+tool::character::Value<CHARACTER_DEFINITION_TYPE>::Encode(RawValueType& output)
+{
+	return CharacterDefinitionType::Encode(m_value, output);
+}
+
+template<typename CHARACTER_DEFINITION_TYPE>
+typename tool::character::Value<CHARACTER_DEFINITION_TYPE>::SizeType 
+tool::character::Value<
+	CHARACTER_DEFINITION_TYPE>::Decode(const RawValueType& input)
+{
+	return CharacterDefinitionType::Decode(input, m_value);
+}
+
 #endif //!TOOL_CHARACTER_VALUE_H_
